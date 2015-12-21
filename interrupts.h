e609251f0ad66b936c8e2d4f528e2c9931ce2a14
Rafał Cieślak <rafalcieslak256@gmail.com>
2015-12-21 22:39:20 +0100

    Initial interrupt support, core timer interrupts and ms timer incrementing

---
+++ interrupts.h
@@ -0,0 +1,35 @@
+#ifndef __INTERRUPTS_H__
+#define __INTERRUPTS_H__
+
+#include "pic32mz.h"
+#include "global_config.h"
+
+/* Disables interrupts. Returns the previous value of Status
+   register. */
+#define di() ({                             \
+    int oldval;                             \
+    asm volatile ("di %0": "=r" (oldval));  \
+    oldval; })
+
+/* Re-enables interrupts previously disabled with di(). */
+#define ei(val) ({mtc0(C0_STATUS,0,(val));})
+
+/* Initializes and enables interrupts. */
+void init_interrupts();
+
+/* This is the single interrupt handler procedure. */
+void interrupt_handler();
+
+
+/* -- CORE TIMER -- */
+
+#define TICKS_PER_MS (1000 * MHZ / 2)
+
+/* Initializes and enables core timer interrupts. */
+void init_timer();
+
+/* Returns the number of ms passed since timer started running. */
+unsigned timer_get_ms();
+
+
+#endif
