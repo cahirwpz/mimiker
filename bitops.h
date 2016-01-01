930bc9353cb226ca9a72df3d993be1d01d299253
Rafał Cieślak <rafalcieslak256@gmail.com>
2016-01-01 17:45:29 +0100

    Created bitops.h with some basic bit operations

---
+++ bitops.h
@@ -0,0 +1,10 @@
+#ifndef __BITOPS_H__
+#define __BITOPS_H__
+
+#define   SET_BIT(n, b) ((n) |=  (1 << (b)) )
+#define CLEAR_BIT(n, b) ((n) &= ~(1 << (b)) )
+#define   INV_BIT(n, b) ((n) ^=  (1 << (b)) )
+
+#define   GET_BIT(n, b) (((n) & (1<<(b))) != 0)
+
+#endif // __BITOPS_H__
