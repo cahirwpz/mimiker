4a6693a2abb500cb2ca50bee1bc1256118032b91
Bożydar <trening302@o2.pl>
2016-01-08 01:34:19 +0100

    Add mips_cpuinfo struct

--- main.c
+++ main.c
@@ -15,6 +15,19 @@
 char str[] = "This is a global string!\n";
 char empty[100]; /* This should land in .bss and get cleared by _start procedure. */
 
+struct mips_cpuinfo 
+{
+    int TLB_entries;
+ 
+    int ic_size;
+    int ic_linesize;
+    int ic_nways;
+    int ic_nsets;
+    int dc_size;
+    int dc_linesize;
+    int dc_nways;
+    int dc_nsets;
+}mips_cpuinfo;
 /*
  * Chip configuration.
  */
@@ -60,115 +73,65 @@ void udelay (unsigned usec)
             break;
     }
 }
-
+/* 
+ * Read configuration registers value, its interpretation 
+ * and save it into the struct mips_cpuinfo
+ */
 void read_config()
 {
     int config = mfc0(16, 0);
-    kprintf ("\nConfig = 0x%08x\n", config);
-    
-    // Default [0 = false, 1 = true]
-    
-    kprintf("Kseg2 and kseg3 [2,7-Uncached]: %d\n", BITS_GET(config, 3, 28) ); // ref. to MD00942 to Table 7.37 Cache Coherency Attributes
-    kprintf("Kuseg and useg [2,7-Uncached]: %d\n", BITS_GET(config, 3, 25) ); // ref. to MD00942 to Table 7.37 Cache Coherency Attributes
-    kprintf("Instruction ScratchPad is present: %d\n", BIT_GET(config,24) );
-    kprintf("Data ScratchPad is present: %d\n", BIT_GET(config,23) );
-    kprintf("User Defined Instruction are implemented: %d\n", BIT_GET(config,22) );
-    kprintf("SimpleBE bus mode is enabled: %d\n", BIT_GET(config,21) );
-    kprintf("Type of MDU [0-Fast, 1-Iterative]: %d\n", BIT_GET(config,20) );
-    kprintf("Diagnostic bit: %d\n", BIT_GET(config,19) ); // ref. to MD00213 "Cache Configuration Application Note"
-    kprintf("Merging [0-No, 2-Allowed, 1,3-Reserved]: %d\n", BITS_GET(config, 2, 17) );
-    kprintf("Type of MDU [0-Fast, 1-Iterative]: %d\n", BIT_GET(config,16) );
-    kprintf("Endian mode [0-Little, 1-Big]: %d\n", BIT_GET(config,15) );
-    kprintf("Architecture type [00-MIPS32]: %d\n", BITS_GET(config, 2, 13) );
-    kprintf("Architecture revision lev. [0-Release1, 1-Release2, 2-7-Reserved]: %d\n", BITS_GET(config, 3, 10) );
-    kprintf("MMU type [1-Standard TLB, 3-Fixed mapping, 0,2,4-7-Reserved]: %d\n", BITS_GET(config, 3, 7) );
-    kprintf("Zeros: %d\n", BITS_GET(config, 4, 3) );
-    kprintf("Kseg0 [2,7-Uncached]: %d\n", BITS_GET(config, 3, 0) ); // ref. to MD00942 to Table 7.37 Cache Coherency Attributes
     
     if( (config & (1<<31)) == 0 )
-        return;
+        return; // PANIC 
     
-    int config1 = mfc0(16, 1);
-    kprintf ("\nConfig1 = 0x%08x\n", config1);
-
-    kprintf("Number of entries in TLB (minus 1): %d\n", BITS_GET(config1, 6, 25) );    
-    kprintf("Number of instruction cache sets per way [0-64, 1-128, 2-256, 3-512, 4-1024, 5-7-Reserved]: %d\n", BITS_GET(config1, 3, 22) );    
-    kprintf("Instruction cache line size: [0-No instr. cache, 3-16 bytes, 1,2,4-7 - Reserved]: %d\n", BITS_GET(config1, 3, 19) );
-    kprintf("Level of instruction cache associativity [0-Direct mapped, 1-2way, 2-3way, 3-4way, 4-7-Reserved]: %d\n", BITS_GET(config1, 3, 16) );    
-    kprintf("Number of data cache sets per way [0-64, 1-128, 2-256, 3-512, 4-1024, 5-7-Reserved]: %d\n", BITS_GET(config1, 3, 13) );    
-    kprintf("data cache line size: [0-No data cache, 3-16 bytes, 1,2,4-7 - Reserved]: %d\n", BITS_GET(config1, 3, 10) );
-    kprintf("Type of the data cache [0-Direct mapped, 1-2way, 2-3way, 3-4way, 4-7-Reserved]: %d\n", BITS_GET(config1, 3, 7) ); 
-    kprintf("Coprocesor 2 present: %d\n", BIT_GET(config1, 6) );
-    kprintf("MDMX is implemented: %d\n", BIT_GET(config1, 5) );
-    kprintf("Peformance Counter register is implemented: %d\n", BIT_GET(config1, 4) );
-    kprintf("One or more Watch registers are implemented: %d\n", BIT_GET(config1, 3) );
-    kprintf("MIPS16e (code compression) is implemented: %d\n", BIT_GET(config1, 2) );
-    kprintf("EJTAG is implemented: %d\n", BIT_GET(config1, 1) );
-    kprintf("FPU is implemented: %d\n", BIT_GET(config1, 0) );
+    int config1 = mfc0(16, 1);   
     
     if( (config1 & (1<<31)) == 0 )
-        return;
+        return;// PANIC 
     
-    int config2 = mfc0(16, 2);
-    kprintf ("\nConfig2 = 0x%08x\n", config2);
-    kprintf ("Bits 31:0 are reserved\n");
-    
-    if( (config2 & (1<<31)) == 0 )
-        return;
-
+    if( (mfc0(16, 2) & (1<<31)) == 0 )
+        return;// PANIC 
+        
     int config3 = mfc0(16, 3);
-    kprintf ("\nConfig3 = 0x%08x\n", config3);
-          
-    kprintf("Zeros: %d\n", BITS_GET(config3, 8, 23) );
-    kprintf("[0-IPL and RIPL filds are 6-bits, 1-...8-bits, 2,3-Reserved]: %d\n", BITS_GET(config3, 2, 21) );
-    kprintf("microMIPS Architecture revision lev. [0-Release1, 1-7-Reserved]: %d\n", BITS_GET(config3, 3, 18) );
-    kprintf("MCU ASE is implemented %d\n", BIT_GET(config3, 17) );
-    kprintf("Vectoring to an exception [0-MIPS32 ISA, 1-microMIPS]: %d\n", BIT_GET(config3, 16) );
-    kprintf("ISA [0-Only MIPS32, 1-Only microMIPS, 2-Both implem. MIPS32 ISA used when coming out of reset, 3-Both implem. MicroMips used ...]: %d\n", BITS_GET(config3, 2, 14) );
-    kprintf("UserLocal register is implemented: %d\n", BIT_GET(config3, 13) );
-    kprintf("The RIE and XIE bits are implemented within the PageGrain: %d\n", BIT_GET(config3, 12) );
-    kprintf("Revision 2 of the MIPS DSP module is implemented: %d\n", BIT_GET(config3, 11) );
-    kprintf("MIPS DSP module extension is implemented: %d\n", BIT_GET(config3, 10) );
-    kprintf("Zero: %d\n", BIT_GET(config3, 9) );
+    //cache size and organization,
     
-    kprintf("iFlowTrace hardware is present: %d\n", BIT_GET(config3, 8) );
-    kprintf("Large physical address support is implemented: %d\n", BIT_GET(config3, 7) );
-    kprintf("Support for EIC interrupt mode is implemented: %d\n", BIT_GET(config3, 6) );
-    kprintf("Vectored interrupts are implemented: %d\n", BIT_GET(config3, 5) );
-    kprintf("Small page support is implemented: %d\n", BIT_GET(config3, 4) );
-    kprintf("Common Device Memory Map is implemented: %d\n", BIT_GET(config3, 3) );
-    kprintf("Zero: %d\n", BIT_GET(config3, 2) );
-    kprintf("SmartMIPS ASE is implemented: %d\n", BIT_GET(config3, 1) );
-    kprintf("Trace logic is implemented: %d\n", BIT_GET(config3, 0) );
-    
-    if( (config3 & (1<<31)) == 0 )
-        return;
-   
-    int config4 = mfc0(16, 4);
-    kprintf ("\nConfig4 = 0x%08x\n", config4);
-     
-    kprintf("Zeros: %d\n", BITS_GET(config4, 30, 0) );
-    
-    if( (config4 & (1<<31)) == 0 )
-        return;
-    
-    int config5 = mfc0(16, 5);
-    kprintf ("\nConfig5 = 0x%08x\n", config5);
-    kprintf("Config5 is present (?) (Should read as 0): %d\n", BIT_GET(config5, 31));
-    kprintf("Zeros: %d\n", BITS_GET(config5, 28, 3) );
-    kprintf("User mode FR instructions allowed: %d\n", BIT_GET(config5, 2));
-    kprintf("Zero: %d\n", BIT_GET(config5, 1));
-    kprintf("Nested Fault feature is present: %d\n", BIT_GET(config5, 0));
+        mips_cpuinfo.ic_size = 0; //instruction_cache_size
+        mips_cpuinfo.ic_linesize = (BITS_GET(config1, 3, 19) == 3 ? 16 : 0);
+        mips_cpuinfo.ic_nways = BITS_GET(config1, 3, 16) /* + 1 (?)*/;
+        mips_cpuinfo.ic_nsets = 1 << (6 + BITS_GET(config1, 3, 22));
+        
+        
+        // Total Cache Size = Associativity * Line Size * Sets Per Way
+        // If the line size is zero, there is no cache implemented.
+        
+        if(mips_cpuinfo.ic_linesize != 0)
+            mips_cpuinfo.ic_size = mips_cpuinfo.ic_nways * mips_cpuinfo.ic_linesize  * mips_cpuinfo.ic_nsets ;
+        
+        mips_cpuinfo.dc_size = 0; //data_cache_size
+        mips_cpuinfo.dc_linesize = ( BITS_GET(config1, 3, 10) == 3 ? 16 : 0);
+        mips_cpuinfo.dc_nways = BITS_GET(config1, 3, 7) /* + 1 (?)*/;
+        mips_cpuinfo.dc_nsets = 1 << (6 + BITS_GET(config1, 3, 13));
+        
+        if(mips_cpuinfo.dc_linesize != 0)
+            mips_cpuinfo.dc_size = mips_cpuinfo.dc_nways * mips_cpuinfo.dc_linesize  * mips_cpuinfo.dc_nsets ;
+       
+        //      Name 	| set size 	| # sets 	            | Associativity
+        //      -----------------------------------------------------------
+        //      Direct 	| 1 line 	| r= # lines per cache 	| 1-way
+        //      Set 	| s lines 	| r/s          	        | s-way
+        //      Fully 	| r lines 	| 1 (one) 	            | r-way
+        
 
-    //How do we know that config7 is present? 
-
-    int config7 = mfc0(16, 7);
-    kprintf ("\nConfig7 = 0x%08x\n", config7);
-    kprintf("Wait IE Ignore: %d\n", BIT_GET(config7, 31));
-    kprintf("Zeros: %d\n", BITS_GET(config7, 12, 19) );
-    kprintf("Hardware Cache Initialization: %d\n", BIT_GET(config7, 18));
-    kprintf("Zeros: %d\n", BITS_GET(config7, 18, 0) );
-
+    //FTLB or/and VTLB sizes, 
+        mips_cpuinfo.TLB_entries = BITS_GET(config1, 6, 25)  + 1; // 0 xor 15+1 xor 31+1
+    
+    //minimum and maximum page size,
+    // "9.2.4 Virtual Aliasing" in MIPS32® microAptivTM UP Processor Core Family Software User’s Manual (?)
+    kprintf("Small page(1KByte) support is implemented: %d\n", BIT_GET(config3, 4) );
+    
+    //... and other interesting facts.
+    //kprintf("Large physical address support is implemented: %d\n", BIT_GET(config3, 7) );
+    //kprintf("Hardware Cache Initialization: %d\n", BIT_GET(config7, 18));
 }
 /*
  * Delays for at least the given number of milliseconds.  May not be
