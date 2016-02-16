c518fee10d10f1575c1ccc11d7ad45c82dd2fbf0
Krystian Bacławski <krystian.baclawski@gmail.com>
2016-02-16 23:53:35 +0100

    Provide stubs for mutexes.

---
+++ include/mutex.h
@@ -0,0 +1,16 @@
+#ifndef __SYS_MUTEX_H__
+#define __SYS_MUTEX_H__
+
+#include <common.h>
+
+typedef volatile uintptr_t mtx_t;
+
+#define mtx_lock(m) __extension__ ({ \
+  m = _mips_intdisable();            \
+})
+
+#define mtx_unlock(m) __extension__ ({ \
+  _mips_intrestore(m);                 \
+})
+
+#endif /* __SYS_MUTEX_H__ */
