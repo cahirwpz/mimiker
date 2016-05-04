c92f767102299092608df314eb8689da6f2d3ccc
Krystian Bacławski <cahirwpz@cs.uni.wroc.pl>
2016-05-04 12:10:56 +0200

    Provide minimalistic kernel_oops routine.

--- interrupts.c
+++ interrupts.c
@@ -1,4 +1,5 @@
 #include <interrupts.h>
+#include <libkern.h>
 #include <mips.h>
 
 extern const char _ebase[];
@@ -24,4 +25,32 @@ void intr_init() {
   mips32_set_c0(C0_STATUS, mips32_get_c0(C0_STATUS) & ~SR_IPL_MASK);
 
   intr_enable();
+}
+
+static const char *exceptions[32] = {
+  [EXC_INTR] = "Interrupt",
+  [EXC_MOD]  = "TLB modification exception",
+  [EXC_TLBL] = "TLB exception (load or instruction fetch)",
+  [EXC_TLBS] = "TLB exception (store)",
+  [EXC_ADEL] = "Address error exception (load or instruction fetch)",
+  [EXC_ADES] = "Address error exception (store)",
+  [EXC_IBE]  = "Bus error exception (instruction fetch)",
+  [EXC_DBE]  = "Bus error exception (data reference: load or store)",
+  [EXC_BP]   = "Breakpoint exception",
+  [EXC_RI]   = "Reserved instruction exception",
+  [EXC_CPU]  = "Coprocessor Unusable exception",
+  [EXC_OVF]  = "Arithmetic Overflow exception",
+  [EXC_TRAP] = "Trap exception",
+  [EXC_FPE]  = "Floating point exception",
+  [EXC_WATCH] = "Reference to watchpoint address",
+  [EXC_MCHECK] = "Machine checkcore",
+};
+
+void kernel_oops() {
+  int code = (mips32_get_c0(C0_CAUSE) & CR_X_MASK) >> CR_X_SHIFT;
+
+  kprintf("[oops] %s at $%08x!\n", exceptions[code], mips32_get_c0(C0_ERRPC));
+
+  if (code == EXC_ADEL || code == EXC_ADES)
+    kprintf("[oops] Caused by reference to $%08x!\n", mips32_get_c0(C0_BADVADDR));
 }
