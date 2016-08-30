d27ce7ece3fd86d9a2fc178e09c71deec0ec4791
Krystian Bacławski <cahirwpz@cs.uni.wroc.pl>
2016-08-30 15:26:44 +0200

    Generate assembly symbols for structure offsets.

---
+++ mips/genassym.c
@@ -0,0 +1,5 @@
+#include <assym.h>
+#include <thread.h>
+
+ASSYM(TD_CONTEXT, offsetof(thread_t, td_context));
+ASSYM(TD_STATE, offsetof(thread_t, td_state));
---
+++ script/genassym.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os.path
+import sys
+import subprocess
+
+if __name__ == "__main__":
+    nm = sys.argv[1]
+    infile = os.path.abspath(sys.argv[2])
+    outfile = os.path.splitext(infile)[0] + '.S'
+    symtab = subprocess.check_output([nm, infile]).decode()
+    symbols = [s.split() for s in symtab.strip().split('\n')]
+    with open(outfile, 'w') as output:
+        for value, _, name in symbols:
+            print("#define", name, "0x%x" % int(value, 16), file=output)
