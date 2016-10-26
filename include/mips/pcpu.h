f124ef7463620819edb5efb3320d78a7d2b50d69
goniz <goni1993@gmail.com>
2016-10-26 21:38:00 +0300

    Introduced pcpu_t structure to hold global cpu state (#85)
    * Used mips/include/pcpu.h and sys/pcpu.h as reference.
    * Implemented both C and ASM api macros.
    * In the future this will hold per-cpu cpu state, for now its a simple global cpu state structure.
    * As of now, only `curthread` is held in pcpu_t.

---
+++ include/mips/pcpu.h
@@ -0,0 +1,13 @@
+#ifndef _MIPS_PCPU_H_
+#define _MIPS_PCPU_H_
+
+#ifdef __ASSEMBLER__
+
+#include <mips/asm.h>
+#include <mips/mips.h>
+
+#define LOAD_PCPU(reg) \
+	LA reg, _pcpu_data
+
+#endif // !__ASSEMBLER__
+#endif // _MIPS_PCPU_H_
--- mips/thread.c
+++ mips/thread.c
@@ -1,6 +1,7 @@
 #include <mips/mips.h>
 #include <thread.h>
+#include <pcpu.h>
 
 thread_t *thread_self() {
-  return (thread_t *)mips32_get_c0(C0_USERLOCAL);
+  return PCPU_GET(curthread);
 }
