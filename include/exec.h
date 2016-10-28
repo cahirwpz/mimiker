bf52ad095008a2eeb811ac8d104b305651a5492a
Rafał Cieślak <rafalcieslak256@gmail.com>
2016-10-28 22:32:28 +0200

    Fixed comments style again

--- include/exec.h
+++ include/exec.h
@@ -5,16 +5,16 @@
 #include <stddef.h>
 
 typedef struct exec_args {
-  // Program name. Temporarily this is just a hardcoded text
-  // identifier of an embedded ELF image to use, eventually this
-  // would become a path to the executable (or an open file
-  // descriptor).
+  /* Program name. Temporarily this is just a hardcoded text
+   * identifier of an embedded ELF image to use, eventually this
+   * would become a path to the executable (or an open file
+   * descriptor). */
   char *prog_name;
-  // Program arguments. These will get copied to the stack of the
-  // starting process.
+  /* Program arguments. These will get copied to the stack of the
+   * starting process. */
   uint8_t argc;
   char **argv;
-  // TODO: Environment
+  /* TODO: Environment */
 } exec_args_t;
 
 int get_elf_image(const exec_args_t *args, uint8_t **out_image,
