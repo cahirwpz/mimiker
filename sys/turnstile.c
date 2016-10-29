a7705778391821302fb4349ed222488b8a7e3c12
coodie <poppinggoovie@gmail.com>
2016-10-29 15:24:49 +0200

    Steal more atomic operations from freebsd

--- sys/turnstile.c
+++ sys/turnstile.c
@@ -25,6 +25,28 @@ void turnstile_signal(turnstile_t* ts) {
     cs_leave();
 }
 
+
+
+
+
+
+
+
+
+
+
+//TODO: MOVE THIS TO OTHER FILE
+#define __MIPS_PLATFORM_SYNC_NOPS ""
+static __inline  void
+mips_sync(void)
+{
+    __asm __volatile (".set noreorder\n"
+            "\tsync\n"
+            __MIPS_PLATFORM_SYNC_NOPS
+            ".set reorder\n"
+            : : : "memory");
+}
+
 static __inline uint32_t
 atomic_cmpset_32(__volatile uint32_t* p, uint32_t cmpval, uint32_t newval)
 {
@@ -44,4 +66,18 @@ atomic_cmpset_32(__volatile uint32_t* p, uint32_t cmpval, uint32_t newval)
         : "r" (cmpval), "r" (newval), "m" (*p)
         : "memory");
     return ret;
+}
+
+/*
+ * Atomically compare the value stored at *p with cmpval and if the
+ * two values are equal, update the value of *p with newval. Returns
+ * zero if the compare failed, nonzero otherwise.
+ */
+static __inline uint32_t
+atomic_cmpset_acq_32(__volatile uint32_t *p, uint32_t cmpval, uint32_t newval)
+{
+    int retval;
+    retval = atomic_cmpset_32(p, cmpval, newval);
+    mips_sync();
+    return (retval);
 }
