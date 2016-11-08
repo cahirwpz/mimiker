feddbe3400ffca26b4883b630029a165be32daf9
Przemysław Leśniak <poppinggoovie@gmail.com>
2016-11-08 17:41:56 +0100

    Add turnstile data structure (#120)

---
+++ sys/turnstile.c
@@ -0,0 +1,27 @@
+#include <sync.h>
+#include <queue.h>
+#include <sched.h>
+#include <turnstile.h>
+
+void turnstile_init(turnstile_t *ts) {
+  TAILQ_INIT(&ts->td_queue);
+}
+
+void turnstile_wait(turnstile_t *ts) {
+  cs_enter();
+  thread_t *td = thread_self();
+  TAILQ_INSERT_TAIL(&ts->td_queue, td, td_lock);
+  td->td_state = TDS_WAITING;
+  cs_leave();
+  sched_yield();
+}
+
+void turnstile_signal(turnstile_t *ts) {
+  cs_enter();
+  thread_t *newtd = TAILQ_FIRST(&ts->td_queue);
+  if (newtd) {
+    TAILQ_REMOVE(&ts->td_queue, newtd, td_lock);
+    sched_add(newtd);
+  }
+  cs_leave();
+}
