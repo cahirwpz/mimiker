2b3ab96200f87726b125541fb0de82913c1f5272
coodie <poppinggoovie@gmail.com>
2016-11-12 16:45:49 +0100

    run autopep8

--- debug/mutex.py
+++ debug/mutex.py
@@ -4,6 +4,7 @@ import os
 sys.path.append(os.path.join(os.getcwd(), 'debug'))
 import tailq
 
+
 def get_mutex_owner(mtx):
     state = mtx['mtx_state']
     td_pointer = gdb.lookup_type('struct thread').pointer()
@@ -13,21 +14,24 @@ def get_mutex_owner(mtx):
         return owner['td_name'].string()
     return None
 
+
 def get_threads_blocked_on_mutex(mtx):
     ts = mtx['turnstile']
     return map(lambda t: t['td_name'].string(), tailq.collect_values(ts['td_queue'], 'td_lock'))
 
+
 def pretty_list_of_string(lst):
     res = "["
-    for i in range(0,len(lst)):
+    for i in range(0, len(lst)):
         res = res + str(lst[i])
-        if(i != len(lst)-1):
+        if(i != len(lst) - 1):
             res = res + ", "
     res = res + "]"
     return res
 
 
 class MutexPrettyPrinter():
+
     def __init__(self, val):
         self.val = val
 
@@ -37,7 +41,8 @@ class MutexPrettyPrinter():
             return "Mutex unowned"
         else:
             res.append("Owner = " + str(get_mutex_owner(self.val)))
-            res.append("Waiting for access = " + pretty_list_of_string(get_threads_blocked_on_mutex(self.val)))
+            res.append("Waiting for access = " +
+                       pretty_list_of_string(get_threads_blocked_on_mutex(self.val)))
         return "\n".join(res)
 
     def display_hint(self):
@@ -50,4 +55,3 @@ def addMutexPrettyPrinter():
     gdb.printing.register_pretty_printer(gdb.current_objfile(), pp)
 
 addMutexPrettyPrinter()
-
