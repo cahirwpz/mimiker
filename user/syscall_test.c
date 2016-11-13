4b210edc44e8bcd96f6c9973ea11cd608aef0480
Rafał Cieślak <rafalcieslak256@gmail.com>
2016-11-13 21:21:25 +0100

    Rewrote syscalls glue to assembly

--- user/syscall_test.c
+++ user/syscall_test.c
@@ -4,7 +4,10 @@
 
 int main(int argc, char **argv) {
   const char *str = "Hello world from a user program!\n";
+  /* Successful syscall */
   write(1, str, 33);
+  /* Unimplemented syscall */
+  sbrk(100);
   /* assert(errno = 0); */
   while (1)
     ;
