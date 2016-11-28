05daa5d5359929c543d8a8f28b5a6e50b2a4e6a4
Krystian Bacławski <cahirwpz@cs.uni.wroc.pl>
2016-11-28 11:20:51 +0100

    Rename genassym.h to assym.h to aid source import from *BSD

--- script/genassym.py
+++ script/genassym.py
@@ -7,7 +7,7 @@ import subprocess
 if __name__ == "__main__":
     nm = sys.argv[1]
     infile = os.path.abspath(sys.argv[2])
-    outfile = os.path.splitext(infile)[0] + '.h'
+    outfile = os.path.abspath(sys.argv[3])
     symtab = subprocess.check_output([nm, infile]).decode()
     symbols = [s.split() for s in symtab.strip().split('\n')]
     with open(outfile, 'w') as output:
