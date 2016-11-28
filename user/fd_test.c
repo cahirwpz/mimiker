c06b85f68defed73ac48243a28a7f8d80a358bf9
Rafał Cieślak <rafalcieslak256@gmail.com>
2016-11-28 16:37:30 +0100

    fd_test.c now also checks errno values

--- user/fd_test.c
+++ user/fd_test.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <assert.h>
 #include <string.h>
+#include <errno.h>
 
 int main(int argc, char **argv) {
   const char *str = "Hello world from a user program!\n";
@@ -15,6 +16,7 @@ int main(int argc, char **argv) {
   /* Open an invalid file */
   fd1 = open("/proc/cpuinfo", 0, NULL);
   assert(fd1 < 0);
+  assert(errno == ENOENT);
   /* Write to /dev/null */
   error = write(fd0, str, strlen(str));
   assert(error >= 0);
@@ -44,35 +46,44 @@ int main(int argc, char **argv) {
   /* Try closing fd0 again. */
   error = close(fd0);
   assert(error < 0);
+  assert(errno == EBADF);
   /* Try writing to fd0 */
   error = write(fd0, str, strlen(str));
   assert(error < 0);
+  assert(errno == EBADF);
   /* Try reading from fd0 */
   memset(buf, -1, 100);
   error = read(fd0, buf, 100);
   assert(error < 0);
+  assert(errno == EBADF);
   assert(buf[10] == -1 && buf[90] == -1);
   /* Try writing to file we never referenced. */
   error = write(42, str, strlen(str));
   assert(error < 0);
+  assert(errno == EBADF);
   /* Try closing a file we never referenced. */
   error = close(42);
   assert(error < 0);
+  assert(errno == EBADF);
   /* Try closing invalid file */
   error = close(-15);
   assert(error < 0);
+  assert(errno == EBADF);
   /* At this point, fd1 and fd2 are still open. */
   /* fd1 is open in write-only mode, reading from it should fail */
   memset(buf, -1, 100);
   error = read(fd1, buf, 100);
   assert(error < 0);
+  assert(errno == EBADF);
   assert(buf[10] == -1 && buf[90] == -1);
   /* fd2 is open in read-only mode, writing to it should fail */
   error = write(fd2, str, strlen(str));
   assert(error < 0);
+  assert(errno == EBADF);
   /* Try opening a valid file with an invalid mode */
   error = open("/dev/null", 0, 42);
   assert(error < 0);
+  assert(errno == EINVAL);
   /* Close fd1 and reopen it in read-write mode */
   close(fd1);
   fd0 = open("/dev/null", 0, O_RDWR);
