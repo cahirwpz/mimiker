6b1d8fdf583a3ff991406cecf9f7961d3772b760
Krystian Bacławski <krystian.baclawski@gmail.com>
2016-12-01 08:41:40 +0100

    Coding style fixes

--- sys/subr_copy.c
+++ sys/subr_copy.c
@@ -8,38 +8,42 @@ static int copyin_vmspace(vm_map_t *vm, const void *restrict udaddr,
   if (vm == get_kernel_vm_map()) {
     memcpy(kaddr, udaddr, len);
     return 0;
-  } else if (vm == get_user_vm_map()) {
-    return copyin(udaddr, kaddr, len);
-  } else {
-    /* copyin on non-active vm maps is not supported */
-    assert(false);
-    __builtin_unreachable();
   }
+
+  if (vm == get_user_vm_map()) {
+    return copyin(udaddr, kaddr, len);
+  }
+
+  panic("copyin on non-active vm maps is not supported");
 }
+
 static int copyout_vmspace(vm_map_t *vm, const void *restrict kaddr,
                            void *restrict udaddr, size_t len) {
   if (vm == get_kernel_vm_map()) {
     memcpy(udaddr, kaddr, len);
     return 0;
-  } else if (vm == get_user_vm_map()) {
-    return copyout(kaddr, udaddr, len);
-  } else {
-    /* copyout on non-active vm maps is not supported */
-    assert(false);
-    __builtin_unreachable();
   }
+
+  if (vm == get_user_vm_map()) {
+    return copyout(kaddr, udaddr, len);
+  }
+
+  panic("copyout on non-active vm maps is not supported");
 }
 
 /* Heavily inspired by NetBSD's uiomove */
 /* This function modifies uio to reflect on the progress. */
 int uiomove(void *buf, size_t n, uio_t *uio) {
   char *cbuf = buf + uio->uio_offset;
-  assert(uio->uio_op == UIO_READ || uio->uio_op == UIO_WRITE);
   int error = 0;
+
+  assert(uio->uio_op == UIO_READ || uio->uio_op == UIO_WRITE);
+
   while (n > 0 && uio->uio_resid > 0) {
     /* Take the first io vector */
     iovec_t *iov = uio->uio_iov;
     size_t cnt = iov->iov_len;
+
     if (cnt == 0) {
       /* If no data left to move in this vector, proceed to the next io vector,
          or finish moving data if this was the last vector. */
@@ -58,7 +62,7 @@ int uiomove(void *buf, size_t n, uio_t *uio) {
       error = copyin_vmspace(uio->uio_vmspace, iov->iov_base, cbuf, cnt);
     /* Exit immediately if there was a problem with moving data */
     if (error)
-      return error;
+      break;
 
     /* Store progress on current io vector */
     iov->iov_base = (char *)iov->iov_base + cnt;
@@ -67,5 +71,6 @@ int uiomove(void *buf, size_t n, uio_t *uio) {
     cbuf += cnt;
     n -= cnt;
   }
+
   return error;
 }
