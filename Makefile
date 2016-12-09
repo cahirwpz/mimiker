5b8d0876523d90c1ae5a63e43160cefd65d36f39
Rafał Cieślak <rafalcieslak256@gmail.com>
2016-12-10 00:55:22 +0100

    Linker sets

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ TESTS = \
 	exec.elf \
 	exec_misbehave.elf \
 	exec_fd_test.elf \
+	linkset.elf \
 	malloc.elf \
 	mutex.elf \
 	physmem.elf \
---
+++ linkset.c
@@ -0,0 +1,25 @@
+#include <common.h>
+#include <stdc.h>
+
+static int some_int_1 = 1;
+static int some_int_2 = 2;
+static int some_int_3 = 3;
+static int some_int_4 = 4;
+static int some_int_5 = 5;
+SET_ENTRY(testset, some_int_1);
+SET_ENTRY(testset, some_int_2);
+SET_ENTRY(testset, some_int_3);
+SET_ENTRY(testset, some_int_4);
+SET_ENTRY(testset, some_int_5);
+
+int main() {
+  SET_DECLARE(testset, int);
+
+  kprintf("# of elements in testset: %d\n", SET_COUNT(testset));
+
+  int **ptr;
+  SET_FOREACH(ptr, testset) {
+    kprintf("Int found in testset: %d\n", **ptr);
+  }
+  return 0;
+}
