67860dde4ef91d8fa6f8cafeaf7fa0ad221b2c20
Krystian Bacławski <cahirwpz@cs.uni.wroc.pl>
2017-01-16 15:44:51 +0100

    add missing vnode reference and lock

--- sys/vfs.c
+++ sys/vfs.c
@@ -225,6 +225,7 @@ int vfs_lookup(const char *path, vnode_t **vp) {
     int error = VOP_LOOKUP(v, component, &v_child);
     vnode_unlock(v);
     vnode_unref(v);
+
     /* Handle the special case of root vnode returning ENOTSUP on lookup. We
        don't have a filesystem at / (root) yet, but we want to get the correct
        error when trying to open a non-existent file. */
@@ -233,6 +234,8 @@ int vfs_lookup(const char *path, vnode_t **vp) {
     if (error)
       return error;
     v = v_child;
+    vnode_ref(v);
+    vnode_lock(v);
   }
 
   vnode_unlock(v);
