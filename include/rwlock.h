f6b56e7ca2ef28ea9bef331fee94f18dd4e0cda2
Szymon Czapiga <Sz.Czapiga@gmail.com>
2017-01-20 00:18:11 +0100

    Removed macro based implementation

--- include/rwlock.h
+++ include/rwlock.h
@@ -10,9 +10,8 @@ typedef enum {
   RW_UNLOCKED = 0,
   RW_RLOCKED = 1,
   RW_WLOCKED = 2,
-  RW_LOCKED = 3,
-  RW_RECURSE = 4
-} rwf_t;
+  RW_LOCKED = 3
+} rwa_t;
 
 void rw_init(rwlock_t *rw, const char *name, bool recurse);
 void rw_destroy(rwlock_t *rw);
@@ -21,7 +20,7 @@ void rw_leave(rwlock_t *rw);
 bool rw_try_upgrade(rwlock_t *rw);
 void rw_downgrade(rwlock_t *rw);
 
-#define rw_assert(rw, flag) __rw_assert((rw), (flag), __FILE__, __LINE__)
-void __rw_assert(rwlock_t *rw, rwf_t flag, const char *file, unsigned line);
+#define rw_assert(rw, what) __rw_assert((rw), (what), __FILE__, __LINE__)
+void __rw_assert(rwlock_t *rw, rwa_t what, const char *file, unsigned line);
 
 #endif /* !_SYS_RWLOCK_H_ */
--- sys/rwlock.c
+++ sys/rwlock.c
@@ -4,46 +4,27 @@
 #include <thread.h>
 #include <stdc.h>
 
-/*
-32   28           16                 0
- |  F |            |                 |
- |  L |   WRITERS  |    READERS OR   |
- |  A |   WAITING  |  WRITER ENTERS  |
- |  G |            |     COUNTER     |
- |  S |            |                 |
- |____|____________|_________________|
-*/
-#define F_MASK 0xF0000000 //FLAGS MASK
-#define S_MASK 0xC0000000 //STATE MASK
-#define WW_MASK 0x0FFF0000 //WRITERS WAITING MASK
-#define CNT_MASK 0x0000FFFF //COUNTER MASK
-
-#define SET_UNLOCKED_STATE(info) ((info) = ((info) & ~S_MASK) | (1 << (31-RW_UNLOCKED)))
-#define SET_RLOCKED_STATE(info) ((info) = ((info) & ~S_MASK) | (1 << (31-RW_READER)) | (1 << (31-RW_LOCKED)))
-#define SET_WLOCKED_STATE(info) ((info) = ((info) & ~S_MASK) | (1 << (31-RW_WLOCKED)) | (1 << (31-RW_LOCKED)))
-#define IS_FLAG_SET(info, flag) ((info & F_MASK) & (1 << (31-(flag))))
-
-#define INC_WRITERS_WAITING(info) ((info) = (((((info) & WW_MASK >> 16)+1) << 16) & WW_MASK) | ((info) & (~WW_MASK)))
-#define DEC_WRITERS_WAITING(info) ((info) = (((((info) & WW_MASK >> 16)-1) << 16) & WW_MASK) | ((info) & (~WW_MASK)))
-#define GET_WRITERS_WAITING(info) (((info) & WW_MASK) >> 16)
-
-#define INC_COUNTER(info) ((info) = ((((info) & CNT_MASK)+1) & CNT_MASK) | ((info) & (~CNT_MASK)))
-#define DEC_COUNTER(info) ((info) = ((((info) & CNT_MASK)-1) & CNT_MASK) | ((info) & (~CNT_MASK)))
-#define GET_COUNTER(info) ((info) & CNT_MASK)
-
 struct rwlock {
-  int info;
+  union {
+    int readers;
+    int writer_enters;
+  };
+  int writers_waiting;
+  thread_t* owner;
+  rwa_t state;
+  bool recurse;
+  const char* name;
   condvar_t condvar;
   mtx_t mutex;
-  const char* name;
-  thread_t* owner;
 };
 
 void rw_init(rwlock_t *rw, const char *name, bool recurse) {
-  rw->info = ((!!recurse) << (31-RW_RECURSE));
-  SET_UNLOCKED_STATE(rw->info);
-  rw->name = name;
+  rw->readers = 0;
+  rw->writers_waiting = 0;
   rw->owner = NULL;
+  rw->state = RW_UNLOCKED;
+  rw->recurse = recurse;
+  rw->name = name;
   mtx_init(&rw->mutex);
   cv_init(&rw->condvar, name);
 }
@@ -54,33 +35,33 @@ void rw_destroy(rwlock_t *rw) {
 void rw_enter(rwlock_t *rw, rwo_t who) {
   mtx_lock(&rw->mutex);
   if(who == RW_READER) {
-    while(IS_FLAG_SET(rw->info, RW_WLOCKED) || GET_WRITERS_WAITING(rw->info) > 0)
+    while(rw->state == RW_WLOCKED || rw->writers_waiting > 0)
       cv_wait(&rw->condvar, &rw->mutex);
-    INC_COUNTER(rw->info);
-    SET_RLOCKED_STATE(rw->info);
+    rw->readers++;
+    rw->state = RW_RLOCKED;
   }
   else if(who == RW_WRITER) {
-    INC_WRITERS_WAITING(rw->info);
-    assert(!(IS_FLAG_SET(rw->info, RW_WLOCKED) && !IS_FLAG_SET(rw->info, RW_RECURSE) && rw->owner == thread_self()));
-    while((IS_FLAG_SET(rw->info, RW_RLOCKED)) ||
-          (IS_FLAG_SET(rw->info, RW_WLOCKED) && !IS_FLAG_SET(rw->info, RW_RECURSE)) ||
-          (IS_FLAG_SET(rw->info, RW_WLOCKED) && IS_FLAG_SET(rw->info, RW_RECURSE) && rw->owner != thread_self()))
+    assert(!(rw->state == RW_WLOCKED && !rw->recurse && rw->owner == thread_self()));
+    rw->writers_waiting++;
+    while((rw->state == RW_RLOCKED) ||
+          (rw->state == RW_WLOCKED && !rw->recurse) ||
+          (rw->state == RW_WLOCKED && rw->recurse && rw->owner != thread_self()))
       cv_wait(&rw->condvar, &rw->mutex);
-    INC_COUNTER(rw->info);
-    SET_WLOCKED_STATE(rw->info);
+    rw->writer_enters++;
+    rw->state = RW_WLOCKED;
     rw->owner = thread_self();
-    DEC_WRITERS_WAITING(rw->info);
+    rw->writers_waiting--;
   }
   mtx_unlock(&rw->mutex);
 }
 
 void rw_leave(rwlock_t *rw) {
   mtx_lock(&rw->mutex);
-  assert(IS_FLAG_SET(rw->info, RW_LOCKED));
-  DEC_COUNTER(rw->info);
-  if(GET_COUNTER(rw->info) == 0) {
+  assert(rw->state & RW_LOCKED);
+  rw->readers--;
+  if(rw->readers == 0) {
     cv_broadcast(&rw->condvar);
-    SET_UNLOCKED_STATE(rw->info);
+    rw->state = RW_UNLOCKED;
     rw->owner = NULL;
   }
   mtx_unlock(&rw->mutex);
@@ -88,10 +69,10 @@ void rw_leave(rwlock_t *rw) {
 
 bool rw_try_upgrade(rwlock_t *rw) {
   mtx_lock(&rw->mutex);
-  assert(IS_FLAG_SET(rw->info, RW_RLOCKED));
+  assert(rw->state == RW_RLOCKED);
   bool result = false;
-  if(GET_COUNTER(rw->info) == 1) {
-    SET_WLOCKED_STATE(rw->info);
+  if(rw->readers == 1) {
+    rw->state = RW_WLOCKED;
     rw->owner = thread_self();
     result = true;
   }
@@ -101,13 +82,13 @@ bool rw_try_upgrade(rwlock_t *rw) {
 
 void rw_downgrade(rwlock_t *rw) {
   mtx_lock(&rw->mutex);
-  assert(IS_FLAG_SET(rw->info, RW_WLOCKED) && rw->owner == thread_self());
-  SET_RLOCKED_STATE(rw->info);
+  assert(rw->state == RW_WLOCKED && rw->owner == thread_self() && rw->writer_enters == 1);
+  rw->state = RW_RLOCKED;
   cv_broadcast(&rw->condvar);
   mtx_unlock(&rw->mutex);
 }
 
-void __rw_assert(rwlock_t *rw, rwf_t flag, const char *file, unsigned line) {
-  if(IS_FLAG_SET(rw->info, flag))
-    panic("rwlock %p: %u flag is not set. File:%s line:%u", rw, flag, file, line);
+void __rw_assert(rwlock_t *rw, rwa_t what, const char *file, unsigned line) {
+  if(!(rw->state == RW_UNLOCKED && what == RW_UNLOCKED) || !(rw->state & what))
+    panic("rwlock %p: has invalid state, expected:%u, actual:%u. File:%s line:%u", rw, what, rw->state, file, line);
 }
