d281d32f21415eb5d5a7604f4d7677369580ea58
Rafał Cieślak <rafalcieslak256@gmail.com>
2017-01-28 02:14:40 +0100

    Cleaned up placeholder names

--- tests/callout.c
+++ tests/callout.c
@@ -7,7 +7,7 @@
 static mtx_t foo_mtx;
 static condvar_t foo_cv;
 
-static void callout_foo(void *arg) {
+static void callout_simple(void *arg) {
   kprintf("The callout got executed!\n");
 
   mtx_lock(&foo_mtx);
@@ -20,7 +20,7 @@ static int test_callout_simple() {
   cv_init(&foo_cv, "test_callout_simple CV");
 
   callout_t callout;
-  callout_setup_relative(&callout, 10, callout_foo, NULL);
+  callout_setup_relative(&callout, 10, callout_simple, NULL);
 
   mtx_lock(&foo_mtx);
   cv_wait(&foo_cv, &foo_mtx);
@@ -29,21 +29,17 @@ static int test_callout_simple() {
   return KTEST_SUCCESS;
 }
 
-KTEST_ADD(callout_simple, test_callout_simple, 0);
-
-static mtx_t bar_mtx;
-static condvar_t bar_cv;
 static int current = 0;
 
-static void callout_bar(void *arg) {
+static void callout_ordered(void *arg) {
   int n = (int)arg;
   ktest_assert(current == n);
   current++;
 
   if (current == 10) {
-    mtx_lock(&bar_mtx);
-    cv_signal(&bar_cv);
-    mtx_unlock(&bar_mtx);
+    mtx_lock(&foo_mtx);
+    cv_signal(&foo_cv);
+    mtx_unlock(&foo_mtx);
   }
 }
 
@@ -55,15 +51,16 @@ static int test_callout_order() {
   int order[10] = {2, 5, 4, 6, 9, 0, 8, 1, 3, 7};
   callout_t callouts[10];
   for (int i = 0; i < 10; i++)
-    callout_setup_relative(&callouts[i], 10 + order[i] * 2, callout_bar,
+    callout_setup_relative(&callouts[i], 10 + order[i] * 4, callout_ordered,
                            (void *)order[i]);
 
-  mtx_lock(&bar_mtx);
-  cv_wait(&bar_cv, &bar_mtx);
-  mtx_unlock(&bar_mtx);
+  mtx_lock(&foo_mtx);
+  cv_wait(&foo_cv, &foo_mtx);
+  mtx_unlock(&foo_mtx);
   ktest_assert(current == 10);
 
   return KTEST_SUCCESS;
 }
 
+KTEST_ADD(callout_simple, test_callout_simple, 0);
 KTEST_ADD(callout_order, test_callout_order, 0);
