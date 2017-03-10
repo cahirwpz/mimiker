e65a2ae21ca341d4db8935667349c4c73c0ee5a6
czapiga <czapiga@users.noreply.github.com>
2017-03-10 16:55:14 +0100

    Initial rwlock implementation (#191)

--- include/rwlock.h
+++ include/rwlock.h
@@ -6,17 +6,16 @@
 typedef struct rwlock rwlock_t;
 
 typedef enum { RW_READER, RW_WRITER } rwo_t;
-typedef enum { RW_RECURSE = 1 } rwf_t;
 typedef enum {
   RW_UNLOCKED = 0,
   RW_RLOCKED = 1,
   RW_WLOCKED = 2,
-  RW_LOCKED = 3,
+  RW_LOCKED = 3
 } rwa_t;
 
-void rw_init(rwlock_t *rw, const char *name, rwf_t flags);
+void rw_init(rwlock_t *rw, const char *name, bool recursive);
 void rw_destroy(rwlock_t *rw);
-void rw_enter(rwlock_t *rw, rw_t who);
+void rw_enter(rwlock_t *rw, rwo_t who);
 void rw_leave(rwlock_t *rw);
 bool rw_try_upgrade(rwlock_t *rw);
 void rw_downgrade(rwlock_t *rw);
