7f471b36c2124416792e51c94633f08615a575ba
Rafał Cieślak <rafalcieslak256@gmail.com>
2017-03-26 23:48:33 +0200

    Implemented thread_join and thread_reap (#223)
    * Implemented thread_join and thread_reap
    * Added synchronization for all_threads list

--- sys/thread.c
+++ sys/thread.c
@@ -11,14 +11,23 @@
 static MALLOC_DEFINE(td_pool, "kernel threads pool");
 
 typedef TAILQ_HEAD(, thread) thread_list_t;
-/* TODO: Synchronize access to the list */
+
+static mtx_t all_threads_mtx;
 static thread_list_t all_threads;
+
+static mtx_t zombie_threads_mtx;
+static thread_list_t zombie_threads;
 
 void thread_init() {
   kmalloc_init(td_pool);
   kmalloc_add_pages(td_pool, 2);
 
+  log("Thread init.");
+  mtx_init(&all_threads_mtx, MTX_DEF);
   TAILQ_INIT(&all_threads);
+
+  mtx_init(&zombie_threads_mtx, MTX_DEF);
+  TAILQ_INIT(&zombie_threads);
 }
 
 /* FTTB such a primitive method of creating new TIDs will do. */
@@ -28,7 +37,30 @@ static tid_t make_tid() {
   return tid++;
 }
 
+void thread_reap() {
+  /* Exit early if there are no zombie threads. This is particularly important
+     during kernel startup. The first thread is created before mtx is
+     initialized! Luckily, we don't need to lock it to check whether the list is
+     empty. */
+  if (TAILQ_EMPTY(&zombie_threads))
+    return;
+
+  mtx_lock(&zombie_threads_mtx);
+  thread_list_t thq = zombie_threads;
+  TAILQ_INIT(&zombie_threads);
+  mtx_unlock(&zombie_threads_mtx);
+
+  thread_t *td;
+  TAILQ_FOREACH (td, &thq, td_zombieq) {
+    log("Reaping thread %ld (%s)", td->td_tid, td->td_name);
+    thread_delete(td);
+  }
+}
+
 thread_t *thread_create(const char *name, void (*fn)(void *), void *arg) {
+
+  thread_reap();
+
   thread_t *td = kmalloc(td_pool, sizeof(thread_t), M_ZERO);
 
   td->td_sleepqueue = sleepq_alloc();
@@ -40,7 +72,19 @@ thread_t *thread_create(const char *name, void (*fn)(void *), void *arg) {
 
   ctx_init(td, fn, arg);
 
+  /* Do not lock the mutex if this call to thread_create was done before any
+     threads exists (from thread_bootstrap). Locking the mutex would cause
+     problems because during thread bootstrap the current thread is NULL, so a
+     fresh unused mutex looks like it is already owned (because mtx->owner also
+     is NULL). The proper solution to this problem would be either to use a
+     dummy value for PCPU(currthread) during thread_bootstrap, or to use a
+     non-NULL (e.g. -1) value for unowned mutexes. Both options require
+     discussion, so let's handle this case manually for now. */
+  if (thread_self() != NULL)
+    mtx_lock(&all_threads_mtx);
   TAILQ_INSERT_TAIL(&all_threads, td, td_all);
+  if (thread_self() != NULL)
+    mtx_unlock(&all_threads_mtx);
 
   td->td_state = TDS_READY;
 
@@ -54,7 +98,9 @@ void thread_delete(thread_t *td) {
   assert(td != thread_self());
   assert(td->td_sleepqueue != NULL);
 
+  mtx_lock(&all_threads_mtx);
   TAILQ_REMOVE(&all_threads, td, td_all);
+  mtx_unlock(&all_threads_mtx);
 
   pm_free(td->td_kstack_obj);
 
@@ -68,33 +114,62 @@ thread_t *thread_self() {
 }
 
 /* For now this is only a stub */
-noreturn void thread_exit() {
+noreturn void thread_exit(int exitcode) {
   thread_t *td = thread_self();
 
   log("Thread '%s' {%p} has finished.", td->td_name, td);
 
-  /* Thread must not exit while in critical section! */
-  assert(td->td_csnest == 0);
+  /* Thread must not exit while in critical section! However, we can't use
+     assert here, because assert also calls thread_exit. Thus, in case this
+     condition is not met, we'll log the problem, and try to fix the problem. */
+  if (td->td_csnest != 0) {
+    log("ERROR: Thread must not exit within a critical section!");
+    while (td->td_csnest--)
+      critical_leave();
+  }
 
   fdtab_release(td->td_fdtable);
 
+  mtx_lock(&zombie_threads_mtx);
+
   critical_enter();
+  TAILQ_INSERT_TAIL(&zombie_threads, td, td_zombieq);
+  td->td_exitcode = exitcode;
+  sleepq_broadcast(&td->td_exitcode);
   td->td_state = TDS_INACTIVE;
-  sched_yield();
   critical_leave();
+
+  mtx_unlock(&zombie_threads_mtx);
+
+  sched_yield();
 
   /* sched_yield will return immediately when scheduler is not active */
   while (true)
     ;
 }
 
+void thread_join(thread_t *p) {
+  thread_t *td = thread_self();
+  thread_t *otd = p;
+  log("Joining '%s' {%p} with '%s' {%p}", td->td_name, td, otd->td_name, otd);
+
+  if (otd->td_state == TDS_INACTIVE)
+    return;
+
+  sleepq_wait(&otd->td_exitcode, "Joining threads");
+}
+
 /* It would be better to have a hash-map from tid_t to thread_t,
  * but using a list is sufficient for now. */
 thread_t *thread_get_by_tid(tid_t id) {
   thread_t *td = NULL;
+
+  mtx_lock(&all_threads_mtx);
   TAILQ_FOREACH (td, &all_threads, td_all) {
     if (td->td_tid == id)
       break;
   }
+  mtx_unlock(&all_threads_mtx);
+
   return td;
 }
---
+++ tests/thread_exit.c
@@ -0,0 +1,52 @@
+#include <stdc.h>
+#include <clock.h>
+#include <thread.h>
+#include <sched.h>
+#include <ktest.h>
+
+static int exit_time[] = {100, 200, 150};
+static realtime_t start;
+
+/* TODO: callout + sleepq, once we've implemented callout_schedule. */
+static void test_thread(void *p) {
+  int e = *(int *)p;
+  while (1) {
+    realtime_t tdiff = clock_get() - start;
+    if (tdiff > e)
+      thread_exit(0);
+    else
+      sched_yield();
+  }
+}
+
+/* This tests both thread_join as well as thread_exit. */
+static int test_thread_join() {
+  thread_t *t1 = thread_create("test-thread-1", test_thread, &exit_time[0]);
+  thread_t *t2 = thread_create("test-thread-2", test_thread, &exit_time[1]);
+  thread_t *t3 = thread_create("test-thread-3", test_thread, &exit_time[2]);
+
+  tid_t t1_id = t1->td_tid;
+  tid_t t2_id = t2->td_tid;
+  tid_t t3_id = t3->td_tid;
+
+  start = clock_get();
+  sched_add(t1);
+  sched_add(t2);
+  sched_add(t3);
+
+  thread_join(t1);
+  thread_join(t2);
+  thread_join(t3);
+
+  /* Ensure that all zombie threads were reaped. */
+  thread_reap();
+
+  /* Checks that the threads no longer exist. */
+  assert(thread_get_by_tid(t1_id) == NULL);
+  assert(thread_get_by_tid(t2_id) == NULL);
+  assert(thread_get_by_tid(t3_id) == NULL);
+
+  return KTEST_SUCCESS;
+}
+
+KTEST_ADD(thread_join, test_thread_join, 0);
