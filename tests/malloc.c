d92e3a77fade8183e716955e2573e1eadd81c1cc
czapiga <Sz.Czapiga@gmail.com>
2017-04-04 19:02:59 +0200

    Multithreaded malloc test

--- tests/malloc.c
+++ tests/malloc.c
@@ -2,6 +2,7 @@
 #include <malloc.h>
 #include <ktest.h>
 #include <thread.h>
+#include <sched.h>
 
 static int malloc_one_allocation(void) {
   MALLOC_DEFINE(mp, "testing memory pool");
@@ -51,6 +52,37 @@ static int malloc_dynamic_pages_addition(void) {
   return KTEST_SUCCESS;
 }
 
+#define THREADS_NUMBER 10
+#define ALLOCATIONS_PER_THREAD 200
+#define ALLOCATIONS (THREADS_NUMBER * ALLOCATIONS_PER_THREAD)
+
+void *ptrs[ALLOCATIONS];
+static malloc_pool_t test_pool[1] = {
+  {{NULL}, MB_MAGIC, "testing memory pool", {NULL, NULL}}};
+
+static void threads_function(void *arg) {
+  const unsigned start = ALLOCATIONS_PER_THREAD * (unsigned)arg;
+  for (unsigned i = 0; i < ALLOCATIONS_PER_THREAD; i++) {
+    ptrs[i + start] = kmalloc(test_pool, 1, 0);
+    assert(NULL != ptrs[i + start]);
+  }
+}
+
+static int malloc_multiple_threads(void) {
+  kmalloc_init(test_pool, 1, 40);
+  thread_t *threads[THREADS_NUMBER];
+  for (int i = 0; i < THREADS_NUMBER; i++)
+    threads[i] =
+      thread_create("Malloc test thread", threads_function, (void *)i);
+  for (int i = 0; i < THREADS_NUMBER; i++)
+    sched_add(threads[i]);
+  for (int i = 0; i < THREADS_NUMBER; i++)
+    thread_join(threads[i]);
+  for (int i = 0; i < ALLOCATIONS; i++)
+    kfree(test_pool, ptrs[i]);
+  return KTEST_SUCCESS;
+}
+
 #define MALLOC_RANDINT_PAGES 64
 static int malloc_random_size(unsigned int randint) {
   if (randint == 0)
@@ -69,6 +101,7 @@ KTEST_ADD(malloc_one_allocation, malloc_one_allocation, 0);
 KTEST_ADD(malloc_invalid_values, malloc_invalid_values, 0);
 KTEST_ADD(malloc_multiple_allocations, malloc_multiple_allocations, 0);
 KTEST_ADD(malloc_dynamic_pages_addition, malloc_dynamic_pages_addition, 0);
+KTEST_ADD(malloc_multiple_threads, malloc_multiple_threads, 0);
 /* Reserve some memory for mem_block_t. */
 #define RESERVED 1024
 KTEST_ADD_RANDINT(malloc_random_size, malloc_random_size, 0,
