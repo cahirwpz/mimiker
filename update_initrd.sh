dd65ef03ceafbdab10d27a39fd798100de877c10
Rafał Cieślak <rafalcieslak256@gmail.com>
2017-04-10 14:56:18 +0200

    Building initrd

---
+++ update_initrd.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+THISSCRIPT=$(basename $0)
+RULEFILES=$(find . -name initrd.rules)
+BUILDDIR="initrd"
+DEPFILE=".initrd.D"
+CPIOFILE="initrd.cpio"
+
+### TODO: It is possible to implement this script so that it would only copy the
+### modified files, and incrementally update the ramdisk. However, this is
+### difficult, and we don't keep thousands of files in the ramdisk, so
+### recreating entire cpio archive will be good for now.
+
+# Clean/create the build directory
+rm -rf $BUILDDIR
+mkdir -p $BUILDDIR
+
+SOURCEFILES=
+
+# Gather all files with rules, and copy files to the build dir
+for rulefile in $RULEFILES; do
+    echo "Processing file $rulefile"
+    sourcedir=$(dirname $rulefile)
+    rules=$(cat $rulefile)
+    while read -r line; do
+        source=$sourcedir/$(echo "$line" | cut  -d' ' -f1)
+        target=$BUILDDIR/$(echo "$line" | cut  -d' ' -f2)
+        echo "$source -> $target"
+        mkdir -p $(dirname $target)
+        cp -r $source $target
+        SOURCEFILES+=" $source"
+    done < $rulefile
+done
+
+# Create cpio archive
+echo "Generating $CPIOFILE..."
+(cd initrd && find -depth -print | cpio --format=crc -o > "../$CPIOFILE")
+
+# Generate depfile
+rm -f $DEPFILE
+echo "initrd.cpio: $THISSCRIPT $RULEFILES $SOURCEFILES" | tr '\n' ' ' >> $DEPFILE
