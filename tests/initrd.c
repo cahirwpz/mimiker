00f9de12afb9d6b5340db192ee6cbcbf26ad8dea
coodie <poppinggoovie@gmail.com>
2017-04-17 15:36:38 +0200

    Save work on tests

--- tests/initrd.c
+++ tests/initrd.c
@@ -4,6 +4,21 @@
 #include <mount.h>
 #include <vm_map.h>
 #include <ktest.h>
+#include <dirent.h>
+
+static void prepare_uio(uio_t *uio, iovec_t *iov, char *buffer, int buf_size)
+{
+  uio->uio_op = UIO_READ;
+
+  /* Read entire file - even too much. */
+  uio->uio_iovcnt = 1;
+  uio->uio_vmspace = get_kernel_vm_map();
+  uio->uio_iov = iov;
+  uio->uio_offset = 0;
+  iov->iov_base = buffer;
+  iov->iov_len = buf_size;
+  uio->uio_resid = buf_size;
+}
 
 static void dump_file(const char *path) {
   vnode_t *v;
@@ -14,20 +29,57 @@ static void dump_file(const char *path) {
   memset(buffer, '\0', sizeof(buffer));
   uio_t uio;
   iovec_t iov;
-  uio.uio_op = UIO_READ;
 
-  /* Read entire file - even too much. */
-  uio.uio_iovcnt = 1;
-  uio.uio_vmspace = get_kernel_vm_map();
-  uio.uio_iov = &iov;
-  uio.uio_offset = 0;
-  iov.iov_base = buffer;
-  iov.iov_len = sizeof(buffer);
-  uio.uio_resid = sizeof(buffer);
+  prepare_uio(&uio, &iov, buffer, sizeof(buffer));
 
   res = VOP_READ(v, &uio);
 
   kprintf("file %s:\n%s\n", path, buffer);
+}
+
+void dump_dirent(dirent_t *dir)
+{
+  kprintf("%s\n", dir->d_name);
+}
+
+void dump_directory(const char *path)
+{
+  vnode_t *v;
+  int res = vfs_lookup(path, &v);
+  assert(res == 0);
+
+  char buffer[50];
+  memset(buffer, '\0', sizeof(buffer));
+  uio_t uio;
+  iovec_t iov;
+
+  prepare_uio(&uio, &iov, buffer, sizeof(buffer));
+  int bytes = 0;
+
+  kprintf("Contents of directory: %s\n", path);
+
+  do
+  {
+    bytes = VOP_READDIR(v, &uio);
+    uio.uio_offset = 0;
+
+    /* Dump directories inside buffer */
+    dirent_t *dir = buffer;
+    for(int off = 0; off < bytes; off += dir->d_reclen)
+    {
+      dump_dirent(dir);
+      dir = _DIRENT_NEXT(dir);
+    }
+    uio.uio_offset = 0;
+  } while(bytes > 0);
+
+  return KTEST_SUCCESS;
+}
+
+/* TODO this test should be moved to */
+static int test_readdir() {
+  dump_directory("/initrd/directory1/");
+  dump_directory("/initrd/");
 }
 
 static int test_ramdisk() {
