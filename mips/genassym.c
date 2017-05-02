cahirwpz/mimiker

commit 3f1bea7c23b424e07fdc33a37af6f685f7ca8fde

Remove unused TD_PROC

mips/genassym.c
@@ -1,14 +1,12 @@
 #include <assym.h>
 #include <context.h>
 #include <thread.h>
-#include <proc.h>
 #include <pcpu.h>
 #include <mips/ctx.h>
 #include <mips/exc.h>
 
 ASSYM(TDF_NEEDSWITCH, TDF_NEEDSWITCH);
 
-ASSYM(TD_PROC, offsetof(thread_t, td_proc));
 ASSYM(TD_UCTX, offsetof(thread_t, td_uctx));
 ASSYM(TD_UCTX_FPU, offsetof(thread_t, td_uctx_fpu));
 ASSYM(TD_KFRAME, offsetof(thread_t, td_kframe));
@@ -88,4 +86,3 @@ ASSYM(EXC_FRAME_SIZ, sizeof(exc_frame_t) + CALLFRAME_SIZ);
 
 ASSYM(PCPU_CURTHREAD, offsetof(pcpu_t, curthread));
 ASSYM(PCPU_USPACE, offsetof(pcpu_t, uspace));
-

commit 50e8d4c3d610b58013157b6c52b78d0bb1109d94

Fix a bug I introduced with 3f1bea7

mips/genassym.c
@@ -2,11 +2,13 @@
 #include <context.h>
 #include <thread.h>
 #include <pcpu.h>
+#include <proc.h>
 #include <mips/ctx.h>
 #include <mips/exc.h>
 
 ASSYM(TDF_NEEDSWITCH, TDF_NEEDSWITCH);
 
+ASSYM(TD_PROC, offsetof(thread_t, td_proc));
 ASSYM(TD_UCTX, offsetof(thread_t, td_uctx));
 ASSYM(TD_UCTX_FPU, offsetof(thread_t, td_uctx_fpu));
 ASSYM(TD_KFRAME, offsetof(thread_t, td_kframe));
@@ -84,5 +86,6 @@ ASSYM(EXC_CAUSE, offsetof(exc_frame_t, cause));
 
 ASSYM(EXC_FRAME_SIZ, sizeof(exc_frame_t) + CALLFRAME_SIZ);
 
+ASSYM(P_USPACE, offsetof(proc_t, p_uspace));
+
 ASSYM(PCPU_CURTHREAD, offsetof(pcpu_t, curthread));
-ASSYM(PCPU_USPACE, offsetof(pcpu_t, uspace));

mips/switch.S
@@ -39,8 +39,11 @@ ctx_resume:
         LOAD_PCPU($t0)
         sw      $s0, PCPU_CURTHREAD($t0)
 
-        lw      $a0, PCPU_USPACE($t0)
-        jal     vm_map_activate
+        lw      $a0, TD_PROC($s0)
+        beqz    $a0, 1f                # switching to kernel thread ?
+        nop
+        lw      $a0, P_USPACE($a0)
+1:      jal     vm_map_activate
         nop
 
         addu    $a1, $s0, TD_KCTX

sys/vm_map.c
@@ -24,6 +24,7 @@ void vm_map_activate(vm_map_t *map) {
 vm_map_t *get_user_vm_map() {
   return PCPU_GET(uspace);
 }
+
 vm_map_t *get_kernel_vm_map() {
   return &kspace;
 }