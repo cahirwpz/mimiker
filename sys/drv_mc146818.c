4cc82f0af69cfc932a6e54ffc0460e14b9227887
Rafał Cieślak <rafalcieslak256@gmail.com>
2017-05-09 22:33:11 +0200

    Creating a mc146818->regs resource

--- sys/drv_mc146818.c
+++ sys/drv_mc146818.c
@@ -19,7 +19,10 @@
 #define MC146818_REG_B_BCD 0x04
 #define MC146818_REG_B_24H 0x02
 
-typedef struct mc146818_state { resource_t *io; } mc146818_state_t;
+typedef struct mc146818_state {
+  resource_t *io;
+  resource_t regs;
+} mc146818_state_t;
 
 static int mc146818_probe(device_t *dev) {
   isa_device_t *isad = isa_device_of(dev);
@@ -32,25 +35,31 @@ static int mc146818_probe(device_t *dev) {
   return 1;
 }
 
-static void mc146818_write_reg(mc146818_state_t *mc146818, uint8_t reg,
-                               uint8_t value) {
-  bus_space_write_1(mc146818->io, MC146818_ADDR, reg);
-  bus_space_write_1(mc146818->io, MC146818_DATA, value);
+static void mc146817_regs_write_1(resource_t* handle, unsigned offset, uint8_t val){
+  mc146818_state_t* mc146818 = handle->r_owner;
+  bus_space_write_1(mc146818->io, MC146818_ADDR, offset);
+  bus_space_write_1(mc146818->io, MC146818_DATA, val);
 }
-static uint8_t mc146818_read_reg(mc146818_state_t *mc146818, uint8_t reg) {
-  bus_space_write_1(mc146818->io, MC146818_ADDR, reg);
+static uint8_t mc146817_regs_read_1(resource_t* handle, unsigned offset){
+  mc146818_state_t* mc146818 = handle->r_owner;
+  bus_space_write_1(mc146818->io, MC146818_ADDR, offset);
   return bus_space_read_1(mc146818->io, MC146818_DATA);
 }
+
+static bus_space_t mc146818_regs_bus_space = {
+  .read_1 = mc146817_regs_read_1,
+  .write_1 = mc146817_regs_write_1
+};
 
 static void mc146818_read_time(mc146818_state_t *mc146818, int *year,
                                int *month, int *day, int *hour, int *minute,
                                int *second) {
-  *second = mc146818_read_reg(mc146818, MC146818_REG_SECOND);
-  *minute = mc146818_read_reg(mc146818, MC146818_REG_MINUTE);
-  *hour = mc146818_read_reg(mc146818, MC146818_REG_HOUR);
-  *day = mc146818_read_reg(mc146818, MC146818_REG_DAY);
-  *month = mc146818_read_reg(mc146818, MC146818_REG_MONTH);
-  *year = mc146818_read_reg(mc146818, MC146818_REG_YEAR) + 2000;
+  *second = bus_space_read_1(&mc146818->regs, MC146818_REG_SECOND);
+  *minute = bus_space_read_1(&mc146818->regs, MC146818_REG_MINUTE);
+  *hour = bus_space_read_1(&mc146818->regs, MC146818_REG_HOUR);
+  *day = bus_space_read_1(&mc146818->regs, MC146818_REG_DAY);
+  *month = bus_space_read_1(&mc146818->regs, MC146818_REG_MONTH);
+  *year = bus_space_read_1(&mc146818->regs, MC146818_REG_YEAR) + 2000;
 }
 
 #define DEV_MC146818_BUFFER_SIZE 32
@@ -86,8 +95,15 @@ static int mc146818_attach(device_t *dev) {
   /* TODO: Only allocate 0070-007f!  */
   mc146818->io = isad->isa_bus;
 
-  mc146818_write_reg(mc146818, MC146818_REG_B,
-                     MC146818_REG_B_24H | MC146818_REG_B_BCD);
+  mc146818->regs = (resource_t){
+    .r_owner = mc146818,
+    .r_bus_space = &mc146818_regs_bus_space,
+    .r_type = RT_IOPORTS,
+    .r_start = 0,
+    .r_end = 0x3f
+  };
+
+  bus_space_write_1(&mc146818->regs, MC146818_REG_B, MC146818_REG_B_24H | MC146818_REG_B_BCD);
 
   static int installed = 0;
   if (!installed++) {
