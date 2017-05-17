bf37a01c19f2c47836994ef6b434b187e94ba296
MoloToha <anton.lunyov@gmail.com>
2017-05-17 17:31:49 +0200

    Removed unnecessary files, added actual correctness verification to the test

--- user/test_dup/test_dup.c
+++ user/test_dup/test_dup.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <assert.h>
 
 int main() {
   int x = open("/tests/dup_test_file", O_RDONLY, 0);
   int y = dup(0);
-  printf("x = %d, y = %d\n", x, y);
   dup2(x, 0);
   char word[100];
   scanf("%[^\n]s", word);
-  printf("%s\n", word);
+  assert(strcmp(word, "Hello, World!") == 0);
   dup2(y, 0);
   return 0;
 }
