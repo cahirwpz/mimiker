4f8ff0776f6e3effc396f70d1b456b38ba3f57e5
czapiga <Sz.Czapiga@gmail.com>
2017-05-19 00:09:17 +0200

    mips/clock time calculation change

--- include/time.h
+++ include/time.h
@@ -12,19 +12,20 @@ typedef struct timeval {
 } timeval_t;
 
 /* Operations on timevals. */
-static inline void timerclear(timeval_t *tvp) {
+static inline void timeval_clear(volatile timeval_t *tvp) {
   *tvp = (timeval_t){.tv_sec = 0, .tv_usec = 0};
 }
 
-static inline bool timerisset(timeval_t *tvp) {
+static inline bool timeval_isset(volatile timeval_t *tvp) {
   return tvp->tv_sec || tvp->tv_usec;
 }
 
-#define timercmp(tvp, uvp, cmp)                                                \
+#define timeval_cmp(tvp, uvp, cmp)                                             \
   (((tvp)->tv_sec == (uvp)->tv_sec) ? (((tvp)->tv_usec)cmp((uvp)->tv_usec))    \
                                     : (((tvp)->tv_sec)cmp((uvp)->tv_sec)))
 
-static inline void timeradd(timeval_t *tvp, timeval_t *uvp, timeval_t *vvp) {
+static inline void timeval_add(volatile timeval_t *tvp, volatile timeval_t *uvp,
+                               volatile timeval_t *vvp) {
   vvp->tv_sec = tvp->tv_sec + uvp->tv_sec;
   vvp->tv_usec = tvp->tv_usec + uvp->tv_usec;
   if (vvp->tv_usec >= 1000000) {
@@ -33,13 +34,18 @@ static inline void timeradd(timeval_t *tvp, timeval_t *uvp, timeval_t *vvp) {
   }
 }
 
-static inline void timersub(timeval_t *tvp, timeval_t *uvp, timeval_t *vvp) {
+static inline void timeval_sub(volatile timeval_t *tvp, volatile timeval_t *uvp,
+                               volatile timeval_t *vvp) {
   vvp->tv_sec = tvp->tv_sec - uvp->tv_sec;
   vvp->tv_usec = tvp->tv_usec - uvp->tv_usec;
   if (vvp->tv_usec < 0) {
     vvp->tv_sec--;
     vvp->tv_usec += 1000000;
   }
+}
+
+static inline long timeval_get_ms(volatile timeval_t *tvp) {
+  return (tvp->tv_sec * 1000) + (tvp->tv_usec / 1000);
 }
 
 #endif /* !_SYS_TIME_H_ */
--- mips/clock.c
+++ mips/clock.c
@@ -7,39 +7,39 @@
 #include <sched.h>
 #include <sysinit.h>
 
-/* This counter is incremented every millisecond. */
-static volatile realtime_t mips_clock_ms;
+/* This counter is incremented every microsecond. */
+static volatile timeval_t mips_clock_us;
 
 static void mips_clock_init() {
   mips32_set_c0(C0_COUNT, 0);
   mips32_set_c0(C0_COMPARE, TICKS_PER_MS);
 
-  mips_clock_ms = 0;
+  timeval_clear(&mips_clock_us);
 
   /* Enable core timer interrupts. */
   mips32_bs_c0(C0_STATUS, SR_IM7);
 }
 
 void mips_clock_irq_handler() {
-  uint32_t compare = mips32_get_c0(C0_COMPARE);
-  uint32_t count = mips32_get_c0(C0_COUNT);
-  int32_t diff = compare - count;
+  volatile uint32_t compare = mips32_get_c0(C0_COMPARE);
+  volatile uint32_t count = mips32_get_c0(C0_COUNT);
+  volatile int32_t diff = compare - count;
 
   /* Should not happen. Potentially spurious interrupt. */
   if (diff > 0)
     return;
 
+  timeval_t us = (timeval_t){.tv_sec = 0, .tv_usec = 1000};
   /* This loop is necessary, because sometimes we may miss some ticks. */
   while (diff < TICKS_PER_MS) {
     compare += TICKS_PER_MS;
-    mips_clock_ms++;
+    timeval_add(&mips_clock_us, &us, &mips_clock_us);
     diff = compare - count;
   }
 
   /* Set compare register. */
   mips32_set_c0(C0_COMPARE, compare);
-
-  clock(mips_clock_ms);
+  clock(timeval_get_ms(&mips_clock_us));
 }
 
 SYSINIT_ADD(mips_clock, mips_clock_init, DEPS("callout", "sched"));
