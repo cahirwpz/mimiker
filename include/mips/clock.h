2126c0ff04c3c1e29a23b76db5e46770a9d2024c
Krystian Bacławski <krystian.baclawski@gmail.com>
2017-05-21 04:11:02 +0200

    Reworked basic infrastructure for CPU interrupts

--- include/mips/clock.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MIPS_CLOCK_H_
-#define _MIPS_CLOCK_H_
-
-#include <clock.h>
-
-/* Processes core timer interrupts. */
-void mips_clock_irq_handler();
-
-#endif /* !_MIPS_CLOCK_H_ */
--- mips/clock.c
+++ mips/clock.c
@@ -1,7 +1,9 @@
 #include <common.h>
 #include <mips/config.h>
 #include <mips/mips.h>
-#include <mips/clock.h>
+#include <mips/intr.h>
+#include <interrupt.h>
+#include <bus.h>
 #include <callout.h>
 #include <mutex.h>
 #include <sched.h>
@@ -10,17 +12,7 @@
 /* This counter is incremented every millisecond. */
 static volatile realtime_t mips_clock_ms;
 
-static void mips_clock_init() {
-  mips32_set_c0(C0_COUNT, 0);
-  mips32_set_c0(C0_COMPARE, TICKS_PER_MS);
-
-  mips_clock_ms = 0;
-
-  /* Enable core timer interrupts. */
-  mips32_bs_c0(C0_STATUS, SR_IM7);
-}
-
-void mips_clock_irq_handler() {
+static void mips_timer_intr() {
   uint32_t compare = mips32_get_c0(C0_COMPARE);
   uint32_t count = mips32_get_c0(C0_COUNT);
   int32_t diff = compare - count;
@@ -42,4 +34,16 @@ void mips_clock_irq_handler() {
   clock(mips_clock_ms);
 }
 
-SYSINIT_ADD(mips_clock, mips_clock_init, DEPS("callout", "sched"));
+static INTR_HANDLER_DEFINE(mips_timer_intr_handler, NULL, mips_timer_intr, NULL,
+                           "MIPS cpu timer", 0);
+
+static void mips_timer_init() {
+  mips32_set_c0(C0_COUNT, 0);
+  mips32_set_c0(C0_COMPARE, TICKS_PER_MS);
+
+  mips_clock_ms = 0;
+
+  mips_intr_setup(mips_timer_intr_handler, 7);
+}
+
+SYSINIT_ADD(mips_timer, mips_timer_init, DEPS("callout", "sched"));
