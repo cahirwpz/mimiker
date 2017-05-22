25f55bba175f56d1627b7b6cb53bdab13ff6e1fb
czapiga <Sz.Czapiga@gmail.com>
2017-05-23 00:44:50 +0200

    Formatting fix

--- include/mips/clock.h
+++ include/mips/clock.h
@@ -7,12 +7,12 @@
 typedef struct timer_event timer_event_t;
 
 struct timer_event {
-      TAILQ_ENTRY(timer_event) tev_link;     /*  entry on list of all time events */
-       timeval_t tev_alarm;                   /*  absolute time when this event should
-                                                  be handled */
-        void (*tev_func)(timer_event_t *self); /*  callback function called in
-                                                   interrupt context */
-         void *tev_data;                        /*  auxiliary data */
+  TAILQ_ENTRY(timer_event) tev_link; /*  entry on list of all time events */
+  timeval_t tev_alarm;               /*  absolute time when this event should
+                                         be handled */
+  void (*tev_func)(timer_event_t *self); /*  callback function called in
+                                             interrupt context */
+  void *tev_data;                        /*  auxiliary data */
 };
 
 /*  call @tev callback after @tvdiff time has passed */
