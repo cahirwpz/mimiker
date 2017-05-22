c4688e0e7ba38b26d02a569e32d83c3f5bd3a8db
czapiga <Sz.Czapiga@gmail.com>
2017-05-22 23:56:13 +0200

    clock_get is using C0_COUNT

--- include/mips/config.h
+++ include/mips/config.h
@@ -2,6 +2,7 @@
 #define __MIPS_CONFIG_H__
 
 #define CPU_FREQ (100 * 1000 * 1000)
+#define TICKS_PER_SEC CPU_FREQ
 #define TICKS_PER_MS (CPU_FREQ / 1000)
 #define TICKS_PER_US (CPU_FREQ / (1000 * 1000))
 
--- sys/clock.c
+++ sys/clock.c
@@ -2,11 +2,14 @@
 #include <thread.h>
 #include <callout.h>
 #include <sched.h>
+#include <mips/config.h>
 
 static realtime_t ms;
 
 realtime_t clock_get() {
-  return ms;
+  uint32_t count = mips32_get_c0(C0_COUNT);
+  timeval_t now = TIMEVAL_INIT(count / TICKS_PER_SEC, count % TICKS_PER_SEC);
+  return timeval_to_ms(&now);
 }
 
 void clock(realtime_t _ms) {
