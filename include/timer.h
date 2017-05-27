b2fcfddbb933ca92cfe7993f3c9eac688fc64cd4
czapiga <Sz.Czapiga@gmail.com>
2017-05-27 17:12:03 +0200

    initial implementation

--- include/timer.h
+++ include/timer.h
@@ -8,8 +8,8 @@ typedef struct timer_event timer_event_t;
 
 struct timer_event {
   TAILQ_ENTRY(timer_event) tev_link; /*  entry on list of all time events */
-  timeval_t tev_alarm;               /*  absolute time when this event should
-                                         be handled */
+  timeval_t tev_when;                /*  absolute time when this event should
+                                          be handled */
   void (*tev_func)(timer_event_t *self); /*  callback function called in
                                              interrupt context */
   void *tev_data;                        /*  auxiliary data */
@@ -19,6 +19,6 @@ int cpu_timer_add_event(timer_event_t *tev);
 int cpu_timer_remove_event(timer_event_t *tev);
 
 /*  Processes core timer interrupts. */
-void cpu_timer_irq_handler(void);
+void cpu_timer_intr(void *arg);
 
 #endif /*  !_TIMER_H_ */
--- mips/timer.c
+++ mips/timer.c
@@ -1,13 +1,61 @@
 #include <timer.h>
-
+#include <interrupt.h>
+#include <mips/m32c0.h>
+#include <mips/config.h>
+#include <mips/intr.h>
+#include <mips/clock.h>
+#include <sysinit.h>
+#include <malloc.h>
+#include <klog.h>
 typedef TAILQ_HEAD(, timer_event) timer_event_list_t;
 static timer_event_list_t events = TAILQ_HEAD_INITIALIZER(events);
 
 int cpu_timer_add_event(timer_event_t *tev) {
+  TAILQ_INSERT_HEAD(&events, tev, tev_link);
   return 0;
 }
+
 int cpu_timer_remove_event(timer_event_t *tev) {
+  timer_event_t *event;
+  TAILQ_FOREACH (event, &events, tev_link)
+    if (event == tev) {
+      TAILQ_REMOVE(&events, event, tev_link);
+      break;
+    }
   return 0;
 }
-void cpu_timer_irq_handler() {
+
+void cpu_timer_intr(void *arg) {
+  uint32_t compare = mips32_get_c0(C0_COMPARE);
+  uint32_t count = mips32_get_c0(C0_COUNT);
+  /* Should not happen. Potentially spurious interrupt. */
+  if (compare - count > 0)
+    return;
+
+  struct timer_event *event;
+  TAILQ_FOREACH (event, &events, tev_link) {
+    if (tv2st(event->tev_when) * 1000 > compare) {
+      compare = event->tev_when.tv_sec * 1000000 + event->tev_when.tv_usec;
+      mips32_set_c0(C0_COMPARE, compare);
+      break;
+    }
+    TAILQ_REMOVE(&events, event, tev_link);
+    event->tev_func(event);
+    break;
+  }
 }
+
+static INTR_HANDLER_DEFINE(cpu_timer_intr_handler, NULL, cpu_timer_intr, NULL,
+                           "CPU timer", 0);
+
+static void cpu_timer_init(void) {
+  mips32_set_c0(C0_COUNT, 0);
+  mips32_set_c0(C0_COMPARE, TICKS_PER_MS);
+  mips_intr_setup(cpu_timer_intr_handler, 7);
+  timer_event_t *clock = kmalloc(M_TEMP, sizeof(*clock), M_ZERO);
+  clock->tev_when = TIMEVAL(0.001);
+  clock->tev_func = mips_clock;
+  cpu_timer_add_event(clock);
+}
+
+SYSINIT_ADD(cpu_timer, cpu_timer_init, DEPS("callout", "sched"));
