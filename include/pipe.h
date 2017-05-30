be7f1c2de71f068a571cc8fec0ac430a805dfa3f
MoloToha <anton.lunyov@gmail.com>
2017-05-30 18:00:45 +0200

    finished pipeops prototypes

--- include/pipe.h
+++ include/pipe.h
@@ -1,11 +1,40 @@
 #include <mutex.h>
 #include <condvar.h>
+#include <thread.h>
+#include <file.h>
+#include <uio.h>
+#include <vnode.h>
 
 /* size of pipe buffer */
 #define PIPE_SIZE 4096
 
+#define PIPE_EOF 0x1
+
+typedef struct pipebuf {
+  size_t cnt;      /* current number of bytes in buffer */
+  uint32_t in;     /* in offset (start of write) */
+  uint32_t out;    /* out offset (start of read) */
+  size_t  size;    /* size of buffer */
+  void *data;
+} pipebuf_t;
+
+typedef enum pipetype {
+  PIPE_READ_END,       /* indicates read end of the pipe */
+  PIPE_WRITE_END       /* indicates write end of the pipe */
+} pipetype_t;
+
+/* pipe structure, currently Mimiker supports only unidirectional pipes */
 typedef struct pipe {
   mtx_t pipe_mtx;         /* pipe mutex */
   condvar_t pipe_read_cv; /* condvar for waiting for input while reading */
-  pipebuf_t pipe_buf;     /* pipe buffer */
+  pipebuf_t pipe_buf;     /* pipe buffer at the read endm NULL for write end */
+  uint32_t pipe_state;    /* pipe state mask */
+  pipetype_t pipe_type;   /* read or write */
+  struct pipe *pipe_end;       /* link with another end of pipe, NULL for read end */
 } pipe_t;
+
+void pipe_init();
+int pipe_read(file_t *f, thread_t *td, uio_t *uio);
+int pipe_write(file_t *f, thread_t *td, uio_t *uio);
+int pipe_close(file_t *f, thread_t *td);
+int pipe_getattr(file_t *f, thread_t *td, vattr_t *va);
--- sys/pipe.c
+++ sys/pipe.c
@@ -5,24 +5,43 @@
 #include <sysent.h>
 #include <malloc.h>
 #include <thread.h>
-
-typedef struct pipebuf {
-  size_t cnt;      /* current number of bytes in buffer */
-  uint32_t in;     /* in offset (start of write) */
-  uint32_t out;    /* out offset (start of read) */
-  size_t  size;    /* size of buffer */
-  void *buffer;
-} pipebuf_t;
-
+#include <pool.h>
+#include <errno.h>
 #include <pipe.h>
+#include <stdc.h>
 
 MALLOC_DEFINE(M_PIPE, "pipe", 1, 4);
 
-pipe_t *pipe_alloc() {
-  pipe_t *pipe = kmalloc(M_PIPE, sizeof(pipe_t), M_ZERO);
+static pool_t pipe_cache;
+
+static void reset_values(pipe_t *pipe) {
+  pipe->pipe_buf.cnt = 0;
+  pipe->pipe_buf.in = 0;
+  pipe->pipe_buf.out = 0;
+  pipe->pipe_state = 0;
+}
+
+static void get_pipe(pipe_t **dest) {
+  *dest = pool_alloc(&pipe_cache, 0);
+  reset_values(*dest);
+}
+
+void pipe_ctor(void *ptr) {
+  pipe_t *pipe = ptr;
   mtx_init(&pipe->pipe_mtx, MTX_DEF);
-  cv_init(&pipe->pipe_read_cv, "Condvar for reading");
-  return 0;
+  cv_init(&pipe->pipe_read_cv, "pipe_read_cv");
+  pipe->pipe_buf.data = kmalloc(M_PIPE, PIPE_SIZE, M_ZERO); /* TODO: improve pooled allocator to be able to contain page-sized elements */
+  pipe->pipe_buf.size = PIPE_SIZE;
+  reset_values(pipe);
+}
+
+void pipe_dtor(void *ptr) {
+  pipe_t *pipe = ptr;
+  kfree(M_PIPE, pipe->pipe_buf.data);
+}
+
+void pipe_init() {
+  pool_init(&pipe_cache, sizeof(pipe_t), pipe_ctor, pipe_dtor);
 }
 
 /* Very simplified versions of NetBSD's pipeops */
@@ -30,6 +49,8 @@ pipe_t *pipe_alloc() {
 int pipe_read(file_t *f, thread_t *td, uio_t *uio) {
   assert(td->td_proc);
   pipe_t *pipe = f->f_data;
+  if (pipe->pipe_type != PIPE_READ_END)
+    return EINVAL;
   pipebuf_t *pipebuf = &pipe->pipe_buf;
   int res = 0;
   int bytes_read = 0;
@@ -38,23 +59,26 @@ int pipe_read(file_t *f, thread_t *td, uio_t *uio) {
 
   while (uio->uio_resid > 0) {
     if (pipebuf->cnt > 0) {
+      /* find number of bytes to feed to uiomove */
       size_t size = pipebuf->size - pipebuf->out;
       if (size > pipebuf->cnt) 
 	size = pipebuf->cnt;
       if (size > uio->uio_resid)
 	size = uio->uio_resid;
 
-      res = uiomove((char *)pipebuf->buffer+pipebuf->out, size, uio);
+      res = uiomove((char *)pipebuf->data+pipebuf->out, size, uio);
 
       if (res)
 	break;
 
+      /* moving out offset by size bytes */
       pipebuf->out += size;
       if (pipebuf->out >= pipebuf->size)
 	pipebuf->out = 0;
 
       pipebuf->cnt -= size;
 
+      /* NetBSD sources say that setting offsets to 0 increases cache hit rate */
       if (pipebuf->cnt == 0) {
 	pipebuf->in = 0;
 	pipebuf->out = 0;
@@ -63,30 +87,108 @@ int pipe_read(file_t *f, thread_t *td, uio_t *uio) {
       bytes_read += size;
       continue;
     }
+    /* widowed pipe, jump to return (should return 0) */
+    if (pipe->pipe_state & PIPE_EOF)
+      break;
+    /* read data, so time to quit */
     if (bytes_read > 0)
       break;
+    /* none of the previous conditions was met so waiting for input */
     cv_wait(&pipe->pipe_read_cv, &pipe->pipe_mtx);
   }
   return res;
 }
 
 int pipe_write(file_t *f, thread_t *td, uio_t *uio) {
-  return 0;
+  assert(td->td_proc);
+  pipe_t *wpipe = f->f_data;
+  if (wpipe->pipe_type != PIPE_WRITE_END)
+    return EINVAL;
+  pipe_t *rpipe = wpipe->pipe_end;
+  pipebuf_t *pipebuf = &rpipe->pipe_buf;
+  int res = 0;
+
+  SCOPED_MTX_LOCK(&wpipe->pipe_mtx);
+
+  while (uio->uio_resid > 0) {
+    if (uio->uio_resid > PIPE_SIZE) {
+      res = ENOSPC;
+      break;
+    }
+    /* calculate free space in buffer */
+    int free_space = pipebuf->size - pipebuf->cnt;
+    /* our writes must be atomic */
+    if (free_space >= uio->uio_resid) {
+      int total_size = uio->uio_resid;
+      int move_size = pipebuf->size - pipebuf->in;
+      if (move_size > total_size)
+	move_size = total_size;
+
+      res = uiomove((char*)pipebuf->data+pipebuf->in, move_size, uio);
+      if (res == 0 && move_size < total_size) {
+	assert(pipebuf->in + move_size == pipebuf->size);
+	res = uiomove((char*)pipebuf->data, total_size - move_size, uio);
+      }
+      if (res)
+	break;
+      pipebuf->in += total_size;
+      if (pipebuf->in >= pipebuf->size) {
+	assert(pipebuf->in == total_size - move_size + pipebuf->size);
+	pipebuf->in = total_size - move_size;
+      }
+      pipebuf->cnt += total_size;
+      assert(pipebuf->cnt <= pipebuf->size);
+    } else {
+      if (rpipe->pipe_state & PIPE_EOF) {
+	res = EPIPE;
+	break;
+      }
+      cv_broadcast(&rpipe->pipe_read_cv);
+    }
+  }
+
+  /* notify read end about successful write */
+  if (pipebuf->cnt > 0)
+    cv_broadcast(&rpipe->pipe_read_cv);
+  
+  return res;
 }
 
 int pipe_close(file_t *f, thread_t *td) {
+  assert(td->td_proc);
+  pipe_t *pipe = f->f_data;
+
+  mtx_lock(&pipe->pipe_mtx);
+  
+  pipe->pipe_state |= PIPE_EOF;
+  if (pipe->pipe_type == PIPE_WRITE_END) {
+    pipe->pipe_end->pipe_state |= PIPE_EOF;
+    cv_broadcast(&pipe->pipe_end->pipe_read_cv);
+    pipe->pipe_end = NULL;
+  }
+
+  mtx_unlock(&pipe->pipe_mtx);
+
+  pool_free(&pipe_cache, pipe);
+
   return 0;
 }
 
 int pipe_getattr(file_t *f, thread_t *td, vattr_t *va) {
+  pipe_t *pipe = f->f_data;
+
+  SCOPED_MTX_LOCK(&pipe->pipe_mtx);
+
+  memset(va, 0, sizeof(vattr_t));
+  va->st_size = pipe->pipe_buf.cnt;
   return 0;
 }
 
 /* pipe syscall */
 
-int do_pipe(int *fds) {
-  pipe_t *pipe = pipe_alloc();
-  pipe++;
+int do_pipe(thread_t *td, int *fds) {
+  pipe_t *pipe;
+  get_pipe(&pipe);
   return 0;
 }
 
