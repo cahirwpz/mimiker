1db45928dc22d06e5c7585a8941ae3b227f9907b
Krystian Bacławski <krystian.baclawski@gmail.com>
2017-05-30 10:55:56 +0200

    Use RTC register definitions from *BSD.

--- tests/rtc.c
+++ tests/rtc.c
@@ -1,53 +1,35 @@
 #include <common.h>
 #include <mips/malta.h>
-#include <rtc.h>
+#include <dev/mc146818reg.h>
 #include <ktest.h>
-
-/* http://geezer.osdevbrasil.net/temp/rtc.txt */
+#include <time.h>
 
 #define RTC_ADDR_R *(volatile uint8_t *)(MIPS_PHYS_TO_KSEG1(MALTA_RTC_ADDR))
 #define RTC_DATA_R *(volatile uint8_t *)(MIPS_PHYS_TO_KSEG1(MALTA_RTC_DATA))
 
-#define REG_B 0x0B
-#define REG_B_SET 128
-#define REG_B_PIE 64
-#define REG_B_AIE 32
-#define REG_B_UIE 16
-#define REG_B_DM 4
-#define REG_B_24 2
-#define REG_B_DSE 1
-
-#define REG_C 0x0C
-#define REG_C_IRQF 128
-#define REG_C_PF 64
-#define REG_C_AF 32
-#define REG_C_UF 16
-
-void rtc_init() {
-  RTC_ADDR_R = REG_B;
-  RTC_DATA_R = RTC_DATA_R | REG_B_DM | REG_B_24 | REG_B_PIE;
+static inline uint8_t rtc_read(unsigned reg) {
+  RTC_ADDR_R = reg;
+  return RTC_DATA_R;
 }
 
-void rtc_read(rtc_time_t *t) {
-  RTC_ADDR_R = 0;
-  t->sec = RTC_DATA_R;
-  RTC_ADDR_R = 2;
-  t->min = RTC_DATA_R;
-  RTC_ADDR_R = 4;
-  t->hour = RTC_DATA_R;
-  RTC_ADDR_R = 6;
-  t->wday = RTC_DATA_R;
-  RTC_ADDR_R = 7;
-  t->mday = RTC_DATA_R;
-  RTC_ADDR_R = 8;
-  t->month = RTC_DATA_R;
-  RTC_ADDR_R = 9;
-  t->year = RTC_DATA_R + 2000;
+static inline void rtc_write(unsigned reg, uint8_t value) {
+  RTC_ADDR_R = reg;
+  RTC_DATA_R = value;
 }
 
-#ifdef _KERNELSPACE
-#include <stdc.h>
-#include <time.h>
+static inline void rtc_setb(unsigned reg, uint8_t mask) {
+  rtc_write(reg, rtc_read(reg) | mask);
+}
+
+static void rtc_gettime(tm_t *t) {
+  t->tm_sec = rtc_read(MC_SEC);
+  t->tm_min = rtc_read(MC_MIN);
+  t->tm_hour = rtc_read(MC_HOUR);
+  t->tm_wday = rtc_read(MC_DOW);
+  t->tm_mday = rtc_read(MC_DOM);
+  t->tm_mon = rtc_read(MC_MONTH);
+  t->tm_year = rtc_read(MC_YEAR) + 2000;
+}
 
 static void tv_delay(timeval_t delay) {
   timeval_t now = get_uptime();
@@ -57,17 +39,16 @@ static void tv_delay(timeval_t delay) {
 }
 
 static int test_rtc() {
-  rtc_init();
+  rtc_setb(MC_REGB, MC_REGB_BINARY | MC_REGB_24HR | MC_REGB_PIE);
 
   while (1) {
-    rtc_time_t rtc;
-    rtc_read(&rtc);
+    tm_t t;
+    rtc_gettime(&t);
 
-    RTC_ADDR_R = REG_C;
-    unsigned reg_c = RTC_DATA_R;
+    uint8_t regc = rtc_read(MC_REGC);
 
-    kprintf("Time is %02d:%02d:%02d, C(%02x)\n", rtc.hour, rtc.min, rtc.sec,
-            reg_c);
+    kprintf("Time is %02d:%02d:%02d, C(%02x)\n", t.tm_hour, t.tm_min, t.tm_sec,
+            regc);
 
     tv_delay(TIMEVAL(1.0));
   }
@@ -75,5 +56,3 @@ static int test_rtc() {
 }
 
 KTEST_ADD(rtc, test_rtc, KTEST_FLAG_NORETURN);
-
-#endif
