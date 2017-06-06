2b007a4e01034934ad48deb6717c67a14c9265e0
Paweł Dybiec <pawel.to.malpa@gmail.com>
2017-06-06 16:01:55 +0200

    lseek CUR test

--- user/test_lseek/lseek_test.c
+++ user/test_lseek/lseek_test.c
@@ -19,9 +19,23 @@ static void test_set(const char *dir_path) {
   assert(strcmp(buf,buf2)==0);
   close(fd);
 }
+static void test_cur(const char *dir_path) {
+  int fd = open(dir_path, 0, O_RDONLY);
+  const int size=256;
+  char buf[size+1];
+  read(fd,buf,size);
+
+  const int shift=128;
+  lseek(fd,-shift,SEEK_CUR);
+  char  buf2[size+1];
+  read(fd,buf2,size);
+  assert(strcmp(buf+size-shift,buf2)==0);
+  close(fd);
+}
 
 int main(int argc, char **argv) {
   const char *mypath="/bin/lseek_test";
   test_set(argc>1?argv[1]:mypath);
+  test_cur(argc>1?argv[1]:mypath);
   return 0;
 }
