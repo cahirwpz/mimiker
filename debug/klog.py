8cbb1a950b0eaf2f742ed53b3c17290bb8047544
Krystian Bacławski <krystian.baclawski@gmail.com>
2017-07-06 13:14:51 +0200

    Rewrite klog gdb module.

--- debug/klog.py
+++ debug/klog.py
@@ -2,53 +2,77 @@ import gdb
 from ptable import ptable, as_hex
 
 
+class TimeVal():
+    def __init__(self, tv):
+        self.sec = int(tv['tv_sec'])
+        self.usec = int(tv['tv_usec'])
+
+    def as_float(self):
+        return float(self.sec) + float(self.usec) * 10e-6
+
+
+class LogEntry():
+    def __init__(self, entry):
+        self.msg = entry['kl_format'].string()
+        # If there is % escaped it is not parameter.
+        nparams = self.msg.count('%') - 2 * self.msg.count('%%')
+        self.params = [entry['kl_params'][i] for i in range(nparams)]
+        self.timestamp = TimeVal(entry['kl_timestamp'])
+        self.source = entry['kl_file'].string()
+        self.line = int(entry['kl_line'])
+        self.origin = str(entry['kl_origin'])
+
+    def format_msg(self):
+        msg = self.msg.replace('"', '\\"')
+        params = ', '.join([str(s) for s in self.params])
+        printf = 'printf "%s", %s' % (msg, params)
+        try:
+            # Using gdb printf so we don't need to dereference addresses.
+            return gdb.execute(printf, to_string=True)
+        except Exception:
+            # Do not format the message, because something went wrong.
+            return printf
+
+
+class LogBuffer():
+    def __init__(self):
+        klog = gdb.parse_and_eval('klog')
+        self.first = int(klog['first'])
+        self.last = int(klog['last'])
+        self.array = klog['array']
+        self.size = int(self.array.type.range()[1]) + 1
+        self.mask = int(klog['mask'])
+        self.verbose = bool(klog['verbose'])
+
+    def __iter__(self):
+        first, last = self.first, self.last
+        while first != last:
+            yield LogEntry(self.array[first])
+            first = (first + 1) % self.size
+
+    def __len__(self):
+        n = self.last - self.first
+        if n < 0:
+            n += self.size
+        return n
+
+
 class KernelLog():
     def invoke(self):
-        messages, klog = self.load_klog()
-        self.dump_general_info(len(messages), klog)
-        self.dump_kernel_logs(messages)
+        klog = LogBuffer()
+        self.dump_info(klog)
+        self.dump_messages(klog)
 
-    def format_message(self, data):
-        message = data['kl_format'].string()
-        arguments = data['kl_params']
-        # If there is % escaped it is not parameter.
-        number_of_parameters = message.count('%') - 2 * message.count('%%')
-        params = ', '.join(str(arguments[i])
-                           for i in range(number_of_parameters))
-        try:
-            message = message.replace('"', '\\"')
-            # Using gdb printf so we don't need to dereference addresses.
-            formated = gdb.execute(
-                'printf "' + message + ' ", ' + params, to_string=True)
-        except Exception:
-            print('''Error in formating message "{}" with parameters "{}"\n
-            Message skipped!!'''.format(message, params))
-            formated = message + params
-        time = "%d.%06d" % (data['kl_timestamp']['tv_sec'],
-                            data['kl_timestamp']['tv_usec'])
-        return [time, "%s:%d" % (data['kl_file'].string(), data['kl_line']),
-                str(data['kl_origin']), str(formated)]
+    def dump_info(self, klog):
+        rows = [["Mask", as_hex(klog.mask)],
+                ["Verbose", str(klog.verbose)],
+                ["Messages", str(len(klog))]]
+        ptable(rows, header=False, fmt='rl')
 
-    def load_klog(self):
-        klog = gdb.parse_and_eval('klog')
-        first = int(klog['first'])
-        last = int(klog['last'])
-        klog_array = klog['array']
-        array_size = int(klog_array.type.range()[1]) + 1
-        messages = []
-        while first != last:
-            messages.append(self.format_message(klog_array[first]))
-            first = (first + 1) % array_size
-        return messages, klog
-
-    def dump_general_info(self, number_of_logs, klog):
-        rows_general = [["Mask", "Verbose", "Log number"]]
-        mask = as_hex(klog['mask'])
-        verbose = bool(klog['verbose'])
-        rows_general.append([mask, str(verbose), str(number_of_logs)])
-        ptable(rows_general, header=True, fmt='l')
-
-    def dump_kernel_logs(self, messages):
-        rows_data = [["Time", "Source", "System", "Message"]]
-        rows_data.extend(messages)
-        ptable(rows_data, header=True, fmt='rrrl')
+    def dump_messages(self, klog):
+        rows = [["Time", "Source", "System", "Message"]]
+        for entry in klog:
+            rows.append(["%.6f" % entry.timestamp.as_float(),
+                         "%s:%d" % (entry.source, entry.line),
+                         entry.origin, entry.format_msg()])
+        ptable(rows, header=True, fmt='rrrl')
