e422e7d8a2ea1983cb29f689ad36d8212eccc906
Krystian Bacławski <krystian.baclawski@gmail.com>
2017-07-08 11:58:35 +0200

    Provide SleepQueue. Merge condvar and mutex into single module.

--- debug/__init__.py
+++ debug/__init__.py
@@ -2,15 +2,14 @@ import gdb
 import ktrace
 import kdump
 import klog
-import mutex
-import condvar
+import sync
 import thread
 
 
 def addPrettyPrinters():
     pp = gdb.printing.RegexpCollectionPrettyPrinter('mimiker')
-    pp.add_printer('mtx', 'mtx', mutex.Mutex)
-    pp.add_printer('condvar', 'condvar', condvar.CondVar)
+    pp.add_printer('mtx', 'mtx', sync.Mutex)
+    pp.add_printer('condvar', 'condvar', sync.CondVar)
     pp.add_printer('thread', 'thread', thread.Thread)
     pp.add_printer('timeval', 'timeval', klog.TimeVal)
     gdb.printing.register_pretty_printer(gdb.current_objfile(), pp)
