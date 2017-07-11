8f27cfe6510f5439c6e1a4645fa9fa9827a12f47
czapiga <Sz.Czapiga@gmail.com>
2017-07-11 21:23:47 +0200

    Basic time syscalls testing in kernel

--- tests/time.c
+++ tests/time.c
@@ -1,32 +1,37 @@
-#include <sys/time.h>
-#include <sys/types.h>
-#include <assert.h>
+#include <ktest.h>
+#include <time.h>
 
-typedef struct test_data {
-  time_t sec;
-  long nsec;
-  long min_us_diff;
-} test_data_t;
+suseconds_t sleep_times[] = {1, 12, 123, 1234, 12345, 123456, 1234567};
 
-#define TESTS_NUM 2
-static test_data_t td[TESTS_NUM] = {{1, 0, 1000000}, {1, 10, 1010000}};
-
-static int test_sleep(time_t sec, long nsec, long min_us_diff) {
+static int test_sleep(time_t sec, long nsec, suseconds_t min_us_diff) {
   timespec_t ts = (timespec_t){.tv_sec = sec, .tv_nsec = nsec};
-  timeval_t start, end;
-  gettimeofday(&start, NULL);
-  int result = nanosleep(&ts, NULL);
-  gettimeofday(&end, NULL);
+  timespec_t start_ts, end_ts;
+  int result = do_clock_gettime(CLOCK_MONOTONIC, &start_ts);
   if (result != 0)
     return result;
-  timeval_t diff = timeval_sub(&end, &start);
-  if (diff.tv_sec * 1000000 + diff.tv_usec < min_us_diff)
+  result = do_clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);
+  if (result != 0)
+    return result;
+  result = do_clock_gettime(CLOCK_MONOTONIC, &end_ts);
+  if (result != 0)
+    return result;
+  timeval_t start_tv = ts2tv(start_ts);
+  timeval_t end_tv = ts2tv(end_ts);
+  timeval_t diff = timeval_sub(&end_tv, &start_tv);
+  if (diff.tv_usec + (suseconds_t)diff.tv_sec * 1000000 < min_us_diff)
     return 1;
   return 0;
 }
 
-int test_nanosleep() {
-  for (int i = 0; i < TESTS_NUM; i++)
-    assert(test_sleep(td[i].sec, td[i].nsec, td[i].min_us_diff) == 0);
+static int time_syscalls(void) {
+  const int n = sizeof(sleep_times) / sizeof(sleep_times[0]);
+  for (int i = 0; i < n; i++) {
+    time_t sec = sleep_times[i] / 1000000;
+    long nsec = (sleep_times[i] % 1000000) * 1000;
+    int result = test_sleep(sec, nsec, sleep_times[i]);
+    assert(result == 0);
+  }
   return 0;
 }
+
+KTEST_ADD(time_syscalls, time_syscalls, 0);
