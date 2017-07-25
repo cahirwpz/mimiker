43040bd3225c5e6ac8fb931170fc040c5232f728
user <pwit@pwit.info>
2017-07-25 19:43:40 +0200

    Elimination of kmem_dump usage.

--- debug/kmem.py
+++ debug/kmem.py
@@ -1,5 +1,7 @@
 import gdb
 import utils
+from ptable import ptable
+from tailq import TailQueue
 
 
 class Kmem(gdb.Command, utils.OneArgAutoCompleteMixin):
@@ -9,35 +11,108 @@ class Kmem(gdb.Command, utils.OneArgAutoCompleteMixin):
     MALLOC_DEFINE(), e.g:
     M_FILE, M_DEV, M_VMOBJ, M_THREAD, M_FD, M_PROC, M_VNODE, M_VFS, M_TEMP,
     M_VMMAP, M_INITRD, M_PMAP
+
+    kmem without arguments prints a short summary
     """
 
     def __init__(self):
         super(Kmem, self).__init__('kmem', gdb.COMMAND_USER)
+        self.pool_names = self.get_pool_names()
 
-    def get_pools(self):
+    def get_pool_names(self):
         output = gdb.execute('info variables', False, True)
         lines = output.split('\n')
         result = []
         for line in lines:
-            if line.startswith('kmem_pool_t'):
-                varname = line.split()[1]
-                varname = varname[1:-1]
+            line = line.split()
+            if (len(line) == 2) and (line[0] == 'kmem_pool_t'):
+                varname = line[1][1:-1]
                 result.append(varname)
-            if line.startswith('static kmem_pool_t'):
-                varname = line.split()[2]
-                varname = varname[1:-1]
+            if (len(line) == 3) and (line[0] == 'static') \
+               and (line[1] == 'kmem_pool_t'):
+                varname = line[2][1:-1]
                 result.append(varname)
 
         return result
 
+    def get_pool_table_header(self):
+        return [["Pool name", "Pool descr.",
+                 "Pages used", "Pages max."]]
+
+    def add_row_to_pool_table(self, table, pool):
+        pool_ptr = gdb.parse_and_eval('%s' % pool)
+        pool_val = pool_ptr.dereference()
+        name = str('%s' % pool)
+        descr = str(pool_val['mp_desc'])
+        pused = str(pool_val['mp_pages_used'])
+        pmax = str(pool_val['mp_pages_max'])
+        table.append([name, descr, pused, pmax])
+        table
+
+    def dump_all_pools(self):
+        tbl = self.get_pool_table_header()
+
+        for pool in self.pool_names:
+            self.add_row_to_pool_table(tbl, pool)
+
+        ptable(tbl, fmt="cccc", header=True)
+
     def dump_pool(self, pool):
         if self.is_valid_pool(pool):
-            gdb.parse_and_eval('kmem_dump(%s)' % pool)
+            tbl = self.get_pool_table_header()
+            self.add_row_to_pool_table(tbl, pool)
+            ptable(tbl, fmt="cccc", header=True)
         else:
             print("Invalid pool name\n")
 
     def is_valid_pool(self, pool):
-        return pool in self.get_pools()
+        return pool in self.pool_names
+
+    def get_arena_header(self):
+        return [["arena addr.", "ma_size", "ma_flags", "ma_freeblks first"]]
+
+    def dump_arenas(self, pool):
+        tbl = self.get_arena_header()
+        pool_ptr = gdb.parse_and_eval('%s' % pool)
+        pool_val = pool_ptr.dereference()
+        arena_queue = TailQueue(pool_val['mp_arena'], 'ma_list')
+
+        for arena in arena_queue:
+            addr = str(arena['ma_list']['tqe_prev'].dereference())
+            size = str(arena['ma_size'])
+            flags = str(arena['ma_flags'])
+            freeblks = str(arena['ma_freeblks']['tqh_first'])
+            tbl.append([addr, size, flags, freeblks])
+
+        print 'Arenas of %s pool' % pool
+        ptable(tbl, fmt="cccc", header=True)
+
+    def get_block_header(self):
+        return [["block addr", "mb_size", "mb_data addr."]]
+
+    def dump_blocks(self, pool):
+        pool_ptr = gdb.parse_and_eval('%s' % pool)
+        pool_val = pool_ptr.dereference()
+        arena_queue = TailQueue(pool_val['mp_arena'], 'ma_list')
+
+        for arena in arena_queue:
+            arena_addr = arena.address
+            print 'Arena addr: %s' % str(arena_addr)
+            tbl = self.get_block_header()
+
+            block_queue = TailQueue(arena['ma_freeblks'], 'mb_list')
+            for block in block_queue:
+                block_addr = str(block.address)
+                mb_size = str(block['mb_size'])
+                mb_data = str(block['mb_data'].address)
+                tbl.append([block_addr, mb_size, mb_data])
+
+            ptable(tbl, fmt="ccc", header=True)
 
     def invoke(self, args, from_tty):
-        self.dump_pool(args)
+        if len(args) < 1:
+            self.dump_all_pools()
+        else:
+            self.dump_pool(args)
+            self.dump_arenas(args)
+            self.dump_blocks(args)
