33d6e43ed54aa1a803a3e5b24f8ab1f300cb9114
pwit81 <pwit@pwit.info>
2017-07-31 11:31:54 +0200

    Autocomple feature to kmem command + some refactorization.

--- debug/kmem.py
+++ debug/kmem.py
@@ -12,29 +12,63 @@ class Kmem(gdb.Command, utils.OneArgAutoCompleteMixin):
     M_FILE, M_DEV, M_VMOBJ, M_THREAD, M_FD, M_PROC, M_VNODE, M_VFS, M_TEMP,
     M_VMMAP, M_INITRD, M_PMAP
 
-    kmem without arguments prints a short summary
+    kmem without arguments prints a short summary of globally defined
+    pools. Locally defined pools are only accessible using "kmem arg"
+    command.
+
     """
 
     def __init__(self):
         super(Kmem, self).__init__('kmem', gdb.COMMAND_USER)
-        self.pool_names = self.get_pool_names()
+        self.pool_names = Kmem.get_global_pools()
 
-    def get_pool_names(self):
-        output = gdb.execute('info variables', False, True)
-        lines = output.split('\n')
+    @staticmethod
+    def is_non_static(decl):
+        return (len(decl) == 2) and (decl[0] == 'kmem_pool_t')
+
+    @staticmethod
+    def is_static(decl):
+        return (len(decl) == 3) and (decl[0] == 'static') \
+               and (decl[1] == 'kmem_pool_t')
+        
+    @staticmethod
+    def get_global_pools():
+        decls_string = gdb.execute('info variables', False, True)
+        decls = decls_string.split('\n')
         result = []
-        for line in lines:
-            line = line.split()
-            if (len(line) == 2) and (line[0] == 'kmem_pool_t'):
-                varname = line[1][1:-1]
+        for decl in decls:
+            decl = decl.split()
+            if Kmem.is_non_static(decl):
+                varname = decl[1][1:-1]
                 result.append(varname)
-            if (len(line) == 3) and (line[0] == 'static') \
-               and (line[1] == 'kmem_pool_t'):
-                varname = line[2][1:-1]
+            if Kmem.is_static(decl):
+                varname = decl[2][1:-1]
                 result.append(varname)
 
         return result
 
+    @staticmethod
+    def is_local_pool(name):
+        type_decl = gdb.execute('whatis %s' % name, False, True)        
+        return (type_decl == "type = kmem_pool_t *\n")
+
+    @staticmethod
+    def get_local_pools():
+        decls_string = gdb.execute('info locals', False, True)
+        decls = decls_string.split('\n');
+        result = []
+
+        if (decls[0] == ''):
+            return
+        
+        for decl in decls[:-1]:
+            name = decl.split()[0]        
+            if Kmem.is_local_pool(name):
+                result.append(name)
+
+        return result
+
+    
     def get_pool_table_header(self):
         return [["Pool name", "Pool descr.",
                  "Pages used", "Pages max."]]
@@ -43,11 +77,10 @@ class Kmem(gdb.Command, utils.OneArgAutoCompleteMixin):
         pool_ptr = gdb.parse_and_eval('%s' % pool)
         pool_val = pool_ptr.dereference()
         name = str('%s' % pool)
-        descr = str(pool_val['mp_desc'])
-        pused = str(pool_val['mp_pages_used'])
-        pmax = str(pool_val['mp_pages_max'])
-        table.append([name, descr, pused, pmax])
-        table
+        mp_desc = str(pool_val['mp_desc'])
+        mp_pages_used = str(pool_val['mp_pages_used'])
+        mp_pages_max = str(pool_val['mp_pages_max'])
+        table.append([name, mp_desc, mp_pages_used, mp_pages_max])
 
     def dump_all_pools(self):
         tbl = self.get_pool_table_header()
@@ -110,9 +143,20 @@ class Kmem(gdb.Command, utils.OneArgAutoCompleteMixin):
             ptable(tbl, fmt="ccc", header=True)
 
     def invoke(self, args, from_tty):
+        self.pool_names = Kmem.get_global_pools()
+        
         if len(args) < 1:
             self.dump_all_pools()
         else:
+            self.pool_names += Kmem.get_local_pools()
             self.dump_pool(args)
             self.dump_arenas(args)
             self.dump_blocks(args)
+
+
+    def options(self):
+        
+        return Kmem.get_global_pools() + \
+            Kmem.get_local_pools()
+            
+    
