8a686ffd0d6f649d7e260f60ba2d3e5094285ad6
pwit81 <pwit@pwit.info>
2017-08-02 10:27:16 +0200

    Kmem implementation now employs metaclasses. Some refactorization performed.

--- debug/kmem.py
+++ debug/kmem.py
@@ -4,6 +4,111 @@ from ptable import ptable
 from tailq import TailQueue
 
 
+class PoolSet:
+    @staticmethod
+    def __is_non_static(decl):
+        return (len(decl) == 2) and (decl[0] == 'kmem_pool_t')
+
+    @staticmethod
+    def __is_static(decl):
+        return (len(decl) == 3) and (decl[0] == 'static') \
+               and (decl[1] == 'kmem_pool_t')
+
+    @staticmethod
+    def get_global_pools():
+        decls_string = gdb.execute('info variables', False, True)
+        decls = decls_string.split('\n')
+        result = []
+        for decl in decls:
+            decl = decl.split()
+            if PoolSet.__is_non_static(decl):
+                varname = decl[1][1:-1]
+                result.append(varname)
+            if PoolSet.__is_static(decl):
+                varname = decl[2][1:-1]
+                result.append(varname)
+
+        return result
+
+    @staticmethod
+    def __is_local_pool(name):
+        type_decl = gdb.execute('whatis %s' % name, False, True)
+        return (type_decl == "type = kmem_pool_t *\n")
+
+    @staticmethod
+    def get_local_pools():
+        decls_string = gdb.execute('info locals', False, True)
+        decls = decls_string.split('\n')
+        result = []
+
+        if (decls[0] == ''):
+            return
+
+        for decl in decls[:-1]:
+            name = decl.split()[0]
+            if PoolSet.__is_local_pool(name):
+                result.append(name)
+
+        return result
+
+
+class Pool():
+    __metaclass__ = utils.GdbStructMeta
+    __ctype__ = 'kmem_pool_t'
+    __cast__ = {'mp_desc': str, 'mp_pages_used': str, 'mp_pages_max': str,
+                'mp_arena': lambda x: map(Arena,
+                                          TailQueue(field='ma_list', tq=x))}
+
+    def __init__(self, obj, name):
+        super(Pool, self).__init__(obj)
+        self.name = name
+        self.address = str(obj.address)
+
+    @staticmethod
+    def get_table_header():
+        return [["Pool name", "Pool descr.",
+                 "Pages used", "Pages max."]]
+
+    def get_row(self):
+        return [self.name, self.mp_desc, self.mp_pages_used, self.mp_pages_max]
+
+
+class Arena():
+    __metaclass__ = utils.GdbStructMeta
+    __ctype__ = 'mem_arena_t'
+    __cast__ = {'ma_size': str, 'ma_flags': str, 'ma_freeblks':
+                lambda x: map(Block, TailQueue(field='mb_list', tq=x))}
+
+    def __init__(self, obj):
+        super(Arena, self).__init__(obj)
+        self.address = str(obj.address)
+
+    @staticmethod
+    def get_header():
+        return [["arena addr.", "ma_size", "ma_flags", "ma_freeblks first"]]
+
+    def get_row(self):
+        return [self.address, self.ma_size, self.ma_flags,
+                str(iter(self.ma_freeblks).next().address)]
+
+
+class Block():
+    __metaclass__ = utils.GdbStructMeta
+    __ctype__ = 'mem_block_t'
+    __cast__ = {'mb_size': str, 'mb_data': str}
+
+    def __init__(self, obj):
+        super(Block, self).__init__(obj)
+        self.address = str(obj.address)
+
+    @staticmethod
+    def get_header():
+        return [["block addr", "mb_size", "mb_data addr."]]
+
+    def get_row(self):
+        return [self.address, self.mb_size, self.mb_data]
+
+
 class Kmem(gdb.Command, utils.OneArgAutoCompleteMixin):
     """dump info about kernel memory pools
 
@@ -20,70 +125,16 @@ class Kmem(gdb.Command, utils.OneArgAutoCompleteMixin):
 
     def __init__(self):
         super(Kmem, self).__init__('kmem', gdb.COMMAND_USER)
-        self.pool_names = Kmem.get_global_pools()
-
-    @staticmethod
-    def is_non_static(decl):
-        return (len(decl) == 2) and (decl[0] == 'kmem_pool_t')
-
-    @staticmethod
-    def is_static(decl):
-        return (len(decl) == 3) and (decl[0] == 'static') \
-               and (decl[1] == 'kmem_pool_t')
-        
-    @staticmethod
-    def get_global_pools():
-        decls_string = gdb.execute('info variables', False, True)
-        decls = decls_string.split('\n')
-        result = []
-        for decl in decls:
-            decl = decl.split()
-            if Kmem.is_non_static(decl):
-                varname = decl[1][1:-1]
-                result.append(varname)
-            if Kmem.is_static(decl):
-                varname = decl[2][1:-1]
-                result.append(varname)
-
-        return result
-
-    @staticmethod
-    def is_local_pool(name):
-        type_decl = gdb.execute('whatis %s' % name, False, True)        
-        return (type_decl == "type = kmem_pool_t *\n")
-
-    @staticmethod
-    def get_local_pools():
-        decls_string = gdb.execute('info locals', False, True)
-        decls = decls_string.split('\n');
-        result = []
-
-        if (decls[0] == ''):
-            return
-        
-        for decl in decls[:-1]:
-            name = decl.split()[0]        
-            if Kmem.is_local_pool(name):
-                result.append(name)
-
-        return result
-
-    
-    def get_pool_table_header(self):
-        return [["Pool name", "Pool descr.",
-                 "Pages used", "Pages max."]]
+        self.pool_names = PoolSet.get_global_pools()
 
     def add_row_to_pool_table(self, table, pool):
         pool_ptr = gdb.parse_and_eval('%s' % pool)
-        pool_val = pool_ptr.dereference()
         name = str('%s' % pool)
-        mp_desc = str(pool_val['mp_desc'])
-        mp_pages_used = str(pool_val['mp_pages_used'])
-        mp_pages_max = str(pool_val['mp_pages_max'])
-        table.append([name, mp_desc, mp_pages_used, mp_pages_max])
+        p = Pool(pool_ptr, name)
+        table.append(p.get_row())
 
     def dump_all_pools(self):
-        tbl = self.get_pool_table_header()
+        tbl = Pool.get_table_header()
 
         for pool in self.pool_names:
             self.add_row_to_pool_table(tbl, pool)
@@ -92,7 +143,7 @@ class Kmem(gdb.Command, utils.OneArgAutoCompleteMixin):
 
     def dump_pool(self, pool):
         if self.is_valid_pool(pool):
-            tbl = self.get_pool_table_header()
+            tbl = Pool.get_table_header()
             self.add_row_to_pool_table(tbl, pool)
             ptable(tbl, fmt="cccc", header=True)
         else:
@@ -101,62 +152,43 @@ class Kmem(gdb.Command, utils.OneArgAutoCompleteMixin):
     def is_valid_pool(self, pool):
         return pool in self.pool_names
 
-    def get_arena_header(self):
-        return [["arena addr.", "ma_size", "ma_flags", "ma_freeblks first"]]
-
     def dump_arenas(self, pool):
-        tbl = self.get_arena_header()
+        tbl = Arena.get_header()
         pool_ptr = gdb.parse_and_eval('%s' % pool)
-        pool_val = pool_ptr.dereference()
-        arena_queue = TailQueue(pool_val['mp_arena'], 'ma_list')
 
-        for arena in arena_queue:
-            addr = str(arena['ma_list']['tqe_prev'].dereference())
-            size = str(arena['ma_size'])
-            flags = str(arena['ma_flags'])
-            freeblks = str(arena['ma_freeblks']['tqh_first'])
-            tbl.append([addr, size, flags, freeblks])
+        p = Pool(pool_ptr, pool)
+        for arena in p.mp_arena:
+            tbl.append(arena.get_row())
 
         print 'Arenas of %s pool' % pool
         ptable(tbl, fmt="cccc", header=True)
 
-    def get_block_header(self):
-        return [["block addr", "mb_size", "mb_data addr."]]
-
     def dump_blocks(self, pool):
-        pool_ptr = gdb.parse_and_eval('%s' % pool)
-        pool_val = pool_ptr.dereference()
-        arena_queue = TailQueue(pool_val['mp_arena'], 'ma_list')
 
-        for arena in arena_queue:
+        pool_ptr = gdb.parse_and_eval('%s' % pool)
+        p = Pool(pool_ptr, pool)
+
+        for arena in p.mp_arena:
             arena_addr = arena.address
             print 'Arena addr: %s' % str(arena_addr)
-            tbl = self.get_block_header()
+            tbl = Block.get_header()
 
-            block_queue = TailQueue(arena['ma_freeblks'], 'mb_list')
+            block_queue = arena.ma_freeblks
             for block in block_queue:
-                block_addr = str(block.address)
-                mb_size = str(block['mb_size'])
-                mb_data = str(block['mb_data'].address)
-                tbl.append([block_addr, mb_size, mb_data])
-
+                tbl.append(block.get_row())
             ptable(tbl, fmt="ccc", header=True)
 
     def invoke(self, args, from_tty):
-        self.pool_names = Kmem.get_global_pools()
-        
+        self.pool_names = PoolSet.get_global_pools()
+
         if len(args) < 1:
             self.dump_all_pools()
         else:
-            self.pool_names += Kmem.get_local_pools()
+            self.pool_names += PoolSet.get_local_pools()
             self.dump_pool(args)
             self.dump_arenas(args)
             self.dump_blocks(args)
 
-
     def options(self):
-        
-        return Kmem.get_global_pools() + \
-            Kmem.get_local_pools()
-            
-    
+        return PoolSet.get_global_pools() + \
+            PoolSet.get_local_pools()
