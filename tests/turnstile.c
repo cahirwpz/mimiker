d6646f113d0fc26ce1ccfe653e0cdee1612cd0f9
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2018-04-05 11:42:45 +0200

    added asserts and no preemption to turnstile test

--- tests/turnstile.c
+++ tests/turnstile.c
@@ -9,22 +9,24 @@
 static mtx_t mtx = MTX_INITIALIZER(MTX_DEF);
 static thread_t *td[T];
 
-static enum prio {
+typedef enum {
   /* Set priorities to multiplies of RunQueue_PriorityPerQueue
    * so that each priority matches different run queue.
    */
   LOW = 0,
   MED = RQ_PPQ,
   HIGH = 2 * RQ_PPQ
-} last_prio;
+} prio_t;
+
+static prio_t last_thread; /* which thread ended running as the last one */
 
 static void high_prio_task(void *arg) {
-  assert(mtx.m_owner == td[2]);
+  assert(mtx.m_owner == td[0]);
 
   mtx_lock(&mtx);
   mtx_unlock(&mtx);
 
-  last_prio = HIGH;
+  last_thread = HIGH;
 }
 
 static void med_prio_task(void *arg) {
@@ -32,40 +34,53 @@ static void med_prio_task(void *arg) {
    * after high_prio_task.
    */
 
-  last_prio = MED;
+  last_thread = MED;
 }
 
 static void low_prio_task(void *arg) {
-  /* As for now, td1, td2 and td3 have artificial priorities (LOW, LOW, HIGH)
+  /* As for now, td1, td2 and td3 have artificial priorities (HIGH, LOW, LOW)
    * to ensure that this code runs first. Now we can set priorities to their
-   * real values (HIGH, MED, LOW).
+   * real values (LOW, MED, HIGH).
    */
-  td[0]->td_prio = HIGH;
-  td[1]->td_prio = MED;
-  td[2]->td_prio = LOW;
 
-  mtx_lock(&mtx);
+  WITH_NO_PREEMPTION {
+    td[0]->td_prio = LOW;
+    td[1]->td_prio = MED;
+    td[2]->td_prio = HIGH;
+
+    mtx_lock(&mtx);
+  }
+
   /* Yield, so that high priority task will have to wait for us. */
   thread_yield();
+
+  /* Our priority should've been raised. */
+  assert(td[0]->td_prio == HIGH);
+
+  last_thread = LOW;
   mtx_unlock(&mtx);
 
-  last_prio = LOW;
+  /* And lowered. */
+  assert(td[0]->td_prio == LOW);
 }
 
 static int test_mutex_priority_inversion(void) {
-  td[0] = thread_create("td1", high_prio_task, NULL);
+  td[0] = thread_create("td1", low_prio_task, NULL);
   td[1] = thread_create("td2", med_prio_task, NULL);
-  td[2] = thread_create("td3", low_prio_task, NULL);
+  td[2] = thread_create("td3", high_prio_task, NULL);
 
-  /* We want to ensure that td3 (low_prio_task) will run as the first one
+  /* We want to ensure that td1 (low_prio_task) will run as the first one
    * and lock mtx.
    */
-  td[0]->td_prio = LOW;
-  td[1]->td_prio = LOW;
-  td[2]->td_prio = HIGH;
 
-  for (int i = 0; i < T; i++)
-    sched_add(td[i]);
+  WITH_NO_PREEMPTION {
+    td[0]->td_prio = HIGH;
+    td[1]->td_prio = LOW;
+    td[2]->td_prio = LOW;
+
+    for (int i = 0; i < T; i++)
+      sched_add(td[i]);
+  }
 
   for (int i = 0; i < T; i++)
     thread_join(td[i]);
@@ -73,7 +88,7 @@ static int test_mutex_priority_inversion(void) {
   /* High priority task should've propagated its priority to low priority
    * task and the last one to run should've been medium priority task.
    */
-  assert(last_prio == MED);
+  assert(last_thread == MED);
 
   return 0;
 }
