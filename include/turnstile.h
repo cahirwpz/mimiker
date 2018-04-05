81c4cefe8035c0ef2e1ae8f6af47052cd6f25876
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2018-04-05 17:27:07 +0200

    [WIP] added some turnstile structs fields & part of trywait function

--- include/turnstile.h
+++ include/turnstile.h
@@ -3,6 +3,9 @@
 
 typedef struct turnstile turnstile_t;
 
+#define TS_EXCLUSIVE_QUEUE 0
+#define TS_SHARED_QUEUE 1
+
 void turnstile_init(void);
 
 turnstile_t *turnstile_alloc(void);
--- sys/turnstile.c
+++ sys/turnstile.c
@@ -1,12 +1,26 @@
 #include <pool.h>
+#include <thread.h>
+#include <spinlock.h>
+
+#define TC_TABLESIZE 256 /* Must be power of 2. */
+#define TC_MASK (TC_TABLESIZE - 1)
+#define TC_SHIFT 8
+#define TC_HASH(wc)                                                            \
+  ((((uintptr_t)(wc) >> TC_SHIFT) ^ (uintptr_t)(wc)) & TC_MASK)
+#define TC_LOOKUP(wc) &turnstile_chains[TC_HASH(wc)]
 
 typedef struct turnstile {
-
+  spinlock_t ts_lock;
+  LIST_HEAD(, turnstile) ts_free;
+  TAILQ_HEAD(, thread) ts_blocked[2];
 } turnstile_t;
 
-typedef struct {
-
+typedef struct turnstile_chain {
+  spinlock_t tc_lock;
+  LIST_HEAD(, turnstile) sc_queues;
 } turnstile_chain_t;
+
+static turnstile_chain_t turnstile_chains[TC_TABLESIZE];
 
 static pool_t P_TURNSTILE;
 
@@ -31,4 +45,16 @@ turnstile_t *turnstile_alloc(void) {
 
 void turnstile_destroy(turnstile_t *ts) {
   pool_free(P_TURNSTILE, ts);
+}
+
+turnstile_t *turnstile_trywait(void *wchan) {
+  // code from http://bxr.su/FreeBSD/sys/kern/subr_turnstile.c#545
+
+  turnstile_chain_t *tc = TC_LOOKUP(wchan);
+  spin_acquire(&tc->tc_lock);
+
+  // TODO
+
+  turnstile_t *ts = (turnstile_t *)42; // TO BE DELETED
+  return ts;
 }
