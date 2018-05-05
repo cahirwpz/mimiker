fb69c18e98fb5366fde0277d9ff3d9d2cae4296e
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2018-05-05 12:15:00 +0200

    revert all changes in tests/mutex.c

--- tests/mutex.c
+++ tests/mutex.c
@@ -4,33 +4,30 @@
 #include <thread.h>
 #include <ktest.h>
 
-static mtx_t mtx_counter = MTX_INITIALIZER(MTX_DEF);
+static mtx_t mtx = MTX_INITIALIZER(MTX_DEF);
 static volatile int32_t value;
 
-/* For T=10 N=13287 : randomly passes or fails mutex; fails turnstile
- * For lower N there is higher chance of passing mutex
- */
-#define N 20000
-#define T 10
+#define N 1000
+#define T 5
 
 static thread_t *td[T];
 
-static void mtx_test_counter_routine(void *arg) {
+static void mtx_test_main(void *arg) {
   for (size_t i = 0; i < N; i++) {
-    mtx_lock(&mtx_counter);
+    mtx_lock(&mtx);
     value++;
-    mtx_unlock(&mtx_counter);
+    mtx_unlock(&mtx);
   }
 }
 
-static int mtx_test_counter(void) {
+static int mtx_test(void) {
   value = 0;
 
-  for (int i = 0; i < T; i++) {
-    char name[20];
-    snprintf(name, sizeof(name), "td%d", i);
-    td[i] = thread_create(name, mtx_test_counter_routine, NULL);
-  }
+  td[0] = thread_create("td1", mtx_test_main, NULL);
+  td[1] = thread_create("td2", mtx_test_main, NULL);
+  td[2] = thread_create("td3", mtx_test_main, NULL);
+  td[3] = thread_create("td4", mtx_test_main, NULL);
+  td[4] = thread_create("td5", mtx_test_main, NULL);
 
   for (int i = 0; i < T; i++)
     sched_add(td[i]);
@@ -42,43 +39,4 @@ static int mtx_test_counter(void) {
   return KTEST_SUCCESS;
 }
 
-static mtx_t mtx1 = MTX_INITIALIZER(MTX_DEF);
-static thread_t *t1;
-static int s1 = 0;
-
-static void mtx_test_simple_routine1(void *arg) {
-  WITH_NO_PREEMPTION {
-    s1 = 1;
-    mtx_lock(&mtx1);
-  }
-  WITH_NO_PREEMPTION {
-    s1 = 2;
-    mtx_unlock(&mtx1);
-  }
-  s1 = 3;
-}
-
-static int mtx_test_simple(void) {
-  t1 = thread_create("td1", mtx_test_simple_routine1, NULL);
-
-  mtx_lock(&mtx1);
-
-  sched_add(t1);
-  while (s1 != 1) {
-    thread_yield();
-  }
-
-  assert(s1 == 1);
-  mtx_unlock(&mtx1);
-
-  thread_join(t1);
-  assert(s1 == 3);
-  // NOTE this assert is implementation-specific
-  // some changes might make it invalid
-  assert(mtx1.m_owner == NULL);
-
-  return KTEST_SUCCESS;
-}
-
-KTEST_ADD(mutex_counter, mtx_test_counter, 0);
-KTEST_ADD(mutex_simple, mtx_test_simple, 0);
+KTEST_ADD(mutex, mtx_test, 0);
