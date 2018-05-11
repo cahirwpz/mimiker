70cddccd05f15d566c9031d9af5aa8a384e3bc84
mzr <dingorth@gmail.com>
2018-05-11 19:23:08 +0200

    use UTEST_ADD_SIGNAL

--- user/utest/exceptions.c
+++ user/utest/exceptions.c
@@ -5,46 +5,18 @@
 #include <sys/signal.h>
 #include <stdlib.h>
 
-static int spawn_process(void (*proc_handler)(void)) {
-  int pid;
-  switch ((pid = fork())) {
-    case -1:
-      exit(EXIT_FAILURE);
-    case 0:
-      proc_handler();
-      exit(EXIT_SUCCESS);
-    default:
-      return pid;
-  }
-}
-
-static int check_exception(void (*code)(void), int signum) {
-  spawn_process(code);
-  int status;
-  wait(&status);
-  assert(WIFSIGNALED(status));
-  assert(signum == WTERMSIG(status));
+int test_exc_cop_unusable(void) {
+  int value;
+  asm volatile("mfc0 %0, $12, 0" : "=r"(value));
   return 0;
 }
 
-static inline void exec_cp0_instr(void) {
-  int value;
-  asm volatile("mfc0 %0, $12, 0" : "=r"(value));
-}
-
-static inline void exec_reserved_instr(void) {
+int test_exc_reserved_instruction(void) {
   /* The choice of reserved opcode was done based on "Table A.2 MIPS32 Encoding
    * of the Opcode Field" from "MIPS® Architecture For Programmers Volume II-A:
    * The MIPS32® Instruction Set" */
   asm volatile(".long 0x00000039"); /* objdump fails to disassemble it */
-}
-
-int test_exc_cop_unusable(void) {
-  return check_exception(exec_cp0_instr, SIGILL);
-}
-
-int test_exc_reserved_instruction(void) {
-  return check_exception(exec_reserved_instr, SIGILL);
+  return 0;
 }
 
 int test_syscall_in_bds(void) {
