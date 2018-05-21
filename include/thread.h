c42244f63666ffe23b77fba2379c2a447efe7ee4
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2018-05-21 23:11:20 +0200

    getters are now static inline

--- include/thread.h
+++ include/thread.h
@@ -43,8 +43,6 @@ typedef enum {
   TDS_RUNNING,
   /*!< thread is waiting on a resource and it has been put on a sleep queue */
   TDS_SLEEPING,
-  /*!< thread is waiting for a lock and it has been put on a turnstile */
-  TDS_LOCKED,
   /*!< thread finished or was terminated by the kernel and awaits recycling */
   TDS_DEAD
 } thread_state_t;
@@ -55,12 +53,6 @@ typedef enum {
 #define TDF_NEEDLOCK 0x00000008   /* acquire td_spin on context switch */
 #define TDF_BORROWING 0x00000010  /* priority propagation */
 #define TDF_SLEEPY 0x00000020     /* thread is about to go to sleep */
-
-/* getters */
-#define TD_IS_BORROWING(tdp) ((tdp)->td_flags & TDF_BORROWING)
-#define TD_IS_SLEEPING(tdp) ((tdp)->td_flags & TDF_SLEEPING)
-#define TD_IS_LOCKED(tdp) ((tdp)->td_flags & TDF_LOCKED)
-#define TD_ON_RUNQ(tdp) ((tdp)->td_flags & TDF_READY)
 
 /*! \brief Thread structure
  *
@@ -158,5 +150,30 @@ void thread_join(thread_t *td);
  * some tests need to explicitly wait until threads are reaped before they can
  * verify test success. */
 void thread_reap(void);
+
+/* Getters. */
+static inline bool td_is_ready(thread_t *td) {
+  return td->td_state == TDS_READY;
+}
+
+static inline bool td_is_dead(thread_t *td) {
+  return td->td_state == TDS_DEAD;
+}
+
+static inline bool td_is_running(thread_t *td) {
+  return td->td_state == TDS_RUNNING;
+}
+
+static inline bool td_is_inactive(thread_t *td) {
+  return td->td_state == TDS_INACTIVE;
+}
+
+static inline bool td_is_sleeping(thread_t *td) {
+  return td->td_state == TDS_SLEEPING;
+}
+
+static inline bool td_is_borrowing(thread_t *td) {
+  return td->td_flags & TDF_BORROWING;
+}
 
 #endif /* !_SYS_THREAD_H_ */
