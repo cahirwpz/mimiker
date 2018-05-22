40c63784d1838891089cbcbf374080b939a37baf
Krystian Bacławski <krystian.baclawski@gmail.com>
2018-05-22 14:01:54 +0200

    Only one thread per process is allowed!

--- include/thread.h
+++ include/thread.h
@@ -76,7 +76,6 @@ typedef struct thread {
   TAILQ_ENTRY(thread) td_runq;    /* a link on run queue */
   TAILQ_ENTRY(thread) td_sleepq;  /* a link on sleep queue */
   TAILQ_ENTRY(thread) td_zombieq; /* a link on zombie queue */
-  TAILQ_ENTRY(thread) td_procq;   /* a link on process threads queue */
   /* Properties */
   proc_t *td_proc; /*!< (t) parent process (NULL for kernel threads) */
   char *td_name;   /*!< (@) name of thread */
