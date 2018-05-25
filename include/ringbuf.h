0119037c59e4b8cc8a4667780d2674e121895315
pwit81 <pwit@pwit.info>
2018-05-25 22:37:58 +0200

    Ringbuf tests and fixes

--- include/ringbuf.h
+++ include/ringbuf.h
@@ -3,13 +3,30 @@
 
 #include <common.h>
 
+typedef struct uio uio_t;
+typedef struct kmem_pool kmem_pool_t;
+
 typedef struct ringbuf {
-  size_t head, tail;
-  size_t count, size;
-  uint8_t *data;
+  size_t head;   /*!< producing data moves head forward */
+  size_t tail;   /*!< consuming data moves tail forward */
+  size_t count;  /*!< number of bytes currently stored in the buffer */
+  size_t size;   /*!< total size of the buffer */
+  uint8_t *data; /*!< buffer that stores data */
 } ringbuf_t;
 
+static inline bool ringbuf_empty(ringbuf_t *buf) {
+  return buf->count == 0;
+}
+
+static inline bool ringbuf_full(ringbuf_t *buf) {
+  return buf->count == buf->size;
+}
+
+ringbuf_t ringbuf_alloc(kmem_pool_t *pool, size_t size);
+void ringbuf_destroy(kmem_pool_t *pool, ringbuf_t buf);
+void ringbuf_reset(ringbuf_t *buf);
 bool ringbuf_putb(ringbuf_t *buf, uint8_t byte);
 bool ringbuf_getb(ringbuf_t *buf, uint8_t *byte_p);
-
+int ringbuf_read(ringbuf_t *buf, uio_t *uio);
+int ringbuf_write(ringbuf_t *buf, uio_t *uio);
 #endif
--- sys/ringbuf.c
+++ sys/ringbuf.c
@@ -1,5 +1,35 @@
-#include <malloc.h>
 #include <ringbuf.h>
+#include <uio.h>
+#include <malloc.h>
+
+#if 0
+/* This should increase cache hit rate */
+if (buf->count == 0) {
+  buf->tail = 0;
+  buf->head = 0;
+}
+#endif
+
+ringbuf_t ringbuf_alloc(kmem_pool_t *pool, size_t size) {
+  ringbuf_t buf;
+
+  assert(size > 0);
+
+  buf.data = kmalloc(pool, size, M_ZERO);
+  buf.size = size;
+  ringbuf_reset(&buf);
+  return buf;
+}
+
+void ringbuf_destroy(kmem_pool_t *pool, ringbuf_t buf) {
+  kfree(pool, buf.data);
+}
+
+void ringbuf_reset(ringbuf_t *buf) {
+  buf->head = 0;
+  buf->tail = 0;
+  buf->count = 0;
+}
 
 bool ringbuf_putb(ringbuf_t *buf, uint8_t byte) {
   if (buf->count == buf->size)
@@ -19,4 +49,45 @@ bool ringbuf_getb(ringbuf_t *buf, uint8_t *byte_p) {
     buf->tail = 0;
   buf->count--;
   return true;
+}
+
+int ringbuf_read(ringbuf_t *buf, uio_t *uio) {
+  /* repeat when used space is split into two parts */
+  while (uio->uio_resid > 0 && buf->count > 0) {
+    /* used space is either [tail, head) or [tail, size) */
+    size_t size =
+      (buf->tail < buf->head) ? buf->head - buf->tail : buf->size - buf->tail;
+    if (size > uio->uio_resid)
+      size = uio->uio_resid;
+    int res = uiomove((char *)buf->data + buf->tail, size, uio);
+    if (res)
+      return res;
+    buf->tail += size;
+    buf->count -= size;
+    if (buf->tail >= buf->size)
+      buf->tail = 0;
+  }
+  return 0;
+}
+
+/* head <- first free, tail <- first occupied*/
+
+int ringbuf_write(ringbuf_t *buf, uio_t *uio) {
+  /* repeat when free space is split into two parts */
+  while (uio->uio_resid > 0 && buf->count < buf->size) {
+    /* free space is either [head, tail) or [head, size) */
+    size_t size =
+      (buf->head < buf->tail) ? buf->tail - buf->head : buf->size - buf->head;
+    if (size > uio->uio_resid)
+      size = uio->uio_resid;
+    int res = uiomove((char *)buf->data + buf->head, size, uio);
+    if (res)
+      return res;
+    buf->head += size;
+    buf->count += size;
+    if (buf->head >= buf->size)
+      buf->head = 0;
+  }
+
+  return 0;
 }
