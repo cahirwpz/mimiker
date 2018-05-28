027072b5cb1c1bdcb9287c12e590396f244247c6
Krystian Bacławski <krystian.baclawski@gmail.com>
2018-05-28 09:37:46 +0200

    Fix produce to actually increase number of available bytes.

--- sys/ringbuf.c
+++ sys/ringbuf.c
@@ -1,14 +1,6 @@
 #include <ringbuf.h>
 #include <uio.h>
 
-#if 0
-/* This should increase cache hit rate */
-if (buf->count == 0) {
-  buf->tail = 0;
-  buf->head = 0;
-}
-#endif
-
 void ringbuf_reset(ringbuf_t *buf) {
   buf->head = 0;
   buf->tail = 0;
@@ -17,7 +9,7 @@ void ringbuf_reset(ringbuf_t *buf) {
 
 static void produce(ringbuf_t *buf, unsigned bytes) {
   assert(buf->count + bytes <= buf->size);
-  buf->count -= bytes;
+  buf->count += bytes;
   buf->head += bytes;
   if (buf->head >= buf->size)
     buf->head = 0;
