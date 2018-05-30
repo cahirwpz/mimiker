3a255d5f251502fd814962fbdc7df0906362fe1d
Wojciech Moczulski <womoczulski@gmail.com>
2018-05-30 20:32:53 +0200

    ptr datatype name

--- include/rman.h
+++ include/rman.h
@@ -3,14 +3,12 @@
 
 #include "bus.h"
 
-typedef intptr_t rman_addr; // TODO resource uses other datatype
-
 typedef struct rman rman_t;
 typedef struct rman_block rman_block_t;
 
 struct rman {
-  rman_addr start;
-  rman_addr end;
+  intptr_t start;
+  intptr_t end;
   mtx_t mtx;
   LIST_HEAD(, resource) resources;
 };
@@ -18,11 +16,11 @@ struct rman {
 void rman_init(rman_t *rm);
 
 // return null if unable to allocate
-resource_t *rman_allocate_resource(rman_t *rm, rman_addr start, rman_addr end,
-                                   rman_addr count);
+resource_t *rman_allocate_resource(rman_t *rm, intptr_t start, intptr_t end,
+                                   intptr_t count);
 
-inline resource_t *rman_allocate_resource_any(rman_t *rm, rman_addr count) {
-  return rman_allocate_resource(rm, 0, (rman_addr)~0, count);
+inline resource_t *rman_allocate_resource_any(rman_t *rm, intptr_t count) {
+  return rman_allocate_resource(rm, 0, (intptr_t)~0, count);
 }
 
 #endif /* _SYS_RMAN_H_ */
