5e759b3e8e27b07405abff4c67bc59d888de1478
mzr <dingorth@gmail.com>
2018-06-01 12:20:02 +0200

    modified rman a little

--- include/rman.h
+++ include/rman.h
@@ -5,27 +5,28 @@
 
 typedef struct rman rman_t;
 typedef struct rman_block rman_block_t;
+typedef long rman_res_t;
 
 struct rman {
-  intptr_t start;
-  intptr_t end;
+  rman_res_t start;
+  rman_res_t end;
   mtx_t mtx;
   LIST_HEAD(, resource) resources;
 };
 
 // returns null if unable to allocate
-resource_t *rman_allocate_resource(rman_t *rm, intptr_t start, intptr_t end,
-                                   intptr_t count);
+resource_t *rman_allocate_resource(rman_t *rm, rman_res_t start, rman_res_t end,
+                                   rman_res_t count);
 
 inline resource_t *rman_allocate_resource_any(rman_t *rm) {
-  return rman_allocate_resource(rm, 0, (intptr_t)~0, 1);
+  return rman_allocate_resource(rm, 0, (rman_res_t)~0, 1);
 }
 
-inline resource_t *rman_allocate_resource_anywhere(rman_t *rm, intptr_t count) {
-  return rman_allocate_resource(rm, 0, (intptr_t)~0, count);
+inline resource_t *rman_allocate_resource_anywhere(rman_t *rm, rman_res_t count) {
+  return rman_allocate_resource(rm, 0, (rman_res_t)~0, count);
 }
 
-void rman_create(rman_t *rm, intptr_t start, intptr_t end);
+void rman_create(rman_t *rm, rman_res_t start, rman_res_t end);
 
 inline void rman_create_from_resource(rman_t *rm, resource_t *res) {
   rman_create(rm, res->r_start, res->r_end);
