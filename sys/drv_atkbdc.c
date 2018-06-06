324563e9af3a45927ab142cfd0cb7711ddf6ba86
mzr <dingorth@gmail.com>
2018-06-06 21:26:53 +0200

    fixed atkbdc bus_alloc_resource call

--- sys/drv_atkbdc.c
+++ sys/drv_atkbdc.c
@@ -119,7 +119,7 @@ static intr_filter_t atkbdc_intr(void *data) {
 
 static int atkbdc_probe(device_t *dev) {
   assert(dev->parent->bus == DEV_BUS_PCI);
-  resource_t *regs = bus_resource_alloc(dev, 2, 0, 0, 0, 0, 0); // whole ISA
+  resource_t *regs = bus_resource_alloc_anywhere(dev, SYS_RES_ISA, 0, 0, RF_SHARED);
 
   if (!kbd_reset(regs)) {
     klog("Keyboard self-test failed.");
