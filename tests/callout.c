4c86b27390adbddcab5aabf5e73f97cc65961bda
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2018-06-10 12:27:56 +0200

    fix callout tests

--- tests/callout.c
+++ tests/callout.c
@@ -2,7 +2,7 @@
 #include <callout.h>
 #include <ktest.h>
 #include <sleepq.h>
-#include <sched.h>
+#include <interrupt.h>
 
 static int counter;
 
@@ -16,7 +16,6 @@ static int test_callout_sync(void) {
   const int N = 7;
 
   callout_t callout[N];
-
   bzero(callout, sizeof(callout_t) * N);
 
   for (int j = 0; j < K; j++) {
@@ -40,11 +39,12 @@ static int test_callout_simple(void) {
   const int N = 100;
 
   callout_t callout;
+  bzero(&callout, sizeof(callout_t));
 
   counter = 0;
 
   for (int i = 0; i < N; i++) {
-    WITH_NO_PREEMPTION {
+    WITH_INTR_DISABLED {
       callout_setup_relative(&callout, 1, callout_simple, NULL);
       sleepq_wait(callout_simple, "callout_simple");
     }
@@ -61,7 +61,7 @@ static int current = 0;
 
 static void callout_ordered(void *arg) {
   /* Atomic increment */
-  WITH_NO_PREEMPTION {
+  WITH_INTR_DISABLED {
     current++;
   }
 
@@ -74,10 +74,11 @@ static int test_callout_order(void) {
 
   int order[10] = {2, 5, 4, 6, 9, 0, 8, 1, 3, 7};
   callout_t callouts[10];
+  bzero(callouts, sizeof(callout_t) * 10);
 
   /* Register callouts within a critical section, to ensure they use the same
      base time! */
-  WITH_NO_PREEMPTION {
+  WITH_INTR_DISABLED {
     for (int i = 0; i < 10; i++)
       callout_setup_relative(&callouts[i], 5 + order[i] * 5, callout_ordered,
                              (void *)order[i]);
@@ -102,8 +103,10 @@ static void callout_good(void *arg) {
 static int test_callout_stop(void) {
   current = 0;
   callout_t callout1, callout2;
+  bzero(&callout1, sizeof(callout_t));
+  bzero(&callout2, sizeof(callout_t));
 
-  SCOPED_NO_PREEMPTION();
+  SCOPED_INTR_DISABLED();
 
   callout_setup_relative(&callout1, 5, callout_bad, NULL);
   callout_setup_relative(&callout2, 10, callout_good, NULL);
@@ -118,6 +121,6 @@ static int test_callout_stop(void) {
 }
 
 KTEST_ADD(callout_sync, test_callout_sync, 0);
-KTEST_ADD(callout_simple, test_callout_simple, KTEST_FLAG_BROKEN);
-KTEST_ADD(callout_order, test_callout_order, KTEST_FLAG_BROKEN);
-KTEST_ADD(callout_stop, test_callout_stop, KTEST_FLAG_BROKEN);
+KTEST_ADD(callout_simple, test_callout_simple, 0);
+KTEST_ADD(callout_order, test_callout_order, 0);
+KTEST_ADD(callout_stop, test_callout_stop, 0);
