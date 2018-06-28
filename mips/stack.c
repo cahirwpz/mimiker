35cdf30c3e9f5bf43f8b267434d4d0c981ef06bb
pwit81 <pwit@pwit.info>
2018-06-28 15:33:19 +0200

    Refactoring continued

--- mips/stack.c
+++ mips/stack.c
@@ -5,7 +5,6 @@
 #include <systm.h>
 #include <errno.h>
 
-
 /* Places program args onto the stack.
  * Also modifies value pointed by stack_bottom_p to reflect on changed
  * stack bottom address.
@@ -35,374 +34,48 @@
  * the now empty program stack, so that it can naturally grow
  * downwards.
  */
-void stack_user_entry_setup(const exec_args_t *args,
-                            vm_addr_t *stack_bottom_p) {
-  vm_addr_t stack_end = *stack_bottom_p;
-  /* Begin by calculting arguments total size. This has to be done */
-  /* in advance, because stack grows downwards. */
-  size_t total_arg_size = 0;
-  for (size_t i = 0; i < args->argc; i++) {
-    total_arg_size += roundup(strlen(args->argv[i]) + 1, 4);
-  }
-  /* Store arguments, creating the argument vector. */
-  vm_addr_t arg_vector[args->argc];
-  vm_addr_t p = *stack_bottom_p - total_arg_size;
-  for (unsigned i = 0; i < args->argc; i++) {
-    size_t n = strlen(args->argv[i]) + 1;
-    arg_vector[i] = p;
-    memcpy((uint8_t *)p, args->argv[i], n);
-    p += n;
-    /* Align to word size */
-    p = align(p, 4);
-  }
-  assert(p == stack_end);
-
-  /* Move the stack down and align it so that the top of the stack will be
-     8-byte alligned. */
-  *stack_bottom_p = (*stack_bottom_p - total_arg_size) & 0xfffffff8;
-  if (args->argc % 2 == 0)
-    *stack_bottom_p -= 4;
-
-  /* Now, place the argument vector on the stack. */
-  size_t arg_vector_size = sizeof(arg_vector);
-  vm_addr_t argv = *stack_bottom_p - arg_vector_size;
-  memcpy((void *)argv, &arg_vector, arg_vector_size);
-  /* Move the stack down. */
-  *stack_bottom_p = *stack_bottom_p - arg_vector_size;
-
-  /* Finally, place argc on the stack */
-  *stack_bottom_p = *stack_bottom_p - sizeof(vm_addr_t);
-  vm_addr_t *stack_args = (vm_addr_t *)*stack_bottom_p;
-  *stack_args = args->argc;
-
-  /* Top of the stack must be 8-byte alligned. */
-  assert((*stack_bottom_p & 0x7) == 0);
-
-  /* TODO: Environment */
-}
 
 void patch_blob(void *blob, size_t addr, bool add) {
 
-  size_t argc = ((size_t *) blob)[0];
-  char **argv = (char **) ( blob + sizeof(argc) );
-
+  size_t argc = ((size_t *)blob)[0];
+  char **argv = (char **)(blob + sizeof(argc));
 
   size_t oldbase = (size_t)(blob);
 
-  
-  for(size_t i = 0; i < argc; i++) {
+  for (size_t i = 0; i < argc; i++) {
 
     argv[i] = argv[i] + (addr - oldbase);
-
   }
-
-
 }
 
-
-void stack_user_entry_setup_proper(const exec_args_t_proper *args,
+void stack_user_entry_setup(const exec_args_t *args,
                             vm_addr_t *stack_bottom_p) {
 
-
-  size_t total_arg_size = args->written;
+  size_t total_arg_size = args->bytes_written;
 
   assert((total_arg_size % 8) == 0);
   assert((*stack_bottom_p % 8) == 0);
   *stack_bottom_p = (*stack_bottom_p - total_arg_size);
-  patch_blob(args->blob, (size_t)(*stack_bottom_p), true);
-  memcpy((uint8_t *)*stack_bottom_p, args->blob, total_arg_size);  
-  
-
-  /* //vm_addr_t stack_end = *stack_bottom_p; */
-  /* size_t total_arg_size = args->written; */
-  /* total_arg_size = roundup(total_arg_size, 8); */
-      
-  /* size_t argc  = ((size_t *)args->blob)[0]; */
-  /* char **argsargv = (char**)(args->blob+ sizeof(argc)); */
-
-  /* /// kprintf("@@@@argc is %d\n", argc); */
-  /* //kprintf("@@@argv[0] is %s\n", argsargv[0]); */
-  /* //kprintf("@@@argv[1] is %s\n", argsargv[1]); */
-  
-  
-  /* /\* /\\* Begin by calculting arguments total size. This has to be done *\\/ *\/ */
-  /* /\* /\\* in advance, because stack grows downwards. *\\/ *\/ */
-  /* /\* size_t total_arg_size = 0; *\/ */
-  /* /\* for (size_t i = 0; i < argc; i++) { *\/ */
-  /* /\*   total_arg_size += roundup(strlen(argsargv[i]) + 1, 4); *\/ */
-  /* /\* } *\/ */
-  /* /\* /\\* Store arguments, creating the argument vector. *\\/ *\/ */
-  /* /\* vm_addr_t arg_vector[argc]; *\/ */
-  /* vm_addr_t p = *stack_bottom_p - total_arg_size; */
-
-
-  /* kprintf("@@@@argc is %d\n", argc); */
-  /* kprintf("@@@argv[0] is %s\n", argsargv[0]) ; */
-
-  
-  /* patch_blob(args->blob, (size_t)p, true); */
-
-  /* memcpy((uint8_t *)p, args->blob, total_arg_size); */
-
-  /* // kprintf("@@@@argc is %d\n", ((size_t *)p)[0]); */
-
-  /*   size_t pargc  = ((size_t *)p)[0]; */
-  /*   char **pargv = ((char**)(((uint8_t *)p) + sizeof(size_t))); */
-  
-  /*  kprintf("@@@pargc is %d\n",   pargc) ; */
-  /*  kprintf("@@@pargv[0] is %s\n",   pargv[0]) ; */
-
-  /* /\* Top of the stack must be 8-byte alligned. *\/ */
-  /* assert((*stack_bottom_p & 0x7) == 0); */
-
-   
-  
-  /* //kprintf("@@@argv[1] is %s\n", argsargv[1]); */
-
-  
-  /* //patching */
-
-     
-  /* vm_addr_t stack_end = *stack_bottom_p; */
-
-  /* size_t argc  = ((size_t *)args->blob)[0]; */
-  /* char **argsargv = (char**)(args->blob+ sizeof(argc)); */
-  
-  /* /\* Begin by calculting arguments total size. This has to be done *\/ */
-  /* /\* in advance, because stack grows downwards. *\/ */
-  /* size_t total_arg_size = 0; */
-  /* for (size_t i = 0; i < argc; i++) { */
-  /*   total_arg_size += roundup(strlen(argsargv[i]) + 1, 4); */
-  /* } */
-  /* /\* Store arguments, creating the argument vector. *\/ */
-  /* vm_addr_t arg_vector[argc]; */
-  /* vm_addr_t p = *stack_bottom_p - total_arg_size; */
-  /* for (unsigned i = 0; i < argc; i++) { */
-  /*   size_t n = strlen(argsargv[i]) + 1; */
-  /*   arg_vector[i] = p; */
-  /*   memcpy((uint8_t *)p, argsargv[i], n); */
-  /*   p += n; */
-  /*   /\* Align to word size *\/ */
-  /*   p = align(p, 4); */
-  /* } */
-  /* assert(p == stack_end); */
-  /* /\* Move the stack down and align it so that the top of the stack will be */
-  /*    8-byte alligned. *\/ */
-  /* *stack_bottom_p = (*stack_bottom_p - total_arg_size) & 0xfffffff8; */
-  /* if (argc % 2 == 0) */
-  /*   *stack_bottom_p -= 4; */
-
-  /* /\* Now, place the argument vector on the stack. *\/ */
-  /* size_t arg_vector_size = sizeof(arg_vector); */
-  /* vm_addr_t argv = *stack_bottom_p - arg_vector_size; */
-  /* memcpy((void *)argv, &arg_vector, arg_vector_size); */
-  /* /\* Move the stack down. *\/ */
-  /* *stack_bottom_p = *stack_bottom_p - arg_vector_size; */
-
-  /* /\* Finally, place argc on the stack *\/ */
-  /* *stack_bottom_p = *stack_bottom_p - sizeof(vm_addr_t); */
-  /* vm_addr_t *stack_args = (vm_addr_t *)*stack_bottom_p; */
-  /* *stack_args = argc; */
-
-  /* /\* Top of the stack must be 8-byte alligned. *\/ */
-  /* assert((*stack_bottom_p & 0x7) == 0); */
-
-  /* /\* TODO: Environment *\/ */
-
-
-  
-  /*   vm_addr_t stack_end = *stack_bottom_p; */
-
-  /* size_t argc  = ((size_t *)args->blob)[0]; */
-  /* char **argsargv = (char**)(args->blob+ sizeof(argc)); */
-  
-  /* /\* Begin by calculting arguments total size. This has to be done *\/ */
-  /* /\* in advance, because stack grows downwards. *\/ */
-  /* size_t total_arg_size = 0; */
-  /* for (size_t i = 0; i < argc; i++) { */
-  /*   total_arg_size += roundup(strlen(argsargv[i]) + 1, 4); */
-  /* } */
-  /* /\* Store arguments, creating the argument vector. *\/ */
-  /* vm_addr_t arg_vector[argc]; */
-  /* vm_addr_t p = *stack_bottom_p - total_arg_size; */
-  /* for (unsigned i = 0; i < argc; i++) { */
-  /*   size_t n = strlen(argsargv[i]) + 1; */
-  /*   arg_vector[i] = p; */
-  /*   memcpy((uint8_t *)p, argsargv[i], n); */
-  /*   p += n; */
-  /*   /\* Align to word size *\/ */
-  /*   p = align(p, 4); */
-  /* } */
-  /* assert(p == stack_end); */
-  /* /\* Move the stack down and align it so that the top of the stack will be */
-  /*    8-byte alligned. *\/ */
-  /* *stack_bottom_p = (*stack_bottom_p - total_arg_size) & 0xfffffff8; */
-  /* if (argc % 2 == 0) */
-  /*   *stack_bottom_p -= 4; */
-
-  /* /\* Now, place the argument vector on the stack. *\/ */
-  /* size_t arg_vector_size = sizeof(arg_vector); */
-  /* vm_addr_t argv = *stack_bottom_p - arg_vector_size; */
-  /* memcpy((void *)argv, &arg_vector, arg_vector_size); */
-  /* /\* Move the stack down. *\/ */
-  /* *stack_bottom_p = *stack_bottom_p - arg_vector_size; */
-
-  /* /\* Finally, place argc on the stack *\/ */
-  /* *stack_bottom_p = *stack_bottom_p - sizeof(vm_addr_t); */
-  /* vm_addr_t *stack_args = (vm_addr_t *)*stack_bottom_p; */
-  /* *stack_args = argc; */
-
-  /* /\* Top of the stack must be 8-byte alligned. *\/ */
-  /* assert((*stack_bottom_p & 0x7) == 0); */
-
-  /* /\* TODO: Environment *\/ */
-
-  /* assert( stack_end > *stack_bottom_p + args->written); */
-
-  /* vm_addr_t stack_end = *stack_bottom_p; */
-
-  /* size_t argc  = ((size_t *)args->blob)[0]; */
-  /* char **argsargv = (char**)(args->blob+ sizeof(argc)); */
-  
-  /* /\* Begin by calculting arguments total size. This has to be done *\/ */
-  /* /\* in advance, because stack grows downwards. *\/ */
-  /* size_t total_arg_size = 0; */
-  /* for (size_t i = 0; i < argc; i++) { */
-  /*   total_arg_size += roundup(strlen(argsargv[i]) + 1, 4); */
-  /* } */
-  /* /\* Store arguments, creating the argument vector. *\/ */
-  /* vm_addr_t arg_vector[argc]; */
-  /* vm_addr_t p = *stack_bottom_p - total_arg_size; */
-  /* for (unsigned i = 0; i < argc; i++) { */
-  /*   size_t n = strlen(argsargv[i]) + 1; */
-  /*   arg_vector[i] = p; */
-  /*   memcpy((uint8_t *)p, argsargv[i], n); */
-  /*   p += n; */
-  /*   /\* Align to word size *\/ */
-  /*   p = align(p, 4); */
-  /* } */
-  /* assert(p == stack_end); */
-  /* /\* Move the stack down and align it so that the top of the stack will be */
-  /*    8-byte alligned. *\/ */
-  /* *stack_bottom_p = (*stack_bottom_p - total_arg_size) & 0xfffffff8; */
-  /* if (argc % 2 == 0) */
-  /*   *stack_bottom_p -= 4; */
-
-  /* /\* Now, place the argument vector on the stack. *\/ */
-  /* size_t arg_vector_size = sizeof(arg_vector); */
-  /* vm_addr_t argv = *stack_bottom_p - arg_vector_size; */
-  /* memcpy((void *)argv, &arg_vector, arg_vector_size); */
-  /* /\* Move the stack down. *\/ */
-  /* *stack_bottom_p = *stack_bottom_p - arg_vector_size; */
-
-  /* /\* Finally, place argc on the stack *\/ */
-  /* *stack_bottom_p = *stack_bottom_p - sizeof(vm_addr_t); */
-  /* vm_addr_t *stack_args = (vm_addr_t *)*stack_bottom_p; */
-  /* *stack_args = argc; */
-
-  /* /\* Top of the stack must be 8-byte alligned. *\/ */
-  /* assert((*stack_bottom_p & 0x7) == 0); */
-
-  /* /\* TODO: Environment *\/ */
-
-
-
-  /* vm_addr_t stack_end = *stack_bottom_p; */
-  /* /\* Begin by calculting arguments total size. This has to be done *\/ */
-  /* /\* in advance, because stack grows downwards. *\/ */
-  /* size_t total_arg_size = 0; */
-  /* for (size_t i = 0; i < args->argc; i++) { */
-  /*   total_arg_size += roundup(strlen(args->argv[i]) + 1, 4); */
-  /* } */
-  /* /\* Store arguments, creating the argument vector. *\/ */
-  /* vm_addr_t arg_vector[args->argc]; */
-  /* vm_addr_t p = *stack_bottom_p - total_arg_size; */
-  /* for (unsigned i = 0; i < args->argc; i++) { */
-  /*   size_t n = strlen(args->argv[i]) + 1; */
-  /*   arg_vector[i] = p; */
-  /*   memcpy((uint8_t *)p, args->argv[i], n); */
-  /*   p += n; */
-  /*   /\* Align to word size *\/ */
-  /*   p = align(p, 4); */
-  /* } */
-  /* assert(p == stack_end); */
-
-  /* /\* Move the stack down and align it so that the top of the stack will be */
-  /*    8-byte alligned. *\/ */
-  /* *stack_bottom_p = (*stack_bottom_p - total_arg_size) & 0xfffffff8; */
-  /* if (args->argc % 2 == 0) */
-  /*   *stack_bottom_p -= 4; */
-
-  /* /\* Now, place the argument vector on the stack. *\/ */
-  /* size_t arg_vector_size = sizeof(arg_vector); */
-  /* vm_addr_t argv = *stack_bottom_p - arg_vector_size; */
-  /* memcpy((void *)argv, &arg_vector, arg_vector_size); */
-  /* /\* Move the stack down. *\/ */
-  /* *stack_bottom_p = *stack_bottom_p - arg_vector_size; */
-
-  /* /\* Finally, place argc on the stack *\/ */
-  /* *stack_bottom_p = *stack_bottom_p - sizeof(vm_addr_t); */
-  /* vm_addr_t *stack_args = (vm_addr_t *)*stack_bottom_p; */
-  /* *stack_args = args->argc; */
-
-  /* /\* Top of the stack must be 8-byte alligned. *\/ */
-  /* assert((*stack_bottom_p & 0x7) == 0); */
+  patch_blob(args->arg_blob, (size_t)(*stack_bottom_p), true);
+  memcpy((uint8_t *)*stack_bottom_p, args->arg_blob, total_arg_size);
 
   /* /\* TODO: Environment *\/ */
 }
 
-
-typedef struct copy_ops
-{
-  int (*cpybytes)(const void* saddr, void* daddr, size_t len);
-  int (*cpystr)(const void* saddr, void* daddr, size_t len, size_t* lencopied);
+typedef struct copy_ops {
+  int (*cpybytes)(const void *saddr, void *daddr, size_t len);
+  int (*cpystr)(const void *saddr, void *daddr, size_t len, size_t *lencopied);
 } copy_ops_t;
 
-copy_ops_t from_userspace = 
-{
-  .cpybytes = copyin,
-  .cpystr = copyinstr
-};
+int copy_ptrs(const char **user_argv, int8_t *blob, size_t blob_size,
+              size_t *argc_out, copy_ops_t co) {
 
-int memcpy_wrapper(const void* saddr, void* daddr, size_t len) {
-
-  memcpy(daddr, saddr, len);
-  return 0;
-}
-int cpystr_wrapper(const void* saddr, void* daddr, size_t len, size_t* lencopied) {
-
-  size_t actlen = strlen(saddr);
-  int result = 0;
-
-  if (actlen + 1 > len) {
-    result = -ENAMETOOLONG; 
-    actlen = len;
-  }
-
-  *lencopied = actlen; 
- memcpy(daddr, saddr, actlen);
- return result;
-}
-
-copy_ops_t from_kernelspace =  {
-
-  .cpybytes = memcpy_wrapper,
-  .cpystr = cpystr_wrapper  
-};
-
-
-
-int copyinargptrs_proper(const char **user_argv,
-			 int8_t* blob, size_t blob_size,
-			 size_t *argc_out, copy_ops_t co) {
-
-  const char** kern_argv = (const char**)blob;
+  const char **kern_argv = (const char **)blob;
 
   char *arg_ptr;
   const size_t ptr_size = sizeof(arg_ptr);
 
-  for (int argc = 0; argc * ptr_size < blob_size; argc++)
-  {
+  for (int argc = 0; argc * ptr_size < blob_size; argc++) {
     int result = co.cpybytes(user_argv + argc, &arg_ptr, ptr_size);
     if (result < 0)
       return result;
@@ -424,79 +97,101 @@ int copyinargptrs_proper(const char **user_argv,
   return -E2BIG;
 }
 
+int copy_strings(size_t argc, const char **kern_argv, int8_t *dst,
+                 size_t dst_size, size_t *bytes_written, copy_ops_t co) {
 
-int marshal_args(const char **user_argv, int8_t *argv_blob,
-		 size_t blob_size, size_t *bytes_written, copy_ops_t co);
+  // int8_t *args = argv_blob + *bytes_written;
+  size_t argsize, i = 0;
+  int result;
 
+  do {
 
-int uspace_marshal_args(const char **user_argv, int8_t *argv_blob,
-		 size_t blob_size, size_t *bytes_written) {
+    result = co.cpystr(kern_argv[i], dst, dst_size - *bytes_written, &argsize);
+    if (result < 0)
+      return (result == -ENAMETOOLONG) ? -E2BIG : result;
 
+    kern_argv[i] = (char *)(dst);
+    argsize = roundup(argsize, 4);
+    dst += argsize;
+    *bytes_written += argsize;
+    i++;
 
-  return marshal_args(user_argv, argv_blob, blob_size, bytes_written, from_userspace);
-  
-} 
+  } while ((i < argc) && (*bytes_written < dst_size));
 
+  if (i < argc)
+    return -E2BIG;
 
-int kspace_marshal_args(const char **user_argv, int8_t *argv_blob,
-		 size_t blob_size, size_t *bytes_written) {
+  *bytes_written = roundup(*bytes_written, 8);
 
+  return 0;
+}
 
-  return marshal_args(user_argv, argv_blob, blob_size, bytes_written, from_kernelspace);
-  
-} 
+int marshal_args(const char **argv, int8_t *dst, size_t dst_size,
+                 size_t *bytes_written, copy_ops_t co) {
 
-
-int marshal_args(const char **user_argv, int8_t *argv_blob,
-		 size_t blob_size, size_t *bytes_written, copy_ops_t co) {
-  
-  assert(sizeof(vm_addr_t) == 4); 
+  assert(sizeof(vm_addr_t) == 4);
   assert(sizeof(size_t) == 4);
-  assert(sizeof(char*) == 4);
-  assert( blob_size >= 4);
+  assert(sizeof(char *) == 4);
+  assert(dst_size >= 4);
 
   size_t argc;
-  char **kern_argv = (char**)(argv_blob + sizeof(argc));
-    
-  int result = copyinargptrs_proper( user_argv, (int8_t*)kern_argv,
-				     blob_size - sizeof(argc),
-				     &argc, co);   
+  const char **kern_argv = (const char **)(dst + sizeof(argc));
+
+  int result =
+    copy_ptrs(argv, (int8_t *)kern_argv, dst_size - sizeof(argc), &argc, co);
 
   if (result < 0)
     return result;
 
   assert(argc > 0);
-  
+
   *bytes_written = argc * sizeof(char *);
 
-
-  ((size_t*)argv_blob)[0] = argc;
+  ((size_t *)dst)[0] = argc;
   *bytes_written += sizeof(argc);
   *bytes_written = roundup(*bytes_written, 8);
 
-  
-  int8_t *args  =  argv_blob + *bytes_written;
-  size_t argsize, i = 0;
-  
-  do {
+  int8_t *args = dst + *bytes_written;
 
-    result = co.cpystr(kern_argv[i], args,
-		       blob_size - *bytes_written , &argsize);
-    if (result < 0)
-      return (result == -ENAMETOOLONG) ? -E2BIG : result;
+  return copy_strings(argc, kern_argv, args, dst_size, bytes_written, co);
+}
 
-    kern_argv[i] = (char*)(args);
-    argsize = roundup(argsize, 4);
-    args += argsize;
-    *bytes_written += argsize;
-    i++;
+copy_ops_t from_uspace = {.cpybytes = copyin, .cpystr = copyinstr};
 
-  } while ( (i < argc) && (*bytes_written < blob_size)); 
+int uspace_marshal_args(const char **argv, int8_t *dst, size_t dst_size,
+                        size_t *bytes_written) {
 
-  if (i < argc)
-    return -E2BIG;
-    
-  *bytes_written = roundup(*bytes_written, 8);
-    
+  return marshal_args(argv, dst, dst_size, bytes_written, from_uspace);
+}
+
+int memcpy_wrapper(const void *src, void *dst, size_t dst_size) {
+
+  memcpy(dst, src, dst_size);
   return 0;
+}
+
+int strlcpy_wrapper(const void *src, void *dst, size_t dst_size,
+                    size_t *lencopied) {
+
+  size_t act_size = strlen(src) + 1;
+  int result = 0;
+
+  strlcpy(dst, src, dst_size);
+
+  if (act_size > dst_size) {
+    result = -ENAMETOOLONG;
+    act_size = dst_size;
+  }
+
+  *lencopied = act_size;
+  return result;
+}
+
+copy_ops_t from_kspace = {.cpybytes = memcpy_wrapper,
+                          .cpystr = strlcpy_wrapper};
+
+int kspace_marshal_args(const char **argv, int8_t *dst, size_t dst_size,
+                        size_t *bytes_written) {
+
+  return marshal_args(argv, dst, dst_size, bytes_written, from_kspace);
 }
