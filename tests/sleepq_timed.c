c5b8f9124f4521a3c29207c745adc27328eca844
Wojciech Jasiński <ilikeheaps@gmail.com>
2018-07-01 02:40:20 +0200

    Introduce test for timed sleep.

---
+++ tests/sleepq_timed.c
@@ -0,0 +1,88 @@
+#include <sleepq.h>
+#include <ktest.h>
+#include <time.h>
+#include <thread.h>
+#include <sched.h>
+#include <runq.h>
+
+/* This test doesn't check if timed sleep is also indeed interruptible. */
+
+/* just to get some unique address for sleepq */
+static int wchan;
+
+#define T 6
+#define SLEEP_TIME_MS 4
+#define EPSILON 1
+
+static volatile int timed_received;
+static volatile int signaled_received;
+
+static volatile int signaled_sent;
+
+thread_t *waiters[T];
+thread_t *waker;
+
+static void snorlax(void *_arg) {
+  systime_t before_sleep = getsystime();
+  sq_wakeup_t status = sleepq_wait_timed(&wchan, __caller(0), SLEEP_TIME_MS);
+  systime_t after_sleep = getsystime();
+    systime_t diff = after_sleep - before_sleep;
+
+  if (status == SQ_TIMED) {
+    timed_received++;
+    assert(diff >= SLEEP_TIME_MS);
+    assert(diff <= SLEEP_TIME_MS + EPSILON);
+  } else if (status == SQ_REGULAR) {
+    signaled_received++;
+    assert(diff <= SLEEP_TIME_MS + EPSILON);
+  } else {
+    assert(false);
+  }
+}
+
+static void poke_flute(void *_arg) {
+  /* try to wake up half of the threads before timeout */
+  for (int i = 0; i < T / 2; i++) {
+    bool status = sleepq_signal(&wchan);
+    if (status)
+      signaled_sent++;
+  }
+}
+
+static int test_sleepq_timed(void) {
+  timed_received = 0;
+  signaled_received = 0;
+  signaled_sent = 0;
+
+  waker = thread_create("waker", poke_flute, NULL);
+  for (int i = 0; i < T; i++) {
+    char name[20];
+    snprintf(name, sizeof(name), "waiter%d", i);
+    waiters[i] = thread_create(name, snorlax, NULL);
+  }
+
+  for (int i = 0; i < T; i++) {
+    WITH_SPINLOCK(waiters[i]->td_spin) {
+      sched_set_prio(waiters[i], RQ_PPQ);
+    }
+  }
+
+  WITH_NO_PREEMPTION {
+    for (int i = 0; i < T; i++)
+      sched_add(waiters[i]);
+    sched_add(waker);
+  }
+
+  thread_join(waker);
+  for (int i = 0; i < T; i++)
+    thread_join(waiters[i]);
+
+  assert(signaled_received == signaled_sent);
+  assert(signaled_received + timed_received == T);
+  /* At most floor(T/2) threads were woken by sleepq_signal */
+  assert(timed_received >= (T + 1) / 2);
+
+  return KTEST_SUCCESS;
+}
+
+KTEST_ADD(sleepq_timed, test_sleepq_timed, 0);
