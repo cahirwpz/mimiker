a16c8da7281c35ff220c44dbe14b3127ae63d2e9
Krystian Bacławski <krystian.baclawski@gmail.com>
2018-07-26 11:24:32 +0200

    Simplify cbus_{read,write}_1.

--- mips/cbus.c
+++ mips/cbus.c
@@ -2,14 +2,12 @@
 #include <bus.h>
 
 static uint8_t cbus_read_1(bus_space_handle_t handle, bus_size_t offset) {
-  paddr_t addr = handle + offset * sizeof(uint64_t);
-  return *(volatile uint8_t *)MIPS_PHYS_TO_KSEG1(addr);
+  return *(volatile uint8_t *)(handle + offset * sizeof(uint64_t));
 }
 
 static void cbus_write_1(bus_space_handle_t handle, bus_size_t offset,
                          uint8_t value) {
-  paddr_t addr = handle + offset * sizeof(uint64_t);
-  *(volatile uint8_t *)MIPS_PHYS_TO_KSEG1(addr) = value;
+  *(volatile uint8_t *)(handle + offset * sizeof(uint64_t)) = value;
 }
 
 static bus_space_t cbus_bus_space = {
@@ -17,7 +15,7 @@ static bus_space_t cbus_bus_space = {
 };
 
 resource_t cbus_uart[1] = {{.r_bus_tag = &cbus_bus_space,
-                            .r_bus_handle = MALTA_CBUS_UART,
+                            .r_bus_handle = MIPS_PHYS_TO_KSEG1(MALTA_CBUS_UART),
                             /* RT_MEMORY since CBUS UART is a device mapped in
                              * common physical address space of MIPS CPU. */
                             .r_type = RT_MEMORY,
