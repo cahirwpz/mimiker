57f4f3d779119f1529b601745c068a7227058cab
pwit81 <pwit@pwit.info>
2018-07-30 16:31:03 +0200

    Environment variables made available to userspace via envp argument of main funct.

--- user/sandbox/main.c
+++ user/sandbox/main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
+extern char **environ;
+
+int main(int argc, char **argv, char **envp) {
   printf("This is a sandbox user program. You can use it for testing and "
          "experiments in userspace. Don't commit any useful kernel tests here, "
-         "please place them in utest program.");
+         "please place them in utest program.\n");
+
+  printf("-----------\n");
+
+  printf("%p %p\n", argv, envp);
+  printf("%s\n", envp[0]);
+  // printf("environ pointer is: %p\n", environ);
+  // printf("%s\n", environ[0]);
+  environ = envp;
+  printf("%s\n", getenv("DUMMY"));
+
+  printf("-----------\n");
 
   return 0;
 }
