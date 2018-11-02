6c8fcef6627511a3bc8177f5035b8e26e4024d69
adam_chudas <adam.chudas@microscopeit.com>
2018-11-02 19:17:23 +0100

    style fixes

--- sys/pg.c
+++ sys/pg.c
@@ -2,8 +2,8 @@
 #include <proc.h>
 
 int setpgid(pid_t pid, pgid_t pgid) {
-	proc_t *p_handle = proc_find(pid);
-	proc_lock(p_handle);
-	p_handle->p_pgid = pgid;
-	proc_unlock(p_handle);
+  proc_t *p_handle = proc_find(pid);
+  proc_lock(p_handle);
+  p_handle->p_pgid = pgid;
+  proc_unlock(p_handle);
 }
