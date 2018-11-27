e35b99933b303e04a18cd50d5c91c2239ebe8c99
Mateusz Urbańczyk <mu@qed.ai>
2018-11-27 19:08:14 +0100

    Docstrings for linker set's macros

--- include/linker_set.h
+++ include/linker_set.h
@@ -3,28 +3,42 @@
 
 #include <common.h>
 
-/* The implementation mostly follows DragonFly BSD's 'sys/linker_set.h' */
+/* The implementation mostly follows DragonFly BSD's 'sys/linker_set.h'
+ *
+ * The following macros are used to declare global sets of objects, which
+ * then are collected by the linker.
+ * */
 
 /* GLOBL macro exists to preserve __start_set_* and __stop_set_* sections of
  * kernel modules which are discarded from binutils 2.17.50+ otherwise. */
 #define __GLOBL1(sym) __asm__(".globl " #sym)
 #define __GLOBL(sym) __GLOBL1(sym)
 
+/* Add \sym entry to the linker set where \sym type needs to be equal to
+ * linker set's \ptype` */
 #define SET_ENTRY(set, sym)                                                    \
   __GLOBL(__CONCAT(__start_set_, set));                                        \
   __GLOBL(__CONCAT(__stop_set_, set));                                         \
   static void const *const __set_##set##_sym_##sym __section("set_" #set)      \
     __used = &sym
 
+/* Define a new linker set with \set name and entries of type \ptype */
 #define SET_DECLARE(set, ptype)                                                \
   extern ptype *__CONCAT(__start_set_, set);                                   \
   extern ptype *__CONCAT(__stop_set_, set)
 
+/* Returns the beggining of the linker set of type **t
+ * where t is the linker set's \ptype
+ * */
 #define SET_BEGIN(set) (&__CONCAT(__start_set_, set))
+
+/* Returns the end of the linker set of type **t
+ * where t is the linker set's \ptype
+ * */
 #define SET_LIMIT(set) (&__CONCAT(__stop_set_, set))
 
-/* Iterate over all the elements of a set.  Sets always contain addresses of
- * things, and "pvar" points to words containing those addresses.  Thus is must
+/* Iterate over all the elements of a set. Sets always contain addresses of
+ * things, and "pvar" points to words containing those addresses. Thus is must
  * be declared as "type **pvar", and the address of each set item is obtained
  * inside the loop by "*pvar". */
 #define SET_FOREACH(pvar, set)                                                 \
