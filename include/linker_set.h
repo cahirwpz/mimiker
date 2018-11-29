04d6722ddb7ee720bd242b24617f5c316ea0e8e0
Krystian Bacławski <krystian.baclawski@gmail.com>
2018-11-29 14:40:10 +0100

    Minor corrections.

--- include/linker_set.h
+++ include/linker_set.h
@@ -5,40 +5,39 @@
 
 /* The implementation mostly follows DragonFly BSD's 'sys/linker_set.h'
  *
- * The following macros are used to declare global sets of objects, which
- * then are collected by the linker.
- * */
+ * The following macros are used to declare global sets of objects. In each
+ * compilation unit you can assign several objects to a set. Linker will
+ * gather all objects belonging to the set and put them into an array of
+ * pointers to the objects. */
 
 /* GLOBL macro exists to preserve __start_set_* and __stop_set_* sections of
  * kernel modules which are discarded from binutils 2.17.50+ otherwise. */
 #define __GLOBL1(sym) __asm__(".globl " #sym)
 #define __GLOBL(sym) __GLOBL1(sym)
 
-/* Add \sym entry to the linker set where \sym type needs to be equal to
- * linker set's \ptype` */
+/* Add \sym entry to the linker set where \sym type must be same as \ptype
+ * of the linker set */
 #define SET_ENTRY(set, sym)                                                    \
   __GLOBL(__CONCAT(__start_set_, set));                                        \
   __GLOBL(__CONCAT(__stop_set_, set));                                         \
   static void const *const __set_##set##_sym_##sym __section("set_" #set)      \
     __used = &sym
 
-/* Define a new linker set with \set name and entries of type \ptype */
+/* Define a new linker set with \set name and entries of \ptype type */
 #define SET_DECLARE(set, ptype)                                                \
   extern ptype *__CONCAT(__start_set_, set);                                   \
   extern ptype *__CONCAT(__stop_set_, set)
 
-/* Returns the beggining of the linker set of type **t
- * where t is the linker set's \ptype
- * */
+/* Returns the begining of the linker set of type **T
+ * where T is \ptype type of the linker set */
 #define SET_BEGIN(set) (&__CONCAT(__start_set_, set))
 
-/* Returns the end of the linker set of type **t
- * where t is the linker set's \ptype
- * */
+/* Returns the end of the linker set of type **T
+ * where T is \ptype type of the linker set */
 #define SET_LIMIT(set) (&__CONCAT(__stop_set_, set))
 
 /* Iterate over all the elements of a set. Sets always contain addresses of
- * things, and "pvar" points to words containing those addresses. Thus is must
+ * objects, and "pvar" points to words containing those addresses. Thus is must
  * be declared as "type **pvar", and the address of each set item is obtained
  * inside the loop by "*pvar". */
 #define SET_FOREACH(pvar, set)                                                 \
