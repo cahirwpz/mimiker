a8f7e7ee3b648d9c69449889b6d59c3a245efb75
Jan Mazur <jmzr@mimiker.ii.uni.wroc.pl>
2018-12-11 19:58:55 +0100

    fix format

--- include/devclass.h
+++ include/devclass.h
@@ -7,27 +7,24 @@
 typedef struct devclass devclass_t;
 
 struct devclass {
-	const char *name;
-	driver_t **drivers;
-	driver_t **set_end;
+  const char *name;
+  driver_t **drivers;
+  driver_t **set_end;
 };
 
-#define DEVCLASS_CREATE(dc_name) \
-	SET_DECLARE(dc_name, driver_t); \
-	devclass_t dc_name##_devclass = {.name = (#dc_name), \
-		.set_end = SET_LIMIT(dc_name), \
-		.drivers = SET_BEGIN(dc_name)}; \
-	SET_ENTRY(devclasses, dc_name##_devclass);
+#define DEVCLASS_CREATE(dc_name)                                               \
+  SET_DECLARE(dc_name, driver_t);                                              \
+  devclass_t dc_name##_devclass = {.name = (#dc_name),                         \
+                                   .set_end = SET_LIMIT(dc_name),              \
+                                   .drivers = SET_BEGIN(dc_name)};             \
+  SET_ENTRY(devclasses, dc_name##_devclass);
 
+#define DEVCLASS_ADD_DRV(dc_name, driver_structure)                            \
+  SET_ENTRY(dc_name, driver_structure);
 
-#define DEVCLASS_ADD_DRV(dc_name, driver_structure) \
-	SET_ENTRY(dc_name, driver_structure);
-
-#define DEVCLASS_FOREACH(drv, dc) \
-	SET_FOREACH_PTR(drv, (dc)->drivers, (dc)->set_end)
+#define DEVCLASS_FOREACH(drv, dc)                                              \
+  SET_FOREACH_PTR(drv, (dc)->drivers, (dc)->set_end)
 
 devclass_t *devclass_find(const char *classname);
 
 #endif
-
-
--- sys/devclass.c
+++ sys/devclass.c
@@ -4,12 +4,12 @@
 
 SET_DECLARE(devclasses, devclass_t);
 
-devclass_t *devclass_find(const char *classname){
-	devclass_t **dc;
+devclass_t *devclass_find(const char *classname) {
+  devclass_t **dc;
 
-	SET_FOREACH (dc, devclasses) {
-		if(strcmp((*dc)->name, classname) == 0)
-			return *dc;
-	}
-	return NULL;
+  SET_FOREACH (dc, devclasses) {
+    if (strcmp((*dc)->name, classname) == 0)
+      return *dc;
+  }
+  return NULL;
 }
