bf87a5a60ad573a3a7851119779d7d8b256ab4fe
Krystian Bacławski <krystian.baclawski@gmail.com>
2018-12-14 09:19:15 +0100

    Rewrite script in more direct style.

--- script/gendevhint.py
+++ script/gendevhint.py
@@ -24,14 +24,14 @@ OUTPUT_TEMPLATE = Template("""
 
 /* memory or io-port address range */
 typedef struct dh_range {
-    uintptr_t start, end;
+  uintptr_t start, end;
 } dh_range_t;
 
 typedef struct {
-    const char *path;
-    dh_range_t **iomem;
-    dh_range_t **ioport;
-    uint32_t irq;
+  const char *path;
+  dh_range_t **iomem;
+  dh_range_t **ioport;
+  uint32_t irq;
 } devhint_t;
 
 devhint_t ${platform}_hints[] = {
@@ -47,62 +47,59 @@ def generate_fdt(filename):
     return dtb
 
 
+def flatten_fdt(root, path):
+    nodes = [(path, root)]
+    for node in root.nodes:
+        nodes.extend(flatten_fdt(node, path + '/' + node.name))
+    return nodes
+
+
 def make_pairs(lst):
     return [(lst[i], lst[i + 1]) for i in range(0, len(lst), 2)]
 
 
-def generate_hints(device, path):
+def convert_node(path, device):
+    hints = {}
     for prop in device.props:
-        yield ('.path', path)
-
+        hints['.path'] = path
         if prop.name == 'iomem':
-            yield ('.iomem', make_pairs(prop.data))
-
+            hints['.iomem'] = make_pairs(prop.data)
         elif prop.name == 'ioport':
-            yield ('.ioport', make_pairs(prop.data))
-
+            hints['.ioport'] = make_pairs(prop.data)
         elif prop.name == 'interrupts':
             assert len(prop.data) == 1, "Only one irq per device supported!"
-            yield ('.irq', prop.data[0])
-
+            hints['.irq'] = prop.data[0]
         else:
             raise ValueError("The following device hint resource is "
                              "not supported: {}".format(prop.name))
-
-
-def flatten_fdt(root, path):
-    child_hints = [flatten_fdt(node, "{}/{}".format(path, node.name))
-                   for node in root.nodes]
-
-    hints = reduce(lambda a, b: {**a, **b}, child_hints, {})
-    current_device_hints = dict(generate_hints(root, path))
-    if current_device_hints:
-        hints[path] = current_device_hints
     return hints
+
+
+def convert_to_hints(nodes):
+    return filter(bool, [convert_node(*node) for node in nodes])
 
 
 def to_c_value(val):
     if isinstance(val, str):
-        return '"{}"'.format(val)
+        return '"' + val + '"'
     if isinstance(val, int):
         return hex(val)
     if isinstance(val, list):
-        ranges = ['&(dh_range_t){.start = 0x%x, .end = 0x%x}' % (start, end)
-                  for start, end in val]
+        ranges = ['&(dh_range_t){.start = 0x%x, .end = 0x%x}' % pair
+                  for pair in val]
         ranges.append('(dh_range_t *)0')
-        return '(dh_range_t *[]){' + ', '.join(ranges) + '}'
+        return '(dh_range_t *[]){\n    ' + ',\n    '.join(ranges) + '\n  }'
     raise ValueError(repr(val))
 
 
 def hint_as_c_entry(hint):
-    fields_as_strs = [
-        "\t{} = {}".format(resource, to_c_value(value))
-        for resource, value in hint.items()]
-    return '{' + ',\n'.join(fields_as_strs) + '}\n,\n'
+    fields = ['  {} = {}'.format(resource, to_c_value(value))
+              for resource, value in hint.items()]
+    return '{\n' + ',\n'.join(fields) + '\n}'
 
 
-def device_hints_as_c_array(hints):
-    return ''.join(map(hint_as_c_entry, hints.values()))
+def hints_as_c_array(hints):
+    return ',\n'.join(map(hint_as_c_entry, hints))
 
 
 if __name__ == '__main__':
@@ -115,10 +112,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     fdt = generate_fdt(args.input)
-    flat_hints = flatten_fdt(fdt.root_node, '/rootdev')
-    hints_as_c_array = device_hints_as_c_array(flat_hints)
+    node_list = flatten_fdt(fdt.root_node, '/rootdev')
+    flat_hints = convert_to_hints(node_list)
+    hints_array = hints_as_c_array(flat_hints)
     with open(args.output, 'w') as f:
         f.write(OUTPUT_TEMPLATE.substitute(
             input=args.input,
             platform=os.path.splitext(args.input)[0],
-            hints=hints_as_c_array))
+            hints=hints_array))
