95fd54428c7bb3b9f598fc5d02a986094b1f2c3c
Krystian Bacławski <krystian.baclawski@gmail.com>
2018-12-19 08:59:44 +0100

    Support limited number of command arguments.

--- bin/mish/mish.c
+++ bin/mish/mish.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 
 #define LINELEN 80
+#define NARGS 10
 
 static int readline(char *line, size_t size) {
   char *s = fgets(line, size, stdin);
@@ -38,6 +39,7 @@ static const builtin_t builtins[] = {
 
 int main(int argc, char *argv[], char *envp[]) {
   char line[LINELEN];
+  char *new_argv[NARGS+1];
   int status;
 
   while (1) {
@@ -45,13 +47,23 @@ int main(int argc, char *argv[], char *envp[]) {
     if (!readline(line, LINELEN))
       break;
 
+    int new_argc;
+    char *brkt, *word;
+    for (new_argc = 0, word = strtok_r(line, " ", &brkt);
+         new_argc < NARGS && word;
+         new_argc++, word = strtok_r(NULL, " ", &brkt))
+    {
+      new_argv[new_argc] = word;
+    }
+    new_argv[new_argc] = NULL;
+
     /* Lookup builtin command */
     const builtin_t *b = NULL;
     for (b = builtins; b->name; b++)
-      if (strcmp(line, b->name) == 0)
+      if (strcmp(new_argv[0], b->name) == 0)
         break;
     if (b->name) {
-      status = b->cmd(0, (char *[]){NULL}, envp);
+      status = b->cmd(new_argc, new_argv, envp);
       if (status)
         break;
       continue;
@@ -65,7 +77,7 @@ int main(int argc, char *argv[], char *envp[]) {
         printf("Child exited with %d\n", status);
     } else {
       fprintf(stderr, "run %s\n", line);
-      execve(line, (char *[]){line, NULL}, envp);
+      execve(new_argv[0], new_argv, envp);
       perror("execve failed");
       return EXIT_FAILURE;
     }
