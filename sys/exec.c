d594ce388d1a4d083fd7713c498fcfdae20f009a
Krystian Bacławski <krystian.baclawski@gmail.com>
2018-12-23 12:01:49 +0100

    Split do_exec implementation into smaller functions.

--- sys/exec.c
+++ sys/exec.c
@@ -117,10 +117,10 @@ static int store_strings(ustack_t *us, char **strv, char **stack_strv,
 
 /*!\brief Places program args onto the stack.
  *
- * Also modifies value pointed by stack_bottom_p to reflect on changed stack
+ * Also modifies value pointed by stack_top_p to reflect on changed stack
  * bottom address.  The stack layout will be as follows:
  *
- *  ----------- stack segment high address
+ *  +----------+ stack segment high address
  *  | envp[m-1]|
  *  |   ...    |  each of envp[i] is a null-terminated string
  *  | envp[1]  |
@@ -147,15 +147,16 @@ static int store_strings(ustack_t *us, char **strv, char **stack_strv,
  *  |    \/    |
  *  |          |
  *  |    ...   |
- *  ----------- stack segment low address
+ *  +----------+ stack segment low address
+ *
  * Here argc is n and both argv[n] and argc[m] store a NULL-pointer.
  * (see System V ABI MIPS RISC Processor Supplement, 3rd edition, p. 30)
  *
- * After this function runs, the value pointed by stack_bottom_p will be the
+ * After this function runs, the value pointed by stack_top_p will be the
  * address where argc is stored, which is also the bottom of the now empty
  * program stack, so that it can naturally grow downwards.
  */
-static int user_entry_setup(const exec_args_t *args, vaddr_t *stack_top_p) {
+static int exec_args_copyout(const exec_args_t *args, vaddr_t *stack_top_p) {
   ustack_t us;
   char **argv, **envp;
   int error;
@@ -200,29 +201,40 @@ fail:
   return error;
 }
 
-int do_exec(const exec_args_t *args) {
-  thread_t *td = thread_self();
+static int open_executable(const exec_args_t *args, vnode_t **vn_p,
+                           vattr_t *attr) {
+  vnode_t *vn = *vn_p;
+  int error;
 
-  assert(td->td_proc != NULL);
+  klog("Loading program: %s", args->prog_name);
 
-  klog("Loading user ELF: %s", args->prog_name);
-
-  vnode_t *elf_vnode;
-  int error = vfs_lookup(args->prog_name, &elf_vnode);
-  if (error == -ENOENT) {
-    klog("Exec failed: No such file '%s'", args->prog_name);
-    return -ENOENT;
-  } else if (error < 0) {
-    klog("Exec failed: Failed to access file '%s'", args->prog_name);
+  /* Translate program name to vnode. */
+  if ((error = vfs_lookup(args->prog_name, &vn)))
     return error;
-  }
 
-  vattr_t elf_attr;
-  error = VOP_GETATTR(elf_vnode, &elf_attr);
-  if (error)
+  if ((error = VOP_GETATTR(vn, attr)))
     return error;
-  size_t elf_size = elf_attr.va_size;
 
+  /* It must be a regular executable file with non-zero size. */
+  if (vn->v_type != V_REG)
+    return -EACCES;
+  if (attr->va_size == 0)
+    return -ENOEXEC;
+  if ((error = VOP_ACCESS(vn, VEXEC)))
+    return error;
+
+  /* TODO Some checks are missing:
+   * 1) Check if NOEXEC bit is set on the filesystem this file resides on.
+   * 2) If file is opened for write then return ETXTBSY */
+
+  *vn_p = vn;
+  return 0;
+}
+
+static int check_elf(vnode_t *vn, vattr_t *attr, Elf32_Ehdr *eh) {
+  int error;
+
+  size_t elf_size = attr->va_size;
   klog("User ELF size: %u", elf_size);
 
   if (elf_size < sizeof(Elf32_Ehdr)) {
@@ -230,13 +242,9 @@ int do_exec(const exec_args_t *args) {
     return -ENOEXEC;
   }
 
-  Elf32_Ehdr eh;
-  uio_t uio;
-
-  uio = UIO_SINGLE_KERNEL(UIO_READ, 0, &eh, sizeof(Elf32_Ehdr));
-  error = VOP_READ(elf_vnode, &uio);
-  if (error < 0) {
-    klog("Exec failed: Elf file reading failed.");
+  uio_t uio = UIO_SINGLE_KERNEL(UIO_READ, 0, eh, sizeof(Elf32_Ehdr));
+  if ((error = VOP_READ(vn, &uio)) < 0) {
+    klog("Exec failed: Reading ELF header failed.");
     return error;
   }
   assert(uio.uio_resid == 0);
@@ -244,164 +252,178 @@ int do_exec(const exec_args_t *args) {
   /* Start by determining the validity of the elf file. */
 
   /* First, check for the magic header. */
-  if (eh.e_ident[EI_MAG0] != ELFMAG0 || eh.e_ident[EI_MAG1] != ELFMAG1 ||
-      eh.e_ident[EI_MAG2] != ELFMAG2 || eh.e_ident[EI_MAG3] != ELFMAG3) {
+  if (eh->e_ident[EI_MAG0] != ELFMAG0 || eh->e_ident[EI_MAG1] != ELFMAG1 ||
+      eh->e_ident[EI_MAG2] != ELFMAG2 || eh->e_ident[EI_MAG3] != ELFMAG3) {
     klog("Exec failed: Incorrect ELF magic number");
     return -ENOEXEC;
   }
   /* Check ELF class */
-  if (eh.e_ident[EI_CLASS] != ELFCLASS32) {
+  if (eh->e_ident[EI_CLASS] != ELFCLASS32) {
     klog("Exec failed: Unsupported ELF class (!= ELF32)");
     return -EINVAL;
   }
   /* Check data format endianess */
-  if (eh.e_ident[EI_DATA] != ELFDATA2LSB) {
+  if (eh->e_ident[EI_DATA] != ELFDATA2LSB) {
     klog("Exec failed: ELF file is not low-endian");
     return -EINVAL;
   }
   /* Ignore version and os abi field */
   /* Check file type */
-  if (eh.e_type != ET_EXEC) {
+  if (eh->e_type != ET_EXEC) {
     klog("Exec failed: ELF is not an executable");
     return -EINVAL;
   }
   /* Check machine architecture field */
-  if (eh.e_machine != EM_MIPS) {
-    klog("[exec] Exec failed: ELF target architecture is not MIPS");
+  if (eh->e_machine != EM_MIPS) {
+    klog("Exec failed: ELF target architecture is not MIPS");
     return -EINVAL;
   }
 
   /* Take note of the entry point */
-  klog("Entry point will be at 0x%08x.", (unsigned int)eh.e_entry);
+  klog("Entry point will be at 0x%08x.", (unsigned int)eh->e_entry);
 
   /* Ensure minimal prog header size */
-  if (eh.e_phentsize < sizeof(Elf32_Phdr)) {
+  if (eh->e_phentsize < sizeof(Elf32_Phdr)) {
     klog("Exec failed: ELF uses too small program headers");
     return -ENOEXEC;
   }
 
-  /* We assume process may only have a single thread. But if there were more
-     than one thread in the process that called exec, all other threads must be
-     forcefully terminated. */
-  proc_t *p = td->td_proc;
+  return 0;
+}
 
-  /*
-   * We can not destroy the current vm map, because exec can still fail,
-   * and in that case we must be able to return to the original address space.
-   */
-  struct {
-    vm_map_t *uspace;
-    vm_segment_t *sbrk;
-    vaddr_t sbrk_end;
-  } old = {p->p_uspace, p->p_sbrk, p->p_sbrk_end};
+static int load_elf_segment(proc_t *p, vnode_t *vn, Elf32_Phdr *ph) {
+  int error;
 
-  /* We are the only live thread in this process. We can safely give it a new
-   * uspace. */
-  vm_map_t *vmap = vm_map_new();
+  /* Avoid creating empty vm_map entries for segments that occupy no space in
+   * memory, as they might overlap with subsequent segments. */
+  if (ph->p_memsz == 0)
+    return 0;
 
-  p->p_uspace = vmap;
-  p->p_sbrk = NULL;
-  p->p_sbrk_end = 0;
-  sbrk_attach(p); /* Attach fresh brk segment. */
+  klog("PT_LOAD: VAddr %08x Offset %08x FileSz %08x MemSz %08x Flags %d",
+       (void *)ph->p_vaddr, (unsigned)ph->p_offset, (unsigned)ph->p_filesz,
+       (unsigned)ph->p_memsz, (unsigned)ph->p_flags);
 
-  vm_map_activate(vmap);
+  if (!is_aligned(ph->p_vaddr, PAGESIZE)) {
+    klog("Exec failed: Segment virtual address is not page aligned!");
+    return -ENOEXEC;
+  }
 
-  /* Iterate over prog headers */
-  klog("ELF has %d program headers", eh.e_phnum);
+  vaddr_t start = ph->p_vaddr;
+  vaddr_t end = roundup(ph->p_vaddr + ph->p_memsz, PAGESIZE);
 
-  assert(eh.e_phoff < 64);
-  assert(eh.e_phentsize < 128);
-  /* We allocate this buffer on stack. It's confirmed to be small. A statically
-     allocated buffer won't do, because it would need restrictive
-     synchronization. Using a malloc pool would be probably an overkill for this
-     case. */
-  size_t phs_size = eh.e_phoff * eh.e_phentsize;
-  char phs[phs_size];
+  /* Temporarily permissive protection. */
+  vm_object_t *obj = vm_object_alloc(VM_ANONYMOUS);
+  vm_segment_t *seg = vm_segment_alloc(
+    obj, start, end, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXEC);
+  error = vm_map_insert(p->p_uspace, seg, VM_FIXED);
+  /* TODO: What if segments overlap? */
+  assert(error == 0);
 
-  /* Read program headers. */
-  uio = UIO_SINGLE_KERNEL(UIO_READ, eh.e_phoff, &phs, phs_size);
-  error = VOP_READ(elf_vnode, &uio);
-  if (error < 0) {
-    klog("Exec failed: Elf file reading failed.");
-    return error;
+  /* Read data from file into the segment */
+  if (ph->p_filesz > 0) {
+    /* TODO: This is a lot of copying! Ideally we would look up the
+     * vm_object associated with the elf vnode, create a shadow vm_object
+     * on top of it using correct size/offset, and we would use it to page
+     * the file contents on demand. But we don't have a vnode_pager yet.
+     */
+    uio_t uio =
+      UIO_SINGLE_KERNEL(UIO_READ, ph->p_offset, (char *)start, ph->p_filesz);
+    if ((error = VOP_READ(vn, &uio)) < 0) {
+      klog("Exec failed: Reading ELF segment failed.");
+      return error;
+    }
+    assert(uio.uio_resid == 0);
+  }
+
+  /* Zero the rest */
+  if (ph->p_filesz < ph->p_memsz)
+    bzero((uint8_t *)start + ph->p_filesz, ph->p_memsz - ph->p_filesz);
+  /* Apply correct permissions */
+  vm_prot_t prot = VM_PROT_NONE;
+  if (ph->p_flags | PF_R)
+    prot |= VM_PROT_READ;
+  if (ph->p_flags | PF_W)
+    prot |= VM_PROT_WRITE;
+  if (ph->p_flags | PF_X)
+    prot |= VM_PROT_EXEC;
+  /* Note: vm_map_protect is not yet implemented, so
+   * this will have no effect as of now */
+  vm_map_protect(p->p_uspace, start, end, prot);
+  return 0;
+}
+
+static int load_elf(proc_t *p, vnode_t *vn, Elf32_Ehdr *eh) {
+  int error;
+
+  assert(eh->e_phoff < 64);
+  assert(eh->e_phentsize < 128);
+
+  /* Read in program headers. */
+  size_t phs_size = eh->e_phoff * eh->e_phentsize;
+  char *phs = kmalloc(M_TEMP, phs_size, 0);
+  uio_t uio = UIO_SINGLE_KERNEL(UIO_READ, eh->e_phoff, phs, phs_size);
+  if ((error = VOP_READ(vn, &uio)) < 0) {
+    klog("Exec failed: Reading program headers failed.");
+    goto fail;
   }
   assert(uio.uio_resid == 0);
 
-  for (uint8_t i = 0; i < eh.e_phnum; i++) {
-    const Elf32_Phdr *ph = (Elf32_Phdr *)(phs + i * eh.e_phentsize);
+  /* Iterate over program headers */
+  klog("ELF has %d program headers", eh->e_phnum);
+  for (int i = 0; i < eh->e_phnum; i++) {
+    Elf32_Phdr *ph = (Elf32_Phdr *)(phs + i * eh->e_phentsize);
     switch (ph->p_type) {
-      case PT_NULL:
-      case PT_NOTE:
-      case PT_PHDR:
-      default:
-        /* Ignore the section. */
+      case PT_LOAD:
+        if ((error = load_elf_segment(p, vn, ph)))
+          goto fail;
         break;
       case PT_DYNAMIC:
       case PT_INTERP:
         klog("Exec failed: ELF file requests dynamic linking"
              "by providing a PT_DYNAMIC and/or PT_INTERP segment.");
-        goto exec_fail;
+        error = -ENOEXEC;
+        goto fail;
       case PT_SHLIB:
         klog("Exec failed: ELF file contains a PT_SHLIB segment");
-        goto exec_fail;
-      case PT_LOAD:
-        klog("PT_LOAD: VAddr %08x Offset %08x FileSz %08x MemSz %08x Flags %d",
-             (void *)ph->p_vaddr, (unsigned)ph->p_offset,
-             (unsigned)ph->p_filesz, (unsigned)ph->p_memsz,
-             (unsigned)ph->p_flags);
-        if (ph->p_vaddr % PAGESIZE) {
-          klog("Exec failed: Segment p_vaddr is not page aligned!");
-          goto exec_fail;
-        }
-        if (ph->p_memsz == 0) {
-          /* Avoid creating empty vm_map entries for segments that
-             occupy no space in memory, as they might overlap with
-             subsequent segments. */
-          continue;
-        }
-        vaddr_t start = ph->p_vaddr;
-        vaddr_t end = roundup(ph->p_vaddr + ph->p_memsz, PAGESIZE);
-        /* Temporarily permissive protection. */
-        vm_object_t *obj = vm_object_alloc(VM_ANONYMOUS);
-        vm_segment_t *seg = vm_segment_alloc(
-          obj, start, end, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXEC);
-        error = vm_map_insert(vmap, seg, VM_FIXED);
-        /* TODO: What if segments overlap? */
-        assert(error == 0);
-
-        /* Read data from file into the segment */
-        if (ph->p_filesz > 0) {
-          /* TODO: This is a lot of copying! Ideally we would look up the
-           * vm_object associated with the elf vnode, create a shadow vm_object
-           * on top of it using correct size/offset, and we would use it to page
-           * the file contents on demand. But we don't have a vnode_pager yet.
-           */
-          uio = UIO_SINGLE_KERNEL(UIO_READ, ph->p_offset, (char *)start,
-                                  ph->p_filesz);
-          error = VOP_READ(elf_vnode, &uio);
-          if (error < 0) {
-            klog("Exec failed: Elf file reading failed.");
-            goto exec_fail;
-          }
-          assert(uio.uio_resid == 0);
-        }
-
-        /* Zero the rest */
-        if (ph->p_filesz < ph->p_memsz)
-          bzero((uint8_t *)start + ph->p_filesz, ph->p_memsz - ph->p_filesz);
-        /* Apply correct permissions */
-        vm_prot_t prot = VM_PROT_NONE;
-        if (ph->p_flags | PF_R)
-          prot |= VM_PROT_READ;
-        if (ph->p_flags | PF_W)
-          prot |= VM_PROT_WRITE;
-        if (ph->p_flags | PF_X)
-          prot |= VM_PROT_EXEC;
-        /* Note: vm_map_protect is not yet implemented, so
-         * this will have no effect as of now */
-        vm_map_protect(vmap, start, end, prot);
+        error = -ENOEXEC;
+        goto fail;
+      /* Ignore following sections. */
+      case PT_NULL:
+      case PT_NOTE:
+      case PT_PHDR:
+      default:
+        break;
     }
   }
+
+fail:
+  kfree(M_TEMP, phs);
+  return error;
+}
+
+typedef struct exec_vmspace {
+  vm_map_t *uspace;
+  vm_segment_t *sbrk;
+  vaddr_t sbrk_end;
+} exec_vmspace_t;
+
+/* stack size is 8 MiB */
+#define MIPS_STACK_TOP 0x7f800000
+#define MIPS_STACK_LIMIT 0x7f000000
+
+static void enter_new_vmspace(proc_t *p, exec_vmspace_t *saved) {
+  saved->uspace = p->p_uspace;
+  saved->sbrk = p->p_sbrk;
+  saved->sbrk_end = p->p_sbrk_end;
+
+  /* We are the only live thread in this process.
+   * We can safely give it a new uspace. */
+  p->p_uspace = vm_map_new();
+
+  /* Attach fresh brk segment. */
+  p->p_sbrk = NULL;
+  p->p_sbrk_end = 0;
+  sbrk_attach(p);
 
   /* Create a stack segment. As for now, the stack size is fixed and
    * will not grow on-demand. Also, the stack info should be saved
@@ -411,42 +433,78 @@ int do_exec(const exec_args_t *args) {
    * a bit lower so that it is easier to spot invalid memory access
    * when the stack underflows.
    */
-  vaddr_t stack_top = 0x7f800000;
-  vaddr_t stack_limit = 0x7f000000; /* stack size is 8 MiB */
-
   vm_object_t *stack_obj = vm_object_alloc(VM_ANONYMOUS);
-  vm_segment_t *stack_seg = vm_segment_alloc(stack_obj, stack_limit, stack_top,
-                                             VM_PROT_READ | VM_PROT_WRITE);
-  error = vm_map_insert(vmap, stack_seg, VM_FIXED);
-  /* TODO: What if this area overlaps with a loaded segment? */
+  vm_segment_t *stack_seg = vm_segment_alloc(
+    stack_obj, MIPS_STACK_LIMIT, MIPS_STACK_TOP, VM_PROT_READ | VM_PROT_WRITE);
+  int error = vm_map_insert(p->p_uspace, stack_seg, VM_FIXED);
   assert(error == 0);
 
+  klog("Stack real top at %p", (void *)MIPS_STACK_TOP);
+
+  vm_map_activate(p->p_uspace);
+}
+
+/* Return to the previous map, unmodified by exec. */
+static void restore_vmspace(proc_t *p, exec_vmspace_t *saved) {
+  p->p_uspace = saved->uspace;
+  p->p_sbrk = saved->sbrk;
+  p->p_sbrk_end = saved->sbrk_end;
+  vm_map_activate(p->p_uspace);
+}
+
+/* Destroy the vm_map we began preparing. */
+static void destroy_vmspace(exec_vmspace_t *saved) {
+  vm_map_delete(saved->uspace);
+}
+
+/* XXX We assume process may only have a single thread. But if there were more
+ * than one thread in the process that called exec, all other threads must be
+ * forcefully terminated. */
+int do_exec(const exec_args_t *args) {
+  thread_t *td = thread_self();
+  proc_t *p = td->td_proc;
+  vnode_t *vn;
+  vattr_t attr;
+  int error;
+
+  assert(p != NULL);
+
+  if ((error = open_executable(args, &vn, &attr)))
+    return error;
+
+  Elf32_Ehdr eh;
+  if ((error = check_elf(vn, &attr, &eh)))
+    return error;
+
+  /* We can not destroy the current vm map, because exec can still fail.
+   * Is such case we must be able to return to the original address space. */
+  exec_vmspace_t saved;
+  enter_new_vmspace(p, &saved);
+
+  if ((error = load_elf(p, vn, &eh)))
+    goto fail;
+
   /* Prepare program stack, which includes storing program args. */
-  klog("Stack real top at %p", (void *)stack_top);
-  user_entry_setup(args, &stack_top);
+  vaddr_t stack_top = MIPS_STACK_TOP;
+  if ((error = exec_args_copyout(args, &stack_top)))
+    goto fail;
 
   /* Set up user context. */
   exc_frame_init(td->td_uframe, (void *)eh.e_entry, (void *)stack_top, EF_USER);
 
   /* At this point we are certain that exec succeeds.  We can safely destroy the
-   * previous vm map, and permanently assign this one to the current process. */
-  vm_map_delete(old.uspace);
+   * previous vm_map, and permanently assign this one to the current process. */
+  destroy_vmspace(&saved);
 
-  vm_map_dump(vmap);
+  vm_map_dump(p->p_uspace);
 
-  klog("Enter userspace with: pc=%p, sp=%p", eh.e_entry, stack_bottom);
+  klog("Enter userspace with: pc=%p, sp=%p", eh.e_entry, stack_top);
   return -EJUSTRETURN;
 
-exec_fail:
-  /* Return to the previous map, unmodified by exec. */
-  p->p_uspace = old.uspace;
-  p->p_sbrk = old.sbrk;
-  p->p_sbrk_end = old.sbrk_end;
-  vm_map_activate(old.uspace);
-  /* Destroy the vm map we began preparing. */
-  vm_map_delete(vmap);
-
-  return -EINVAL;
+fail:
+  restore_vmspace(p, &saved);
+  destroy_vmspace(&saved);
+  return error;
 }
 
 noreturn void run_program(const exec_args_t *prog) {
