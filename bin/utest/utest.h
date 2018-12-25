dc82c25a4838efd2c8b5255bb230a61c55ffeae6
Adam Chudas <achudas@mimiker.ii.uni.wroc.pl>
2018-12-25 21:30:29 +0100

    unit tests

--- bin/utest/utest.h
+++ bin/utest/utest.h
@@ -30,6 +30,11 @@ int test_signal_segfault(void);
 int test_fork_wait(void);
 int test_fork_signal(void);
 int test_fork_sigchld_ignored(void);
+ 
+int test_pgid_args(void);
+int test_pgid_misbehave(void);
+int test_pgid_inheritance(void);
+int test_pgid_change_by_parent(void);
 
 int test_lseek_basic(void);
 int test_lseek_errors(void);
