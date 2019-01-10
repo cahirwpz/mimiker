3c7b5d9cd09fe438669950267363e951206af801
Krystian Bacławski <krystian.baclawski@gmail.com>
2019-01-10 18:55:23 +0100

    Implement extra functionalities for Lua to enable #513 (#520)
    * Implement nice error handling for Lua wrappers in Unix module.
    * Minimalistic getopt implementation in Lua.
    * Make actual use of getopt in ls_rec code.

--- bin/ls_rec/ls_rec.lua
+++ bin/ls_rec/ls_rec.lua
@@ -8,19 +8,37 @@ filetype = {
   [unix.DT_SOCK] = "sock"
 }
 
-function list_recursive(dir_path)
-  fd = unix.open(dir_path, 0)
-  dirents = unix.getdirentries(fd)
-  unix.close(fd)
+function list(opts, dir_path)
+  success, maybe_fd = pcall(unix.open, dir_path, 0)
+  if not success then print(maybe_fd.msg .. ": " .. dir_path); return end
+  dirents = unix.getdirentries(maybe_fd)
+  unix.close(maybe_fd)
   for _, dirent in ipairs(dirents) do
     d_fileno, d_type, d_name = dirent.d_fileno, dirent.d_type, dirent.d_name
     sep = dir_path:sub(-1) == "/" and "" or "/"
     path = dir_path .. sep .. d_name
-    printf("[%4s, ino=%d] %s\n", filetype[d_type] or "???", d_fileno, path)
+    printf("[%4s, ino=%d] %s%s\n", filetype[d_type] or "???", d_fileno, path,
+           (opts.p and d_type == unix.DT_DIR) and "/" or "")
     if d_type == unix.DT_DIR and d_name ~= "." and d_name ~= ".." then
-      list_recursive(path)
+      if opts.R then list(opts, path) end
     end
   end
 end
 
-list_recursive(arg[1] or "/")
+usage = [[
+  -p  Write a '/' after each filename that is a directory.
+  -R  Recursively list directories.
+]]
+
+if not pcall(getopt, arg, "Rp") then
+  print(arg[0] .. ": " .. opts)
+  print("Usage: " .. arg[0] .. " [-Rp] [file1 file2 ...]\n")
+  print(usage)
+  os.exit(false)
+end
+
+if #arg == 0 then arg[1] = "/" end
+
+for i = 1, #arg do
+  list(arg, arg[i])
+end
--- bin/lua/init.lua
+++ bin/lua/init.lua
@@ -1,3 +1,39 @@
 function printf(s, ...)
   return io.write(s:format(...))
 end
+
+--[[
+getopt(arg, "a:vpb:h")
+for k, v in pairs(arg) do
+  print("arg[" .. tostring(k) .. "] -> " .. tostring(v))
+end
+--]]
+function getopt(arg, optstr)
+  -- parse options
+  li = 1
+  while li <= #arg do
+    if arg[li] == "--" then li = li + 1; break end
+    if not arg[li]:match("^%-.") then break end
+    for oi = 2, #arg[li] do
+      opt = arg[li]:sub(oi, oi)
+      optdsc = optstr:match(opt .. ":?")
+      if not optdsc then error("unknown option -- " .. opt, 0) end
+      if #optdsc == 1 then
+        arg[opt] = true
+      else
+        li = li + 1
+        if li > #arg then error("option requires an argument -- " .. opt, 0) end
+        arg[opt] = arg[li]
+      end
+    end
+    li = li + 1
+  end
+
+  -- move trailing arguments to the beginning of the table
+  j = 1
+  for i = li, #arg do
+    arg[j], arg[i] = arg[i], nil
+    j = j + 1
+  end
+  for i = j, #arg do arg[i] = nil end
+end
