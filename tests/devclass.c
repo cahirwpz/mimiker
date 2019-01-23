23067bbf8a4f7b200a8987864f286d6e4a7c9b73
Mateusz Urbańczyk <mu@qed.ai>
2019-01-23 12:44:34 +0100

    Correct formatting

--- tests/devclass.c
+++ tests/devclass.c
@@ -2,15 +2,15 @@
 #include <devclass.h>
 
 static int test_devclass_find(void) {
-    devclass_t *dc;
+  devclass_t *dc;
 
-    dc = devclass_find("pci");
-    assert(strcmp(dc->name, "pci") == 0);
+  dc = devclass_find("pci");
+  assert(strcmp(dc->name, "pci") == 0);
 
-    dc = devclass_find("this_devclass_surely_doesnt_exist");
-    assert(dc == NULL);
+  dc = devclass_find("this_devclass_surely_doesnt_exist");
+  assert(dc == NULL);
 
-    return KTEST_SUCCESS;
+  return KTEST_SUCCESS;
 }
 
 KTEST_ADD(devclass_find, test_devclass_find, 0);
