0c36d5bca1d22ca43b170d5338cf72b1d194a5f6
Mateusz Urbańczyk <mu@qed.ai>
2019-01-25 16:36:48 +0100

    Provide test DTS file

--- tests/fdt.c
+++ tests/fdt.c
@@ -5,22 +5,20 @@
 
 #define PATHBUF_LEN 100
 
-extern uint8_t __malta_dtb_start[];
-const void *fdt = (const void *)__malta_dtb_start;
+extern uint8_t __test_dtb_start[];
+const void *fdt = (const void *)__test_dtb_start;
 
 static int test_fdt_path_offset(void) {
-  // TODO: This test depends on actual dts file, which may change in future
-  // and is platform-dependent
-  assert(fdt_path_offset(fdt, "/pci@0/isa@1000") != -1);
+  assert(fdt_path_offset(fdt, "/pci@0/isa@0") != -1);
+  assert(fdt_path_offset(fdt, "/pci@0/isa@0/rtc@0") != -1);
   assert(fdt_path_offset(fdt, "/pci/isa") != -1);
+  assert(fdt_path_offset(fdt, "/pci/isa/rtc@0") != -1);
   assert(fdt_path_offset(fdt, "/non/existing/path") == -1);
   return KTEST_SUCCESS;
 }
 
 static int test_fdt_get_path(void) {
-  // TODO: This test depends on actual dts file, which may change in future
-  // and is platform-dependent
-  const char *expected_path = "/pci@0/isa@1000";
+  const char *expected_path = "/pci@0/isa@/rtc@0";
   char actual_path[PATHBUF_LEN];
 
   int offset = fdt_path_offset(fdt, expected_path);
@@ -34,11 +32,12 @@ static int test_fdt_getprop(void) {
   int proplen;
   int nodeoffset;
   const fdt_property_t *prop_ptr;
-  const char *node_path = "/pci@0/isa@1000/atkbdc@60";
+  const char *node_path = "/pci@0/isa@0/uart@0";
 
   nodeoffset = fdt_path_offset(fdt, node_path);
-  prop_ptr = fdt_getprop(fdt, nodeoffset, "compatible", &proplen);
-  assert(!strcmp(prop_ptr->data, "atkbdc"));
+  prop_ptr = fdt_getprop(fdt, nodeoffset, "status", &proplen);
+  klog("prop: %s", (char*)prop_ptr->data);
+  assert(!strcmp(prop_ptr->data, "ns16550"));
 
   nodeoffset = fdt_path_offset(fdt, node_path);
   prop_ptr = fdt_getprop(fdt, nodeoffset, "non_existing_prop", &proplen);
@@ -46,14 +45,6 @@ static int test_fdt_getprop(void) {
 
   return KTEST_SUCCESS;
 }
-
-/*
-static int test_for_each_subnode(void) {
-    // TODO: do we want this kind of test?
-    // FDT_FOR_EACH_SUBNODE(child_nodeoffset, fdt, bus_nodeoffset) {
-    // }
-}
-*/
 
 KTEST_ADD(fdt_path_offset, test_fdt_path_offset, 0);
 KTEST_ADD(fdt_get_path, test_fdt_get_path, 0);
