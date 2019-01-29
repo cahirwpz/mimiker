b749e07fb1bf909c8572757f1e8faee2d3d2bbbb
Mateusz Urbańczyk <mu@qed.ai>
2019-01-29 17:37:25 +0100

    Test get on non existing attribute

--- tests/device.c
+++ tests/device.c
@@ -13,6 +13,15 @@ static int test_operate_on_devprops_attrs(void) {
   return KTEST_SUCCESS;
 }
 
+static int test_get_non_existing_deprop_attr_res_null(void) {
+  devprop_attr_t *attr;
+  devprop_res_key_t key = FDT_PATH;
+  attr = get_device_prop_attr(gt_pci, key);
+
+  assert(attr == NULL);
+  return KTEST_SUCCESS;
+}
+
 static int test_override_exisiting_devprop_attr(void) {
   devprop_attr_t *attr;
   devprop_res_key_t key = FDT_PATH;
@@ -25,5 +34,7 @@ static int test_override_exisiting_devprop_attr(void) {
 }
 
 KTEST_ADD(operate_on_devprops_attrs, test_operate_on_devprops_attrs, 0);
+KTEST_ADD(get_non_existing_deprop_attr_res_null,
+          test_get_non_existing_deprop_attr_res_null, 0);
 KTEST_ADD(override_exisiting_devprop_attr, test_override_exisiting_devprop_attr,
           0);
