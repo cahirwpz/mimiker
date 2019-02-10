520585107424e56cc6778d2f9ed0f9956fc9b0c7
Adam Chudas <achudas@mimiker.ii.uni.wroc.pl>
2019-02-10 22:53:13 +0100

    formatting for additional macros

--- sys/callout.c
+++ sys/callout.c
@@ -154,7 +154,7 @@ void callout_process(systime_t time) {
     callout_t *elem, *next;
 
     /* Detach triggered callouts from ci_list queue. */
-    TAILQ_FOREACH_SAFE(elem, head, c_link, next) {
+    TAILQ_FOREACH_SAFE (elem, head, c_link, next) {
       if (elem->c_time <= time) {
         callout_set_active(elem);
         callout_clear_pending(elem);
