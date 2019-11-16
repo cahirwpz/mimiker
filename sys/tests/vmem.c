97e3c4824ff846617a86a081d8a2d6baadc078c3
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2019-11-17 00:28:12 +0100

    Add much better vmem test

--- sys/tests/vmem.c
+++ sys/tests/vmem.c
@@ -1,24 +1,59 @@
 #include <sys/mimiker.h>
 #include <sys/ktest.h>
 #include <sys/vmem.h>
+#include <sys/errno.h>
+
+typedef struct span {
+  vmem_addr_t addr;
+  vmem_size_t size;
+} span_t;
+
+static void assert_addr_is_in_span(vmem_addr_t addr, vmem_size_t size,
+                                   span_t *span) {
+  assert(addr >= span->addr && addr + size <= span->addr + span->size);
+}
 
 static int test_vmem(void) {
-  vmem_t *vm = vmem_create("test vmem", 0, 0, 4096, VMEM_NOSLEEP);
+  int quantum = 1 << 12;
+  vmem_t *vm = vmem_create("test vmem", 0, 0, quantum, VMEM_NOSLEEP);
   assert(vm != NULL);
 
   int rc;
   vmem_addr_t addr;
+  vmem_size_t size;
 
-  rc = vmem_add(vm, 0, 50, VMEM_NOSLEEP);
+  /* add span #1 */
+  span_t span1 = {.addr = 2 * quantum, .size = 8 * quantum};
+  rc = vmem_add(vm, span1.addr, span1.size, VMEM_NOSLEEP);
   assert(rc == 0);
-  rc = vmem_add(vm, 100, 200, VMEM_NOSLEEP);
+
+  /* add span #2 */
+  span_t span2 = {.addr = 100 * quantum, .size = 20 * quantum};
+  rc = vmem_add(vm, span2.addr, span2.size, VMEM_NOSLEEP);
   assert(rc == 0);
 
-  rc = vmem_alloc(vm, 1000, VMEM_INSTANTFIT, &addr);
-  assert(rc != 0);
+  /* alloc 16 quantums, should return addr from span #2 */
+  size = 16 * quantum;
+  rc = vmem_alloc(vm, size, VMEM_INSTANTFIT, &addr);
+  assert(rc == 0);
+  assert_addr_is_in_span(addr, size, &span2);
 
-  rc = vmem_alloc(vm, 100, VMEM_INSTANTFIT, &addr);
-  assert(rc == 0 && addr != 0);
+  /* alloc 8 quantums, should return addr from span #1 */
+  size = 8 * quantum;
+  rc = vmem_alloc(vm, size, VMEM_INSTANTFIT, &addr);
+  assert(rc == 0);
+  assert_addr_is_in_span(addr, size, &span1);
+
+  /* alloc 1 quantum, should return addr from span #2 */
+  size = 1 * quantum;
+  rc = vmem_alloc(vm, size, VMEM_INSTANTFIT, &addr);
+  assert(rc == 0);
+  assert_addr_is_in_span(addr, size, &span2);
+
+  /* alloc 10 quantums, should fail */
+  size = 10 * quantum;
+  rc = vmem_alloc(vm, size, VMEM_INSTANTFIT, &addr);
+  assert(rc == ENOMEM);
 
   return KTEST_SUCCESS;
 }
