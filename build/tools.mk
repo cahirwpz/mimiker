350eac9ca0364c99547d541caeaa96e018852b33
Jakub Piecuch <j.piecuch96@gmail.com>
2019-11-18 15:18:08 +0100

    Add working Clang build option.
    For now only the kernel is built successfully, building the remaining code
    requires either making changes or disabling warnings in a couple of places.

--- build/tools.mk
+++ build/tools.mk
@@ -7,10 +7,32 @@ ifneq ($(TOOLCHAIN_FOUND), 0)
   $(error $(TARGET) toolchain not found. Please refer to README.md)
 endif
 
-CC       = $(TARGET)-gcc $(ABIFLAGS) -g
+ifeq ($(CLANG), 1)
+CLANG_FOUND = $(shell which clang > /dev/null; echo $$?)
+ifneq ($(CLANG_FOUND), 0)
+  $(error Clang not found. Please refer to README.md)
+endif
+endif
+
+
+TARGET_GCC = $(TARGET)-gcc $(GCC_ABIFLAGS) -g
+TARGET_CLANG = clang $(CLANG_ABIFLAGS) -g
+
+# Pass "CLANG=1" at command line to switch kernel compiler to Clang.
+ifeq ($(CLANG), 1)
+CC_AS = $(TARGET_CLANG)
+# The genassym script produces C code with asm statements that have
+# garbage instructions, which Clang checks using its built-in assembler
+# and refuses to compile. This option disables this check.
+ASSYM_CFLAGS += -no-integrated-as
+else
+CC_AS = $(TARGET_GCC)
+endif
+
+CC       = $(CC_AS)
+AS       = $(CC_AS)
+LD       = $(TARGET_GCC)
 AR       = $(TARGET)-ar
-AS       = $(TARGET)-gcc $(ABIFLAGS) -g
-LD       = $(TARGET)-gcc $(ABIFLAGS) -g
 NM       = $(TARGET)-nm
 GDB      = $(TARGET)-gdb
 RANLIB	 = $(TARGET)-ranlib
