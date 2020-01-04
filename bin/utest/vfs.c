ea7eff2827222158c8e7081a4d497947d5cf7466
Jakub Urbańczyk <xthaid@gmail.com>
2020-01-04 21:01:38 +0100

    Add tests for read/write for tmpfs

--- bin/utest/vfs.c
+++ bin/utest/vfs.c
@@ -2,15 +2,77 @@
 
 #include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+
+static int n;
+#define FD_OFFSET 3
+#include "utest_fd.h"
 
 #define TESTDIR "/tmp"
 
 #define assert_ok(expr) assert(expr == 0)
 #define assert_fail(expr, err) assert(expr == -1 && errno == err)
+
+/* Generate pseudo random data */
+static void gen_test_data(size_t n, uint32_t *data) {
+  uint32_t a = 1;
+  for (size_t i = 0; i < n; i++) {
+    a ^= a << 13;
+    a ^= a >> 17;
+    a ^= a << 5;
+    data[i] = a;
+  }
+}
+
+int test_vfs_rw(void) {
+  uint32_t *buff1 = malloc(4096 * sizeof(uint32_t));
+  uint32_t *buff2 = malloc(4096 * sizeof(uint32_t));
+
+  gen_test_data(4096, buff1);
+
+  assert_open_ok(0, TESTDIR "/file", 0, O_RDWR | O_CREAT);
+
+  /* Write and read aligned amount of bytes */
+  assert_write_ok(0, buff1, 1024 * sizeof(uint32_t));
+  assert_lseek_ok(0, 0, SEEK_SET);
+  assert(read(3, buff2, 5000) == 4096);
+  assert(!memcmp(buff1, buff2, 1024 * sizeof(uint32_t)));
+  assert(read(3, buff2, 5000) == 0);
+  assert_lseek_ok(0, 0, SEEK_SET);
+
+  /* Write 3000 bytes in two batches and then partially read */
+  assert_write_ok(0, buff1, 2000 * sizeof(uint32_t));
+  assert_write_ok(0, buff1 + 2000, 1000 * sizeof(uint32_t));
+  assert_lseek_ok(0, 0, SEEK_SET);
+  assert(read(3, buff2, 1500 * sizeof(uint32_t)) == 1500 * sizeof(uint32_t));
+  assert(!memcmp(buff1, buff2, 1500 * sizeof(uint32_t)));
+  assert(read(3, buff2 + 1500, 3000 * sizeof(uint32_t)) ==
+         1500 * sizeof(uint32_t));
+  assert(!memcmp(buff1, buff2, 1500 * sizeof(uint32_t)));
+  assert_lseek_ok(0, 0, SEEK_SET);
+
+  /* Write 64KB to test indirect blocks */
+  for (int i = 0; i < 4; i++)
+    assert_write_ok(0, buff1, 4096 * sizeof(uint32_t));
+  assert_lseek_ok(0, 0, SEEK_SET);
+  for (int i = 0; i < 4; i++) {
+    assert(read(3, buff2, 4096 * sizeof(uint32_t)) == 4096 * sizeof(uint32_t));
+    assert(!memcmp(buff1, buff2, 4096 * sizeof(uint32_t)));
+  }
+
+  free(buff1);
+  free(buff2);
+
+  close(3);
+  unlink(TESTDIR "/file");
+
+  return 0;
+}
 
 int test_vfs_dir(void) {
   assert_ok(mkdir(TESTDIR "/test", 0));
