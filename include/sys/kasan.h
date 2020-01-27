7592de9837b82fcbbcf7380908ad145aa02c51ed
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2020-01-27 17:58:03 +0100

    Delete kasan_shadow_map

--- include/sys/kasan.h
+++ include/sys/kasan.h
@@ -4,6 +4,5 @@
 void kasan_init(void);
 void kasan_mark(const void *addr, size_t size, size_t sz_with_redz,
                 uint8_t code);
-void kasan_shadow_map(void *addr, size_t size);
 
 #endif /* !_SYS_KASAN_H_ */
