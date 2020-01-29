e0ec9f7df53edb30013307b72428b0a9afa7e861
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2020-01-29 12:18:05 +0100

    Better flags.kern.mk

--- build/flags.kern.mk
+++ build/flags.kern.mk
@@ -5,7 +5,8 @@ CPPFLAGS += -I$(TOPDIR)/include -D_KERNEL
 LDFLAGS  += -nostdlib
 
 # KASAN
-CFLAGS += -fsanitize=kernel-address --param asan-globals=1 -fsanitize-address-use-after-scope -fasan-shadow-offset=0xF0000000
+CFLAGS_NOKASAN := $(CFLAGS)
+CFLAGS += -fsanitize=kernel-address --param asan-globals=1 --param asan-stack=1 -fsanitize-address-use-after-scope -fasan-shadow-offset=0xD8000000
 
-boot.o : CFLAGS = -std=gnu11 -Og -ggdb3 -Wall -Wextra -Wno-unused-parameter -Wstrict-prototypes -Werror -fno-builtin -nostdinc -nostdlib -ffreestanding -G 0
-kasan.o : CFLAGS = -std=gnu11 -Og -ggdb3 -Wall -Wextra -Wno-unused-parameter -Wstrict-prototypes -Werror -fno-builtin -nostdinc -nostdlib -ffreestanding -G 0
+boot.o : CFLAGS = $(CFLAGS_NOKASAN)
+kasan.o : CFLAGS = $(CFLAGS_NOKASAN)
