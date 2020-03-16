26e6610b63003a4a6548ddc1bbfb215f39604649
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2020-03-16 18:00:10 +0100

    Fix formatting

--- build/arch.mips.mk
+++ build/arch.mips.mk
@@ -6,7 +6,7 @@ ELFTYPE := elf32-littlemips
 # Set KASAN flags
 ifeq ($(KASAN), 1)
 ifeq ($(KERNEL), 1)
-  # Added to files that are sanitized
+	# Added to files that are sanitized
 	CFLAGS_KASAN = -fsanitize=kernel-address --param asan-globals=1 --param asan-stack=1 -fasan-shadow-offset=0xD8000000
 	# Added to all files
 	CFLAGS += -DKASAN
