69b53056f2db14e5cd728850578f717d6a2b085f
Jakub Piecuch <j.piecuch96@gmail.com>
2020-04-23 12:51:00 +0200

    Don't unlock the process in sig_kill.
    This only led to errors in sig_kill where we forgot to unlock it.

--- sys/kern/signal.c
+++ sys/kern/signal.c
@@ -153,7 +153,7 @@ void sig_kill(proc_t *proc, signo_t sig) {
 
   /* Zombie processes shouldn't accept any signals. */
   if (proc->p_state == PS_ZOMBIE)
-    goto out;
+    return;
 
   thread_t *td = proc->p_thread;
 
@@ -166,7 +166,7 @@ void sig_kill(proc_t *proc, signo_t sig) {
     proc->p_state = PS_NORMAL;
   } else if (handler == SIG_IGN ||
              (defact(sig) == SA_IGNORE && handler == SIG_DFL)) {
-    goto out;
+    return;
   }
 
   /* If stopping or continuing,
@@ -204,9 +204,6 @@ void sig_kill(proc_t *proc, signo_t sig) {
       }
     }
   }
-
-out:
-  proc_unlock(proc);
 }
 
 int sig_check(thread_t *td) {
