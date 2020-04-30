7bc832c02d22ee09c29473c71830ed225b61ab16
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2020-04-30 09:16:49 +0200

    Indent arch.mips.mk using spaces

--- build/arch.mips.mk
+++ build/arch.mips.mk
@@ -7,11 +7,11 @@ ELFTYPE := elf32-littlemips
 # Set KASAN flags
 ifeq ($(KASAN), 1)
 ifeq ($(KERNEL), 1)
-	# Added to files that are sanitized
-	CFLAGS_KASAN = -fsanitize=kernel-address -fasan-shadow-offset=0xD8000000 \
-								 --param asan-globals=1 \
-								 --param asan-stack=1 
-	# Added to all files
-	CFLAGS += -DKASAN
+  # Added to files that are sanitized
+  CFLAGS_KASAN = -fsanitize=kernel-address -fasan-shadow-offset=0xD8000000 \
+                 --param asan-globals=1 \
+                 --param asan-stack=1 
+  # Added to all files
+  CFLAGS += -DKASAN
 endif
 endif
