57900562bd88bff04cc8009bace0174de9a66a3a
Jakub Piecuch <j.piecuch96@gmail.com>
2020-05-01 10:14:18 +0200

    Check for PID 0 in sys_getsid.

--- sys/kern/syscalls.c
+++ sys/kern/syscalls.c
@@ -838,6 +838,9 @@ static int sys_getsid(proc_t *p, getsid_args_t *args, register_t *res) {
   pid_t sid;
   int error;
 
+  if (pid == 0)
+    pid = p->p_pid;
+
   klog("getsid(%d)", pid);
 
   if (!(error = proc_getsid(pid, &sid)))
