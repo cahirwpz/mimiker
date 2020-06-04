bc1ab794f663ea7e806172db597b657e5b30f35a
Wiktor Pilarczyk <wiktorpilar99@gmail.com>
2020-06-04 03:59:44 +0200

    Comment about using rusage instead of new krusage.

--- lib/libc/sys/wait4.c
+++ lib/libc/sys/wait4.c
@@ -2,6 +2,7 @@
 #include <sys/resource.h>
 
 pid_t wait4(pid_t pid, int *status, int options, struct rusage *rusage) {
+  /* Notice that bintime_t and timeval_t have the same size*/
   int rv = _wait4(pid, status, options, (struct krusage *)rusage);
   if (rusage) {
     bt2tv((bintime_t *)&rusage->ru_utime, &rusage->ru_utime);
