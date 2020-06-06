c09f58c496e29d047a2696d5ee915a7f5b83a89b
Franciszek Zdobylak <zdobyl.frank@gmail.com>
2020-06-06 14:20:23 +0200

    Add tests for get/setgroups

--- bin/utest/cred.c
+++ bin/utest/cred.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <assert.h>
+#include <limits.h>
 
 int test_get_set_uid(void) {
   uid_t ruid, euid, suid;
@@ -87,5 +88,66 @@ int test_get_set_gid(void) {
   getresgid(&rgid, &egid, &sgid);
   assert(rgid == 1 && egid == 3 && sgid == 3);
 
+  return 0;
+}
+
+int test_get_set_groups(void) {
+  gid_t rgrp[NGROUPS_MAX], gidset[NGROUPS_MAX] = {0, 1, 2, 3, 4, 5};
+  int r, ngroups = 6;
+  uid_t euid;
+
+  /* check if we are a root at start */
+  getresuid(NULL, &euid, NULL);
+  assert(euid == 0);
+  r = getgroups(0, NULL);
+  assert(r == 0);
+
+  /* setting too many groups */
+  r = setgroups(NGROUPS_MAX + 2, gidset);
+  assert(r == -1 && errno == EINVAL);
+
+  /* setting groups (without fail now) */
+  r = setgroups(ngroups, gidset);
+  assert(r == 0);
+  r = getgroups(NGROUPS_MAX, rgrp);
+  assert(r = ngroups);
+  for (int i = 0; i < r; ++i)
+    assert(rgrp[i] == gidset[i]);
+
+  /* first argument is too small */
+  r = getgroups(ngroups - 1, rgrp);
+  assert(r == -1 && errno == EINVAL);
+
+  /* dropping all supplementary groups */
+  r = setgroups(0, NULL);
+  assert(r == 0);
+  r = getgroups(NGROUPS_MAX, rgrp);
+  assert(r == 0);
+
+  /* setting for further tests */
+  r = setgroups(ngroups, gidset);
+  assert(r == 0);
+
+  /* dropping privileges */
+  setresuid(1, 1, 1);
+
+  /* dropping shouldn't affect supplementary groups */
+  r = getgroups(NGROUPS_MAX, rgrp);
+  assert(r = ngroups);
+  for (int i = 0; i < r; ++i)
+    assert(rgrp[i] == gidset[i]);
+
+  /* we can't change suplementary groups when we are not root user */
+  r = setgroups(ngroups, gidset);
+  assert(r == -1 && errno == EPERM);
+
+  r = getgroups(NGROUPS_MAX, rgrp);
+  assert(r = ngroups);
+  for (int i = 0; i < r; ++i)
+    assert(rgrp[i] == gidset[i]);
+
+  /* first argument is too small */
+  r = getgroups(3, rgrp);
+  assert(r == -1 && errno == EINVAL);
   return 0;
 }
--- bin/utest/utest.h
+++ bin/utest/utest.h
@@ -86,5 +86,6 @@ int test_killpg_other_group(void);
 
 int test_get_set_uid(void);
 int test_get_set_gid(void);
+int test_get_set_groups(void);
 
 #endif /* __UTEST_H__ */
