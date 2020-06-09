8ebeb45dd763f5d0344e3cbeb040393a79a79e9d
Paweł Jasiak <pawel@jasiak.xyz>
2020-06-09 07:23:36 +0200

    Map rootdev into virtual address space (#719)
    * Map rootdev and its children into virtual address space
    * Enable pci ports and memory in activate_resource
    * Create generic bus_space_map
    * Create generic_bus_space object
    * Introduce kva_alloc which allocates a virtual address range with no underlying object and no initial mapping to physical memory
    * Introduce kva_free which releases a region of kernel virtual memory allocated with kva_alloc
    * Increase static pool size for vmem's boundary tags.
    
    Co-authored-by: Krystian Bacławski <krystian.baclawski@gmail.com>

--- include/sys/kmem.h
+++ include/sys/kmem.h
@@ -8,4 +8,8 @@ void *kmem_alloc(size_t size, kmem_flags_t flags) __warn_unused;
 void *kmem_map(paddr_t pa, size_t size) __warn_unused;
 void kmem_free(void *ptr, size_t size);
 
+/* Kernel virtual address space allocator. */
+vaddr_t kva_alloc(size_t size);
+void kva_free(vaddr_t ptr, size_t size);
+
 #endif /* !_SYS_KMEM_H_ */
