0b087c22e29c02f0c941a92c3d90f1a8fbf0ec74
wiklam <wiktorpilar99@gmail.com>
2020-06-14 16:19:36 +0200

    Spell correction.

--- sys/tests/time.c
+++ sys/tests/time.c
@@ -4,45 +4,45 @@
 #include <sys/time.h>
 
 static int test_do_clock_nanosleep(void) {
-  timespec_t rmt, rqt;
+  timespec_t rqt, rmt;
   int clk_mono = CLOCK_MONOTONIC, clk_rltm = CLOCK_REALTIME;
   /* Incorrect requested time argument */
   rqt.tv_sec = -1;
   rqt.tv_nsec = 0;
-  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rmt, &rqt) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rmt, &rqt) == EINVAL);
-  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rmt, &rqt) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rmt, &rqt) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rqt, &rmt) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rqt, &rmt) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rqt, &rmt) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rqt, &rmt) == EINVAL);
   rqt.tv_sec = 0;
   rqt.tv_nsec = -1;
-  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rmt, &rqt) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rmt, &rqt) == EINVAL);
-  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rmt, &rqt) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rmt, &rqt) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rqt, &rmt) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rqt, &rmt) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rqt, &rmt) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rqt, &rmt) == EINVAL);
   rqt.tv_nsec = 1000000000;
-  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rmt, &rqt) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rmt, &rqt) == EINVAL);
-  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rmt, &rqt) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rmt, &rqt) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_ABSTIME, &rqt, &rmt) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_ABSTIME, &rqt, &rmt) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, TIMER_RELTIME, &rqt, &rmt) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, TIMER_RELTIME, &rqt, &rmt) == EINVAL);
 
   rqt.tv_nsec = 100000;
   /* Incorrect flags */
-  assert(do_clock_nanosleep(clk_mono, ~TIMER_ABSTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, ~TIMER_ABSTIME, &rmt, NULL) == EINVAL);
-  assert(do_clock_nanosleep(clk_mono, ~TIMER_ABSTIME, &rmt, &rqt) == EINVAL);
-  assert(do_clock_nanosleep(clk_rltm, ~TIMER_ABSTIME, &rmt, &rqt) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, ~TIMER_ABSTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, ~TIMER_ABSTIME, &rqt, NULL) == EINVAL);
+  assert(do_clock_nanosleep(clk_mono, ~TIMER_ABSTIME, &rqt, &rmt) == EINVAL);
+  assert(do_clock_nanosleep(clk_rltm, ~TIMER_ABSTIME, &rqt, &rmt) == EINVAL);
 
   return KTEST_SUCCESS;
 }
