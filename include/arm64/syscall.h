b3da2ef667a089d2d7ae77ea7d5e0cea909f914f
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-07-17 17:58:51 +0200

    Add missing assembly files.

--- include/arm64/syscall.h
+++ include/arm64/syscall.h
@@ -3,14 +3,23 @@
 
 #ifdef __ASSEMBLER__
 
+#include <arm64/asm.h>
+#include <sys/errno.h>
+#include <sys/syscall.h>
 /* Do a syscall that is missing an implementation. */
-#define SYSCALL_MISSING(name)
+#define SYSCALL_MISSING(name) \
+  ENTRY(name);                                                                  \
+  b _C_LABEL(__sc_missing);                                                    \
+  END(name)
 
 /* Do a syscall that cannot fail. */
 #define SYSCALL_NOERROR(name, num)
 
 /* Do a normal syscall. */
-#define SYSCALL(name, num)
+#define SYSCALL(name, num) \
+  ENTRY(name);                                                                  \
+  b _C_LABEL(__sc_missing);                                                    \
+  END(name)
 
 #endif /* !__ASSEMBLER__ */
 
