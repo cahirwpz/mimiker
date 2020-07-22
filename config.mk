25849e04983143eb524ffce0a11e6588f6f728d6
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-07-22 15:20:00 +0200

    Use only one variable for platform / architecture choice.

--- config.mk
+++ config.mk
@@ -1,7 +1,11 @@
-# ARCH := arm64
-# PLATFORM := rpi3
-# ARCH := mips
-# PLATFORM := malta
+BOARD ?= malta
 
+ifeq ($(BOARD), malta)
 ARCH ?= mips
 PLATFORM ?= malta
+endif
+
+ifeq ($(BOARD), rpi3)
+ARCH := arm64
+PLATFORM := rpi3
+endif
