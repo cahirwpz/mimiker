e41279c6d6b320a0d40c0deae6e3a3cc2e30f448
Paweł Jasiak <pawel@jasiak.xyz>
2020-07-28 22:46:32 +0200

    Introduce get_cpu.

--- sys/aarch64/boot.c
+++ sys/aarch64/boot.c
@@ -1,6 +1,14 @@
 #include <sys/mimiker.h>
+#include <aarch64/armreg.h>
+
+/* Return active CPU number [0-3]. */
+__boot_text static long get_cpu(void) {
+  return READ_SPECIALREG(MPIDR_EL1) & 3UL;
+}
 
 __boot_text void *aarch64_init(void) {
+  long cpu = get_cpu();
+  (void)cpu;
   for (;;)
     ;
 }
