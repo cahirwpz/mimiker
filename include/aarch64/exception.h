090d94ed59fd00dd245a94d3c1337612655f58e3
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-08-14 18:22:46 +0200

    Simplify as much as possible.

--- include/aarch64/exception.h
+++ include/aarch64/exception.h
@@ -5,9 +5,12 @@
 #error "Do not use this header file outside kernel machine dependent code!"
 #endif
 
+#include <stdint.h>
+
 typedef struct exc_frame {
   uint64_t sp;
   uint64_t lr;
+  uint64_t far;
   uint64_t elr;
   uint32_t spsr;
   uint32_t esr;
