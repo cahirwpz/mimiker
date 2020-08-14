b96bb8bac3a85a1efba1355fcac619bea7755dbe
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-08-14 14:36:34 +0200

    Kernel should not use floating point registers.

--- build/arch.aarch64.mk
+++ build/arch.aarch64.mk
@@ -1,6 +1,7 @@
 TARGET := aarch64-mimiker-elf
 GCC_ABIFLAGS := -DELFSIZE=64
 CLANG_ABIFLAGS := -target aarch64-elf -DELFSIZE=64
+CFLAGS += -mcpu=cortex-a53+nofp -march=armv8-a+nofp -mgeneral-regs-only
 ELFTYPE := elf64-littleaarch64
 ELFARCH := aarch64
 
