c4cc838ab098579db9b18267e210be5ba07d1104
Paweł Jasiak <pawel@jasiak.xyz>
2020-08-28 00:16:11 +0200

    First attempt to implement pmap_kenter.

--- sys/aarch64/pmap.c
+++ sys/aarch64/pmap.c
@@ -3,10 +3,16 @@
 #include <sys/mimiker.h>
 #include <aarch64/exception.h>
 #include <aarch64/pmap.h>
+#include <aarch64/pte.h>
 #include <sys/pmap.h>
 #include <sys/vm.h>
 #include <sys/spinlock.h>
 #include <bitstring.h>
+#include <sys/vm_physmem.h>
+
+#define ADDR_MASK 0x8ffffffff000
+#define DMAP_BASE 0xffffff8000000000 /* last 512GB */
+#define PHYS_TO_DMAP(x) ((intptr_t)(x) + DMAP_BASE)
 
 /* Kernel page directory entries. */
 alignas(PAGESIZE) pde_t _kernel_pmap_pde[PD_ENTRIES];
@@ -27,6 +33,45 @@ static asid_t alloc_asid(void) {
   }
   klog("alloc_asid() = %d", free);
   return free;
+}
+
+static pde_t *pmap_l0(pmap_t *pmap, vaddr_t va) {
+  return &pmap->pde[L0_INDEX(va)];
+}
+
+static pde_t *pmap_l1(pmap_t *pmap, vaddr_t va) {
+  pde_t l1 = *pmap_l0(pmap, va) & ADDR_MASK;
+  pde_t *dmap_l1 = (pde_t *)PHYS_TO_DMAP(l1);
+  return &dmap_l1[L1_INDEX(va)];
+}
+
+static pde_t *pmap_l2(pmap_t *pmap, vaddr_t va) {
+  pde_t l2 = *pmap_l1(pmap, va) & ADDR_MASK;
+  pde_t *dmap_l2 = (pde_t *)PHYS_TO_DMAP(l2);
+  return &dmap_l2[L2_INDEX(va)];
+}
+
+static pte_t *pmap_l3(pmap_t *pmap, vaddr_t va) {
+  pte_t l3 = *pmap_l2(pmap, va) & ADDR_MASK;
+  pte_t *dmap_l3 = (pte_t *)PHYS_TO_DMAP(l3);
+  return &dmap_l3[L3_INDEX(va)];
+}
+
+static pde_t *pmap_alloc_pde(pmap_t *pmap, vaddr_t vaddr, int level) {
+  vm_page_t *pg = vm_page_alloc(1);
+  vm_page_t *dmap_pg = (vm_page_t *)PHYS_TO_DMAP(pg);
+  pde_t *pde = (pde_t *)PHYS_TO_DMAP(dmap_pg->paddr);
+
+  if (level == 3)
+    TAILQ_INSERT_TAIL(&pmap->pte_pages, dmap_pg, pageq);
+
+  klog("Page table level %d for 0x%016lx allocated at 0x%016lx", level, vaddr,
+       dmap_pg->paddr);
+
+  for (int i = 0; i < PD_ENTRIES; ++i)
+    pde[i] = 0;
+
+  return pde;
 }
 
 vaddr_t pmap_start(pmap_t *pmap) {
@@ -60,8 +105,42 @@ void pmap_delete(pmap_t *pmap) {
   panic("Not implemented!");
 }
 
+static const pte_t pte_default =
+  L3_PAGE | ATTR_AF | ATTR_SH(ATTR_SH_IS) | ATTR_IDX(ATTR_NORMAL_MEM_WB);
+
+static const pte_t vm_prot_map[] = {
+  [VM_PROT_NONE] = ATTR_XN | pte_default,
+  [VM_PROT_READ] = ATTR_AP(ATTR_AP_RO) | ATTR_XN | pte_default,
+  [VM_PROT_WRITE] = ATTR_AP(ATTR_AP_RW) | ATTR_XN | pte_default,
+  [VM_PROT_READ | VM_PROT_WRITE] = ATTR_AP(ATTR_AP_RW) | ATTR_XN | pte_default,
+  [VM_PROT_EXEC] = pte_default,
+  [VM_PROT_READ | VM_PROT_EXEC] = ATTR_AP(ATTR_AP_RO) | pte_default,
+  [VM_PROT_WRITE | VM_PROT_EXEC] = ATTR_AP(ATTR_AP_RW) | pte_default,
+  [VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXEC] =
+    ATTR_AP(ATTR_AP_RW) | pte_default,
+};
+
 void pmap_kenter(paddr_t va, paddr_t pa, vm_prot_t prot, unsigned flags) {
-  panic("Not implemented!");
+  pmap_t *pmap = pmap_kernel();
+
+  pte_t pte = vm_prot_map[prot];
+
+  WITH_MTX_LOCK (&pmap->mtx) {
+    pde_t *l0 = pmap_l0(pmap, va);
+    if (*l0 == 0)
+      *l0 = (pde_t)pmap_alloc_pde(pmap, va, 1) | L0_TABLE;
+
+    pde_t *l1 = pmap_l1(pmap, va);
+    if (*l1 == 0)
+      *l1 = (pde_t)pmap_alloc_pde(pmap, va, 2) | L1_TABLE;
+
+    pde_t *l2 = pmap_l2(pmap, va);
+    if (*l2 == 0)
+      *l2 = (pde_t)pmap_alloc_pde(pmap, va, 3) | L2_TABLE;
+
+    pte_t *l3 = pmap_l3(pmap, va);
+    *l3 = pa | pte;
+  }
 }
 
 void pmap_enter(pmap_t *pmap, vaddr_t va, vm_page_t *pg, vm_prot_t prot,
