eb3b2b342649ad555df75fe3358fab07b529bc27
Paweł Jasiak <pawel@jasiak.xyz>
2020-09-03 22:04:33 +0200

    exception frame

--- include/aarch64/exception.h
+++ include/aarch64/exception.h
@@ -10,11 +10,14 @@
 typedef struct exc_frame {
   uint64_t sp;
   uint64_t lr;
-  uint64_t far;
-  uint64_t elr;
-  uint32_t spsr;
-  uint32_t esr;
+  uint64_t far; /* fault address register */
+  uint64_t pc; /* exception link register */
+  uint32_t spsr; /* saved program status register */
+  uint32_t esr; /* exception syndrome register */
   uint64_t x[30];
 } exc_frame_t;
+
+/* TODO exception frame for userspace */
+#define cpu_exc_frame_t exc_frame_t
 
 #endif /* !_AARCH64_EXCEPTION_H_ */
--- sys/aarch64/exception.c
+++ sys/aarch64/exception.c
@@ -1,21 +1,40 @@
 #include <sys/mimiker.h>
 #include <aarch64/exception.h>
+#include <aarch64/armreg.h>
+#include <sys/libkern.h>
+#include <sys/exception.h>
 
 void exc_frame_init(exc_frame_t *frame, void *pc, void *sp, unsigned flags) {
-  panic("Not implemented!");
+  /* TODO(pj) implement usermode */
+  assert((flags & EF_USER) == 0);
+
+  bzero(frame, sizeof(exc_frame_t));
+
+  frame->pc = (register_t)pc;
+  frame->sp = (register_t)sp;
+  
+  /* We don't handle fast interrupts in kernel. */
+  frame->spsr = PSR_F | PSR_M_EL1h; 
 }
 
 void exc_frame_copy(exc_frame_t *to, exc_frame_t *from) {
-  panic("Not implemented!");
+  memcpy(to, from, sizeof(exc_frame_t));
 }
 
 void exc_frame_setup_call(exc_frame_t *frame, register_t retaddr,
                           register_t arg) {
-  panic("Not implemented!");
+  frame->lr = retaddr;
+  frame->x[0] = arg;
 }
 
 void exc_frame_set_retval(exc_frame_t *frame, register_t value,
                           register_t error) {
+  frame->x[0] = value;
+  frame->x[1] = error;
+  frame->pc += 4;
+}
+
+void kern_exc_leave(void) {
   panic("Not implemented!");
 }
 
