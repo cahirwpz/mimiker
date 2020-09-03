5558f53a3dafac4d3fc03f1768c410d07e732e26
Paweł Jasiak <pawel@jasiak.xyz>
2020-09-03 22:04:05 +0200

    ctx structure

--- include/aarch64/context.h
+++ include/aarch64/context.h
@@ -6,6 +6,9 @@
 #endif
 
 typedef struct ctx {
+  register_t x[30];
+  register_t sp, fp, lr;
+  register_t pc, spsr;
 } ctx_t;
 
 #endif /* !_AARCH64_CONTEXT_H_ */
--- sys/aarch64/context.c
+++ sys/aarch64/context.c
@@ -1,13 +1,20 @@
 #include <sys/mimiker.h>
 #include <sys/thread.h>
 #include <aarch64/context.h>
+#include <sys/libkern.h>
+#include <aarch64/armreg.h>
 
 void ctx_init(ctx_t *ctx, void *pc, void *sp) {
-  panic("Not implemented!");
+  bzero(ctx, sizeof(ctx_t));
+
+  ctx->pc = (register_t)pc;
+  ctx->sp = (register_t)sp;
+
+  ctx->spsr = PSR_F | PSR_M_EL1h;
 }
 
 void ctx_set_retval(ctx_t *ctx, long value) {
-  panic("Not implemented!");
+  ctx->x[0] = value;
 }
 
 long ctx_switch(thread_t *from, thread_t *to) {
