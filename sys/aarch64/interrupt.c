93feedc4e3cc91f1c233bb3e5e54a59399d6fc64
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-09-04 12:57:03 +0200

    Small cleanup.

--- sys/aarch64/interrupt.c
+++ sys/aarch64/interrupt.c
@@ -3,14 +3,14 @@
 #include <aarch64/armreg.h>
 
 void cpu_intr_disable(void) {
-  __asm __volatile("msr daifset, #2");
+  __asm __volatile("msr daifset, %0" ::"i"(DAIF_I));
 }
 
 void cpu_intr_enable(void) {
-  __asm __volatile("msr daifclr, #2");
+  __asm __volatile("msr daifclr, %0" ::"i"(DAIF_I));
 }
 
 bool cpu_intr_disabled(void) {
   uint32_t daif = READ_SPECIALREG(daif);
-  return (daif & 2) == 0;
+  return (daif & DAIF_I) == 0;
 }
