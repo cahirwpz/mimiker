7611a62c601ef6f7901b22f2f7e8ddc0d0ddfd4d
Paweł Jasiak <pawel@jasiak.xyz>
2020-09-04 10:25:50 +0200

    Implement cpu_intr_disable, cpu_intr_enable, cpu_intr_disabled.

--- sys/aarch64/interrupt.c
+++ sys/aarch64/interrupt.c
@@ -1,14 +1,16 @@
 #include <sys/mimiker.h>
 #include <aarch64/interrupt.h>
+#include <aarch64/armreg.h>
 
 void cpu_intr_disable(void) {
-  panic("Not implemented!");
+  __asm __volatile("msr daifset, #2");
 }
 
 void cpu_intr_enable(void) {
-  panic("Not implemented!");
+  __asm __volatile("msr daifclr, #2");
 }
 
 bool cpu_intr_disabled(void) {
-  panic("Not implemented!");
+  uint32_t daif = READ_SPECIALREG(daif);
+  return (daif & 2) == 0;
 }
