b490f82352a6530f85d2dd1ebfe8fb8d88ea5abf
Paweł Jasiak <pawel@jasiak.xyz>
2020-09-05 15:05:05 +0200

    Implement user_ctx_setup_call.

--- sys/aarch64/context.c
+++ sys/aarch64/context.c
@@ -13,7 +13,8 @@ void user_ctx_copy(user_ctx_t *to, user_ctx_t *from) {
 }
 
 void user_ctx_setup_call(user_ctx_t *ctx, register_t retaddr, register_t arg) {
-  panic("Not implemented!");
+  _REG(ctx, LR) = retaddr;
+  _REG(ctx, X0) = arg;
 }
 
 void user_ctx_set_retval(user_ctx_t *ctx, register_t value, register_t error) {
