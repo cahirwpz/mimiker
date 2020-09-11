9cc233d80c3476ff44c9da9391f8cf4ad71f67ce
Paweł Jasiak <pawel@jasiak.xyz>
2020-09-11 22:09:04 +0200

    Implement tests for pmap.
    - test_pmap_kenter for pmap_kenter and pmap_kremove
    - test_pmap_page for pmap_copy_page and pmap_zero_page
    - test_pmap_extract for pmap_extract

--- sys/tests/pmap.c
+++ sys/tests/pmap.c
@@ -4,6 +4,7 @@
 #include <sys/vm_physmem.h>
 #include <sys/ktest.h>
 #include <sys/sched.h>
+#include <sys/kmem.h>
 
 #define PAGES 16
 
@@ -124,6 +125,113 @@ static int test_rmbits(void) {
   return KTEST_SUCCESS;
 }
 
+static int test_pmap_kenter(void) {
+  SCOPED_NO_PREEMPTION();
+
+  pmap_t *orig = pmap_user();
+  pmap_t *pmap = pmap_kernel();
+
+  pmap_activate(pmap);
+
+  vm_page_t *pg = vm_page_alloc(1);
+  assert(pg);
+  vaddr_t va = kva_alloc(PAGESIZE);
+  assert(va);
+
+  pmap_kenter(va, pg->paddr, VM_PROT_READ | VM_PROT_WRITE, 0);
+
+#ifndef KASAN
+  volatile uint64_t *ptr = (uint64_t *)va;
+  *ptr = 0xDEADC0DE;
+  assert(*ptr == 0xDEADC0DE);
+#endif /* !KASAN */
+
+  pmap_kremove(va, va + PAGESIZE);
+  kva_free(va, PAGESIZE);
+  vm_page_free(pg);
+  pmap_activate(orig);
+
+  return KTEST_SUCCESS;
+}
+
+static int test_pmap_page(void) {
+  SCOPED_NO_PREEMPTION();
+
+#ifndef KASAN
+  pmap_t *orig = pmap_user();
+  pmap_t *pmap = pmap_kernel();
+
+  pmap_activate(pmap);
+
+  vm_page_t *pg1 = vm_page_alloc(1);
+  vm_page_t *pg2 = vm_page_alloc(2);
+  assert(pg1 && pg2);
+
+  volatile vaddr_t va = kva_alloc(PAGESIZE);
+  assert(va);
+
+  pmap_kenter(va, pg1->paddr, VM_PROT_READ | VM_PROT_WRITE, 0);
+
+  volatile uint8_t *buf = (uint8_t *)va;
+  for (int i = 0; i < PAGESIZE; i++)
+    buf[i] = i % 123;
+
+  pmap_copy_page(pg1, pg2);
+  pmap_zero_page(pg1);
+
+  for (int i = 0; i < PAGESIZE; i++)
+    assert(buf[i] == 0);
+
+  pmap_kremove(va, va + PAGESIZE);
+
+  pmap_kenter(va, pg2->paddr, VM_PROT_READ, 0);
+
+  for (int i = 0; i < PAGESIZE; i++)
+    assert(buf[i] == i % 123);
+
+  pmap_kremove(va, va + PAGESIZE);
+  kva_free(va, PAGESIZE);
+  vm_page_free(pg1);
+  vm_page_free(pg2);
+  pmap_activate(orig);
+
+#endif /* !KASAN */
+  return KTEST_SUCCESS;
+}
+
+static int test_pmap_extract(void) {
+  SCOPED_NO_PREEMPTION();
+
+  pmap_t *orig = pmap_user();
+  pmap_t *pmap = pmap_kernel();
+
+  pmap_activate(pmap);
+
+  vm_page_t *pg = vm_page_alloc(1);
+  assert(pg);
+
+  vaddr_t va = kva_alloc(PAGESIZE);
+  assert(va);
+
+  pmap_kenter(va, pg->paddr, VM_PROT_READ, 0);
+
+  paddr_t pa;
+  bool rc = pmap_extract(pmap, va, &pa);
+  assert(rc);
+  assert(pa == pg->paddr);
+
+  pmap_kremove(va, va + PAGESIZE);
+
+  kva_free(va, PAGESIZE);
+  vm_page_free(pg);
+  pmap_activate(orig);
+
+  return KTEST_SUCCESS;
+}
+
 KTEST_ADD(pmap_kernel, test_kernel_pmap, KTEST_FLAG_BROKEN);
 KTEST_ADD(pmap_user, test_user_pmap, 0);
 KTEST_ADD(pmap_rmbits, test_rmbits, 0);
+KTEST_ADD(pmap_kenter, test_pmap_kenter, 0);
+KTEST_ADD(pmap_page, test_pmap_page, 0);
+KTEST_ADD(pmap_extract, test_pmap_extract, 0);
