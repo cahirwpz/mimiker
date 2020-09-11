6b44d0e1a2740e079ca791f3150d7c451156e952
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-09-11 15:34:40 +0200

    Clean up context.c for mips & aarch64.

--- sys/aarch64/context.c
+++ sys/aarch64/context.c
@@ -1,38 +1,6 @@
 #include <sys/libkern.h>
-#include <sys/mimiker.h>
-#include <sys/thread.h>
 #include <aarch64/context.h>
 #include <aarch64/armreg.h>
-
-void user_ctx_init(user_ctx_t *ctx, void *pc, void *sp) {
-  bzero(ctx, sizeof(user_ctx_t));
-
-  _REG(ctx, PC) = (register_t)pc;
-  _REG(ctx, SP) = (register_t)sp;
-}
-
-void user_ctx_copy(user_ctx_t *to, user_ctx_t *from) {
-  memcpy(to, from, sizeof(user_ctx_t));
-}
-
-void user_ctx_set_retval(user_ctx_t *ctx, register_t value, register_t error) {
-  _REG(ctx, X0) = value;
-  _REG(ctx, X1) = error;
-  _REG(ctx, PC) += 4;
-}
-
-void user_exc_leave(void) {
-  panic("Not implemented!");
-}
-
-void kern_exc_leave(void) {
-  panic("Not implemented!");
-}
-
-void ctx_setup_call(ctx_t *ctx, register_t retaddr, register_t arg) {
-  _REG(ctx, LR) = retaddr;
-  _REG(ctx, X0) = arg;
-}
 
 void ctx_init(ctx_t *ctx, void *pc, void *sp) {
   bzero(ctx, sizeof(ctx_t));
@@ -43,6 +11,28 @@ void ctx_init(ctx_t *ctx, void *pc, void *sp) {
   _REG(ctx, SPSR) = PSR_F | PSR_M_EL1h;
 }
 
+void ctx_setup_call(ctx_t *ctx, register_t retaddr, register_t arg) {
+  _REG(ctx, LR) = retaddr;
+  _REG(ctx, X0) = arg;
+}
+
 void ctx_set_retval(ctx_t *ctx, long value) {
   _REG(ctx, X0) = value;
+}
+
+void user_ctx_copy(user_ctx_t *to, user_ctx_t *from) {
+  memcpy(to, from, sizeof(user_ctx_t));
+}
+
+void user_ctx_init(user_ctx_t *ctx, void *pc, void *sp) {
+  bzero(ctx, sizeof(user_ctx_t));
+
+  _REG(ctx, PC) = (register_t)pc;
+  _REG(ctx, SP) = (register_t)sp;
+}
+
+void user_ctx_set_retval(user_ctx_t *ctx, register_t value, register_t error) {
+  _REG(ctx, X0) = value;
+  _REG(ctx, X1) = error;
+  _REG(ctx, PC) += 4;
 }
