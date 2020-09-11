24216559df6faefe7e2f6384b171a8c10a0b60a0
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-09-11 15:59:19 +0200

    Use tpidr_el1 to store pointer to pcpu structure.

--- include/aarch64/pcpu.h
+++ include/aarch64/pcpu.h
@@ -4,16 +4,4 @@
 #define PCPU_MD_FIELDS                                                         \
   struct {}
 
-#ifdef _MACHDEP
-#ifdef __ASSEMBLER__
-
-/* clang-format off */
-.macro  LOAD_PCPU reg
-        ldr     \reg, _pcpu_data
-.endm
-/* clang-format on */
-
-#endif /* !__ASSEMBLER__ */
-#endif /* !_MACHDEP */
-
 #endif /* !_AARCH64_PCPU_H_ */
