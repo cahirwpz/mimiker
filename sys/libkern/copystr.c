75e5ee7fdc66a3e969530e082a30231653281487
Paweł Jasiak <pawel@jasiak.xyz>
2020-10-02 19:36:34 +0200

    Add comments.

--- sys/libkern/copystr.c
+++ sys/libkern/copystr.c
@@ -10,6 +10,15 @@
  * Copy a NIL-terminated string, at most maxlen characters long.  Return the
  * number of characters copied (including the NIL) in *done.  If the
  * string is too long, return ENAMETOOLONG; else return 0.
+ *
+ * This function must be compiled without KASAN because it is used by copyinstr
+ * which cannot failed.
+ *
+ * For the same reason, it needs to be compiled with the -fomit-frame-pointer
+ * flag in order not to use the stack in copyinstr.
+ *
+ * If you plan to change this function look at machine dependent implementation
+ * of copyerr.
  */
 int copystr(const void *kfaddr, void *kdaddr, size_t len, size_t *done) {
   const char *src = kfaddr;
