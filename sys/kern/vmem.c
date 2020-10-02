9d201a5de9e785ea9b3bc58d9874ee07dd9975f9
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-10-02 09:56:37 +0200

    Revert a change in vmem.

--- sys/kern/vmem.c
+++ sys/kern/vmem.c
@@ -75,7 +75,7 @@ typedef struct bt {
 
 static POOL_DEFINE(P_VMEM, "vmem", sizeof(vmem_t));
 static POOL_DEFINE(P_BT, "vmem boundary tag", sizeof(bt_t));
-static alignas(PAGESIZE) uint8_t P_VMEM_BOOTPAGE[PAGESIZE * 2];
+static alignas(PAGESIZE) uint8_t P_VMEM_BOOTPAGE[PAGESIZE];
 /* Note: in the future, the amount of static memory for boundary tags should
  * be reduced by more clever tag allocation technique that always keeps some
  * number of free tags. For more information, please see bt_alloc and bt_refill
@@ -207,7 +207,6 @@ static void vmem_check_sanity(vmem_t *vm) {
 
 vmem_t *vmem_create(const char *name, vmem_size_t quantum) {
   vmem_t *vm = pool_alloc(P_VMEM, M_ZERO);
-
   assert(vm != NULL);
 
   vm->vm_quantum = quantum;
