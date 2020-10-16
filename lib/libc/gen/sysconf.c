37912a9bdd94bc15a2b3aa49841c5230a3df3106
Franciszek Zdobylak <zdobyl.frank@gmail.com>
2020-10-16 13:34:34 +0200

    Add handling of all flags defined in unistd.h

--- lib/libc/gen/sysconf.c
+++ lib/libc/gen/sysconf.c
@@ -12,6 +12,7 @@ long sysconf(int name) {
       return NGROUPS_MAX;
     case _SC_OPEN_MAX:
       return OPEN_MAX;
+    case _SC_JOB_CONTROL:
     default:
       errno = EINVAL;
       return -1;
