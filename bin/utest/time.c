abfb1fa1ad09d91674bf2583be5aa0842c4c8d4e
wiklam <wiktorpilar99@gmail.com>
2020-10-18 16:55:00 +0200

    Deterministic tests for nanosleep.

--- bin/utest/time.c
+++ bin/utest/time.c
@@ -3,7 +3,6 @@
 #include <assert.h>
 #include <sys/errno.h>
 #include <sys/time.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 int test_gettimeofday(void) {
@@ -13,8 +12,10 @@ int test_gettimeofday(void) {
   for (int g = 0; g < 100; g++) {
     assert(gettimeofday(&time1, NULL) == 0);
     assert(gettimeofday(&time2, NULL) == 0);
+
     /* Time can't move backwards */
     assert(timercmp(&time2, &time1, <) == 0);
+
     /* Time belong to XXI century */
     assert(time1.tv_sec >= start_of_century);
     assert(time1.tv_sec <= end_of_century);
@@ -26,37 +27,38 @@ int test_gettimeofday(void) {
 }
 
 int test_nanosleep(void) {
-  timespec_t treq = (timespec_t){.tv_sec = -1, .tv_nsec = 1}, trem;
+  timespec_t treq, trem;
   timeval_t time1, time2, diff;
   int error;
+
   /* Incorrect arguments */
+  treq.tv_sec = -1;
+  treq.tv_nsec = 1;
   assert(nanosleep(&treq, NULL) == EINVAL);
   assert(nanosleep(&treq, &trem) == EINVAL);
+
   treq.tv_sec = 0;
   treq.tv_nsec = -1;
   assert(nanosleep(&treq, NULL) == EINVAL);
   assert(nanosleep(&treq, &trem) == EINVAL);
+
   treq.tv_nsec = 1000000000;
   assert(nanosleep(&treq, NULL) == EINVAL);
   assert(nanosleep(&treq, &trem) == EINVAL);
 
   /* Check if sleept at least requested time */;
-
-  assert(gettimeofday(&time1, NULL) == 0);
-  srand(time1.tv_usec);
-  for (int g = 0; g < 10; g++) {
-    treq.tv_nsec = (rand() % 1000000000);
-    if (treq.tv_nsec < 0) {
-      treq.tv_nsec += 1000000000;
-      treq.tv_nsec %= 1000000000;
-    }
+  
+  for (int g = 0; g < 20; g++) {  
+    treq.tv_nsec = (1000 << g);
     diff = *((timeval_t *)&treq);
     diff.tv_usec /= 1000;
+
     assert(gettimeofday(&time1, NULL) == 0);
     while ((error = nanosleep(&treq, &trem)) == EINTR)
       treq = trem;
     assert(error == 0);
     assert(gettimeofday(&time2, NULL) == 0);
+
     timeradd(&time1, &diff, &time1);
     assert(timercmp(&time1, &time2, <=));
   }
