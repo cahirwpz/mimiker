ab4881b8f25220469bdd9ce208c20d6c125cb6bb
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-10-23 17:38:46 +0200

    Return real path from ttyname.

--- lib/libc/gen/ttyname.c
+++ lib/libc/gen/ttyname.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 const char *ttyname(int fd) {
-  static char name[] = "ttyname not implemented";
+  static char name[] = "/dev/tty";
   /* TODO(fzdob): to implement */
   return name;
 }
