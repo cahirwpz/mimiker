8a2d73d7fc94db5a8082af66a8e91452a658d800
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-10-23 17:40:37 +0200

    Return error from ttyname instead of cheating.

--- lib/libc/gen/ttyname.c
+++ lib/libc/gen/ttyname.c
@@ -1,7 +1,8 @@
+#include <errno.h>
 #include <unistd.h>
 
 const char *ttyname(int fd) {
-  static char name[] = "/dev/tty";
   /* TODO(fzdob): to implement */
-  return name;
+  errno = ENOTTY;
+  return NULL;
 }
