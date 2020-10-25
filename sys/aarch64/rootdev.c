84ea1ed08eff16168ad1fcd10b37b2119006f71c
Paweł Jasiak <pawel@jasiak.xyz>
2020-10-25 22:41:14 +0100

    Add memory resources in rootdev.

--- sys/aarch64/rootdev.c
+++ sys/aarch64/rootdev.c
@@ -9,17 +9,6 @@
 #include <sys/timer.h>
 #include <sys/interrupt.h>
 
-static bus_driver_t rootdev_driver;
-
-DEVCLASS_CREATE(root);
-
-static device_t rootdev = (device_t){
-  .children = TAILQ_HEAD_INITIALIZER(rootdev.children),
-  .driver = (driver_t *)&rootdev_driver,
-  .state = NULL,
-  .devclass = &DEVCLASS(root),
-};
-
 /*
  * TODO(pj): This is only temporary solution. Timer should be initialized by
  * device subsystem.
@@ -71,7 +60,7 @@ static vaddr_t va;
 void intr_tick(void) {
   tm_trigger(&timer);
 
-  /* 
+  /*
    * https://developer.arm.com/docs/ddi0595/h/aarch64-system-registers/cntp_cval_el0
    */
   uint64_t count = READ_SPECIALREG(cntpct_el0);
@@ -94,10 +83,6 @@ static void _init_clock(void) {
   *timerp = *timerp | (1 << BCM2836_INT_CNTPNSIRQ);
 }
 
-void init_devices(void) {
-  _init_clock();
-}
-
 void init_arm_timer(void) {
   uint64_t freq = READ_SPECIALREG(cntfrq_el0);
 
@@ -109,7 +94,111 @@ void init_arm_timer(void) {
   tm_select(&timer);
 }
 
-/*
- * XXX(pj): linker set needs at least one element otherwise ld will be very sad
- */
+typedef struct rootdev {
+} rootdev_t;
+
+static vaddr_t rootdev_local_handle;
+static rman_t rm_mem[2];
+
+/* Use pre mapped memory. */
+static int rootdev_bs_map(bus_addr_t addr, bus_size_t size,
+                          bus_space_handle_t *handle_p) {
+  if (addr >= BCM2836_ARM_LOCAL_BASE &&
+      addr < BCM2836_ARM_LOCAL_BASE + BCM2836_ARM_LOCAL_SIZE) {
+    *handle_p = rootdev_local_handle;
+    return 0;
+  }
+  return generic_bs_map(addr, size, handle_p);
+}
+
+/* clang-format off */
+static bus_space_t *rootdev_bus_space = &(bus_space_t){
+  .bs_map = rootdev_bs_map,
+  .bs_read_1 = generic_bs_read_1,
+  .bs_read_2 = generic_bs_read_2,
+  .bs_read_4 = generic_bs_read_4,
+  .bs_write_1 = generic_bs_write_1,
+  .bs_write_2 = generic_bs_write_2,
+  .bs_write_4 = generic_bs_write_4,
+  .bs_read_region_1 = generic_bs_read_region_1,
+  .bs_read_region_2 = generic_bs_read_region_2,
+  .bs_read_region_4 = generic_bs_read_region_4,
+  .bs_write_region_1 = generic_bs_write_region_1,
+  .bs_write_region_2 = generic_bs_write_region_2,
+  .bs_write_region_4 = generic_bs_write_region_4,
+};
+/* clang-format on */
+
+static void rootdev_intr_setup(device_t *dev, unsigned num,
+                               intr_handler_t *handler) {
+}
+
+static void rootdev_intr_teardown(device_t *dev, intr_handler_t *handler) {
+}
+
+static int rootdev_attach(device_t *bus) {
+  rman_init(&rm_mem[0], "BCM2835 peripherials", BCM2835_PERIPHERALS_BASE,
+            BCM2835_PERIPHERALS_BASE + BCM2835_PERIPHERALS_SIZE - 1, RT_MEMORY);
+  rman_init(&rm_mem[1], "ARM local", BCM2836_ARM_LOCAL_BASE,
+            BCM2836_ARM_LOCAL_BASE + BCM2836_ARM_LOCAL_SIZE - 1, RT_MEMORY);
+
+  /* Map BCM2836 shared processo only once. */
+  rootdev_local_handle = kva_alloc(BCM2836_ARM_LOCAL_SIZE);
+  pmap_kenter(rootdev_local_handle, BCM2836_ARM_LOCAL_BASE,
+              VM_PROT_READ | VM_PROT_WRITE, PMAP_NOCACHE);
+  return 0;
+}
+
+static resource_t *rootdev_alloc_resource(device_t *bus, device_t *child,
+                                          res_type_t type, int rid,
+                                          rman_addr_t start, rman_addr_t end,
+                                          size_t size, res_flags_t flags) {
+  resource_t *r;
+
+  r = rman_alloc_resource(&rm_mem[0], start, end, size, 1, RF_NONE, child);
+  if (r == NULL)
+    r = rman_alloc_resource(&rm_mem[1], start, end, size, 1, RF_NONE, child);
+
+  if (r) {
+    r->r_bus_tag = rootdev_bus_space;
+    if (flags & RF_ACTIVE)
+      bus_space_map(r->r_bus_tag, r->r_start, r->r_end - r->r_start + 1,
+                    &r->r_bus_handle);
+    device_add_resource(child, r, rid);
+  }
+
+  return r;
+}
+
+static bus_driver_t rootdev_driver = {
+  .driver =
+    {
+      .size = sizeof(rootdev_t),
+      .desc = "RPI3 platform root bus driver",
+      .attach = rootdev_attach,
+    },
+  .bus =
+    {
+      .intr_setup = rootdev_intr_setup,
+      .intr_teardown = rootdev_intr_teardown,
+      .alloc_resource = rootdev_alloc_resource,
+    },
+};
+
+DEVCLASS_CREATE(root);
+
+static device_t rootdev = (device_t){
+  .children = TAILQ_HEAD_INITIALIZER(rootdev.children),
+  .driver = (driver_t *)&rootdev_driver,
+  .instance = &(rootdev_t){},
+  .state = NULL,
+  .devclass = &DEVCLASS(root),
+};
+
 DEVCLASS_ENTRY(root, rootdev);
+
+void init_devices(void) {
+  device_attach(&rootdev);
+  /* TODO(pj) move it to other place. */
+  _init_clock();
+}
