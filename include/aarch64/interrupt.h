c8f8e073046d383a03684785c390ddd2fbc637f8
Paweł Jasiak <pawel@jasiak.xyz>
2020-10-28 19:05:29 +0100

    Use intr_event_t & intr_handler_t for interrupts management.

--- include/aarch64/interrupt.h
+++ include/aarch64/interrupt.h
@@ -10,4 +10,12 @@ void cpu_intr_enable(void);
 /*! \brief Check if local CPU interrupts are disabled. */
 bool cpu_intr_disabled(void);
 
+typedef struct intr_handler intr_handler_t;
+
+void init_aarch64_intr(void);
+
+void aarch64_intr_setup(intr_handler_t *handler, unsigned irq);
+
+void aarch64_intr_teardown(intr_handler_t *handler);
+
 #endif /* !_AARCH64_INTERRUPT_H_ */
--- sys/aarch64/interrupt.c
+++ sys/aarch64/interrupt.c
@@ -1,5 +1,7 @@
 #include <sys/mimiker.h>
 #include <sys/context.h>
+#include <sys/interrupt.h>
+#include <sys/exception.h>
 #include <aarch64/interrupt.h>
 #include <aarch64/armreg.h>
 
@@ -18,6 +20,33 @@ bool cpu_intr_disabled(void) {
   return (daif & DAIF_I_MASKED) != 0;
 }
 
+/* TODO(pj) check if it is a correct value */
+#define N_IRQ 64
+
+static intr_event_t aarch64_intr_event[N_IRQ];
+
+void init_aarch64_intr(void) {
+  for (int i = 0; i < N_IRQ; i++) {
+    intr_event_init(&aarch64_intr_event[i], i, NULL, NULL, NULL, NULL);
+    intr_event_register(&aarch64_intr_event[i]);
+  }
+}
+
+void aarch64_intr_setup(intr_handler_t *handler, unsigned irq) {
+  intr_event_t *event = &aarch64_intr_event[irq];
+  intr_event_add_handler(event, handler);
+}
+
+void aarch64_intr_teardown(intr_handler_t *handler) {
+  intr_event_remove_handler(handler);
+}
+
 void cpu_intr_handler(ctx_t *ctx) {
+  intr_disable();
+
+  /* TODO(pj) handle interrupts */
   panic("cpu_intr_handler");
+
+  intr_enable();
+  on_exc_leave();
 }
