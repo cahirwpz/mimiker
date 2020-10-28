05237a7087a74b952f6660301cbb531ea1db4058
Paweł Jasiak <pawel@jasiak.xyz>
2020-10-28 19:07:40 +0100

    Init interrupts from board_init.

--- sys/aarch64/rpi3.c
+++ sys/aarch64/rpi3.c
@@ -6,9 +6,11 @@
 #include <sys/thread.h>
 #include <sys/vm_physmem.h>
 #include <sys/context.h>
+#include <sys/interrupt.h>
 #include <aarch64/atags.h>
 #include <aarch64/mcontext.h>
 #include <aarch64/vm_param.h>
+#include <aarch64/interrupt.h>
 
 static int count_atags(atag_tag_t *atags) {
   int ntokens = 0;
@@ -85,5 +87,7 @@ static void rpi3_physmem(void) {
 __noreturn void board_init(void) {
   init_klog();
   rpi3_physmem();
+  init_aarch64_intr();
+  intr_enable();
   kernel_init();
 }
