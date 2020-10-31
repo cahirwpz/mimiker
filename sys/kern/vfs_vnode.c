a3e936a2e909f707f22090b36969dd47c5ff81f0
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-10-31 09:18:44 +0100

    Vnode lock initialization as separate function.

--- sys/kern/vfs_vnode.c
+++ sys/kern/vfs_vnode.c
@@ -14,6 +14,8 @@
 
 static POOL_DEFINE(P_VNODE, "vnode", sizeof(vnode_t));
 
+static void vnlock_init(vnlock_t *vl);
+
 /* Actually, vnode management should be much more complex than this, because
    this stub does not recycle vnodes, does not store them on a free list,
    etc. So at some point we may need a more sophisticated memory management here
@@ -25,8 +27,7 @@ vnode_t *vnode_new(vnodetype_t type, vnodeops_t *ops, void *data) {
   v->v_data = data;
   v->v_ops = ops;
   v->v_usecnt = 1;
-  spin_init(&v->v_lock.vl_interlock, 0);
-  cv_init(&v->v_lock.vl_cv, "vnode sleep cv");
+  vnlock_init(&v->v_lock);
   return v;
 }
 
@@ -34,6 +35,12 @@ vnode_t *vnode_new(vnodetype_t type, vnodeops_t *ops, void *data) {
  * but sometimes we need to sleep, e.g. in VOP_READ.
  * This solves the problem, but should be replaced by a proper lock
  * that allows sleeping. */
+
+static void vnlock_init(vnlock_t *vl) {
+  spin_init(&vl->vl_interlock, 0);
+  cv_init(&vl->vl_cv, "vnode sleep cv");
+}
+
 void vnode_lock(vnode_t *v) {
   vnlock_t *vl = &v->v_lock;
   WITH_SPIN_LOCK (&vl->vl_interlock) {
