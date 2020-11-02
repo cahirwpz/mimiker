5b68deaea4a10f952e5ac48e12a5d4ddc569d957
Franciszek Zdobylak <zdobyl.frank@gmail.com>
2020-11-02 11:18:37 +0100

    Fix locking while copying credentials during fork.

--- sys/kern/cred.c
+++ sys/kern/cred.c
@@ -1,7 +1,9 @@
 #include <sys/cred.h>
 #include <sys/proc.h>
 #include <sys/libkern.h>
+#include <sys/mutex.h>
 
 void cred_fork(proc_t *to, proc_t *from) {
+  assert(mtx_owned(&from->p_lock));
   memcpy(&to->p_cred, &from->p_cred, sizeof(cred_t));
 }
