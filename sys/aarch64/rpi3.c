d833251e1100bfe5bec9d9eba31a17dbf45d5619
Paweł Jasiak <pawel@jasiak.xyz>
2020-11-02 01:55:46 +0100

    Mark space between kern_start and kern_end as used.

--- sys/aarch64/rpi3.c
+++ sys/aarch64/rpi3.c
@@ -72,6 +72,7 @@ static void rpi3_physmem(void) {
   paddr_t rd_end = rd_start + ramdisk_get_size();
 
   vm_physseg_plug(ram_start, kern_start);
+  vm_physseg_plug_used(kern_start, kern_end);
 
   if (rd_start != rd_end) {
     vm_physseg_plug(kern_end, rd_start);
