da28f2310c73d66e466403bd5f8ce27952128500
Paweł Jasiak <pawel@jasiak.xyz>
2020-11-04 14:28:05 +0100

    Remove unused headers.

--- sys/aarch64/rootdev.c
+++ sys/aarch64/rootdev.c
@@ -3,7 +3,6 @@
 #include <sys/bus.h>
 #include <sys/devclass.h>
 #include <aarch64/bcm2835reg.h>
-#include <aarch64/interrupt.h>
 #include <sys/kmem.h>
 #include <sys/pmap.h>
 #include <sys/interrupt.h>
