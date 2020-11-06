a1895a819dec198abd7785206b7395965dfec715
Krystian Bacławski <krystian.baclawski@gmail.com>
2020-11-06 15:42:56 +0100

    Consolidate arm timer state in one structure.

--- sys/aarch64/timer.c
+++ sys/aarch64/timer.c
@@ -11,115 +11,101 @@
 
 #define CNTCTL_ENABLE 1
 
-/*
- * TODO(pj): This should be defined in one header for machine dependent AArch64
- * code.
- */
-#define __isb() __asm__ volatile("ISB" ::: "memory")
-
 typedef struct arm_timer_state {
-  resource_t *rm_mem;
+  resource_t *regs;
   vaddr_t va_page;
+  intr_handler_t intr_handler;
+  timer_t timer;
+  uint64_t step;
 } arm_timer_state_t;
 
-static int timer_start(timer_t *tm, unsigned flags, const bintime_t start,
+static int arm_timer_start(timer_t *tm, unsigned flags, const bintime_t start,
                        const bintime_t period) {
-  uint64_t step = bintime_mul(period, tm->tm_frequency).sec;
+  arm_timer_state_t *state = ((device_t *)tm->tm_priv)->state;
+  state->step = bintime_mul(period, tm->tm_frequency).sec;
 
   WITH_INTR_DISABLED {
     uint64_t count = READ_SPECIALREG(cntpct_el0);
-    WRITE_SPECIALREG(cntp_cval_el0, count + step);
+    WRITE_SPECIALREG(cntp_cval_el0, count + state->step);
     WRITE_SPECIALREG(cntp_ctl_el0, CNTCTL_ENABLE);
-
-    __isb();
   }
 
   return 0;
 }
 
-static int timer_stop(timer_t *tm) {
+static int arm_timer_stop(timer_t *tm) {
   WRITE_SPECIALREG(cntp_ctl_el0, 0);
   return 0;
 }
 
-static bintime_t timer_gettime(timer_t *tm) {
+static bintime_t arm_timer_gettime(timer_t *tm) {
   uint64_t count = READ_SPECIALREG(cntpct_el0);
-  bintime_t val = bintime_mul(tm->tm_min_period, count);
-  return val;
+  return bintime_mul(tm->tm_min_period, count);
 }
 
-static intr_filter_t arm_timer_intr(void *data) {
-  tm_trigger(data);
+static intr_filter_t arm_timer_intr(void *data /* device_t* */) {
+  arm_timer_state_t *state = ((device_t *)data)->state;
+
+  tm_trigger(&state->timer);
 
   /*
    * https://developer.arm.com/docs/ddi0595/h/aarch64-system-registers/cntp_cval_el0
    */
-  uint64_t count = READ_SPECIALREG(cntpct_el0);
-  uint64_t freq = READ_SPECIALREG(cntfrq_el0);
-  WRITE_SPECIALREG(cntp_cval_el0, count + freq);
-
-  __isb();
+  uint64_t prev = READ_SPECIALREG(cntp_cval_el0);
+  WRITE_SPECIALREG(cntp_cval_el0, prev + state->step);
 
   return IF_FILTERED;
 }
-
-typedef struct timer_priv {
-  device_t *dev;
-  intr_handler_t intr_handler;
-} timer_priv_t;
-
-static timer_priv_t timer_priv;
-
-static timer_t timer = (timer_t){
-  .tm_name = "arm-cpu-timer",
-  .tm_flags = TMF_PERIODIC,
-  .tm_start = timer_start,
-  .tm_stop = timer_stop,
-  .tm_gettime = timer_gettime,
-  .tm_priv = &timer_priv,
-};
 
 static int arm_timer_probe(device_t *dev) {
   return 1;
 }
 
 static int arm_timer_attach(device_t *dev) {
-  /* Save link to timer device. */
-  timer_priv_t *priv = timer.tm_priv;
-  priv->dev = dev;
-  priv->intr_handler =
-    INTR_HANDLER_INIT(arm_timer_intr, NULL, &timer, "ARM CPU timer", 0);
-
-  uint64_t freq = READ_SPECIALREG(cntfrq_el0);
-
-  timer.tm_frequency = freq;
-  timer.tm_min_period = HZ2BT(freq);
-  /* TODO(pj): Document what this value means. */
-  timer.tm_max_period = bintime_mul(HZ2BT(freq), 1LL << 30);
-  tm_register(&timer);
-  tm_select(&timer);
-
   arm_timer_state_t *state = dev->state;
 
-  /* Request for shared memory. */
-  state->rm_mem =
+  /* Request for timer registers. */
+  resource_t *regs =
     bus_alloc_resource(dev, RT_MEMORY, 1, BCM2836_ARM_LOCAL_BASE,
                        BCM2836_ARM_LOCAL_BASE + BCM2836_ARM_LOCAL_SIZE - 1,
                        BCM2836_ARM_LOCAL_SIZE, RF_ACTIVE);
+  assert(regs != NULL);
 
-  assert(state->rm_mem != NULL);
-  bus_space_map(state->rm_mem->r_bus_tag, state->rm_mem->r_start,
-                state->rm_mem->r_end - state->rm_mem->r_start + 1,
-                &state->rm_mem->r_bus_handle);
+  bus_space_map(regs->r_bus_tag, regs->r_start, regs->r_end - regs->r_start + 1,
+                &regs->r_bus_handle);
 
-  state->va_page = state->rm_mem->r_bus_handle;
+  state->regs = regs;
+  state->va_page = regs->r_bus_handle;
+
+  /* Save link to timer device. */
+  state->timer = (timer_t){
+    .tm_name = "arm-cpu-timer",
+    .tm_flags = TMF_PERIODIC,
+    .tm_start = arm_timer_start,
+    .tm_stop = arm_timer_stop,
+    .tm_gettime = arm_timer_gettime,
+    .tm_priv = dev,
+  };
+
+  state->intr_handler =
+    INTR_HANDLER_INIT(arm_timer_intr, NULL, dev, "ARM CPU timer", 0);
+
+  timer_t *tm = &state->timer;
+
+  uint64_t freq = READ_SPECIALREG(cntfrq_el0);
+  tm->tm_frequency = freq;
+  tm->tm_min_period = HZ2BT(freq);
+  /* TODO(pj): Document what this value means. */
+  tm->tm_max_period = bintime_mul(HZ2BT(freq), 1LL << 30);
+  tm_register(tm);
+  tm_select(tm);
 
   /* Enable interrupt for CPU0. */
   size_t offset = BCM2836_LOCAL_TIMER_IRQ_CONTROLN(0);
   volatile uint32_t *timerp = (uint32_t *)(state->va_page + offset);
   *timerp = *timerp | (1 << BCM2836_INT_CNTPNSIRQ);
 
-  bus_intr_setup(dev, BCM2836_INT_CNTPNSIRQ_CPUN(0), &priv->intr_handler);
+  bus_intr_setup(dev, BCM2836_INT_CNTPNSIRQ_CPUN(0), &state->intr_handler);
 
   return 0;
 }
