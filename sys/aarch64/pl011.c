841ed344fa6ff6c528fdebefb3a49b65538fdd30
Paweł Jasiak <pawel@jasiak.xyz>
2020-11-13 12:39:43 +0100

    Copy part of configuration from prototype & add TODO.

--- sys/aarch64/pl011.c
+++ sys/aarch64/pl011.c
@@ -5,6 +5,7 @@
 #include <sys/devclass.h>
 #include <sys/rman.h>
 #include <aarch64/bcm2835reg.h>
+#include <aarch64/plcomreg.h>
 
 #define UART0_BASE BCM2835_PERIPHERALS_BUS_TO_PHYS(BCM2835_UART0_BASE)
 
@@ -31,6 +32,48 @@ static int pl011_attach(device_t *dev) {
   assert(r != NULL);
 
   state->regs = r;
+
+  /* Disable UART0. */
+  bus_space_write_4(r->r_bus_tag, r->r_bus_handle, PL011COM_CR, 0);
+  /* Clear pending interrupts. */
+  bus_space_write_4(r->r_bus_tag, r->r_bus_handle, PL011COM_ICR,
+                    PL011_INT_ALLMASK);
+
+  /* TODO(pj) do magic with mail buffer */
+
+  /* TODO(pj) configure gpio */
+
+  /*
+   * Set integer & fractional part of baud rate.
+   * Divider = UART_CLOCK/(16 * Baud)
+   * Fraction part register = (Fractional part * 64) + 0.5
+   * UART_CLOCK = 3000000; Baud = 115200.
+   *
+   * Divider = 4000000/(16 * 115200) = 2.17 = ~2.
+   * Fractional part register = (.17 * 64) + 0.5 = 11.3 = ~11 = 0xb.
+   */
+
+  bus_space_write_4(r->r_bus_tag, r->r_bus_handle, PL011COM_IBRD, 2);
+  bus_space_write_4(r->r_bus_tag, r->r_bus_handle, PL011COM_FBRD, 0xb);
+
+  /* Enable FIFO & 8 bit data transmission (1 stop bit, no parity). */
+  bus_space_write_4(r->r_bus_tag, r->r_bus_handle, PL011COM_LCRH,
+                    PL01X_LCR_FEN | PL01X_LCR_8BITS);
+
+  /* Mask all interrupts. */
+  bus_space_write_4(r->r_bus_tag, r->r_bus_handle, PL011COM_IMSC,
+                    PL011_INT_ALLMASK);
+
+  /* Enable UART0, receive & transfer part of UART. */
+  bus_space_write_4(r->r_bus_tag, r->r_bus_handle, PL011COM_CR,
+                    PL01X_CR_UARTEN | PL011_CR_TXE | PL011_CR_RXE);
+
+  /* Enable receive interrupt. */
+  bus_space_write_4(r->r_bus_tag, r->r_bus_handle, PL011COM_IMSC, PL011_INT_RX);
+
+  /* TODO(pj) register UART0 handler. */
+
+  /* TODO(pj) enable UART0 IRQ. */
 
   return 1;
 }
