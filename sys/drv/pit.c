f6bb2eb4fe1e36717cc90fb1b7486352619aa9c3
wiklam <wiktorpilar99@gmail.com>
2020-11-16 12:40:46 +0100

    Pit timer clean up

--- sys/drv/pit.c
+++ sys/drv/pit.c
@@ -9,48 +9,64 @@
 #include <sys/spinlock.h>
 #include <sys/devclass.h>
 
+typedef struct counter {
+  uint32_t lo;
+  uint32_t hi;
+} counter_t;
+
 typedef struct pit_state {
   resource_t *regs;
-  spin_t lock;
   intr_handler_t intr_handler;
   timer_t timer;
-  uint64_t period_frac;        /* period as a fraction of a second */
   uint16_t period_cntr;        /* period as PIT counter value */
-  volatile uint16_t last_cntr; /* last seen counter value */
-  volatile bool overflow;      /* counter overflow detected? */
-  bintime_t time;              /* last time measured by the timer */
 } pit_state_t;
 
 #define inb(addr) bus_read_1(pit->regs, (addr))
 #define outb(addr, val) bus_write_1(pit->regs, (addr), (val))
 
 static void pit_set_frequency(pit_state_t *pit, uint16_t period) {
-  assert(spin_owned(&pit->lock));
-
   outb(TIMER_MODE, TIMER_SEL0 | TIMER_16BIT | TIMER_RATEGEN);
   outb(TIMER_CNTR0, period & 0xff);
   outb(TIMER_CNTR0, period >> 8);
   pit->period_cntr = period;
 }
 
-static uint16_t pit_get_counter(pit_state_t *pit) {
-  assert(spin_owned(&pit->lock));
+static uint16_t pit_get_count(pit_state_t *pit) {
+  uint16_t count = 0;
+  WITH_INTR_DISABLED {
+    outb(TIMER_MODE, TIMER_SEL0 | TIMER_LATCH);
+    count |= inb(TIMER_CNTR0);
+    count |= inb(TIMER_CNTR0) << 8;
+  }
+  return count;
+}
 
-  outb(TIMER_MODE, TIMER_SEL0 | TIMER_LATCH);
-  uint16_t value = 0;
-  value |= inb(TIMER_CNTR0);
-  value |= inb(TIMER_CNTR0) << 8;
-  return pit->period_cntr - value;
+static counter_t pit_get_counter(pit_state_t *pit) {
+  static uint16_t count_last = 0;
+  static counter_t counter_last = {0,0};
+  uint16_t count_now, ticks;
+  uint32_t oldlow;
+
+  count_now = pit_get_count(pit);
+  if(count_last >= count_now)
+    ticks = count_last - count_now;
+  else
+    ticks = count_last + (pit->period_cntr - count_now);
+  
+  count_last = count_now;
+
+  oldlow = counter_last.lo;
+
+  if(oldlow > (counter_last.lo = oldlow + ticks))
+    counter_last.hi++;
+
+  return counter_last;  
 }
 
 static intr_filter_t pit_intr(void *data) {
   pit_state_t *pit = data;
 
-  WITH_SPIN_LOCK (&pit->lock) {
-    bintime_add_frac(&pit->time, pit->period_frac);
-    pit->overflow = false;
-    pit->last_cntr = pit_get_counter(pit);
-  }
+  pit_get_counter(pit);
   tm_trigger(&pit->timer);
   return IF_FILTERED;
 }
@@ -67,13 +83,9 @@ static int timer_pit_start(timer_t *tm, unsigned flags, const bintime_t start,
   device_t *dev = device_of(tm);
   pit_state_t *pit = dev->state;
 
-  pit->time = binuptime();
-  pit->period_frac = period.frac;
   uint16_t counter = bintime_mul(period, TIMER_FREQ).sec;
-  WITH_SPIN_LOCK (&pit->lock) {
-    pit_set_frequency(pit, counter);
-    pit->last_cntr = pit_get_counter(pit);
-  }
+  pit_set_frequency(pit, counter);
+
   bus_intr_setup(dev, 0, &pit->intr_handler);
   return 0;
 }
@@ -89,21 +101,15 @@ static int timer_pit_stop(timer_t *tm) {
 static bintime_t timer_pit_gettime(timer_t *tm) {
   device_t *dev = device_of(tm);
   pit_state_t *pit = dev->state;
-  uint16_t cntr = 0;
-  bintime_t bt;
-  WITH_SPIN_LOCK (&pit->lock) {
-    uint16_t last_cntr = pit->last_cntr;
-    bool overflow = pit->overflow;
-    bt = pit->time;
-    cntr = pit_get_counter(pit);
-    pit->last_cntr = cntr;
-    if (cntr < last_cntr || overflow) {
-      cntr += pit->period_cntr;
-      pit->overflow = true;
-    }
-  }
-  bintime_t offset = bintime_mul(tm->tm_min_period, cntr);
-  bintime_add_frac(&bt, offset.frac);
+  bintime_t period = HZ2BT(tm->tm_frequency);
+  counter_t counter = pit_get_counter(pit);
+
+  bintime_t bt = bintime_mul(period, counter.hi);
+  bintime_t bt2 = bintime_mul(period, counter.lo);
+  bt = bintime_mul(bt, (1<<16));
+  bt = bintime_mul(bt, (1<<16));
+  bintime_add(&bt, &bt2);
+
   return bt;
 }
 
@@ -117,7 +123,6 @@ static int pit_attach(device_t *dev) {
                        IO_TMRSIZE, RF_ACTIVE);
   assert(pit->regs != NULL);
 
-  pit->lock = SPIN_INITIALIZER(0);
   pit->intr_handler = INTR_HANDLER_INIT(pit_intr, NULL, pit, "i8254 timer", 0);
 
   pit->timer = (timer_t){
