11bcde567e41dcf6265c4fe33712bd046dfb34f0
franciscozdo <44414770+franciscozdo@users.noreply.github.com>
2020-11-16 13:10:27 +0100

    execve: change process identity according to set-uid and set-gid bits (#821)

--- sys/kern/cred.c
+++ sys/kern/cred.c
@@ -7,3 +7,15 @@ void cred_fork(proc_t *to, proc_t *from) {
   assert(mtx_owned(&from->p_lock));
   memcpy(&to->p_cred, &from->p_cred, sizeof(cred_t));
 }
+
+void cred_exec_setid(proc_t *p, uid_t uid, gid_t gid) {
+  assert(mtx_owned(&p->p_lock));
+  if (uid != (uid_t)-1) {
+    p->p_cred.cr_suid = p->p_cred.cr_euid;
+    p->p_cred.cr_euid = uid;
+  }
+  if (gid != (gid_t)-1) {
+    p->p_cred.cr_sgid = p->p_cred.cr_egid;
+    p->p_cred.cr_egid = gid;
+  }
+}
