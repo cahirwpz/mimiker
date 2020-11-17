bc6210836fa498be4603494788a9ae7a7b4c0024
Paweł Jasiak <pawel@jasiak.xyz>
2020-11-17 20:45:43 +0100

    Add comments in gpio.h.

--- include/aarch64/gpio.h
+++ include/aarch64/gpio.h
@@ -3,6 +3,9 @@
 
 struct resource_t;
 
+/* Select GPIO alt function. For more information look at
+ * https://cs140e.sergio.bz/docs/BCM2837-ARM-Peripherals.pdf and
+ * https://pinout.xyz */
 void gpio_function_select(resource_t *r, unsigned pin, unsigned func);
 
 void gpio_set_pull(resource_t *r, unsigned pin, unsigned pud);
