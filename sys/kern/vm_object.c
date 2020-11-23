291981e42c8a2f128440e59cfa0ee2a4e6128660
Barbara Kaczorowska <bkjg252@gmail.com>
2020-11-23 11:22:11 +0100

    make format

--- sys/kern/vm_object.c
+++ sys/kern/vm_object.c
@@ -28,7 +28,7 @@ vm_object_t *vm_object_alloc(vm_pgr_type_t type) {
 }
 
 void vm_object_free(vm_object_t *obj) {
-  WITH_MTX_LOCK(&obj->mtx) {
+  WITH_MTX_LOCK (&obj->mtx) {
     if (!refcnt_release(&obj->ref_counter)) {
       return;
     }
