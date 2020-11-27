01367acc3157e82f6ed371bce70d745bfb4b114f
Mikolaj Kowalik <kowalikmikolaj95@gmail.com>
2020-11-27 12:07:01 +0100

    correct formatting of rtl8139_reg.h

--- include/dev/rtl8139_reg.h
+++ include/dev/rtl8139_reg.h
@@ -4,19 +4,19 @@
  * Copied from FreeBSD: sys/dev/rl/if_rlreg.h
  */
 
-#define RL_COMMAND              0x0037          /* command register */
+#define RL_COMMAND 0x0037 /* command register */
 
-#define RL_8139_CFG1            0x0052          /* config register #1 */
+#define RL_8139_CFG1 0x0052 /* config register #1 */
 
 /*
  * Command register.
  */
-#define RL_CMD_RESET            0x0010
+#define RL_CMD_RESET 0x0010
 
 /*
  * Config 1 register
  */
-#define RL_CFG1_PWRON           0x00
+#define RL_CFG1_PWRON 0x00
 
-#define RL_TIMEOUT              (10*1000)
+#define RL_TIMEOUT (10 * 1000)
 #endif /* !_RTL8139_REG_H_ */
