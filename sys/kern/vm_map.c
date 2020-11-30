954f28a3ca8e0b3782235147dffcf10b5e1b19c1
Barbara Kaczorowska <bkjg252@gmail.com>
2020-11-30 16:14:02 +0100

    Add comment about VM_SHARED flag

--- sys/kern/vm_map.c
+++ sys/kern/vm_map.c
@@ -333,6 +333,10 @@ vm_map_t *vm_map_clone(vm_map_t *map) {
     TAILQ_FOREACH (it, &map->entries, link) {
       vm_object_t *obj;
       vm_segment_t *seg;
+
+      /* VM_SHARED flag can be set by sys_mmap when it is called with
+       * MAP_SHARED flag (because definition from sys/mman.h fit into
+       * vm_flags_t */
       if (it->flags & VM_SHARED) {
         refcnt_acquire(&it->object->ref_counter);
         obj = it->object;
