d56880001e61eae573160721c1f10335a0ae9bfa
Barbara Kaczorowska <bkjg252@gmail.com>
2020-11-30 15:40:49 +0100

    Change file descriptor to -1 in tests for shared memory

--- bin/utest/vm_map.c
+++ bin/utest/vm_map.c
@@ -12,7 +12,7 @@
 
 int test_sharing_memory_simple(void) {
   char *map =
-    mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, 0, 0);
+    mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0);
 
   assert(map != (char *)MAP_FAILED);
 
@@ -39,7 +39,7 @@ int test_sharing_memory_simple(void) {
 
 int test_sharing_memory_child_and_grandchild(void) {
   char *map =
-    mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, 0, 0);
+    mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0);
 
   assert(map != (char *)MAP_FAILED);
 
