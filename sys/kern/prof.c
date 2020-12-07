556883b56e2b49638670beb48bc696e1e4c00b61
wiklam <wiktorpilar99@gmail.com>
2020-12-07 05:09:41 +0100

    mcount

--- sys/kern/prof.c
+++ sys/kern/prof.c
@@ -1,6 +1,7 @@
 #include <sys/param.h>
 #include <sys/kmem.h>
 #include <sys/gmon.h>
+#include <sys/param.h>
 #include <machine/vm_param.h>
 
 gmonparam_t _gmonparam = { .state = GMON_PROF_OFF };
@@ -30,4 +31,110 @@ void init_prof(void) {
   p->tos = (tostruct_t *)profptr;
   profptr += p->tossize;
   p->froms = (u_short *)profptr;
+}
+
+
+_MCOUNT_DECL(u_long frompc, u_long selfpc) {
+  u_short *frompcindex;
+  tostruct_t *top, *prevtop;
+  gmonparam_t *p = &_gmonparam;
+  long toindex;
+  int s;
+  if (p->state != GMON_PROF_ON)
+    return;
+  
+  intr_disable();
+
+  /* TODO: Handle SMP */
+  
+  p->state = GMON_PROF_BUSY;
+
+  /* Checking if frompc is in range of kernel space 
+     - signal catchers get called from the stack*/
+  frompc -= p->lowpc; 
+  if (frompc > p->textsize)
+    goto done;
+
+
+  size_t index = (frompc / (p->hashfraction * sizeof(*p->froms)));
+  frompcindex = &p->froms[index];
+	toindex = *frompcindex;
+	if (toindex == 0) {
+		/*
+		 *	first time traversing this arc
+		 */
+		toindex = ++p->tos[0].link;
+		if (toindex >= p->tolimit){
+			/* We can not profile it any more */
+	      p->state = GMON_PROF_ERROR;
+				goto done;
+    }
+
+		*frompcindex = (u_short)toindex;
+		top = &p->tos[(size_t)toindex];
+		top->selfpc = selfpc;
+		top->count = 1;
+		top->link = 0;
+		goto done;
+	}
+	top = &p->tos[(size_t)toindex];
+	if (top->selfpc == selfpc) {
+		/*
+		 * arc at front of chain; usual case.
+		 */
+		top->count++;
+		goto done;
+	}
+	/*
+	 * have to go looking down chain for it.
+	 * top points to what we are looking at,
+	 * prevtop points to previous top.
+	 * we know it is not at the head of the chain.
+	 */
+	while (true) {
+		if (top->link == 0) {
+			/*
+			 * top is end of the chain and none of the chain
+			 * had top->selfpc == selfpc.
+			 * so we allocate a new tostruct
+			 * and link it to the head of the chain.
+			 */
+			toindex = ++p->tos[0].link;
+			if (toindex >= p->tolimit) {
+	      p->state = GMON_PROF_ERROR;
+				goto done;
+      }
+
+			top = &p->tos[(size_t)toindex];
+			top->selfpc = selfpc;
+			top->count = 1;
+			top->link = *frompcindex;
+			*frompcindex = (u_short)toindex;
+			goto done;
+		}
+		/*
+		 * otherwise, check the next arc on the chain.
+		 */
+		prevtop = top;
+		top = &p->tos[top->link];
+		if (top->selfpc == selfpc) {
+			/*
+			 * there it is.
+			 * increment its count
+			 * move it to the head of the chain.
+			 */
+			top->count++;
+			toindex = prevtop->link;
+			prevtop->link = top->link;
+			top->link = *frompcindex;
+			*frompcindex = (u_short)toindex;
+			goto done;
+		}
+	}
+done:
+  if(p->state != GMON_PROF_ERROR)
+	  p->state = GMON_PROF_ON;
+  /* TODO: End handling SMP */
+  intr_enable();
+  return;
 }
