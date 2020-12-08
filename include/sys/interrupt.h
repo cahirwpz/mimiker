8664e26b87139fa4935432a6e4acff195f99f73f
Tomasz Stachowski <46850701+ThomasStachovsky@users.noreply.github.com>
2020-12-08 08:58:42 +0100

    Start ithread when first handler with service routine is added (#844)
    Co-authored-by: Krystian Bacławski <krystian.baclawski@gmail.com>

--- include/sys/interrupt.h
+++ include/sys/interrupt.h
@@ -9,9 +9,6 @@
 typedef struct ctx ctx_t;
 typedef struct device device_t;
 
-/*! \brief Called during kernel initialization. */
-void init_ithreads(void);
-
 /*! \brief Disables hardware interrupts.
  *
  * Calls to \fn intr_disable can nest, you must use the same number of calls to
@@ -67,33 +64,18 @@ typedef intr_filter_t ih_filter_t(void *);
 typedef void ih_service_t(void *);
 typedef void ie_action_t(intr_event_t *);
 
-struct intr_handler {
-  TAILQ_ENTRY(intr_handler) ih_link;
-  ih_filter_t *ih_filter;   /* interrupt filter routine (run in irq ctx) */
-  ih_service_t *ih_service; /* interrupt service routine (run in thread ctx) */
-  intr_event_t *ih_event;   /* event we are connected to */
-  void *ih_argument;        /* argument to pass to filter/service routines */
-  const char *ih_name;      /* name of the handler */
-  /* XXX: do we really need ih_prio? it has no real use cases so far... */
-  prio_t ih_prio; /* handler's priority (sort key for ie_handlers) */
-};
-
-typedef TAILQ_HEAD(, intr_handler) ih_list_t;
-
 /* Software representation of interrupt line. */
 typedef struct intr_event {
   spin_t ie_lock;
   TAILQ_ENTRY(intr_event) ie_link; /* link on list of all interrupt events */
-  ih_list_t ie_handlers;   /* interrupt handlers sorted by descending ih_prio */
+  TAILQ_HEAD(, intr_handler) ie_handlers; /* sorted by descending ih_prio */
   ie_action_t *ie_disable; /* called before ithread delegation (mask irq) */
   ie_action_t *ie_enable;  /* called after ithread delagation (unmask irq) */
   void *ie_source;         /* additional argument for actions */
   const char *ie_name;     /* individual event name */
   unsigned ie_irq;         /* physical interrupt request line number */
-  unsigned ie_count;       /* number of handlers attached */
+  thread_t *ie_ithread;    /* associated interrupt thread */
 } intr_event_t;
-
-typedef TAILQ_HEAD(, intr_event) ie_list_t;
 
 intr_event_t *intr_event_create(void *source, int irq, ie_action_t *disable,
                                 ie_action_t *enable, const char *name);
