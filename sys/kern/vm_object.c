86a054486493763fc13f999a4112163cfa20ba36
Barbara Kaczorowska <bkjg252@gmail.com>
2020-12-10 17:09:56 +0100

    Fix bugs

--- sys/kern/vm_object.c
+++ sys/kern/vm_object.c
@@ -99,15 +99,9 @@ void vm_object_remove_page(vm_object_t *obj, vm_page_t *page) {
   vm_object_remove_page_nolock(obj, page);
 }
 
-void vm_object_remove_page(vm_object_t *obj, vm_page_t *page) {
-  SCOPED_MTX_LOCK(&obj->mtx);
-
-  vm_object_remove_page_nolock(obj, page);
-}
-
 void vm_object_remove_range(vm_object_t *object, off_t offset, size_t length) {
   vm_page_t *pg, *next;
-  
+
   SCOPED_RW_ENTER(&object->mtx, RW_WRITER);
 
   TAILQ_FOREACH_SAFE (pg, &object->list, obj.list, next) {
