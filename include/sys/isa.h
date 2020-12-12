b9a872cde1bd4cc7b1ec0acdceff435d4d2e30a7
mohrcore <krzys.boronski@gmail.com>
2020-12-12 15:07:18 +0100

    Added isa.h

---
+++ include/sys/isa.h
@@ -0,0 +1,12 @@
+#ifndef _SYS_ISA_H_
+#define _SYS_ISA_H_
+
+#include <sys/device.h>
+#include <sys/resource.h>
+#include <sys/rman.h>
+#include <sys/interrupt.h>
+#include <sys/bus.h>
+
+#define ICU_LEN 16 /* number of ISA IRQs */
+
+#endif
