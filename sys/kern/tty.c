583cc2e646dcf922afba698bab33fde8f016668f
Jakub Piecuch <j.piecuch96@gmail.com>
2020-12-15 16:47:01 +0100

    tty: notify driver when there's space in the input queue.

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -169,7 +169,7 @@ tty_t *tty_alloc(void) {
   tty->t_line.ln_size = LINEBUF_SIZE;
   tty->t_line.ln_count = 0;
   tty_init_termios(&tty->t_termios);
-  tty->t_ops.t_notify_out = NULL;
+  tty->t_ops = (ttyops_t){0};
   tty->t_data = NULL;
   tty->t_column = 0;
   tty->t_rocol = tty->t_rocount = 0;
@@ -415,21 +415,23 @@ static int tty_wait_background(tty_t *tty, int sig) {
   }
 }
 
-static void tty_bell(tty_t *tty) {
+static void tty_in_hiwat(tty_t *tty) {
+  tty->t_flags |= TF_IN_HIWAT;
+
   if (tty->t_iflag & IMAXBEL) {
     tty_output(tty, CTRL('g'));
     tty_notify_out(tty);
   }
 }
 
-void tty_input(tty_t *tty, uint8_t c) {
+bool tty_input(tty_t *tty, uint8_t c) {
   int iflag = tty->t_iflag;
   int lflag = tty->t_lflag;
   uint8_t *cc = tty->t_cc;
 
   if (c == '\r') {
     if (iflag & IGNCR)
-      return;
+      return true;
     else if (iflag & ICRNL)
       c = '\n';
   } else if (c == '\n' && (iflag & INLCR)) {
@@ -445,7 +447,7 @@ void tty_input(tty_t *tty, uint8_t c) {
         tty_erase(tty, erased);
         tty_notify_out(tty);
       }
-      return;
+      return true;
     }
 
     if (CCEQ(cc[VKILL], c)) {
@@ -462,7 +464,7 @@ void tty_input(tty_t *tty, uint8_t c) {
         tty->t_rocount = 0;
       }
       tty_notify_out(tty);
-      return;
+      return true;
     }
 
     bool is_break = tty_is_break(tty, c);
@@ -471,8 +473,8 @@ void tty_input(tty_t *tty, uint8_t c) {
     if ((tty->t_inq.count + tty->t_line.ln_count >= TTY_QUEUE_SIZE - 1 ||
          tty->t_line.ln_count == LINEBUF_SIZE - 1) &&
         !is_break) {
-      tty_bell(tty);
-      return;
+      tty_in_hiwat(tty);
+      return false;
     }
 
     tty_line_putc(tty, c);
@@ -493,17 +495,28 @@ void tty_input(tty_t *tty, uint8_t c) {
         tty_output(tty, '\b');
     }
     tty_notify_out(tty);
-    return;
+    return true;
   } else {
     /* Raw (non-canonical) mode */
     if (tty->t_inq.count >= TTY_QUEUE_SIZE) {
-      tty_bell(tty);
-      return;
+      tty_in_hiwat(tty);
+      return false;
     }
 
     ringbuf_putb(&tty->t_inq, c);
     tty_wakeup(tty);
+    return true;
+  }
+}
+
+static void tty_check_in_lowat(tty_t *tty) {
+  if (!(tty->t_flags & TF_IN_HIWAT))
     return;
+
+  if (tty->t_inq.count < TTY_IN_LOW_WATER) {
+    tty->t_flags &= ~TF_IN_HIWAT;
+    if (tty->t_ops.t_notify_in)
+      tty->t_ops.t_notify_in(tty);
   }
 }
 
@@ -551,6 +564,8 @@ static int tty_read(file_t *f, uio_t *uio) {
       ringbuf_getb(&tty->t_inq, &c);
       error = uiomove(&c, 1, uio);
     }
+
+    tty_check_in_lowat(tty);
   }
 
   /* Don't report errors on partial reads. */
@@ -564,6 +579,7 @@ static void tty_discard_input(tty_t *tty) {
   assert(mtx_owned(&tty->t_lock));
   ringbuf_reset(&tty->t_inq);
   tty->t_line.ln_count = 0;
+  tty_check_in_lowat(tty);
 }
 
 /*
