a831546b3d67a7ace35ba8c1b9d4ec665b4d4ea5
Mikolaj Kowalik <kowalikmikolaj95@gmail.com>
2020-12-19 21:50:38 +0100

    RTL8139: Draft version of interrupt handler

--- sys/drv/rtl8139.c
+++ sys/drv/rtl8139.c
@@ -8,13 +8,19 @@
 #include <sys/devfs.h>
 #include <sys/devclass.h>
 #include <dev/rtl8139_reg.h>
+#include <sys/pmap.h>
+#include <sys/kmem.h>
 
 #define RTL8139_VENDOR_ID 0x10ec
 #define RTL8139_DEVICE_ID 0x8139
 
 typedef struct rtl8139_state {
   resource_t *regs;
+  resource_t *irq_res;
+  vaddr_t rx_buf;
+  paddr_t paddr;
 } rtl8139_state_t;
+
 
 static int rtl8139_probe(device_t *dev) {
   pci_device_t *pcid = pci_device_of(dev);
@@ -32,17 +38,52 @@ static int rtl_reset(rtl8139_state_t *state) {
   return 1;
 }
 
-static int rtl8139_attach(device_t *dev) {
+static intr_filter_t rtl8139_intr(void *data) {
+  rtl8139_state_t *state = data;
+  // TODO: add enqueue routines
+  bus_write_2(state->regs, 0x3E, 0x1);
+  return IF_FILTERED;
+}
+
+// TODO: replace all magic constants
+int rtl8139_attach(device_t *dev) {
   rtl8139_state_t *state = dev->state;
 
+  pci_enable_busmaster(dev);
   state->regs = device_take_memory(dev, 1, RF_ACTIVE);
-  if (state->regs == NULL)
+
+  /*
+   * XXX: there is no irq routing in mimiker
+   *      so in this case we need to add irq manually
+   */
+  device_add_irq(dev, 0, 10);
+  state->irq_res = device_take_irq(dev, 0, RF_ACTIVE);
+  if (!state->regs || !state->irq_res) {
+    klog("rtl8139 failed to init resources");
     return ENXIO;
+  }
+  bus_intr_setup(dev, state->irq_res, rtl8139_intr, NULL, state,
+                 "RTL8139");
+
+  // TODO: mark this memory as PMAP_NOCACHE
+  // TODO: assure contig memory area
+  state->rx_buf = (vaddr_t)kmem_alloc(2 * PAGESIZE, M_ZERO);
+  if (!pmap_kextract(state->rx_buf, &state->paddr)) {
+    klog("failed to find paddr");
+    return ENXIO;
+  }
+  // TODO: introduce ring buffer
 
   if (rtl_reset(state)) {
     klog("RTL8139: Failed to reset device!");
     return ENXIO;
   }
+
+  bus_write_4(state->regs, 0x30, state->paddr);
+  bus_write_4(state->regs, 0x44, 0xf | (1 << 7));
+  bus_write_1(state->regs, 0x37, 0x0C);
+  // IMR - mask rx enable
+  bus_write_2(state->regs, 0x3C, 0x1);
 
   return 0;
 }
