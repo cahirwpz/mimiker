58c4c8f4a6fa0e6a2ba180cfef968993a7a9030b
wiklam <wiktorpilar99@gmail.com>
2020-12-21 04:51:10 +0100

    Correct error returning in do_clock_nanosleep

--- sys/kern/syscalls.c
+++ sys/kern/syscalls.c
@@ -533,24 +533,22 @@ static int sys_clock_nanosleep(proc_t *p, clock_nanosleep_args_t *args,
   const timespec_t *u_rqtp = SCARG(args, rqtp);
   timespec_t *u_rmtp = SCARG(args, rmtp);
   timespec_t rqtp, rmtp;
-  int error;
+  int error, copy_err;
 
   error = copyin_s(u_rqtp, rqtp);
-  if (error) {
-    *res = error;
-    return 0;
-  }
+  if (error)
+    return error;
 
   error = do_clock_nanosleep(clock_id, flags, &rqtp, u_rmtp ? &rmtp : NULL);
-  *res = error;
+
   if ((u_rmtp == NULL || (error != 0 && error != EINTR)))
-    return 0;
+    return error;
 
   /*  TIMER_ABSTIME - sleep to an absolute deadline */
-  if ((flags & TIMER_ABSTIME) == 0 && (error = copyout_s(rmtp, u_rmtp)))
-    *res = error;
+  if ((flags & TIMER_ABSTIME) == 0 && (copy_err = copyout_s(rmtp, u_rmtp)))
+    return copy_err;
 
-  return 0;
+  return error;
 }
 
 static int sys_sigaltstack(proc_t *p, sigaltstack_args_t *args,
