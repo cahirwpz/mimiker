29be69e9bf0def005c179792922014cec7c51de8
Jakub Piecuch <j.piecuch96@gmail.com>
2021-01-02 21:19:01 +0100

    Use ERESTARTSYS to stop background processes, remove t_background_cv.

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -427,51 +427,43 @@ static int tty_wait_background(tty_t *tty, int sig) {
   thread_t *td = thread_self();
   proc_t *p = td->td_proc;
   pgrp_t *pg;
-  int error;
 
   assert(sig == SIGTTIN || sig == SIGTTOU);
   assert(mtx_owned(&tty->t_lock));
 
-  while (true) {
-  tryagain:
-    WITH_PROC_LOCK(p) {
-      /*
-       * The process should only sleep, when:
-       * - This terminal is the controlling terminal
-       * - Its process group is not the foreground process
-       *   group
-       * - The signal to send to the process isn't masked
-       * - Its process group is not orphaned
-       */
-      if (!tty_is_ctty(tty, p) || p->p_pgrp == tty->t_pgrp)
-        return 0;
+tryagain:
+  WITH_PROC_LOCK(p) {
+    /*
+     * The current process group should only get the signal, when:
+     * - This terminal is the controlling terminal
+     * - The process group is not the foreground process group
+     * - The signal to send to the process isn't masked
+     * - Its process group is not orphaned
+     */
+    if (!tty_is_ctty(tty, p) || p->p_pgrp == tty->t_pgrp)
+      return 0;
 
-      if (p->p_sigactions[sig].sa_handler == SIG_IGN ||
-          __sigismember(&td->td_sigmask, sig))
-        return (sig == SIGTTOU ? 0 : EIO);
+    if (p->p_sigactions[sig].sa_handler == SIG_IGN ||
+        __sigismember(&td->td_sigmask, sig))
+      return (sig == SIGTTOU ? 0 : EIO);
 
-      pg = p->p_pgrp;
-    }
-
-    WITH_MTX_LOCK (&pg->pg_lock) {
-      /* Our process group might have changed: check. */
-      if (p->p_pgrp != pg)
-        goto tryagain;
-
-      /* Unlocked read of pg_jobc: not a big deal. */
-      if (pg->pg_jobc == 0)
-        return EIO;
-
-      /*
-       * Send the signal and sleep until we're in the foreground
-       * process group.
-       */
-      sig_pgkill(pg, &DEF_KSI_RAW(sig));
-    }
-
-    if ((error = tty_wait(tty, &tty->t_background_cv)))
-      return error;
+    pg = p->p_pgrp;
   }
+
+  WITH_MTX_LOCK (&pg->pg_lock) {
+    /* Our process group might have changed: check. */
+    if (p->p_pgrp != pg)
+      goto tryagain;
+
+    /* Unlocked read of pg_jobc: not a big deal. */
+    if (pg->pg_jobc == 0)
+      return EIO;
+
+    sig_pgkill(pg, &DEF_KSI_RAW(sig));
+  }
+
+  /* Handle the signal at userspace boundary. */
+  return ERESTARTSYS;
 }
 
 static void tty_in_hiwat(tty_t *tty) {
@@ -943,7 +935,6 @@ static int tty_set_fg_pgrp(tty_t *tty, pgid_t pgid) {
       if (tty->t_session != p->p_pgrp->pg_session)
         return ENOTTY;
       tty->t_pgrp = pg;
-      cv_broadcast(&tty->t_background_cv);
     }
   }
   return 0;
@@ -1010,7 +1001,6 @@ void tty_detach_driver(tty_t *tty) {
   cv_broadcast(&tty->t_incv);
   cv_broadcast(&tty->t_outcv);
   cv_broadcast(&tty->t_serialize_cv);
-  cv_broadcast(&tty->t_background_cv);
 
   tty_maybe_destroy(tty);
 }
