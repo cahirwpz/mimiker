e8af10c2a68028dba60159d28f63b64cfb1ecc35
Barbara Kaczorowska <bkjg252@gmail.com>
2021-01-04 20:31:19 +0100

    Initialize error

--- sys/kern/vm_map.c
+++ sys/kern/vm_map.c
@@ -254,7 +254,7 @@ int vm_map_insert(vm_map_t *map, vm_segment_t *seg, vm_flags_t flags) {
   size_t length = seg->end - seg->start;
   vm_seg_flags_t seg_flags = 0;
 
-  int error;
+  int error = 0;
 
   if ((flags & VM_FIXED)) {
     error = vm_map_preparespace_nolock(map, start, length);
