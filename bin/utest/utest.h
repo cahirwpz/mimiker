1a0e3822ced506e7bc61410d74693015cdfd57a5
Barbara Kaczorowska <bkjg252@gmail.com>
2021-01-07 19:37:26 +0100

    Merge with github-bkjg/copy-on-write - fix

--- bin/utest/utest.h
+++ bin/utest/utest.h
@@ -98,6 +98,6 @@ int test_get_set_groups(void);
 
 int test_sharing_memory_simple(void);
 int test_sharing_memory_child_and_grandchild(void);
-int test_cow_private_simple(void);
+int test_anon_private_simple(void);
 
 #endif /* __UTEST_H__ */
