3525cadd98136e2f71038c075914e129ee5f1ad7
Paweł Jasiak <pawel@jasiak.xyz>
2021-01-16 23:37:07 +0100

    Save FPU context on ctx_switch, restore on user_exc_leave.

--- sys/aarch64/trap.c
+++ sys/aarch64/trap.c
@@ -139,6 +139,10 @@ void user_trap_handler(mcontext_t *uctx) {
       sig_trap(ctx, SIGILL);
       break;
 
+    case EXCP_FP_SIMD:
+      thread_self()->td_pflags |= TDP_FPCTXVALID;
+      break;
+
     default:
       kernel_oops(ctx);
   }
