fc7a703a425276d3d305f1d9183b3764e1cdacbd
Paweł Jasiak <pawel@jasiak.xyz>
2021-01-18 14:28:36 +0100

    Format libc.

--- lib/libc/quad/umoddi3.c
+++ lib/libc/quad/umoddi3.c
@@ -40,11 +40,9 @@
 /*
  * Return remainder after dividing two unsigned quads.
  */
-u_quad_t
-__umoddi3(u_quad_t a, u_quad_t b)
-{
-	u_quad_t r;
+u_quad_t __umoddi3(u_quad_t a, u_quad_t b) {
+  u_quad_t r;
 
-	(void)__qdivrem(a, b, &r);
-	return (r);
+  (void)__qdivrem(a, b, &r);
+  return (r);
 }
