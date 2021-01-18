c83e85c9354b939db175309d7287faf2ce01159d
Paweł Jasiak <pawel@jasiak.xyz>
2021-01-18 17:00:18 +0100

    Format libc.

--- lib/libc/quad/udivdi3.c
+++ lib/libc/quad/udivdi3.c
@@ -40,9 +40,7 @@
 /*
  * Divide two unsigned quads.
  */
-u_quad_t
-__udivdi3(u_quad_t a, u_quad_t b)
-{
+u_quad_t __udivdi3(u_quad_t a, u_quad_t b) {
 
-	return (__qdivrem(a, b, (u_quad_t *)0));
+  return (__qdivrem(a, b, (u_quad_t *)0));
 }
