e254b3c1e4b03623d7c1589a66e9bfc4636d990c
Jakub Piecuch <j.piecuch96@gmail.com>
2021-01-24 21:55:21 +0100

    do_setitimer: undo ts2hz's increment.

--- sys/kern/time.c
+++ sys/kern/time.c
@@ -252,7 +252,8 @@ int do_setitimer(proc_t *p, int which, const struct itimerval *itval,
     return EINVAL;
 
   systime_t next = tv2hz(&itval->it_value);
-  systime_t interval = tv2hz(&itval->it_interval);
+  systime_t interval =
+    tv2hz(&itval->it_interval) - 1; /* ts2hz increments its result by 1 */
 
   if (next == UINT_MAX || interval == UINT_MAX)
     return EINVAL;
