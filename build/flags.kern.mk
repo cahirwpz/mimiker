b56556258327f7c085b5ea9f4fba0e5dde0f47c5
wiklam <wiktorpilar99@gmail.com>
2021-02-01 02:49:07 +0100

    I will add aarch later

--- build/flags.kern.mk
+++ build/flags.kern.mk
@@ -5,10 +5,3 @@ CPPFLAGS += -I$(TOPDIR)/include -D_KERNEL
 LDFLAGS  += -nostdlib
 
 KERNEL := 1
-
-KPROF ?= 0
-ifeq ($(KPROF), 1)
-  CFLAGS_KPROF = -finstrument-functions -finstrument-functions-exclude-file-list=sys/kern/prof.c,lib -finstrument-functions-exclude-function-list=intr_disable,intr_enable,thread_self,intr_root_handler
-endif
-# Added to all files
-CFLAGS += -DKPROF=$(KPROF)
