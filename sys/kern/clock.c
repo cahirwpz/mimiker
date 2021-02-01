5f9ff68a80ccc3642efc97b540348ecb96719fd0
wiklam <wiktorpilar99@gmail.com>
2021-02-01 03:06:48 +0100

    Statclock

--- sys/kern/clock.c
+++ sys/kern/clock.c
@@ -4,21 +4,47 @@
 #include <sys/mimiker.h>
 #include <sys/klog.h>
 #include <sys/timer.h>
+#include <sys/context.h>
+#include <sys/gmon.h>
 
 static systime_t now = 0;
 static timer_t *clock = NULL;
-
 systime_t getsystime(void) {
   return now;
 }
 
+#if KPROF
+static void statclock(void) {
+  gmonparam_t *g = &_gmonparam;
+  thread_t *td = thread_self();
+  klog("Got thread - ctx %d.", td->td_kframe);
+  if (td->td_kframe == NULL)
+    return;
+  uintptr_t pc = ctx_get_pc(td->td_kframe), instr;
+  klog("Got pc register.");
+  if (g->state == GMON_PROF_ON && pc >= g->lowpc) {
+    klog("Gmon working.");
+    instr = pc - g->lowpc;
+    if (instr < g->textsize) {
+      klog("Gmon collecting stats.");
+      instr /= HISTFRACTION * sizeof(*g->kcount);
+      g->kcount[instr]++;
+      klog("Gmon updated instr %d kcountsize %d.", instr, g->kcountsize);
+    }
+  }
+}
+#else
+static void statclock(void) {
+}
+#endif
+
 static void clock_cb(timer_t *tm, void *arg) {
   bintime_t bin = binuptime();
   now = bt2st(&bin);
+  statclock();
   callout_process(now);
   sched_clock();
 }
-
 void init_clock(void) {
   clock = tm_reserve(NULL, TMF_PERIODIC);
   if (clock == NULL)
