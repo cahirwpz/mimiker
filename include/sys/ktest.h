6d758b9c6ffe33fb75efb058035e03e48a760cdc
Krystian Bacławski <krystian.baclawski@gmail.com>
2021-02-02 14:15:23 +0100

    hot-fix: improve test run output on test failure.
     * Put user binaries into artifacts as well.
     * Divert ktest output to klog.
     * Simplify ktest module.
     * Disable `cpu tlb` since it sometimes hangs.
     * run_tests.py: set up terminal size for launch.

--- include/sys/ktest.h
+++ include/sys/ktest.h
@@ -12,26 +12,25 @@
 
 #define KTEST_NAME_MAX 50
 
-#define KTEST_SUCCESS 0
-#define KTEST_FAILURE 1
+typedef enum test_result {
+  KTEST_SUCCESS = 0,
+  KTEST_FAILURE = 1,
+} test_result_t;
 
-/* Signifies that a test does not return. */
-#define KTEST_FLAG_NORETURN 0x01
-/* Signifies that a test irreversibly breaks internal kernel state, and any
-   further test done without restarting the kernel will be inconclusive. */
-#define KTEST_FLAG_DIRTY 0x02
-/* Indicates that a test enters usermode. */
-#define KTEST_FLAG_USERMODE 0x04
-/* Excludes the test from being run in auto mode. This flag is only useful for
-   temporarily marking some tests while debugging the testing framework. */
-#define KTEST_FLAG_BROKEN 0x08
-/* Marks that the test wishes to receive a random integer as an argument. */
-#define KTEST_FLAG_RANDINT 0x10
+typedef enum test_flag {
+  /* Indicates that a test enters usermode. */
+  KTEST_FLAG_USERMODE = 1,
+  /* Excludes the test from being run in auto mode. This flag is only useful for
+   * temporarily marking some tests while debugging the testing framework. */
+  KTEST_FLAG_BROKEN = 2,
+  /* Marks that the test wishes to receive a random integer as an argument. */
+  KTEST_FLAG_RANDINT = 4,
+} test_flag_t;
 
 typedef struct {
   const char test_name[KTEST_NAME_MAX];
   int (*test_func)(void);
-  uint32_t flags;
+  test_flag_t flags;
   uint32_t randint_max;
 } test_entry_t;
 
@@ -44,11 +43,6 @@ __noreturn void ktest_main(const char *test);
 #define KTEST_ADD_RANDINT(name, func, flags, max)                              \
   test_entry_t name##_test = {#name, func, flags | KTEST_FLAG_RANDINT, max};   \
   SET_ENTRY(tests, name##_test);
-
-/* These are canonical result messages printed to standard output / UART. A
- * script running the kernel may want to grep for them. */
-#define TEST_PASSED_STRING "[TEST PASSED]\n"
-#define TEST_FAILED_STRING "[TEST FAILED]\n"
 
 /* This function is called both by run_test, as well as ktest_assert.
  * It displays some troubleshooting info about the failing test. */
