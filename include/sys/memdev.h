527a3c6c3132d8c359567858608e1ca938e57ef8
mohrcore <krzys.boronski@gmail.com>
2021-02-08 18:11:18 +0100

    Fixed formatting

--- include/sys/memdev.h
+++ include/sys/memdev.h
@@ -4,14 +4,13 @@
 #include <sys/types.h>
 #include <sys/device.h>
 
-typedef int(*readblock_t)(device_t *, uint32_t, unsigned char *, uint32_t);
-typedef int(*writeblock_t)(device_t *, uint32_t, const uint8_t *, uint32_t);
+typedef int (*readblock_t)(device_t *, uint32_t, unsigned char *, uint32_t);
+typedef int (*writeblock_t)(device_t *, uint32_t, const uint8_t *, uint32_t);
 
 typedef struct blockdev_methods {
   readblock_t read;
   writeblock_t write;
 } blockdev_methods_t;
-
 
 #define BLOCK_MEM_METHODS(dev)                                                 \
   (*(bus_methods_t *)(dev)->driver->interfaces[DIF_BUS])
