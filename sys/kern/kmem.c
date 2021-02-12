61bba7b5a78a09919a3c41b87ddf97cb7c919d7f
Krystian Bacławski <krystian.baclawski@gmail.com>
2021-02-12 12:21:44 +0100

    kmem: allocate contiguous physical memory (#992)

--- sys/kern/kmem.c
+++ sys/kern/kmem.c
@@ -4,6 +4,7 @@
 #include <sys/libkern.h>
 #include <sys/param.h>
 #include <sys/pmap.h>
+#include <sys/kmem.h>
 #include <sys/vmem.h>
 #include <sys/vm.h>
 #include <sys/vm_physmem.h>
@@ -37,6 +38,11 @@ void kva_free(vaddr_t ptr, size_t size) {
   vmem_free(kvspace, ptr, size);
 }
 
+static void kva_map_page(vaddr_t va, paddr_t pa, size_t n, unsigned flags) {
+  for (size_t i = 0; i < n; i++, va += PAGESIZE, pa += PAGESIZE)
+    pmap_kenter(va, pa, VM_PROT_READ | VM_PROT_WRITE, flags);
+}
+
 void kva_map(vaddr_t ptr, size_t size, kmem_flags_t flags) {
   assert(page_aligned_p(size));
 
@@ -51,13 +57,10 @@ void kva_map(vaddr_t ptr, size_t size, kmem_flags_t flags) {
     kick_swapper();
 
   vaddr_t va = ptr;
-
   vm_page_t *pg;
   TAILQ_FOREACH (pg, &pglist, pageq) {
-    paddr_t pa = pg->paddr;
-    size_t n = pg->size;
-    for (size_t i = 0; i < n; i++, va += PAGESIZE, pa += PAGESIZE)
-      pmap_kenter(va, pa, VM_PROT_READ | VM_PROT_WRITE, 0);
+    kva_map_page(va, pg->paddr, pg->size, 0);
+    va += pg->size * PAGESIZE;
   }
 
   if (flags & M_ZERO)
@@ -101,13 +104,34 @@ void *kmem_alloc(size_t size, kmem_flags_t flags) {
   return (void *)start;
 }
 
+vaddr_t kmem_alloc_contig(paddr_t *pap, size_t size, unsigned flags) {
+  assert(page_aligned_p(size) && powerof2(size));
+
+  size_t n = size / PAGESIZE;
+  vm_page_t *pg = vm_page_alloc(n);
+  if (!pg)
+    return 0;
+
+  vaddr_t va;
+  if (vmem_alloc(kvspace, size, &va, M_NOGROW))
+    kick_swapper();
+
+  /* Mark the entire block as valid */
+  kasan_mark_valid((void *)va, size);
+
+  kva_map_page(va, pg->paddr, pg->size, flags);
+
+  *pap = pg->paddr;
+  return va;
+}
+
 void kmem_free(void *ptr, size_t size) {
   klog("%s: free %p of size %ld", __func__, ptr, size);
   kva_unmap((vaddr_t)ptr, size);
   vmem_free(kvspace, (vmem_addr_t)ptr, size);
 }
 
-vaddr_t kmem_map(paddr_t pa, size_t size, unsigned flags) {
+vaddr_t kmem_map_contig(paddr_t pa, size_t size, unsigned flags) {
   assert(page_aligned_p(pa) && page_aligned_p(size));
 
   vmem_addr_t start;
