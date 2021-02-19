d0d725d9dbd9c71b37037825a7fa7e200c7d061a
Paweł Jasiak <pawel@jasiak.xyz>
2021-02-19 12:10:37 +0100

    AArch64/dbg: adjust ctx.py to AArch64.

--- sys/debug/ctx.py
+++ sys/debug/ctx.py
@@ -5,21 +5,33 @@ from .utils import TextTable
 
 
 class Context():
-    names = ['at', 'v0', 'v1', 'a0', 'a1', 'a2', 'a3',
-             't0', 't1', 't2', 't3', 't4', 't5', 't6', 't7',
-             's0', 's1', 's2', 's3', 's4', 's5', 's6', 's7',
-             't8', 't9', 'k0', 'k1', 'gp', 'sp', 's8', 'ra',
-             'lo', 'hi', 'cause', 'pc', 'sr', 'bad']
+    names = {
+        'mips': ['at', 'v0', 'v1', 'a0', 'a1', 'a2', 'a3',
+                't0', 't1', 't2', 't3', 't4', 't5', 't6', 't7',
+                's0', 's1', 's2', 's3', 's4', 's5', 's6', 's7',
+                't8', 't9', 'k0', 'k1', 'gp', 'sp', 's8', 'ra',
+                'lo', 'hi', 'cause', 'pc', 'sr', 'bad'],
+        'aarch64': ['x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7',
+                    'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15',
+                    'x16', 'x17', 'x18', 'x19', 'x20', 'x21', 'x22', 'x23',
+                    'x24', 'x25', 'x26', 'x27', 'x28', 'x29', 'lr', 'sp', 'pc']
+    }
 
     def __init__(self):
+        if gdb.selected_frame().architecture().name() == 'mips:isa32r2':
+            self.arch = 'mips'
+            self.reg_size = 32
+        if gdb.selected_frame().architecture().name() == 'aarch64':
+            self.arch = 'aarch64'
+            self.reg_size = 64
         self.regs = OrderedDict()
-        for name in self.names:
+        for name in self.names[self.arch]:
             self.regs[name] = 0
 
     @classmethod
     def from_kctx(cls, kctx):
         c = Context()
-        for i, name in enumerate(cls.names):
+        for i, name in enumerate(cls.names[c.arch]):
             c.regs[name] = int(kctx['__gregs'][i])
         # `kctx` points to the context on the kernel stack.
         # The program counter in that context is actually the return address
@@ -38,13 +50,17 @@ class Context():
     @classmethod
     def current(cls):
         c = Context()
-        for name in cls.names:
+        for name in cls.names[c.arch]:
             val = gdb.parse_and_eval('$' + name)
             c.regs[name] = int(val.cast(gdb.lookup_type('__greg_t')))
         return c
 
     def dump(self):
         table = TextTable(align='rl')
-        table.add_rows([[name, '0x%08x' % (val & 0xffffffff)]
-                        for name, val in self.regs.items()])
+        if self.reg_size == 32:
+            table.add_rows([[name, '0x%08x' % (val & 0xffffffff)]
+                            for name, val in self.regs.items()])
+        elif self.reg_size == 64:
+            table.add_rows([[name, '0x%16x' % (val & 0xffffffffffffffff)]
+                            for name, val in self.regs.items()])
         print(table)
