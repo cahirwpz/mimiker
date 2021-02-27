56947d959c4a1fc6d266b23bd3fda03cf9343faf
Jakub Piecuch <j.piecuch96@gmail.com>
2021-02-27 12:43:25 +0100

    callout: Reschedule callouts from a callback (#950)
    Co-authored-by: Krystian Bacławski <krystian.baclawski@gmail.com>

--- include/sys/callout.h
+++ include/sys/callout.h
@@ -20,28 +20,43 @@ typedef struct callout {
 /* callout has been delegated to callout thread and will be executed soon */
 #define CALLOUT_ACTIVE 0x0001
 #define CALLOUT_PENDING 0x0002 /* callout is waiting for timeout */
+#define CALLOUT_STOPPED 0x0004 /* disallow rescheduling */
 
 /*! \brief Called during kernel initialization. */
 void init_callout(void);
 
-/*
- * Add a callout to the queue.
- * At tick @time function @fn is called with argument @arg.
- */
-void callout_setup(callout_t *handle, systime_t time, timeout_t fn, void *arg);
+/* Set up callout @co to call @fn with argument @arg. */
+void callout_setup(callout_t *co, timeout_t fn, void *arg);
 
 /*
- * Add a callout to the queue, using timing relative to current time.
- * After ticks @time passed the function @fn is called with argument @arg.
+ * Add a callout to the queue, using time relative to current time.
+ * After ticks @tm passed callout's function will be called.
  */
-void callout_setup_relative(callout_t *handle, systime_t time, timeout_t fn,
-                            void *arg);
+void callout_schedule(callout_t *co, systime_t tm);
+
+/*
+ * Add a callout to the queue, using absolute time.
+ * After @tm tick passed callout's function will be called.
+ * Caller must provide @tm that is not lesser than current system tick.
+ */
+void callout_schedule_abs(callout_t *co, systime_t tm);
+
+/*
+ * Reschedule a running callout.
+ * This function is intended to be called from the callout's function.
+ * It can be used to implement e.g. periodic timers.
+ * `tm` is an absolute time, same as in callout_schedule_abs().
+ * Returns false if the rescheduling failed due to the callout being stopped.
+ */
+bool callout_reschedule(callout_t *co, systime_t tm);
 
 /*
  * Cancel a callout if it is currently pending.
  *
  * \return True if the callout was pending and has been stopped, false if the
  * callout has already been delegated to callout thread or executed.
+ * A callout can't be rescheduled using callout_reschedule() after calling this
+ * function on it until it is scheduled again using callout_schedule*().
  *
  * \warning It's not safe to deallocate callout memory after it has been
  * stopped. You should use \a callout_drain if you need that.
