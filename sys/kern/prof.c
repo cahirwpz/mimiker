b0896960f065a5199fdcdf80d6792e4d00cae502
wiklam <wiktorpilar99@gmail.com>
2021-03-01 02:40:01 +0100

    Adding alignment asserts

--- sys/kern/prof.c
+++ sys/kern/prof.c
@@ -40,10 +40,13 @@ void init_prof(void) {
     klog("Not enough memory for profiling!\n");
     return;
   }
+  assert(is_aligned(profptr, sizeof(tostruct_t)));
   p->tos = (tostruct_t *)profptr;
   profptr += p->tossize;
+  assert(is_aligned(profptr, sizeof(u_short)));
   p->kcount = (u_short *)profptr;
   profptr += p->kcountsize;
+  assert(is_aligned(profptr, sizeof(u_short)));
   p->froms = (u_short *)profptr;
   p->state = GMON_PROF_ON;
 }
