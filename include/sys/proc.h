c2d0b6119f6aa6c6b88400c9ec9061c33a6e58b3
franciscozdo <44414770+franciscozdo@users.noreply.github.com>
2021-03-08 18:58:31 +0100

    procstat device: reports list of all processes (#974)

--- include/sys/proc.h
+++ include/sys/proc.h
@@ -23,6 +23,7 @@ typedef TAILQ_HEAD(, pgrp) pgrp_list_t;
 typedef TAILQ_HEAD(, session) session_list_t;
 
 extern mtx_t *all_proc_mtx;
+extern proc_list_t proc_list, zombie_list;
 
 /*! \brief Called during kernel initialization. */
 void init_proc(void);
