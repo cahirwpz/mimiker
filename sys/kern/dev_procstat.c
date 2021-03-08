3e532f4421168185aa897718b7b87564707c82ee
Franciszek Zdobylak <zdobyl.frank@gmail.com>
2021-03-08 11:39:00 +0100

    Change names

--- sys/kern/dev_procstat.c
+++ sys/kern/dev_procstat.c
@@ -30,14 +30,14 @@
 /* we want to have at most 10 instances of procstat */
 #define MAX_PROCSTAT 10
 
-static char state[4] = {
+static char proc_state[4] = {
   [PS_NORMAL] = 'R',
   [PS_STOPPED] = 'S',
   [PS_DYING] = 'D',
   [PS_ZOMBIE] = 'Z',
 };
 
-typedef struct proc_info {
+typedef struct ps_entry {
   pid_t pid;
   pid_t ppid;
   uid_t uid;
@@ -45,19 +45,19 @@ typedef struct proc_info {
   sid_t sid;
   proc_state_t state;
   char *elfpath;
-} proc_info_t;
+} ps_entry_t;
 
-typedef struct pstat {
+typedef struct ps_buf {
   int nproc;
   int cur_proc;
   size_t offset; /* offset in buffer of current proc string */
   size_t psize;  /* size of current proc string */
   char buf[MAX_P_STRING];
-  proc_info_t ps[];
-} pstat_t;
+  ps_entry_t ps[];
+} ps_buf_t;
 
-static int instances = 0;
-static mtx_t instances_mtx;
+static int ps_opencnt = 0;
+static mtx_t procstat_lock;
 
 static int dev_procstat_read(file_t *f, uio_t *uio);
 static int dev_procstat_close(vnode_t *v, file_t *fp);
@@ -75,34 +75,34 @@ static vnodeops_t dev_procstat_vnodeops = {
   .v_close = dev_procstat_close,
 };
 
-static void fill_proc_info(proc_t *p, proc_info_t *pi) {
+static void ps_entry_fill(proc_t *p, ps_entry_t *pi) {
   SCOPED_MTX_LOCK(&p->p_lock);
   pi->uid = p->p_cred.cr_euid;
   pi->pid = p->p_pid;
   pi->ppid = p->p_parent->p_pid;
   pi->pgrp = p->p_pgrp->pg_id;
   pi->sid = p->p_pgrp->pg_session->s_sid;
-  pi->state = p->p_state;
+  pi->proc_state = p->p_state;
   pi->elfpath = kstrndup(M_TEMP, p->p_elfpath, PATH_MAX);
 }
 
 /* buf must be at least MAX_P_STRING long
  * returns length of written string
  */
-static int sprint_proc(char *buf, proc_info_t *pi) {
+static int ps_entry_tostring(char *buf, ps_entry_t *pi) {
   int r = snprintf(buf, MAX_P_STRING, "%d\t%d\t%d\t%d\t%d\t%c\t%s\n", pi->uid,
-                   pi->pid, pi->ppid, pi->pgrp, pi->sid, state[pi->state],
-                   pi->elfpath);
+                   pi->pid, pi->ppid, pi->pgrp, pi->sid,
+                   proc_state[pi->proc_state], pi->elfpath);
 
   return MIN(r, MAX_P_STRING);
 }
 
 static int dev_procstat_open(vnode_t *v, int mode, file_t *fp) {
-  WITH_MTX_LOCK (&instances_mtx) {
-    if (instances >= MAX_PROCSTAT)
+  WITH_MTX_LOCK (&procstat_lock) {
+    if (ps_opencnt >= MAX_PROCSTAT)
       return EMFILE;
-    instances++;
-    assert(instances <= MAX_PROCSTAT);
+    ps_opencnt++;
+    assert(ps_opencnt <= MAX_PROCSTAT);
   }
 
   int error;
@@ -110,8 +110,8 @@ static int dev_procstat_open(vnode_t *v, int mode, file_t *fp) {
     return error;
 
   proc_t *p;
-  pstat_t *ps =
-    kmalloc(M_TEMP, MAX_PROC * sizeof(proc_info_t) + sizeof(pstat_t), 0);
+  ps_buf_t *ps =
+    kmalloc(M_TEMP, MAX_PROC * sizeof(ps_entry_t) + sizeof(ps_buf_t), 0);
   if (ps == NULL)
     return ENOMEM;
 
@@ -123,17 +123,17 @@ static int dev_procstat_open(vnode_t *v, int mode, file_t *fp) {
         continue; /* we don't want to show proc0 to user */
       if (ps->nproc >= MAX_PROC)
         goto out;
-      fill_proc_info(p, &ps->ps[ps->nproc++]);
+      ps_entry_fill(p, &ps->ps[ps->nproc++]);
     }
     TAILQ_FOREACH (p, &zombie_list, p_zombie) {
       if (ps->nproc >= MAX_PROC)
         goto out;
-      fill_proc_info(p, &ps->ps[ps->nproc++]);
+      ps_entry_fill(p, &ps->ps[ps->nproc++]);
     }
   }
 out:
   /* get first proc into buffer */
-  ps->psize = sprint_proc(ps->buf, &ps->ps[0]);
+  ps->psize = ps_entry_tostring(ps->buf, &ps->ps[0]);
   ps->offset = 0;
   ps->cur_proc = 0;
 
@@ -143,7 +143,7 @@ out:
 }
 
 static int dev_procstat_read(file_t *f, uio_t *uio) {
-  pstat_t *ps = f->f_data;
+  ps_buf_t *ps = f->f_data;
   int error = 0;
 
   if (ps->cur_proc >= ps->nproc)
@@ -161,7 +161,7 @@ static int dev_procstat_read(file_t *f, uio_t *uio) {
     if (ps->offset >= ps->psize) {
       if (++ps->cur_proc >= ps->nproc)
         break;
-      ps->psize = sprint_proc(ps->buf, &ps->ps[ps->cur_proc]);
+      ps->psize = ps_entry_tostring(ps->buf, &ps->ps[ps->cur_proc]);
       ps->offset = 0;
     }
   }
@@ -171,21 +171,21 @@ static int dev_procstat_read(file_t *f, uio_t *uio) {
 }
 
 static int dev_procstat_close(vnode_t *v, file_t *fp) {
-  pstat_t *ps = fp->f_data;
+  ps_buf_t *ps = fp->f_data;
   for (int i = 0; i < ps->nproc; ++i) {
     kfree(M_TEMP, ps->ps[i].elfpath);
   }
   kfree(M_TEMP, ps);
 
-  WITH_MTX_LOCK (&instances_mtx) {
-    instances--;
-    assert(instances >= 0);
+  WITH_MTX_LOCK (&procstat_lock) {
+    ps_opencnt--;
+    assert(ps_opencnt >= 0);
   }
   return 0;
 }
 
 static void init_dev_procstat(void) {
-  mtx_init(&instances_mtx, 0);
+  mtx_init(&procstat_lock, 0);
   devfs_makedev(NULL, "procstat", &dev_procstat_vnodeops, NULL, NULL);
 }
 
