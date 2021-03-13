87a9cc1fe82228682929c817753d49a5c94b8b84
Jakub Piecuch <j.piecuch96@gmail.com>
2021-03-13 16:08:54 +0100

    Add OPTFLAGS make variable

--- build/flags.mk
+++ build/flags.mk
@@ -1,4 +1,5 @@
 ASFLAGS  += -Wall -Wextra -Werror
 WFLAGS   += -Wall -Wextra -Wno-unused-parameter -Wstrict-prototypes -Werror
-CFLAGS   += -std=gnu11 -Og -ggdb3 -fomit-frame-pointer
+OPTFLAGS ?= -Og
+CFLAGS   += -std=gnu11 $(OPTFLAGS) -ggdb3 -fomit-frame-pointer
 CPPFLAGS += -DDEBUG
