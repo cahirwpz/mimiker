8d7e35a885718892e33ea707e8734139b5d4652b
Jakub Piecuch <j.piecuch96@gmail.com>
2021-03-18 18:00:35 +0100

    Make Clang & GCC happy

--- lib/libc/sys/signal.c
+++ lib/libc/sys/signal.c
@@ -1,16 +1,18 @@
 #include <errno.h>
+#include <string.h>
 #include <signal.h>
 
 extern sigset_t __sigintr; /* Shared with siginterrupt() */
 
 sig_t signal(int sig, sig_t handler) {
-  sigaction_t act = {0}, oldact;
+  sigaction_t act, oldact;
 
   if (handler == SIG_ERR || sig < 1 || sig >= NSIG) {
     errno = EINVAL;
     return SIG_ERR;
   }
 
+  memset(&act, 0, sizeof(sigaction_t));
   act.sa_handler = handler;
   if (!__sigismember(&__sigintr, sig))
     act.sa_flags |= SA_RESTART;
