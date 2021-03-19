83c69de0d6044cd932271dbedad0299c094d37d4
Jakub Urbańczyk <xthaid@gmail.com>
2021-03-19 21:21:20 +0100

    Bump gcc version to 11

--- toolchain/gnu/config.mk
+++ toolchain/gnu/config.mk
@@ -1,14 +1,14 @@
 # vim: tabstop=8 shiftwidth=8 noexpandtab list:
 
-VERSION = 1.4
+VERSION = 1.5
 
 ISL = isl-0.18
 MPFR = mpfr-4.0.2
 GMP = gmp-6.1.2
 MPC = mpc-1.1.0
 CLOOG = cloog-0.18.4
-BINUTILS = binutils-2.32
-GCC = gcc-9.2.0
+BINUTILS = binutils-2.35
+GCC = gcc-11-20210314
 GDB = gdb-10.1
 
 ISL-URL = "http://isl.gforge.inria.fr/$(ISL).tar.xz"
@@ -17,7 +17,7 @@ GMP-URL = "https://gmplib.org/download/gmp/$(GMP).tar.xz"
 MPC-URL = "https://ftp.gnu.org/gnu/mpc/$(MPC).tar.gz"
 CLOOG-URL = "http://www.bastoul.net/cloog/pages/download/$(CLOOG).tar.gz"
 BINUTILS-URL = "https://ftp.gnu.org/gnu/binutils/$(BINUTILS).tar.xz"
-GCC-URL = "https://ftp.gnu.org/gnu/gcc/$(GCC)/$(GCC).tar.xz"
+GCC-URL = "ftp://ftp.fu-berlin.de/unix/languages/gcc/snapshots/11-20210314/$(GCC).tar.xz"
 GDB-URL = "https://ftp.gnu.org/gnu/gdb/$(GDB).tar.xz"
 
 TARGETS = mipsel aarch64
