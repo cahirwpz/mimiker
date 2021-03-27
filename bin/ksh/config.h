9f50bc5ab53dc86cc9e00899b418b413d0afa27d
Jakub Piecuch <j.piecuch96@gmail.com>
2021-03-27 16:37:28 +0100

    Add note about location of .kshrc

--- bin/ksh/config.h
+++ bin/ksh/config.h
@@ -229,6 +229,7 @@
 #define POSIXLY_CORRECT 1
 
 /* Specify default $ENV? */
+/* TODO: change this back to "$HOME/.kshrc" once we have home directories. */
 #define DEFAULT_ENV	"/etc/.kshrc"
 
 /* Include shl(1) support? */
