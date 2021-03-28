07f1b1d1a71ae649acc4df57378525801b6befb5
Paweł Jasiak <pawel@jasiak.xyz>
2021-03-28 14:53:53 +0200

    build: retab arch.aarch64.mk

--- build/arch.aarch64.mk
+++ build/arch.aarch64.mk
@@ -5,21 +5,21 @@ ELFTYPE := elf64-littleaarch64
 ELFARCH := aarch64
 
 ifeq ($(KERNEL), 1)
-KASAN ?= 0
-CFLAGS += -mcpu=cortex-a53+nofp -march=armv8-a+nofp -mgeneral-regs-only
-ifeq ($(KASAN), 1)
-  # Added to files that are sanitized
-  CFLAGS_KASAN = -fsanitize=kernel-address \
-		   -fasan-shadow-offset=0xe0001f0000000000 \
-                 --param asan-globals=1 \
-                 --param asan-stack=1 \
-                 --param asan-instrument-allocas=1
-endif
-CFLAGS += -DKASAN=$(KASAN)
+	KASAN ?= 0
+	CFLAGS += -mcpu=cortex-a53+nofp -march=armv8-a+nofp -mgeneral-regs-only
+	ifeq ($(KASAN), 1)
+	# Added to files that are sanitized
+	CFLAGS_KASAN = -fsanitize=kernel-address \
+		       -fasan-shadow-offset=0xe0001f0000000000 \
+		       --param asan-globals=1 \
+		       --param asan-stack=1 \
+		       --param asan-instrument-allocas=1
+	endif
+	CFLAGS += -DKASAN=$(KASAN)
 else
-CFLAGS += -mcpu=cortex-a53 -march=armv8-a
+	CFLAGS += -mcpu=cortex-a53 -march=armv8-a
 endif
 
 ifeq ($(BOARD), rpi3)
-KERNEL-IMAGE := mimiker.img.gz
+	KERNEL-IMAGE := mimiker.img.gz
 endif
