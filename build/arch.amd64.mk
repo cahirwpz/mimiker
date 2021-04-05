538bb32c3a4301ebccbba6a198534214319adad0
MichalBlk <michal.blaszczyk09@gmail.com>
2021-04-06 00:18:28 +0200

    Don't  use the framebuffer for now.

--- build/arch.amd64.mk
+++ build/arch.amd64.mk
@@ -10,7 +10,7 @@ GCC_ABIFLAGS :=
 CLANG_ABIFLAGS := -target x86_64-elf
 ELFTYPE := elf64-x86-64
 ELFARCH := i386:x86-64
-LDFLAGS += -z max-page-size=0x200000
+LDFLAGS += -z max-page-size=0x1000
 
 ifeq ($(KERNEL), 1)
 # Interrupts in kernel mode doesn't cause stack switch,
