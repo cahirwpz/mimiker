c35cc05948c8d1d5085532d9f1b990f2966cb890
MichalBlk <michal.blaszczyk09@gmail.com>
2021-04-05 23:38:32 +0200

    Disable SIMD registers and add descriptions.

--- build/arch.amd64.mk
+++ build/arch.amd64.mk
@@ -1,10 +1,23 @@
+# vim: tabstop=8 shiftwidth=8 noexpandtab:
+#
+# Common makefile which specifies aarch64 architecture specific settings.
+#
+# Required common variables: KERNEL, BOARD.
+#
+
 TARGET := x86_64-mimiker-elf
 GCC_ABIFLAGS :=
 CLANG_ABIFLAGS := -target x86_64-elf
 ELFTYPE := elf64-x86-64
 ELFARCH := i386:x86-64
-LDFLAGS += -z max-page-size=0x1000
+LDFLAGS += -z max-page-size=0x200000
 
 ifeq ($(KERNEL), 1)
-CFLAGS += -mno-red-zone
+# Interrupts in kernel mode doesn't cause stack switch,
+# hence leaf functions are in danger.
+CFLAGS += -mno-red-zone 
+# Avoid SIMD registers to increase performance.
+CFLAGS += -mno-mmx -mno-sse -mno-avx
+# Ensure absence of x87 registers.
+CFLAGS += -msoft-float
 endif
