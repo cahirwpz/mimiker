eee72209b8b3b385ac50f60e2b48bc3be9ac9ca7
MichalBlk <michal.blaszczyk09@gmail.com>
2021-04-12 11:54:57 +0200

    Comment red zones.

--- build/arch.amd64.mk
+++ build/arch.amd64.mk
@@ -15,8 +15,12 @@ LDFLAGS += -z max-page-size=0x200000
 KERNEL-IMAGE := mimiker.elf32
 
 ifeq ($(KERNEL), 1)
-# Interrupts in kernel mode doesn't cause stack switch,
-# hence leaf functions are in danger.
+# In kernel the privilege level is 0, and thus an interrupt handling 
+# doesn't switch the stack, therefore the current stack is used. 
+# Leaf function are free to use so called red zones specified in
+# https://uclibc.org/docs/psABI-x86_64.pdf (3.2.2 The Stack Frame).
+# The following flag ensures that GCC won't use red zones throughout 
+# the kernel (instead, data will be explicitly allocated on the stack).
 CFLAGS += -mno-red-zone 
 # Avoid SIMD registers to increase performance.
 CFLAGS += -mno-mmx -mno-sse -mno-avx
