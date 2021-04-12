c42a75a619d51d2573b2f0f5682ab9c9208ea2cf
wiklam <wiktorpilar99@gmail.com>
2021-04-12 03:25:29 +0200

    Update excluded functions.

--- build/arch.mips.mk
+++ build/arch.mips.mk
@@ -26,7 +26,7 @@ GCC_ABIFLAGS += -msoft-float
 CLANG_ABIFLAGS += -msoft-float
 ifeq ($(KGPROF), 1)
   CFLAGS_KGPROF = -finstrument-functions \
-                  -finstrument-functions-exclude-file-list=sys/mips,spinlock.c  \
-                  -finstrument-functions-exclude-function-list=intr_disable,intr_enable,thread_self,intr_root_handler
+                  -finstrument-functions-exclude-file-list=spinlock.c,trap.c \
+                  -finstrument-functions-exclude-function-list=intr_disable,intr_enable,thread_self,intr_root_handler,user_mode_p
 endif
 endif
