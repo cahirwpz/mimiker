91c4ffc62ed640cd120bb83949caf431b9e520c2
Franciszek Zdobylak <zdobyl.frank@gmail.com>
2021-04-22 17:47:22 +0200

    Rename uvm -> vm (in anons)

---
+++ sys/tests/vm_anon.c
@@ -0,0 +1,25 @@
+#include <sys/klog.h>
+#include <sys/ktest.h>
+#include <sys/vm_anon.h>
+
+static int test_anon_simple(void) {
+
+  vm_anon_t *anon = vm_anon_alloc();
+  assert(anon != NULL);
+
+  vm_anon_lock(anon);
+
+  vm_anon_hold(anon);
+  vm_anon_hold(anon);
+  assert(anon->an_ref == 2);
+
+  vm_anon_drop(anon);
+  assert(anon->an_ref == 1);
+
+  vm_anon_lock(anon);
+  vm_anon_drop(anon);
+
+  return KTEST_SUCCESS;
+}
+
+KTEST_ADD(vm_anon_simple, test_anon_simple, 0);
