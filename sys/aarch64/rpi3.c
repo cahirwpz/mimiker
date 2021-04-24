e015353ae570fd7b2752715e65ebda3837d36d20
Paweł Jasiak <pawel@jasiak.xyz>
2021-04-24 17:48:53 +0200

    aarch64/boot: use dtb instead of atags
    * remove atags from Mimiker
    * grab memory size, initrd location, kernel cmdline from dtb
    * minimal dts for rpi3
    * boot qemu with dtb for rpi3 board
    * move common memory macros to pmap.h
    
    dtb is located at physical memory so all accesses to data use direct mapping. We
    don't have working kmem in board_stack so we can't use virtual memory in simply
    way - see bugs in physmem.

--- sys/aarch64/rpi3.c
+++ sys/aarch64/rpi3.c
@@ -8,60 +8,84 @@
 #include <sys/context.h>
 #include <sys/interrupt.h>
 #include <sys/kasan.h>
-#include <aarch64/atags.h>
+#include <sys/fdt.h>
 #include <aarch64/mcontext.h>
 #include <aarch64/vm_param.h>
+#include <aarch64/pmap.h>
 
-extern uint8_t _atags[];
-
-static int count_atags(void) {
-  atag_tag_t *atags = (atag_tag_t *)_atags;
-  int ntokens = 0;
-  atag_tag_t *atag;
-  ATAG_FOREACH(atag, atags) {
-    uint32_t tag = ATAG_TAG(atag);
-    if (tag == ATAG_MEM) {
-      ntokens++;
-    } else if (tag == ATAG_INITRD2) {
-      ntokens += 2;
-    } else if (tag == ATAG_CMDLINE) {
-      ntokens += cmdline_count_tokens(atag->tag_cmd.command);
-    }
-  }
-  return ntokens;
+/* Return offset of path at dtb or die. */
+static int dtb_offset(void *dtb, const char *path) {
+  int offset = fdt_path_offset(dtb, path);
+  if (offset < 0)
+    panic("Failed to find offset at dtb!");
+  return offset;
 }
 
-static void process_atags(char **tokens, kstack_t *stk) {
-  atag_tag_t *atags = (atag_tag_t *)_atags;
+static uint32_t process_dtb_memsize(void *dtb) {
+  int offset = dtb_offset(dtb, "/memory@0");
+  int len;
+  const uint32_t *prop = fdt_getprop(dtb, offset, "reg", &len);
+  /* reg contains start (4 bytes) and end (4 bytes) */
+  if (prop == NULL || (size_t)len < 2 * sizeof(uint32_t))
+    panic("Failed to get memory size from dtb!");
+
+  return fdt32_to_cpu(prop[1]);
+}
+
+static int process_dtb_rd_start(void *dtb) {
+  int offset = dtb_offset(dtb, "/chosen");
+  int len;
+  const uint32_t *prop = fdt_getprop(dtb, offset, "linux,initrd-start", &len);
+  if (prop == NULL || (size_t)len < sizeof(uint32_t))
+    panic("Failed to get initrd-start from dtb!");
+
+  return fdt32_to_cpu(*prop);
+}
+
+static int process_dtb_rd_size(void *dtb) {
+  int offset = dtb_offset(dtb, "/chosen");
+  int len;
+  const uint32_t *prop = fdt_getprop(dtb, offset, "linux,initrd-end", &len);
+  if (prop == NULL || (size_t)len < sizeof(uint32_t))
+    panic("Failed to get initrd-start from dtb!");
+
+  return fdt32_to_cpu(*prop) - process_dtb_rd_start(dtb);
+}
+
+static const char *process_dtb_cmdline(void *dtb) {
+  int offset = dtb_offset(dtb, "/chosen");
+  const char *prop = fdt_getprop(dtb, offset, "bootargs", NULL);
+  if (prop == NULL)
+    panic("Failed to get cmdline from dtb!");
+
+  return prop;
+}
+
+static void process_dtb(char **tokens, kstack_t *stk, void *dtb) {
+  if (fdt_check_header(dtb))
+    panic("dtb incorrect header!");
+
   char buf[32];
-
-  atag_tag_t *atag;
-  ATAG_FOREACH(atag, atags) {
-    uint32_t tag = ATAG_TAG(atag);
-    if (tag == ATAG_MEM) {
-      snprintf(buf, sizeof(buf), "memsize=%d", atag->tag_mem.size);
-      tokens = cmdline_extract_tokens(stk, buf, tokens);
-    } else if (tag == ATAG_INITRD2) {
-      snprintf(buf, sizeof(buf), "rd_start=%d", atag->tag_initrd.start);
-      tokens = cmdline_extract_tokens(stk, buf, tokens);
-      snprintf(buf, sizeof(buf), "rd_size=%d", atag->tag_initrd.size);
-      tokens = cmdline_extract_tokens(stk, buf, tokens);
-    } else if (tag == ATAG_CMDLINE) {
-      tokens = cmdline_extract_tokens(stk, atag->tag_cmd.command, tokens);
-    }
-  }
-
+  snprintf(buf, sizeof(buf), "memsize=%d", process_dtb_memsize(dtb));
+  tokens = cmdline_extract_tokens(stk, buf, tokens);
+  snprintf(buf, sizeof(buf), "rd_start=%d", process_dtb_rd_start(dtb));
+  tokens = cmdline_extract_tokens(stk, buf, tokens);
+  snprintf(buf, sizeof(buf), "rd_size=%d", process_dtb_rd_size(dtb));
+  tokens = cmdline_extract_tokens(stk, buf, tokens);
+  tokens = cmdline_extract_tokens(stk, process_dtb_cmdline(dtb), tokens);
   *tokens = NULL;
 }
 
-void *board_stack(void) {
+void *board_stack(paddr_t dtb) {
   kstack_t *stk = &thread0.td_kstack;
 
   thread0.td_uctx = kstack_alloc_s(stk, mcontext_t);
 
-  int ntokens = count_atags();
-  char **kenvp = kstack_alloc(stk, (ntokens + 2) * sizeof(char *));
-  process_atags(kenvp, stk);
+  /*
+   * NOTE: memsize, rd_start, rd_size, cmdline + 2 = 6
+   */
+  char **kenvp = kstack_alloc(stk, 6 * sizeof(char *));
+  process_dtb(kenvp, stk, (void *)PHYS_TO_DMAP(dtb));
   kstack_fix_bottom(stk);
   init_kenv(kenvp);
 
