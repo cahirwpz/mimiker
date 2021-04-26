0f927966b1701f89435422718fc8693af6678a55
wiklam <wiktorpilar99@gmail.com>
2021-04-26 12:09:13 +0200

    e.g.

--- sys/kern/mcount.c
+++ sys/kern/mcount.c
@@ -88,7 +88,7 @@ static SPIN_DEFINE(mcount_lock, 0);
  *  tos[0] + 1  is the smallest index of an unused tos entry
  * 
  * \warning This function while unlocking spinlock can turn on interrupts when 
- * thread's td_idnest == 0, this problem occurs in mips_exc_handler,
+ * thread's td_idnest == 0, this problem occurs e.g. in mips_exc_handler,
  * this is why we do not profile user_mode_p.
  */
 
