874d4480cde97ba483473ce9fccbcee59735427a
Franciszek Zdobylak <zdobyl.frank@gmail.com>
2021-04-29 16:33:29 +0200

    Simplify splitting test

--- sys/tests/vm_amap.c
+++ sys/tests/vm_amap.c
@@ -82,31 +82,26 @@ static int test_amap_split(void) {
 
   vm_amap_add(&aref, an1, 3 * PAGESIZE);
   vm_amap_add(&aref, an2, 7 * PAGESIZE);
-  vm_amap_add(&aref, an3, 2 * PAGESIZE);
-  vm_amap_add(&aref, an4, 14 * PAGESIZE);
 
   vm_aref_t new_aref = vm_amap_split(&aref, 5 * PAGESIZE);
   vm_amap_lock(amap);
   assert(amap->am_ref == 2);
   vm_amap_unlock(amap);
 
-  vm_anon_t *an5 = vm_anon_alloc();
-  vm_anon_t *an6 = vm_anon_alloc();
-
-  vm_amap_add(&aref, an5, 1 * PAGESIZE);
-  vm_amap_add(&new_aref, an6, 1 * PAGESIZE);
+  vm_amap_add(&aref, an3, 1 * PAGESIZE);
+  vm_amap_add(&new_aref, an4, 1 * PAGESIZE);
 
   vm_amap_lock(amap);
-  assert(amap->am_nused == 6);
-  assert(amap->am_anon[1] == an5);
-  assert(amap->am_anon[6] == an6);
-  assert(amap->am_slot[4] == 1);
-  assert(amap->am_slot[5] == 6);
+  assert(amap->am_nused == 4);
+  assert(amap->am_anon[1] == an3);
+  assert(amap->am_anon[6] == an4);
+  assert(amap->am_slot[2] == 1);
+  assert(amap->am_slot[3] == 6);
   vm_amap_unlock(amap);
 
-  assert(vm_amap_lookup(&aref, 1 * PAGESIZE) == an5);
-  assert(vm_amap_lookup(&new_aref, 1 * PAGESIZE) == an6);
-  assert(vm_amap_lookup(&new_aref, (14 - 5) * PAGESIZE) == an4);
+  assert(vm_amap_lookup(&aref, 1 * PAGESIZE) == an3);
+  assert(vm_amap_lookup(&new_aref, 1 * PAGESIZE) == an4);
+  assert(vm_amap_lookup(&new_aref, (7 - 5) * PAGESIZE) == an2);
 
   /* we have to drop amap twice becouse we have two refs now */
   vm_amap_lock(amap);
