8af23c7c627a8452ced5ca00f611d65da3949376
Paweł Jasiak <pawel@jasiak.xyz>
2021-05-01 16:34:58 +0200

    dtb: add comments

--- include/sys/dtb.h
+++ include/sys/dtb.h
@@ -3,9 +3,23 @@
 
 #include <sys/types.h>
 
+/*
+ * NOTE: for dtb parser look at sys/fdt.h.
+ */
+
+/* Return handle (va) to dtb provided by bootloader. */
 void *dtb_root(void);
+
+/* 
+ * Return physical address of dtb. That address should be provided by
+ * bootlader.
+ */
 paddr_t dtb_early_root(void);
+
+/* Inittialize dtb subsystem from boot code. */
 void dtb_early_init(paddr_t dtb, size_t size);
+
+/* Initialize dtb subsystem from MI code; nop if dtb is not supported. */
 void dtb_init(void);
 
 #endif /* !_SYS_DTB_ */
