22ba13309389bbf27620b419c3c1f8e7dc00aa23
Maksymilian Czudziak <maks.czudziak@outlook.com>
2021-05-21 10:52:02 +0200

    revert to first version

--- bin/utest/pipe.c
+++ bin/utest/pipe.c
@@ -13,22 +13,13 @@
 #include "utest.h"
 #include "util.h"
 
-int parent_signaled_passed = 0;
-void parent_sigpipe_handler(int signo) {
-  parent_signaled_passed = 1;
-  return;
-}
-
 int test_pipe_parent_signaled(void) {
-  signal(SIGPIPE, &parent_sigpipe_handler);
   int pipe_fd[2];
   pid_t child_pid;
 
-  if (pipe2(pipe_fd, 0) < 0) {
+  if (pipe2(pipe_fd, 0) < 0)
     perror("pipe2");
-    exit(EXIT_FAILURE);
-  }
-  int wstatus;
+
   switch (child_pid = fork()) {
     case -1: /* error */
       perror("fork");
@@ -43,46 +34,15 @@ int test_pipe_parent_signaled(void) {
       close(pipe_fd[0]); /* closing read end of pipe */
   }
 
+  int wstatus;
   if (waitpid(child_pid, &wstatus, 0) < 0)
     perror("waitpid");
-  write(pipe_fd[1], "hello world\n", 12);
 
-  assert(parent_signaled_passed);
-  signal(SIGPIPE, SIG_DFL);
+  // This is supposed to trigger SIGPIPE and get us killed
+  write(pipe_fd[1], "hello world\n", 12);
 
   return 0;
 }
-
-// int test_pipe_parent_signaled(void) {
-//   int pipe_fd[2];
-//   pid_t child_pid;
-
-//   if (pipe2(pipe_fd, 0) < 0)
-//     perror("pipe2");
-
-//   switch (child_pid = fork()) {
-//     case -1: /* error */
-//       perror("fork");
-//       exit(EXIT_FAILURE);
-
-//     case 0:              /* child */
-//       close(pipe_fd[1]); /* closing write end of pipe */
-//       close(pipe_fd[0]); /* closing read end of pipe */
-//       exit(EXIT_SUCCESS);
-
-//     default:             /* parent */
-//       close(pipe_fd[0]); /* closing read end of pipe */
-//   }
-
-//   int wstatus;
-//   if (waitpid(child_pid, &wstatus, 0) < 0)
-//     perror("waitpid");
-
-//   // This is supposed to trigger SIGPIPE and get us killed
-//   write(pipe_fd[1], "hello world\n", 12);
-
-//   return 0;
-// }
 
 int test_pipe_child_signaled(void) {
   int child_signaled_passed = 0;
