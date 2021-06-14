1801aac3e97672cfc70977dd6e9cfb03605c5e26
Maksymilian Czudziak <maks.czudziak@outlook.com>
2021-06-14 12:58:18 +0200

    maybe fdp?

--- sys/kern/syscalls.c
+++ sys/kern/syscalls.c
@@ -433,18 +433,20 @@ static int sys_pipe2(proc_t *p, pipe2_args_t *args, register_t *res) {
 
   klog("pipe2(%x, %d)", u_fdp, flags);
 
-  if (flags)
+  if (flags) {
     // if (mode & O_NONBLOCK)
     //   fp->f_flags |= IO_NONBLOCK;
 
     if ((flags & O_NONBLOCK) != 0) {
-      // fflags |= FNONBLOCK;
-      fds[0]->f_flags |= IO_NONBLOCK;
-      fds[1]->f_flags |= IO_NONBLOCK;
+
+      u_fdp->f_flags |= IO_NONBLOCK;
+      // fds[0]->f_flags |= IO_NONBLOCK;
+      // fds[1]->f_flags |= IO_NONBLOCK;
 
     } else {
       klog("sys_pipe2: non-zero and O_NONBLOCK flags not handled!");
     }
+  }
 
   if ((error = do_pipe(p, fds)))
     return error;
