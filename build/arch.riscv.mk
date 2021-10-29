6df75f474b0ebc2a3840a6a39619f6ba65802158
MichalBlk <michal.blaszczyk09@gmail.com>
2021-10-29 12:56:20 +0200

    Changes relating OpenSBI.

--- build/arch.riscv.mk
+++ build/arch.riscv.mk
@@ -12,19 +12,8 @@ ELFTYPE := elf32-littleriscv
 ELFARCH := riscv
 
 ifeq ($(BOARD), vexriscv)
-	KERNEL_PHYS = 0xc0000000
+	KERNEL_PHYS = 0x84000000
 	KERNEL-IMAGES = mimiker.img
-
-	EMULATOR-BIN = $(TOPDIR)/sys/emulator.bin
-	EMULATOR-URL = https://dl.antmicro.com/projects/renode/litex_vexriscv--emulator.bin-s_9028-796a4227b806997c6629462fdf0dcae73de06929
-
-$(EMULATOR-BIN):
-	$(CURL) -o $@ $(EMULATOR-URL)
-endif
-
-ifneq ($(EMULATOR-BIN),)
-sys-download: $(EMULATOR-BIN)
-CLEAN-FILES += $(EMULATOR-BIN)
 endif
 
 ifeq ($(KERNEL), 1)
