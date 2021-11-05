df5f867103d254baa89486853cadbed706950a1d
MichalBlk <michal.blaszczyk09@gmail.com>
2021-11-05 16:34:25 +0100

    Change cpp to gcc.

--- build/tools.mk
+++ build/tools.mk
@@ -33,7 +33,7 @@ endif
 CC       = $(TARGET_CC)
 AS       = $(TARGET_CC)
 LD       = $(TARGET)-gcc $(GCC_ABIFLAGS) -g
-CPP      = $(TARGET)-cpp -E
+CPP      = $(TARGET)-gcc -E
 AR       = $(TARGET)-ar
 NM       = $(TARGET)-nm
 GDB      = $(TARGET)-gdb
