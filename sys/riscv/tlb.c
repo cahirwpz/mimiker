85e6e2b9d35967eae6bfa67c8322b2f0c32be2a1
MichalBlk <michal.blaszczyk09@gmail.com>
2022-04-25 14:05:49 +0200

    Physical map management.

---
+++ sys/riscv/tlb.c
@@ -0,0 +1,18 @@
+#include <riscv/tlb.h>
+
+/*
+ * NOTE: the `sfence.vma` instruction has multiple variants
+ * which perform the fence operation with different granularity.
+ * Unfortunately, some cores doesn't support the more specialized
+ * variants, e.g. an `sfence.vma` with arguments executed on
+ * the VexRiscv softcore results in an illegal instruction exception.
+ * Thereby, Mimiker relies on the generic flush without any arguments.
+ */
+
+void tlb_invalidate(vaddr_t va __unused, asid_t asid __unused) {
+  __asm __volatile("sfence.vma" ::: "memory");
+}
+
+void tlb_invalidate_asid(asid_t asid __unused) {
+  __asm __volatile("sfence.vma" ::: "memory");
+}
