f3cda97d1178819f21b18cceba4f20f566d4480e
MichalBlk <michal.blaszczyk09@gmail.com>
2022-05-26 15:51:11 +0200

    Cleanup.

--- sys/kern/main.c
+++ sys/kern/main.c
@@ -82,7 +82,7 @@ __noreturn void kernel_init(void) {
   init_kmem();
   init_vm_map();
 
-  // init_cons();
+  init_cons();
 
   /* Make dispatcher & scheduler structures ready for use. */
   init_sleepq();
@@ -104,11 +104,14 @@ __noreturn void kernel_init(void) {
 
   /* Mount filesystems (including devfs). */
   mount_fs();
+
   /* Some clocks has been found during device init process,
    * so it's high time to start system clock. */
   init_clock();
 
   init_kgprof();
+
+  klog("Kernel initialized!");
 
   init_kcsan();
 
