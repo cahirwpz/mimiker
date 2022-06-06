e1f2c16477ea8626a0594b53f917704e815d8334
Michał Błaszczyk <64263550+MichalBlk@users.noreply.github.com>
2022-06-06 17:39:24 +0200

    [dev] Fix LiteX VexRiscv rootdev device node assignment (#1273)

--- sys/drv/litex_riscv_rootdev.c
+++ sys/drv/litex_riscv_rootdev.c
@@ -186,7 +186,10 @@ static int rootdev_probe(device_t *bus) {
 
 static int rootdev_attach(device_t *bus) {
   rootdev_t *rd = bus->state;
-  bus->node = 0;
+
+  bus->node = FDT_finddevice("/cpus/cpu/interrupt-controller");
+  if (bus->node == FDT_NODEV)
+    return ENXIO;
 
   rman_init(&rd->mem_rm, "RISC-V I/O space");
   rman_manage_region(&rd->mem_rm, 0xf0000000, 0x10000000);
