9120739dd5fe1f22d6cc0e7700dbc18d54dfd295
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-07-25 16:41:44 +0200

    Introduce FDT_getname function.

--- sys/drv/bcm2835_gpio.c
+++ sys/drv/bcm2835_gpio.c
@@ -96,14 +96,12 @@ static int bcm2835_gpio_read_fdt_entry(device_t *dev, phandle_t node) {
   ssize_t pull_cnt = 0;
   ssize_t intr_detect_cnt = 0;
 
-  int entry_name_len = FDT_getproplen(node, "name");
-  char *entry_name = kmalloc(M_DEV, entry_name_len, M_ZERO);
-  FDT_getprop(node, "name", (pcell_t *)entry_name, entry_name_len);
+  const char *entry_name = FDT_getname(node);
 
   pin_cnt = FDT_getencprop_alloc_multi(node, "pins", sizeof(*pin_cfgs),
                                        (void **)&pin_cfgs);
   if (pin_cnt == -1) {
-    klog("Warning: GPIO FDT entry \"%s\" with no `pins` property", entry_name);
+    klog("Warning: GPIO FDT entry `%s` with no `pins` property", entry_name);
     error = ENOENT;
     goto cleanup;
   }
@@ -111,7 +109,7 @@ static int bcm2835_gpio_read_fdt_entry(device_t *dev, phandle_t node) {
   function_cnt = FDT_getencprop_alloc_multi(
     node, "function", sizeof(*function_cfgs), (void **)&function_cfgs);
   if (function_cnt == -1) {
-    klog("Warning: GPIO FDT entry \"%s\" with no `function` property",
+    klog("Warning: GPIO FDT entry `%s` with no `function` property",
          entry_name);
     error = EINVAL;
     goto cleanup;
@@ -120,7 +118,7 @@ static int bcm2835_gpio_read_fdt_entry(device_t *dev, phandle_t node) {
   pull_cnt = FDT_getencprop_alloc_multi(node, "pull", sizeof(*pull_cfgs),
                                         (void **)&pull_cfgs);
   if (pull_cnt == -1) {
-    klog("Warning: GPIO FDT entry \"%s\" with no `pull` property", entry_name);
+    klog("Warning: GPIO FDT entry `%s` with no `pull` property", entry_name);
     error = EINVAL;
     goto cleanup;
   }
@@ -128,7 +126,7 @@ static int bcm2835_gpio_read_fdt_entry(device_t *dev, phandle_t node) {
   intr_detect_cnt = FDT_getencprop_alloc_multi(
     node, "intr_detect", sizeof(*intr_detect_cfgs), (void **)&intr_detect_cfgs);
   if (intr_detect_cnt == -1) {
-    klog("Warning: GPIO FDT entry \"%s\" with no `intr_detect` property",
+    klog("Warning: GPIO FDT entry `%s` with no `intr_detect` property",
          entry_name);
     error = EINVAL;
     goto cleanup;
@@ -157,8 +155,6 @@ static int bcm2835_gpio_read_fdt_entry(device_t *dev, phandle_t node) {
   }
 
 cleanup:
-  FDT_free(entry_name);
-
   if (pin_cnt > 0)
     FDT_free(pin_cfgs);
   if (function_cnt > 0)
