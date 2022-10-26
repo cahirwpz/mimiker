6a030d1555c31acc8771786145a49fb5c3f62aca
franciscozdo <zdobyl.frank@gmail.com>
2022-10-26 09:55:48 +0200

    Update install script according to suggestions

--- install-tools.sh
+++ install-tools.sh
@@ -1,32 +1,85 @@
 #!/bin/bash
+nocolor='\033[0m'
+col_red='\033[0;31m'
+col_orange='\033[0;33m'
+col_blue='\033[0;34m'
 
-# This script is created based on Dockerfile.
-# It should be run with root privileges.
+function fatal() {
+    echo -e "${col_red}ERROR${nocolor}: "$1
+    exit 1
+}
 
-apt-get -q update
+function warn() {
+    echo -e "${col_orange}WARN${nocolor}: "$1
+}
 
-apt-get install -y --no-install-recommends \
-      git make ccache cpio curl gnupg universal-ctags cscope socat patch \
-      gperf quilt bmake byacc python3-pip device-tree-compiler tmux \
-      libmpfr6 libfdt1 libpython3.9 libsdl2-2.0-0 libglib2.0-0 libpixman-1-0
+function info() {
+    echo -e "${col_blue}INFO${nocolor}: "$1
+}
 
-apt-key adv --fetch-keys https://apt.llvm.org/llvm-snapshot.gpg.key
-echo "deb http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye-14 main" > \
-      /etc/apt/sources.list.d/llvm-14.list && apt-get update
-apt-get install -y --no-install-recommends \
-      clang-14 clang-format-14 llvm-14 lld-14
+function ask() {
+    read -p "Do you want to proceed? (Y/n) " choice
+    case $choice in
+        [yY] ) return;;
+        [nN] ) echo exiting...;
+            exit 1;;
+        * ) echo invalid response. Assuming NO.;
+            exit 1;;
+    esac
+}
 
-# patch & quilt required by lua and programs in contrib/
-# gperf required by libterminfo
-# socat & tmux required by launch
+# all checks before script is run
+[ "`id -u`" -eq 0 ] && fatal "You are running as root... it is not supported"
+[ -f /etc/apt/sources.list.d/llvm.list ] && warn 'You already have llvm sources.list. The script will override it.' && ask
+[ -f requirements.txt ] || fatal 'You are not in the Mimiker repo topdir.'
+[ -f /usr/bin/apt ] || fatal '/usr/bin/apt is missing; is your distro debian based?'
+[ -f /usr/bin/sudo ] || fatal 'sudo is missing; (run `su -c "apt install sudo"`)'
 
-curl -O https://mimiker.ii.uni.wroc.pl/download/mipsel-mimiker-elf_latest_amd64.deb && \
-    dpkg -i mipsel-mimiker-elf_latest_amd64.deb && rm -f mipsel-mimiker-elf_latest_amd64.deb
-curl -O https://mimiker.ii.uni.wroc.pl/download/aarch64-mimiker-elf_latest_amd64.deb && \
-    dpkg -i aarch64-mimiker-elf_latest_amd64.deb && rm -f aarch64-mimiker-elf_latest_amd64.deb
-curl -O https://mimiker.ii.uni.wroc.pl/download/riscv32-mimiker-elf_latest_amd64.deb && \
-    dpkg -i riscv32-mimiker-elf_latest_amd64.deb && rm -f riscv32-mimiker-elf_latest_amd64.deb
-curl -O https://mimiker.ii.uni.wroc.pl/download/riscv64-mimiker-elf_latest_amd64.deb && \
-    dpkg -i riscv64-mimiker-elf_latest_amd64.deb && rm -f riscv64-mimiker-elf_latest_amd64.deb
-curl -O https://mimiker.ii.uni.wroc.pl/download/qemu-mimiker_latest_amd64.deb && \
-    dpkg -i qemu-mimiker_latest_amd64.deb && rm -f qemu-mimiker_latest_amd64.deb
+DEBS="git make ccache cpio curl gnupg universal-ctags cscope socat patch gperf quilt byacc python3 python3-pip python3-virtualenv device-tree-compiler tmux lsb-release gdb-multiarch qemu-system-aarch64 qemu-system-riscv64"
+
+info "Install required packages"
+info "Upgrade privileges to root user"
+info "Install packages from base repository"
+
+sudo -u root sh -c "apt-get update -q && apt install ${DEBS}"
+
+info "Drop privileges to $USER"
+info "Install local python dependencies"
+
+python3 -m venv mimiker-env
+. mimiker-env/bin/activate
+pip3 install -r requirements.txt
+
+LLVM_VERSION=14
+DEBS_LLVM="clang-${LLVM_VERSION} clang-format-${LLVM_VERSION} llvm-${LLVM_VERSION} lld-${LLVM_VERSION}"
+
+CODENAME=$(lsb_release -sc 2>/dev/null)
+case $CODENAME in
+    bullseye|buster)
+
+        info "Prepare sources.list for llvm && install llvm-${LLVM_VERSION}"
+        LLVM_REPO="deb http://apt.llvm.org/${CODENAME}/ llvm-toolchain-${CODENAME}-${LLVM_VERSION} main"
+        sudo -u root sh -c "apt-key adv --fetch-keys https://apt.llvm.org/llvm-snapshot.gpg.key && echo ${LLVM_REPO} > /etc/apt/sources.list.d/llvm.list && apt-get update -q && apt-get install ${DEBS_LLVM}"
+
+        WORKDIR=$(mktemp -d)
+        info "${WORKDIR} created"
+        info "Download prebuild toolchain"
+
+        curl -o $WORKDIR/mipsel-mimiker-elf_latest_amd64.deb https://mimiker.ii.uni.wroc.pl/download/mipsel-mimiker-elf_latest_amd64.deb
+        curl -o $WORKDIR/aarch64-mimiker-elf_latest_amd64.deb https://mimiker.ii.uni.wroc.pl/download/aarch64-mimiker-elf_latest_amd64.deb
+        curl -o $WORKDIR/riscv32-mimiker-elf_latest_amd64.deb https://mimiker.ii.uni.wroc.pl/download/riscv32-mimiker-elf_latest_amd64.deb
+        curl -o $WORKDIR/riscv64-mimiker-elf_latest_amd64.deb https://mimiker.ii.uni.wroc.pl/download/riscv64-mimiker-elf_latest_amd64.deb
+        curl -o $WORKDIR/qemu-mimiker_latest_amd64.deb https://mimiker.ii.uni.wroc.pl/download/qemu-mimiker_latest_amd64.deb
+
+        info "Install toolchain"
+        sudo -u root sh -c "dpkg -i $WORKDIR/*.deb"
+
+        info "Cleanup $WORKDIR"
+        rm -r $WORKDIR
+        ;;
+    *)
+        warn "Your distribution doesn't support our prebuild toolchain"
+        ;;
+esac
+
+exit 0
