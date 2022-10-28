30438da0ca5c4827c5c10cd682fea7d63a0dc2a7
franciscozdo <zdobyl.frank@gmail.com>
2022-10-28 16:49:06 +0200

    Fix protection bits on Aarch64

--- bin/utest/vm_map_prot.c
+++ bin/utest/vm_map_prot.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <stdio.h>
 
-typedef int(*func_int_t)(int);
+typedef int (*func_int_t)(int);
 
 static int func_inc(int x) {
   return x + 1;
@@ -23,19 +23,15 @@ int test_vmmap_text_w(void) {
   printf("func_dec: 0x%p\n", func_dec);
   printf("Writing data at address: 0x%p\n", fun);
 
-  /* that memory write should fail */
   char *p = (char *)func_inc;
   char prev_value0 = p[0];
   char prev_value1 = p[1];
+
+  /* These memory writes should fail */
   p[0] = prev_value0 + 1;
   p[1] = prev_value1 + 1;
 
-  /* TODO: how to properly determine function length? */
-  //size_t len = (char*) func_inc - (char*) func_dec;
-  //printf("len: %ld\n", len);
-  //memcpy(fun, func_inc, len);
-
-  /* This will fail if values has changed. */
+  /* Assertions will fail if memory writes succeeded. */
   assert(p[0] == prev_value0);
   assert(p[1] == prev_value1);
   return -1;
@@ -43,9 +39,9 @@ int test_vmmap_text_w(void) {
 
 int test_vmmap_data_x(void) {
   static char func_buf[1024];
+
   /* TODO: how to properly determine function length? */
-  size_t len = (char*) func_inc - (char*) func_dec;
-  printf("len: %ld\n", len);
+  size_t len = (char *)func_inc - (char *)func_dec;
   memcpy(func_buf, func_inc, len);
 
   func_int_t ff = (func_int_t)func_buf;
