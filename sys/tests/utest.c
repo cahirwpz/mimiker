5b48723b8ec4789e9e27b8f440233ec85154e261
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-11-15 13:07:17 +0100

    Make all tests pass on MIPS.
     * Use RI & XI bits correctly.
     * Set up hardware so that RI & XI are respected,
       and exceptions are correctly reported.

--- sys/tests/utest.c
+++ sys/tests/utest.c
@@ -62,9 +62,7 @@ static int utest_generic(const char *name, int status_success) {
 UTEST_ADD_SIGNAL(vmmap_text_w, SIGSEGV);
 UTEST_ADD_SIGNAL(vmmap_rodata_w, SIGSEGV);
 UTEST_ADD_SIGNAL(vmmap_data_x, SIGSEGV);
-#ifndef __mips__
 UTEST_ADD_SIGNAL(vmmap_rodata_x, SIGSEGV);
-#endif
 
 UTEST_ADD_SIMPLE(mmap);
 UTEST_ADD_SIMPLE(munmap);
