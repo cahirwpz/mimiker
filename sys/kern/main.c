b9c18fb98d24bfa2437b4975c5537ebcf1d414e3
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-11-17 18:47:06 +0100

    Restore initial priority change.

--- sys/kern/main.c
+++ sys/kern/main.c
@@ -44,13 +44,11 @@ static __noreturn void start_init(__unused void *arg) {
   proc_t *p = proc_self();
   int error;
 
-#if 0
   thread_t *td = thread_self();
 
   WITH_MTX_LOCK (td->td_lock) {
     sched_set_prio(td, prio_uthread(PRIO_MID));
   }
-#endif
 
   /* [SECOND_PASS] Init devices that need extra kernel API to be functional. */
   init_devices();
