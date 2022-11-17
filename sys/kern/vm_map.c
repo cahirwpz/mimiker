a2588e8dcfa1170fb2fdb076107ef35ec359015e
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-11-17 16:09:07 +0100

    Make sure there're no changes to binary code so we can trigger the bug

--- sys/kern/vm_map.c
+++ sys/kern/vm_map.c
@@ -202,7 +202,7 @@ void vm_map_delete(vm_map_t *map) {
 void vm_map_protect(vm_map_t *map, vaddr_t start, vaddr_t end, vm_prot_t prot) {
   SCOPED_MTX_LOCK(&map->mtx);
 
-#if 0
+#if 1
   klog("vm_map_protect: 0x%x - 0x%x %c%c%c", start, end,
        (prot & VM_PROT_READ) ? 'r' : '-', (prot & VM_PROT_WRITE) ? 'w' : '-',
        (prot & VM_PROT_EXEC) ? 'x' : '-');
@@ -370,7 +370,7 @@ void vm_map_dump(vm_map_t *map) {
          (it->prot & VM_PROT_READ) ? 'r' : '-',
          (it->prot & VM_PROT_WRITE) ? 'w' : '-',
          (it->prot & VM_PROT_EXEC) ? 'x' : '-');
-#if 0
+#if 1
     vm_object_dump(it->object);
 #endif
   }
