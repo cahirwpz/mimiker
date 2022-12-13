71be4f712159b1320cd37587c236db92e5548dd5
Antoni Pokusiński <apokusinski@o2.pl>
2022-12-13 19:27:16 +0100

    contrib/sbase: add sbase-box.c generation script

---
+++ contrib/sbase/box_src_generator.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from contextlib import redirect_stdout
+
+
+def generate_core_file(program: str, tools: list):
+    main_prefixes = [tool.replace('-', '_') for tool in tools]
+
+    with open(program + '.c', 'w') as f:
+        with redirect_stdout(f):
+            print('#include <libgen.h>')
+            print('#include <stdio.h>')
+            print('#include <stdlib.h>')
+            print('#include <string.h>')
+            print('#include "util.h"')
+
+            for prefix in main_prefixes:
+                print(f'int {prefix}_main(int, char**);')
+
+            print('int main(int argc, char *argv[]) { \
+                char *s = basename(argv[0]);')
+            print(f'if(!strcmp(s,\"{program}\")) {{ \
+                argc--; argv++; s = basename(argv[0]); }} if(0) ;')
+
+            if "test" in tools:
+                print('else if (!strcmp(s, \"[\")) \
+                    return test_main(argc, argv);')
+            for tool, prefix in zip(tools, main_prefixes):
+                print(f'else if(!strcmp(s, \"{tool}\")) \
+                    return {prefix}_main(argc, argv);')
+
+            print('else {')
+            if "test" in tools:
+                print('fputs("[ ", stdout);')
+            for tool in tools:
+                print(f'fputs(\"{tool} \", stdout);')
+            print('putchar(0xa); }; return 0; }')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Generate sbase-box core source file.')
+    parser.add_argument('-p', '--program', type=str,
+                        help='Executable file name.')
+    parser.add_argument('-t', '--tools', nargs='+',
+                        help='List of supported tools.')
+
+    args = parser.parse_args()
+
+    generate_core_file(args.program, args.tools)
