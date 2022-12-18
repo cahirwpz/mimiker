1ce829f841d96c425eebfcebad9e54e5142ffe56
Franciszek Malinka <franciszek.malinka@gmail.com>
2022-12-18 21:58:19 +0100

    [WIP] Sigtimedwait finished, first test
    Finished sigtimedwait, but there's a bug, test crashes in some
    unexpected place.

--- sys/tests/utest.c
+++ sys/tests/utest.c
@@ -90,6 +90,7 @@ UTEST_ADD_SIMPLE(signal_stop);
 UTEST_ADD_SIMPLE(signal_cont_masked);
 UTEST_ADD_SIMPLE(signal_mask);
 UTEST_ADD_SIMPLE(signal_mask_nonmaskable);
+UTEST_ADD_SIMPLE(signal_sigtimedwait);
 UTEST_ADD_SIMPLE(signal_sigsuspend);
 UTEST_ADD_SIMPLE(signal_sigsuspend_stop);
 UTEST_ADD_SIMPLE(signal_handler_mask);
