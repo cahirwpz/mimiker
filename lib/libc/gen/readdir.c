0a28ff65ea3bd519f835bbbeb1a279b98113bc49
Antoni Pokusiński <apokusinski@o2.pl>
2023-01-08 13:53:50 +0100

    [libc] readdir - dont return errno on EOD

--- lib/libc/gen/readdir.c
+++ lib/libc/gen/readdir.c
@@ -48,6 +48,7 @@
  */
 struct dirent *_readdir_unlocked(DIR *dirp, int skipdeleted) {
   struct dirent *dp;
+  int saved_errno = errno;
 
   for (;;) {
     if (dirp->dd_loc >= dirp->dd_size) {
@@ -56,10 +57,14 @@ struct dirent *_readdir_unlocked(DIR *dirp, int skipdeleted) {
       dirp->dd_loc = 0;
     }
     if (dirp->dd_loc == 0 && !(dirp->dd_flags & __DTF_READALL)) {
-      dirp->dd_size = getdents(dirp->dd_fd, dirp->dd_buf, (size_t)dirp->dd_len);
-      if (dirp->dd_size <= 0)
-        return (NULL);
       dirp->dd_seek = lseek(dirp->dd_fd, (off_t)0, SEEK_CUR);
+      dirp->dd_size = getdents(dirp->dd_fd, dirp->dd_buf, (size_t)dirp->dd_len);
+      if (dirp->dd_size < 0)
+        return (NULL);
+      if (dirp->dd_size == 0) {
+        errno = saved_errno;
+        return (NULL);
+      }
     }
     dp = (struct dirent *)(void *)(dirp->dd_buf + (size_t)dirp->dd_loc);
     if ((intptr_t)dp & _DIRENT_ALIGN(dp)) /* bogus pointer check */
