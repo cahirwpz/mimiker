57d140b63c81f569360b1f300f880e9256734768
franciscozdo <zdobyl.frank@gmail.com>
2023-05-11 09:35:17 +0200

    Use format tool specified in Makefiles

--- verify-format.sh
+++ verify-format.sh
@@ -1,12 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-modify=-n
-if [ "$1" = "--fix" ]; then
-  modify=
+MODIFY=-n
+if [ "$1" == "--fix" ]; then
+  MODIFY=
 fi
+
+FORMAT_TOOL=$(make dump-tools | grep FORMAT | cut -d' ' -f2-)
 
 CFILES=$(find -name "*.[ch]" | grep -vf .format-exclude)
 
-clang-format-14 $modify --Werror --style=file -i $CFILES || exit 1
+$FORMAT_TOOL $MODIFY --Werror -i $CFILES
+
+if [ $? -ne 0 ]; then
+  echo ""
+  echo "C files are not formatted correctly."
+  echo "If you want to format them automatically please use 'verify-format.sh --fix'"
+  echo ""
+  echo "If you see this message in CI report consider using our git hook 'scripts/pre-commit'."
+  echo "It could be installed using git-setup.sh script."
+  exit 1
+fi
 
 echo "Formatting correct for C files."
