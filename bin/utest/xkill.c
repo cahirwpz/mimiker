fb3dbceee04a050f48077263f084c96a675e8ae5
Krystian Bacławski <krystian.baclawski@gmail.com>
2023-07-15 21:30:23 +0200

    Wrap kill as well.

---
+++ bin/utest/xkill.c
@@ -0,0 +1,16 @@
+#include "utest.h"
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+
+extern int __real_kill(pid_t pid, int sig);
+
+int __wrap_kill(pid_t pid, int sig) {
+  int result = __real_kill(pid, sig);
+
+  if (result < 0)
+    die("kill: %s", strerror(errno));
+
+  return 0;
+}
