5c43cb745e4b38891700d1e6b86cb3c14e088649
Krystian Bacławski <krystian.baclawski@gmail.com>
2023-07-31 13:04:42 +0200

    Replace libwrap with macros.

--- bin/utest/libwrap/xmunmap.c
+++
@@ -1,9 +0,0 @@
-#include "utest.h"
-#include <sys/mman.h>
-
-void xmunmap(void *addr, size_t length) {
-  int result = munmap(addr, length);
-
-  if (result < 0)
-    die("munmap: %s", strerror(errno));
-}
--- bin/utest/libwrap/xpipe.c
+++
@@ -1,9 +0,0 @@
-#include "utest.h"
-#include <unistd.h>
-
-void xpipe(int pipefd[2]) {
-  int result = pipe(pipefd);
-
-  if (result < 0)
-    die("pipe: %s", strerror(errno));
-}
