acb7b6dedc811f826fe0b0c6be1e1bc29835e4db
Krystian Bacławski <krystian.baclawski@gmail.com>
2023-08-01 09:10:50 +0200

    Run tests with both KASAN and KCSAN (#1411)
    * Use build matrix for all kernel builds and tests.
    * Enable KCSAN tests for AArch64.
    * Increase timeout for AArch64 tests.
    * Ignore KCSAN failures.

---
+++ action.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+case $1 in
+  build)
+    case $2 in
+      mips-kasan)
+        make BOARD=malta KASAN=1 LOCKDEP=1 ;;
+      mips-kcsan)
+        make BOARD=malta KCSAN=1 LOCKDEP=1 ;;
+      aarch64-kasan)
+        make BOARD=rpi3 KASAN=1 LOCKDEP=1 ;;
+      aarch64-kcsan)
+        make BOARD=rpi3 KCSAN=1 LOCKDEP=1 ;;
+      riscv32-kasan)
+        make BOARD=litex-riscv KASAN=1 LOCKDEP=1 ;;
+      riscv64-kasan)
+        make BOARD=sifive_u KASAN=1 LOCKDEP=1 ;;
+      riscv64-kcsan)
+        make BOARD=sifive_u KCSAN=1 LOCKDEP=1 ;;
+      *)
+        exit 1 ;;
+    esac ;;
+  tests)
+    case $2 in
+      mips-*)
+        ./run_tests.py --board malta --timeout=100 --times=50 ;;
+      aarch64-kcsan)
+        ./run_tests.py --board rpi3 --timeout=100 --times=50
+        # do not report it as failed because we have no people working on
+        # fixing concurrency issues
+        exit 0 ;;
+      aarch64-*)
+        ./run_tests.py --board rpi3 --timeout=100 --times=50 ;;
+      riscv64-*)
+        ./run_tests.py --board sifive_u --timeout=100 --times=50 ;;
+      *)
+        exit 1 ;;
+    esac ;;
+  *)
+    exit 1 ;;
+esac
