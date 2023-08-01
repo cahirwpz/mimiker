0286b74864a1aec71b5d1451d23ffd5231757b6e
Krystian Bacławski <krystian.baclawski@gmail.com>
2023-08-01 08:39:20 +0200

    Enable KCSAN tests for AArch64. Fix action.sh script.

--- action.sh
+++ action.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
 
 case $1 in
-  "build")
+  build)
     case $2 in
-      "mips-kasan")
+      mips-kasan)
         make BOARD=malta KASAN=1 LOCKDEP=1 ;;
-      "mips-kcsan")
+      mips-kcsan)
         make BOARD=malta KCSAN=1 LOCKDEP=1 ;;
-      "aarch64-kasan")
+      aarch64-kasan)
         make BOARD=rpi3 KASAN=1 LOCKDEP=1 ;;
-      "aarch64-kcsan")
+      aarch64-kcsan)
         make BOARD=rpi3 KCSAN=1 LOCKDEP=1 ;;
-      "riscv32-kasan")
+      riscv32-kasan)
         make BOARD=litex-riscv KASAN=1 LOCKDEP=1 ;;
-      "riscv64-kasan")
+      riscv64-kasan)
         make BOARD=sifive_u KASAN=1 LOCKDEP=1 ;;
-      "riscv64-kcsan")
+      riscv64-kcsan)
         make BOARD=sifive_u KCSAN=1 LOCKDEP=1 ;;
       *)
         exit 1 ;;
     esac ;;
-  "tests")
+  tests)
     case $2 in
-      "mips-*")
+      mips-*)
         ./run_tests.py --board malta --timeout=100 --times=50 ;;
-      "aarch64-*")
+      aarch64-*)
         ./run_tests.py --board rpi3 --timeout=60 --times=50 ;;
-      "riscv64-*")
+      riscv64-*)
         ./run_tests.py --board sifive_u --timeout=100 --times=50 ;;
+      *)
+        exit 1 ;;
     esac ;;
-  "*")
+  *)
     exit 1 ;;
 esac
